76e58e89a574978889c5e361ecba188db3b95cf5
Klaus Mosthaf <Klaus.Mosthaf@iws.uni-stuttgart.de>
2008-04-29 15:41:00 +0000

    changed constrel.hh

--- dumux/material/constrel.hh
+++ dumux/material/constrel.hh
@@ -8,101 +8,71 @@
 /** \ingroup properties
  * @brief base class for constitutive relationships
  */
-
-
-class Solubility {
-public:
-//	virtual double operator() (double T=283.15) = 0; 
+namespace Dune
+{
 	
-	double Henry (double T=283.15)
-	{
-		double celsius = T - 273.15;
-
-		return ((0.8942 + 1.47 * exp(-0.04394*celsius) )*1e-10); // [1/Pa]
-	};
-	
-	double Antoine(double T=283.15)
-	{
-		const double constA = 8.19621; 
-		const double constB = 1730.63;
-		const double constC = 233.436;
-		double celsius;
-		double exponent, pwsat;
-
-		celsius = T - 273.15;
-
-		exponent = constA - (constB / (celsius + constC));
-
-		pwsat = pow (10.0, exponent) *100; /*1mbar = 100Pa*/
-
-		return(pwsat);
-	};
-	
-	
-	virtual double Xwn (double pn, double T=283.15)
-	{
-		double pwsat(0.0);
-		double Xwn(0.0);
-	
-		pwsat = Antoine(T);
-		Xwn = pwsat / pn;
+class Solubility
+{
+	public:
+	//	virtual double operator() (double T=283.15) = 0; 
 		
-		return(Xwn);
-	};
-
-	virtual double Xaw (double pn, double T=283.15)
-	{
-		double pan;
-		double Xaw(0.0);
-		double hagw;
+		virtual double Xwn (double pn=1e5, double T=283.15)
+		{
+			double pwsat;
+			double result;
 		
-		pan = pn * (1-Xwn(pn,T));
-		hagw = Henry(T);
-		Xaw = pan * hagw;
-		
-		return(Xaw);
-	};
-
+			pwsat = vaporPressure(T);
+			result = pwsat / pn;
+			
+			return(result);
+		}
 	
-	Solubility()
-	{}
-//
-	virtual ~Solubility()
-	{  }
-  
+		virtual double Xaw (double pn=1e5, double T=283.15)
+		{
+			double pan;
+			double result;
+			double hagw;
+			
+			pan = pn * (1-Xwn(pn,T));
+			hagw = HenryWaterAir(T);
+			result = pan * hagw;
+			
+			return(result);
+		}
+
+		virtual ~Solubility()
+		{}
+		
+		
+	protected:
+
+		virtual double HenryWaterAir(double T=283.15)
+		{
+			double celsius = T - 273.15;
+			double result = ((0.8942 + 1.47 * exp(-0.04394*celsius) )*1e-10);
+	
+			return (result); // [1/Pa]
+		}
+		
+		virtual double vaporPressure(double T=283.15)//Antoine(double T=283.15)
+		{
+			const double constA = 8.19621; 
+			const double constB = 1730.63;
+			const double constC = 233.436;
+			double celsius;
+			double exponent, pwsat;
+	
+			celsius = T - 273.15;
+	
+			exponent = constA - (constB / (celsius + constC));
+	
+			pwsat = pow (10.0, exponent) *100; /*1mbar = 100Pa*/
+	
+			return(pwsat);
+		}
+
 };
 
-//class Henry : public Solubility
-//{
-//public:	
-//	/** @brief Henry coefficient
-//	 * @param T Temperature \f$ \left[ K \right] \f$
-//	 * @return Henry coefficient \f$ \left[ 1/Pa \right] \f$
-//	 */
-//	virtual double operator() (double T) = 0; 
-//	
-//	virtual ~Henry() 
-//	{ }
-//};
-//
-///**\ingroup properties
-// * @brief class for the Henry coefficient of a binary air/water system
-// */
-//class HenryWaterAir : public Henry
-//{
-//public:
-//	double Henry(double T)
-//	{
-//		double pws;
-//		return (pws) =  (0.8942 + 1.47 * exp(-0.04394*T) )*1e-10; // [1/Pa]
-//	};
-//
-////	virtual double operator() (double T)
-////	{
-////		return (henry) =(0.8942 + 1.47 * exp(-0.04394*T) )*1e-10; // [1/Pa]
-////	}
-//};
-
-
+}
 #endif
 
