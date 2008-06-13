d70d6afc194cfd96c12b0eaba949d686b07d6b10
Klaus Mosthaf <Klaus.Mosthaf@iws.uni-stuttgart.de>
2008-06-13 11:31:40 +0000

    renamed constrel.hh to solubilities.hh, implemented changes in properties.hh

--- dumux/material/properties.hh
+++ dumux/material/properties.hh
@@ -1,9 +1,11 @@
 #ifndef DUNE_PROPERTIES_HH
 #define DUNE_PROPERTIES_HH
-
+#include <dumux/material/constrel/constrelco2.hh>
+#include <dumux/material/constrel/constrelwater.hh>
+#include <dumux/material/constrel/constrelbrine.hh>
 /**
  * \ingroup material
- * \defgroup properties Fluid properties
+ * \defgroup properties Fluid and Soil properties
  */
 
 /** \ingroup properties
@@ -17,7 +19,7 @@ public:
    * @param p Pressure \f$ \left[ Pa \right] \f$
    * @return kinematic viscosity \f$ \left[ \frac{kg}{ms} \right] \f$
    */
-  virtual double viscosity (double T=283.15, double p=1e5) const = 0;
+  virtual double viscosity (double T=283.15, double p=1e5, double rho=0.) const = 0;
 
   /** @brief density
    * @param T Temperature \f$ \left[ K \right] \f$
@@ -30,6 +32,16 @@ public:
    * @return residual saturation \f$ \left[ \, \right] \f$
    */
   virtual double Sr() const = 0;
+
+  /** @brief enthalpy
+   * @param T Temperature \f$ \left[ K \right] \f$
+   * @param p Pressure \f$ \left[ Pa \right] \f$
+   * @return enthalpy \f$ \left[ \frac{J}{kg} \right] \f$
+   */
+  virtual double enthalpy (double T=283.15, double p=1e3) const
+  {
+	  return 0;
+  }
   
   /**
    * @brief molar Mass of Component
@@ -45,8 +57,35 @@ public:
 	  return 0;
   }
   
+  virtual double intEnergy(double T=283.15, double p=1e3) const
+  {
+	  return 0;
+  }
+ 
   virtual ~Medium()
   { }
+};
+
+/** \ingroup properties
+ * @brief base class for soil properties
+ */
+class Soil
+{
+public:
+	double heatConductivity (double lDry = 0.32, double lSw = 2.7, double Sw = 1.)
+	{
+		double l_wurz;
+		if(Sw<0.0) Sw = 0.0; /* ACHTUNG Regularisierung */
+		if(Sw>1.) Sw = 1.; /* ACHTUNG Regularisierung */
+
+		l_wurz = lDry + sqrt(Sw)*(lSw-lDry);
+
+		if(isnan(l_wurz)) {
+//			std::cout <<"isnan heatcondwurzel \n"<<std::endl;
+			l_wurz = 0.0;
+		}
+		return(l_wurz);
+	}
 };
 
 /** \ingroup properties
@@ -56,7 +95,7 @@ class Water : public Medium
 {
 public:
 	Water(double Sr = 0.0):Sr_(Sr) {}	
-	double viscosity (double T=283.15, double p=1e5) const
+	double viscosity (double T=283.15, double p=1e5, double rho=0.) const
 	{
 		return 1e-3; //[kg/(ms)]
 	}
@@ -90,7 +129,7 @@ private:
 class Air : public Medium
 {
 public:
-	double viscosity ( double T=283.15, double p=1e5) const
+	double viscosity ( double T=283.15, double p=1e5, double rho=0.) const
 	{
 		return 1.8e-5;//[kg/(ms)]
 	}
@@ -111,15 +150,34 @@ public:
 
 class Brine : public Medium
 {
-public:
-	double viscosity ( double T=283.15, double p=1e5) const
-	{
+	ConstrelBrine constRelBrine;
 	
-		return 2.535e-4;//800e-3;//[kg/(ms)]
+public:
+	double Salinity() const
+	{
+		return 0.1;
+	}
+	double viscosity ( double T=283.15, double p=1e5, double rho=0.) const
+	{	
+			double S;
+			S = Salinity();
+			return constRelBrine.viscosity_brine(T,S);
+//  		 return 2.535e-4; // [kg/(ms)]
 	}
 	double density ( double T=283.15, double p=1e5) const
 	{
-		return 1045.0;//820.0; // [kg/m^3]
+		double S, x_CO2_w;
+		x_CO2_w = 0.0;
+		S = Salinity();
+		return constRelBrine.mass_density_brine_CO2(T,p,S,x_CO2_w);
+//		return 1045.0; // [kg/m^3]
+	}
+	double enthalpy (double T=283.15, double p=1e5) const
+	{
+		double S;
+		S = Salinity();
+		return constRelBrine.enthalpy_brine(T,p,S);
+		//return 180627.65;
 	}
 	double Sr() const
 	{
@@ -129,13 +187,19 @@ public:
 	{
 		return 0; // [kg/mole]
 	}
+	double intEnergy(double T=283.15, double p=1e5) const
+	{
+		double intenergy;
+		intenergy = enthalpy(T,p);
+		return intenergy; 
+	}
 };
 
 class Oil : public Medium
 {
 public:
 	Oil(double Sr = 0.0):Sr_(Sr) {}
-	double viscosity ( double T=283.15, double p=1e5) const
+	double viscosity ( double T=283.15, double p=1e5, double rho=0.) const
 	{
 		return 1e-3;//800e-3;//[kg/(ms)]
 	}
@@ -162,7 +226,7 @@ private:
 class Uniform : public Medium
 {
 public:
-	double viscosity ( double T=283.15, double p=1e5) const
+	double viscosity ( double T=283.15, double p=1e5, double rho=0.) const
 	{
 		return 1.0;//[kg/(ms)]
 	}
@@ -186,7 +250,7 @@ public:
 class DNAPL : public Medium
 {
 public:
-	double viscosity ( double T=283.15, double p=1e5) const
+	double viscosity ( double T=283.15, double p=1e5, double rho=0.) const
 	{
 		return 5.7e-4;//[kg/(ms)]
 	}
@@ -209,23 +273,48 @@ public:
  */
 class CO2 : public Medium
 {
-public:
-        double viscosity ( double T=432.0, double p=3.086e7) const // according to "webbook.nist.gov/chemistry/fluid/"
+ ConstrelCO2 constRelCO2;
+	
+ public:
+	    
+		double density ( double T, double p) const
         {
-                return 3.95e-5;//[kg/(ms)] // given in "CO2 leakage through an abandoned well.." A.Ebigbo,H.Class,R.Helmig 
-        }
-        double density ( double T=432.0, double p=3.086e7) const
-        {
-                return 479.0; // [kg/m^3] // given in "CO2 leakage through an abandoned well.." A.Ebigbo,H.Class,R.Helmig 
-        }
+//			return 479.;
+			return constRelCO2.density(T,p);
+		}
+		double viscosity ( double T=432., double p=3.086e7, double rho=0.) const 
+		{
+//			return 3.95e-5;	
+			return constRelCO2.viscosity(T,p,rho);
+		}
+		double enthalpy ( double T=432.,double p=3.086e7) const
+		{
+			return constRelCO2.enthalpy(T,p);
+		//	return 13141.5;
+       //	return 0;
+		}
         double Sr() const
         {
-                return 0.0;
+            return 0.0;
         }
         double molarMass() const
         {
-                return 44.01; // [kg/mole]
+                return 0.04401; // [kg/mole]
         }
+        double intEnergy( double T=432, double p=3.086e7) const
+        {
+         double u;
+         double rho_mass = density(T,p);
+         double h = enthalpy(T,p);
+         
+         u = h - (p / rho_mass);
+         return u;
+        }
+
+private:     
+	
+
+
 };
 
 /**\ingroup properties
--- dumux/material/solubilities.hh
+++ dumux/material/solubilities.hh
@@ -16,6 +16,7 @@ class Solubility
 	public:
 	//	virtual double operator() (double T=283.15) = 0; 
 		
+		//mole fractions, converted in mass fractions
 		virtual double Xwn (double pn=1e5, double T=283.15)
 		{
 			double pwsat;
@@ -23,6 +24,8 @@ class Solubility
 		
 			pwsat = vaporPressure(T);
 			result = pwsat / pn;
+			
+			result = conversionMoleToMassFraction(result);
 			
 			return(result);
 		}
@@ -34,8 +37,10 @@ class Solubility
 			double hagw;
 			
 			pan = pn * (1-Xwn(pn,T));
-			hagw = HenryWaterAir(T);
+			hagw = henryWaterAir(T);
 			result = pan * hagw;
+			
+			result = conversionMoleToMassFraction(result);
 			
 			return(result);
 		}
@@ -46,31 +51,43 @@ class Solubility
 		
 	protected:
 
-		virtual double HenryWaterAir(double T=283.15)
+		virtual double henryWaterAir(double T=283.15)
 		{
 			double celsius = T - 273.15;
 			double result = ((0.8942 + 1.47 * exp(-0.04394*celsius) )*1e-10);
 	
 			return (result); // [1/Pa]
 		}
-		
-		virtual double vaporPressure(double T=283.15)//Antoine(double T=283.15)
+
+		// Antoine equation for calculating the vapor pressure
+		virtual double vaporPressure(double T=283.15)
 		{
 			const double constA = 8.19621; 
 			const double constB = 1730.63;
-			const double constC = 233.436;
+			const double constC = 233.426;
+
 			double celsius;
-			double exponent, pwsat;
+			double exponent, psat;
 	
 			celsius = T - 273.15;
 	
 			exponent = constA - (constB / (celsius + constC));
 	
-			pwsat = pow (10.0, exponent) *100; /*1mbar = 100Pa*/
+			psat = pow (10.0, exponent) *100; //1mbar = 100Pa
 	
-			return(pwsat);
+			return(psat);
 		}
 
+		double conversionMoleToMassFraction(double molefrac)
+		{
+			double result;
+			double molarMass1 = 0.02896; //air
+			double molarMass2 = 0.018016; // water
+				
+			result = molefrac * molarMass1 / (molarMass1*molefrac + molarMass2*(1-molefrac));
+			
+			return (result);
+		}
 };
 
 }
