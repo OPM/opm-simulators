f59be03392e01cb7ec0a5592eb5927a521f72906
Bernd Flemisch <Bernd.Flemisch@iws.uni-stuttgart.de>
2009-06-19 11:58:37 +0000

    added author informations

--- examples/tutorial_decoupled.cc
+++ examples/tutorial_decoupled.cc
@@ -1,6 +1,6 @@
 // $Id$
 /*****************************************************************************
- *   Copyright (C) <YEARS> by <ADD_AUTHOR_HERE>                              *
+ *   Copyright (C) 2008-2009 by Markus Wolff                                 *
  *   Institute of Hydraulic Engineering                                      *
  *   University of Stuttgart, Germany                                        *
  *   email: <givenname>.<name>@iws.uni-stuttgart.de                          *
