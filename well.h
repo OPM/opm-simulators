fca8a08b29290aa564425b87a4b1640901b2184f
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2010-09-15 17:27:59 +0000

    Separate well connections (typically static) from well controls   (often dynamically changing throughout simulation).

--- well.h
+++ well.h
@@ -1,33 +1,20 @@
 #ifndef WELL_H_INCLUDED
 #define WELL_H_INCLUDED
 
-enum well_type         { INJECTOR, PRODUCER };
-enum well_control_type { BHP     , RATE     };
-
-#define WELL_DESCRIPTOR                         \
-    int                     number_of_wells;    \
-                                                \
-    enum well_type         *type;               \
-                                                \
-    enum well_control_type *control_type;       \
-    double                 *target;
-
-#define WELL_TOPOLOGY                           \
-    int *well_connpos;                          \
-    int *well_cells;
-
-#define WELL_CONNECTION_DATA                    \
-    double *WI;                                 \
-    double *depth;
-
-/* ---------------------------------------------------------------------- */
+enum well_type    { INJECTOR, PRODUCER };
+enum well_control { BHP     , RATE     };
 
 typedef struct {
-    WELL_DESCRIPTOR
-    WELL_TOPOLOGY
-    WELL_CONNECTION_DATA
+    int  number_of_wells;
+    int *well_connpos;
+    int *well_cells;
 } well_t;
 
+typedef struct {
+    enum well_type    *type;
+    enum well_control *ctrl;
+    double            *target;
+} well_control_t;
 
 int
 allocate_cell_wells(int nc, well_t *W, int **cwpos, int **cwells);
