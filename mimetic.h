14f682a0b4bf82082832e2586f1c5a4e46aa7063
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2010-10-01 12:21:24 +0000

    Documented mim_ip_simple_all.

--- mimetic.h
+++ mimetic.h
@@ -27,6 +27,39 @@ void mim_ip_simple(int nf, int nconn, int d,
                    double *Binv,
                    double *work, int lwork);
 
+
+/** Compute the mimetic inner products given a grid and cellwise
+ *  permeability tensors.
+ *
+ * @param ncells Number of cells in grid.
+ * @param d Number of space dimensions.
+ * @param max_ncf Maximum number of faces per cell.
+ * @param ncf Number of faces per cell.
+ * @param pconn Start indices in conn for each cell, plus end
+ *              marker. The size of pconn is (ncells + 1), and for a
+ *              cell i, [conn[pconn[i]], conn[pconn[i+1]]) is a
+ *              half-open interval containing the indices of faces
+ *              adjacent to i.
+ * @param conn Cell to face mapping. Size shall be equal to the sum of
+ *             ncf. See pconn for explanation.
+ * @param fneighbour Face to cell mapping. Its size shall be equal to
+ *                   the number of faces times 2. For each face, the
+ *                   two entries are either a cell number or -1
+ *                   (signifying the outer boundary). The face normal
+ *                   points out of the first cell and into the second.
+ * @param fcentroid Face centroids. Size shall be equal to the number
+ *                  of faces times d.
+ * @param fnormal Face normale. Size shall be equal to the number
+ *                of faces times d.
+ * @param farea Face areas.
+ * @param ccentroid Cell centroids. Size shall be ncells*d.
+ * @param cvol Cell volumes.
+ * @param perm Permeability. Size shall be ncells*d*d, storing a
+ *             d-by-d positive definite tensor per cell.
+ * @param[out] Binv This is where the inner product will be
+ *                  stored. Its size shall be equal to \f$\sum_i
+ * n_i^2\f$.
+ */
 void mim_ip_simple_all(int ncells, int d, int max_ncf, int *ncf,
                        int *pconn, int *conn,
                        int *fneighbour, double *fcentroid, double *fnormal,
