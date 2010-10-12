6530a16785f78c21b001d9b6416c0c8aee41faae
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2010-10-12 07:25:46 +0000

    Added copyright block to all source code files.

--- dfs.c
+++ dfs.c
@@ -1,13 +1,23 @@
-/*======================================================================
+/*
+  Copyright 2010 SINTEF ICT, Applied Mathematics.
 
-  File: dfs.c
+  This file is part of the Open Porous Media project (OPM).
 
-  Created: Tue May  6 09:27:26 2008
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-  Author: Jostein R. Natvig <Jostein.R.Natvig@sintef.no>
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-  Revision: $Id$
-  ====================================================================*/
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #include <assert.h>
 
 #include "dfs.h"
--- dfs.h
+++ dfs.h
@@ -1,4 +1,25 @@
+/*
+  Copyright 2010 SINTEF ICT, Applied Mathematics.
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef DFS_H_INCLUDED
 #define DFS_H_INCLUDED
+
 void dfs (int size, int *ia, int *ja, int *ncolors, int *color, int* work);
+
 #endif
