64ac03c4c6ecc0dec71642df88ba9195295dd291
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2010-11-12 13:18:27 +0100

    Added lots of stuff originally from the samcode repository.

---
+++ dune/porsol/blackoil/fluid/MiscibilityProps.cpp
@@ -0,0 +1,34 @@
+//===========================================================================
+//                                                                           
+// File: MiscibilityProps.cpp                                                 
+//                                                                           
+// Created: Wed Feb 10 09:05:05 2010                                         
+//                                                                           
+// Author: Bjørn Spjelkavik <bsp@sintef.no>
+//                                                                           
+// Revision: $Id$
+//                                                                           
+//===========================================================================
+
+#include "MiscibilityProps.hpp"
+
+using namespace std;
+
+namespace samcode
+{
+
+
+    //------------------------------------------------------------------------
+    // Member functions
+    //-------------------------------------------------------------------------
+
+    /// Constructor
+    MiscibilityProps::MiscibilityProps()
+    {
+    }
+
+    MiscibilityProps::~MiscibilityProps()
+    {
+    }
+
+} // namespace samcode
