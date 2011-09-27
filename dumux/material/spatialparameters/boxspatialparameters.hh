5da017865bb64e341dd374daa07865c1505b2362
Andreas Lauser <Andreas.Lauser@iws.uni-stuttgart.de>
2011-09-27 09:46:37 +0000

    box models: introduce context objects in infrastructure and 2p model
    allows to better decouple the problem from the model and will also
    make the generic implementation of multiple spatial and temporal
    discretizations _much_ easier.
    
    for the box 1p test, also increase the grid resolution from 10x10 to 100x100
    
    v2: add missing asImp_() in the internal storage variant of
        boxlocalresidual::eval(). three days lost on this, ouch!

--- dumux/material/spatialparameters/boxspatialparameters.hh
+++ dumux/material/spatialparameters/boxspatialparameters.hh
@@ -64,7 +64,6 @@ class BoxSpatialParameters: public BoxSpatialParametersOneP<TypeTag>
     typedef typename GridView::template Codim<0>::Entity Element;
     typedef typename GET_PROP_TYPE(TypeTag, FVElementGeometry) FVElementGeometry;
     typedef typename GET_PROP_TYPE(TypeTag, FluxVariables) FluxVariables;
-    typedef typename GET_PROP_TYPE(TypeTag, ElementVolumeVariables) ElementVolumeVariables;
 
     typedef typename GridView::ctype CoordScalar;
     typedef Dune::FieldVector<CoordScalar,dimWorld> GlobalPosition;
@@ -83,53 +82,35 @@ public:
      * \param scvIdx The index of the sub-control volume.
      * \return the material parameters object
      */
-    const MaterialLawParams& materialLawParams(const Element &element,
-            const FVElementGeometry &fvElemGeom,
-            int scvIdx) const
+    template <class Context>
+    DUNE_DEPRECATED
+    const MaterialLawParams& materialLawParams(const Context &context, int localIdx) const
     {
-            return asImp_().materialLawParamsAtPos(element.geometry().center());
+        return asImp_().materialLawParams(context.element(),
+                                          context.fvElemGeom(),
+                                          localIdx);
     }
+
+    DUNE_DEPRECATED
+    const MaterialLawParams& materialLawParams(const Element &element,
+                                               const FVElementGeometry &fvElemGeom,
+                                               int localIdx) const
+    {
+        return asImp_().materialLawParams(fvElemGeom.subContVol[localIdx].global);
+    }
+
 
     /*!
      * \brief Function for defining the parameters needed by constitutive relationships (kr-Sw, pc-Sw, etc.).
      *
-     * \return the material parameters object
      * \param globalPos The position of the center of the element
+     * \return the material parameters object
      */
     const MaterialLawParams& materialLawParamsAtPos(const GlobalPosition& globalPos) const
     {
         DUNE_THROW(Dune::InvalidStateException,
                    "The spatial parameters do not provide "
                    "a materialLawParamsAtPos() method.");
-    }
-
-    /*!
-     * \brief Calculate the heat flux \f$[W/m^2]\f$ through the
-     *        rock matrix based on the temperature gradient \f$[K / m]\f$
-     *        at the integration point of a (boundary or SCV) face
-     *
-     * This is only required for non-isothermal models that use outflow
-     * boundary conditions.
-     *
-     * \param heatFlux The resulting heat flux vector
-     * \param fluxDat The flux variables
-     * \param vDat The volume variables
-     * \param face The boundary or sub-control-volume face
-     * \param element The current finite element
-     * \param fvElemGeom The finite volume geometry of the current element
-     * \tparam FaceType The type of the face (boundary face / SCV face)
-     */
-    template <class FaceType>
-    void boundaryMatrixHeatFlux(Vector &heatFlux,
-            const FluxVariables &fluxDat,
-            const ElementVolumeVariables &vDat,
-            const FaceType &face,
-            const Element &element,
-            const FVElementGeometry &fvElemGeom) const
-    {
-        DUNE_THROW(Dune::InvalidStateException,
-                   "The spatial parameters do not provide "
-                   "a matrixHeatFlux() method.");
     }
 
 private:
--- dumux/material/spatialparameters/boxspatialparameters1p.hh
+++ dumux/material/spatialparameters/boxspatialparameters1p.hh
@@ -68,6 +68,7 @@ class BoxSpatialParametersOneP
     typedef typename GridView::ctype CoordScalar;
     typedef Dune::FieldMatrix<Scalar, dimWorld, dimWorld> Tensor;
     typedef Dune::FieldVector<CoordScalar,dimWorld> GlobalPosition;
+    typedef Dune::FieldVector<Scalar,dimWorld> Vector;
 
 public:
     BoxSpatialParametersOneP(const GridView &gv)
@@ -75,53 +76,6 @@ public:
 
     ~BoxSpatialParametersOneP()
     {}
-
-    /*!
-     * \brief Returns the factor by which the volume of a sub control
-     *        volume needs to be multiplied in order to get cubic
-     *        meters.
-     *
-     * \param element The current finite element
-     * \param fvElemGeom The current finite volume geometry of the element
-     * \param scvIdx The index sub-control volume face where the
-     *                      factor ought to be calculated.
-     *
-     * By default that's just 1.0
-     */
-    Scalar extrusionFactorScv(const Element &element,
-                              const FVElementGeometry &fvElemGeom,
-                              int scvIdx) const
-    DUNE_DEPRECATED // use the extrusion factor of the volume variables
-    { return 1.0; }
-
-    /*!
-     * \brief Returns the factor by which the area of a sub control
-     *        volume face needs to be multiplied in order to get
-     *        square meters.
-     *
-     * \param element The current finite element
-     * \param fvElemGeom The current finite volume geometry of the element
-     * \param scvfIdx The index sub-control volume face where the
-     *                      factor ought to be calculated.
-     *
-     * By default it is the arithmetic mean of the extrusion factor of
-     * the face's two sub-control volumes.
-     */
-    Scalar extrusionFactorScvf(const Element &element,
-                              const FVElementGeometry &fvElemGeom,
-                              int scvfIdx) const
-    DUNE_DEPRECATED // use the extrusion factor of the volume variables
-    {
-        return
-            0.5 *
-            (asImp_().extrusionFactorScv(element,
-                                         fvElemGeom,
-                                         fvElemGeom.subContVolFace[scvfIdx].i)
-             +
-             asImp_().extrusionFactorScv(element,
-                                         fvElemGeom,
-                                         fvElemGeom.subContVolFace[scvfIdx].j));
-    }
 
     /*!
      * \brief Averages the intrinsic permeability (Scalar).
@@ -158,6 +112,16 @@ public:
                 result[i][j] = harmonicMean(K1[i][j], K2[i][j]);
     }
 
+    template <class Context>
+    const Tensor &intrinsicPermeability(const Context &context,
+                                        int localIdx) const
+    {
+        return intrinsicPermeability(context.element(), 
+                                     context.fvElemGeom(),
+                                     localIdx);
+                                     
+    }
+
     /*!
      * \brief Function for defining the intrinsic (absolute) permeability.
      *
@@ -166,9 +130,10 @@ public:
      * \param scvIdx The index of the sub-control volume.
      * \return the intrinsic permeability
      */
-    const Tensor& intrinsicPermeability (const Element &element,
-            const FVElementGeometry &fvElemGeom,
-            int scvIdx) const
+    DUNE_DEPRECATED
+    const Tensor &intrinsicPermeability(const Element &element,
+                                        const FVElementGeometry &fvElemGeom,
+                                        int scvIdx) const
     {
         return asImp_().intrinsicPermeabilityAtPos(element.geometry().center());
     }
@@ -179,11 +144,22 @@ public:
      * \return intrinsic (absolute) permeability
      * \param globalPos The position of the center of the element
      */
+    DUNE_DEPRECATED
     const Tensor& intrinsicPermeabilityAtPos (const GlobalPosition& globalPos) const
     {
         DUNE_THROW(Dune::InvalidStateException,
                    "The spatial parameters do not provide "
                    "a intrinsicPermeabilityAtPos() method.");
+    }
+
+    template <class Context>
+    Scalar porosity(const Context &context,
+                    int localIdx) const
+    {
+        return porosity(context.element(), 
+                        context.fvElemGeom(),
+                        localIdx);
+                                     
     }
 
     /*!
@@ -194,9 +170,10 @@ public:
      * \param scvIdx The index of the sub-control volume.
      * \return porosity
      */
+    DUNE_DEPRECATED
     Scalar porosity(const Element &element,
-            const FVElementGeometry &fvElemGeom,
-            int scvIdx) const
+                    const FVElementGeometry &fvElemGeom,
+                    int scvIdx) const
     {
         return asImp_().porosityAtPos(element.geometry().center());
     }
@@ -207,6 +184,7 @@ public:
      * \return porosity
      * \param globalPos The position of the center of the element
      */
+    DUNE_DEPRECATED
     Scalar porosityAtPos(const GlobalPosition& globalPos) const
     {
         DUNE_THROW(Dune::InvalidStateException,
