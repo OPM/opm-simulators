5ee2c381c3a23e4aed6c58695fc71322d6fa4241
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2011-10-05 20:03:07 +0200

    Import glue code for calling into UMFPACK using an OPMPressure CSRMatrix.

---
+++ examples/call_umfpack.h
@@ -0,0 +1,49 @@
+/*===========================================================================
+//
+// File: call_umfpack.h
+//
+// Created: 2011-10-05 19:55:58+0200
+//
+// Authors: Ingeborg S. Ligaarden <Ingeborg.Ligaarden@sintef.no>
+//          Jostein R. Natvig     <Jostein.R.Natvig@sintef.no>
+//          Halvor M. Nilsen      <HalvorMoll.Nilsen@sintef.no>
+//          Atgeirr F. Rasmussen  <atgeirr@sintef.no>
+//          Bård Skaflestad       <Bard.Skaflestad@sintef.no>
+//
+//==========================================================================*/
+
+
+/*
+  Copyright 2011 SINTEF ICT, Applied Mathematics.
+  Copyright 2011 Statoil ASA.
+
+  This file is part of the Open Porous Media Project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef OPM_CALL_UMFPACK_H_HEADER
+#define OPM_CALL_UMFPACK_H_HEADER
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct CSRMatrix;
+
+void call_UMFPACK(struct CSRMatrix *A, const double *b, double *x);
+
+#ifdef __cplusplus
+}
+#endif
+#endif  /* OPM_CALL_UMFPACK_H_HEADER */
