c4f594130892779669067456a20445cf32ab8ef1
Andreas Lauser <Andreas.Lauser@iws.uni-stuttgart.de>
2011-10-13 17:23:38 +0200

    2pni: introduce heat conduction laws
    this is implemented analogous to the capillary pressure laws
    
    to do: implement the energy equation generically

---
+++ dumux/material/heatconduction/somerton.hh
@@ -0,0 +1,131 @@
+/*****************************************************************************
+ *   Copyright (C) 2011 by Andreas Lauser                                    *
+ *   Institute for Modelling Hydraulic and Environmental Systems             *
+ *   University of Stuttgart, Germany                                        *
+ *   email: <givenname>.<name>@iws.uni-stuttgart.de                          *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file somerton.hh
+ *
+ * \brief Implements the Somerton law of heat conductivity in a
+ * porous medium.
+ *
+ * See:
+ *
+ * W.H. Somerton, A.H. El-Shaarani and S.M. Mobarak: High
+ * Temperature Behavior of Rocks Associated with Geothermal Type
+ * Reservoirs, paper SPE-4897 presentet at SPE California Regional
+ * Meeting 1974, 1974
+ *
+ * or
+ *
+ * H. Class: Theorie und numerische Modellierung nichtisothermer
+ * Mehrphasenprozesse in NAPL kontaminierten poroesen Medien, PhD
+ * thesis, Technical University of Braunschweig, 2000
+ */
+#ifndef DUMUX_SOMERTON_HH
+#define DUMUX_SOMERTON_HH
+
+#include "somertonparams.hh"
+
+#include <algorithm>
+
+namespace Dumux
+{
+/*!
+ * \ingroup material
+ *
+ * \brief Implements the Somerton law of heat conductivity in a
+ * porous medium.
+ *
+ * See:
+ *
+ * W.H. Somerton, A.H. El-Shaarani and S.M. Mobarak: High
+ * Temperature Behavior of Rocks Associated with Geothermal Type
+ * Reservoirs, paper SPE-4897 presentet at SPE California Regional
+ * Meeting 1974, 1974
+ *
+ * or
+ *
+ * H. Class: Theorie und numerische Modellierung nichtisothermer
+ * Mehrphasenprozesse in NAPL kontaminierten poroesen Medien, PhD
+ * thesis, Technical University of Braunschweig, 2000
+ */
+template <int wPhaseIdx,
+          class ScalarT, 
+          class ParamsT = SomertonParams</*numPhases=*/2, ScalarT> >
+class Somerton
+{
+public:
+    typedef ParamsT Params;
+    typedef typename Params::Scalar Scalar;
+
+    /*!
+     * \brief Given a fluid state, return the effective heat conductivity [W/m^2 / (K/m)] of the porous
+     *        medium.
+     *
+     * The Somerton law is given by:
+     * \f[
+     \lambda_{pm} = 
+     \lambda_{vac} +
+     \sqrt{S_w}(\lambda_{ful,w} - \lambda_{ful,n}) +
+     \sqrt{S_n}(\lambda_{ful,n} - \lambda_{ful,w})
+     \f]
+     *
+     * where \f$\lambda_{vac}\f$ is the heat condctivity of the porous
+     * medium at vacuum, \f$\lambda_{ful,\alpha}\f$ is the heat
+     * conductivty of the porous medium if it is fully saturated by
+     * phase \f$\alpha\f$ and \f$S_\alpha\f$ is the saturation of phase \f$\alpha\f$.
+     */
+    template <class FluidState>
+    static Scalar heatConductivity(const Params &params, 
+                                   const FluidState &fluidState)
+    {
+        // the Somerton law as implemented here only 
+        // applies to twophase flow...
+        assert(FluidState::numPhases == 2);
+
+        int nPhaseIdx = 1 - wPhaseIdx;
+        return
+            params.fullySaturatedLambda(nPhaseIdx)
+            +
+            regularizedSqrt_(fluidState.saturation(wPhaseIdx))
+            * (params.fullySaturatedLambda(wPhaseIdx) - params.fullySaturatedLambda(wPhaseIdx)); 
+    }
+
+protected:
+    static Scalar regularizedSqrt_(Scalar x)
+    {
+        const Scalar xMin = 1e-2; 
+        const Scalar sqrtXMin = std::sqrt(xMin);
+        const Scalar fPrimeXMin = 1.0/(2*std::sqrt(xMin));
+        const Scalar fPrime0 = 2*fPrimeXMin;
+        typedef Dumux::Spline<Scalar, 2> Spline;
+        static Spline sqrtRegSpline(0, xMin, // x0, x1
+                                    0, sqrtXMin, // y0, y1
+                                    fPrime0, fPrimeXMin); // m0, m1
+        
+        if (x > xMin)
+            return std::sqrt(x);
+        else if (x <= 0)
+            return fPrime0 * x;
+        else
+            return sqrtRegSpline.eval(x);
+    }
+};
+}
+
+#endif
---
+++ dumux/material/heatconduction/somertonparams.hh
@@ -0,0 +1,95 @@
+/*****************************************************************************
+ *   Copyright (C) 2011 by Andreas Lauser                                    *
+ *   Institute for Modelling Hydraulic and Environmental Systems             *
+ *   University of Stuttgart, Germany                                        *
+ *   email: <givenname>.<name>@iws.uni-stuttgart.de                          *
+ *                                                                           *
+ *   This program is free software: you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation, either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ *****************************************************************************/
+/*!
+ * \file somertonparams.hh 
+ *
+ * Reference implementation of parameters for the Somerton law of
+ * heat conductivity in a porous medium.
+ *
+ * See:
+ *
+ * W.H. Somerton, A.H. El-Shaarani and S.M. Mobarak: High
+ * Temperature Behavior of Rocks Associated with Geothermal Type
+ * Reservoirs, paper SPE-4897 presentet at SPE California Regional
+ * Meeting 1974, 1974
+ *
+ * or
+ *
+ * H. Class: Theorie und numerische Modellierung nichtisothermer
+ * Mehrphasenprozesse in NAPL kontaminierten poroesen Medien, PhD
+ * thesis, Technical University of Braunschweig, 2000
+ */
+#ifndef DUMUX_SOMERTON_PARAMS_HH
+#define DUMUX_SOMERTON_PARAMS_HH
+
+namespace Dumux
+{
+/*!
+ * \brief 
+ */
+template <int numPhases, class ScalarT>
+class SomertonParams
+{
+    // do not copy!
+    SomertonParams(const SomertonParams &)
+    {};
+
+public:
+    typedef ScalarT Scalar;
+
+    SomertonParams()
+    { }
+
+    /*!
+     * \brief Return the "fully saturated" heat conductivity of the
+     *        porous medium [W/m^2 / (K/m)].
+     *
+     * In this context "fully saturated" means that the whole pore
+     * space of the porous medium is filled by a given fluid phase.
+     */
+    Scalar fullySaturatedLambda(int phaseIdx) const
+    { 
+        assert(0 <= phaseIdx && phaseIdx < numPhases);
+
+        return fullySaturatedLambda_[phaseIdx];
+    }
+
+    /*!
+     * \brief Set the "fully saturated" heat conductivity of the
+     *        porous medium [W/m^2 / (K/m)].
+     *
+     * In this context "fully saturated" means that the whole pore
+     * space of the porous medium is filled by a given fluid phase.
+     */
+    void setFullySaturatedLambda(int phaseIdx, Scalar value)
+    { 
+        assert(0 <= phaseIdx && phaseIdx < numPhases);
+        assert(value > 0);
+
+        fullySaturatedLambda_[phaseIdx] = value;
+    }
+
+private:
+    Scalar fullySaturatedLambda_[numPhases];
+};
+
+} // namespace Dumux
+
+#endif
