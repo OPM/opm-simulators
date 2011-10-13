be6ae76d88ff7e6bd2320d25a1db66f7100d5bc7
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2011-10-13 19:38:33 +0200

    Don't reference (non-existent) Dune directories.  Remove disabled code.

--- src/CSRMatrixUmfpackSolver.hpp
+++ src/CSRMatrixUmfpackSolver.hpp
@@ -36,7 +36,7 @@
 #ifndef OPM_CSRMATRIXUMFPACKSOLVER_HPP_HEADER
 #define OPM_CSRMATRIXUMFPACKSOLVER_HPP_HEADER
 
-#include <dune/porsol/opmtransport/examples/call_umfpack.h>
+#include <call_umfpack.h>
 
 namespace Opm {
     namespace ImplicitTransportLinAlgSupport {
