b7aab50731adf6ef9feb5d636ab25a4b630b33cb
Bernd Flemisch <Bernd.Flemisch@iws.uni-stuttgart.de>
2011-10-27 14:30:37 +0000

    Worked further on the Doxygen modules. The structure seems quite ok now, and the classes seem to be associated with the correct groups.
    Also improved documentation by getting rid of more than 250 Doxygen
    warnings.

--- examples/tutorialproblem_decoupled.hh
+++ examples/tutorialproblem_decoupled.hh
@@ -141,7 +141,8 @@ class TutorialProblemDecoupled: public IMPESProblem2P<TypeTag> /*@\label{tutoria
     typedef typename GET_PROP_TYPE(TypeTag, PTAG(FluidState)) FluidState;
 
     typedef typename GET_PROP_TYPE(TypeTag, PTAG(BoundaryTypes)) BoundaryTypes;
-    typedef typename GET_PROP(TypeTag, PTAG(SolutionTypes))::PrimaryVariables PrimaryVariables;
+    typedef typename GET_PROP(TypeTag, PTAG(SolutionTypes)) SolutionTypes;
+    typedef typename SolutionTypes::PrimaryVariables PrimaryVariables;
 
     enum
     {
