89ff5ad8fc4d2870cb3a1e39f6afe8b5b369053b
Bernd Flemisch <Bernd.Flemisch@iws.uni-stuttgart.de>
2011-11-01 16:55:36 +0000

    Reverting previous commits and going back to revision 6808. There is no need to rush things. We agreed on first having the meeting and then porting the change to the non-MpNc-models. Moreover, the change has not been carried out according to what has been discussed at the meeting. E.g. there was no completeState functionality of the models, and the simple models also introduced parameter caches, although it was thought to not necessarily having to do so.

--- examples/tutorialproblem_decoupled.hh
+++ examples/tutorialproblem_decoupled.hh
@@ -93,7 +93,7 @@ public:
 };
 
 // Set the non-wetting phase
-SET_PROP(TutorialProblemDecoupled, NonWettingPhase)
+SET_PROP(TutorialProblemDecoupled, NonwettingPhase)
 {
 private:
     typedef typename GET_PROP_TYPE(TypeTag, PTAG(Scalar)) Scalar;
