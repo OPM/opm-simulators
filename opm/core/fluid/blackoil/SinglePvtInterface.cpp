2c5cf2c9e24682f110d37bbb5a72071064fb4f5d
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2012-01-04 15:44:53 +0100

    Copied and renamed basic building blocks for black oil pvt.

---
+++ opm/core/fluid/blackoil/SinglePvtInterface.cpp
@@ -0,0 +1,52 @@
+//===========================================================================
+//                                                                           
+// File: MiscibilityProps.cpp                                                 
+//                                                                           
+// Created: Wed Feb 10 09:05:05 2010                                         
+//                                                                           
+// Author: Bjørn Spjelkavik <bsp@sintef.no>
+//                                                                           
+// Revision: $Id$
+//                                                                           
+//===========================================================================
+/*
+  Copyright 2010 SINTEF ICT, Applied Mathematics.
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "MiscibilityProps.hpp"
+
+using namespace std;
+
+namespace Opm
+{
+
+
+    //------------------------------------------------------------------------
+    // Member functions
+    //-------------------------------------------------------------------------
+
+    /// Constructor
+    MiscibilityProps::MiscibilityProps()
+    {
+    }
+
+    MiscibilityProps::~MiscibilityProps()
+    {
+    }
+
+} // namespace Opm
