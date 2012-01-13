0a68e1a18be16be88ffbbd8f0d780436e5e2f4a9
Klaus Mosthaf <Klaus.Mosthaf@iws.uni-stuttgart.de>
2012-01-13 15:44:11 +0000

    Added some required typedefs

--- dumux/material/spatialparameters/boxspatialparameters.hh
+++ dumux/material/spatialparameters/boxspatialparameters.hh
@@ -62,9 +62,12 @@ class BoxSpatialParameters: public BoxSpatialParametersOneP<TypeTag>
 
     typedef typename GridView::template Codim<0>::Entity Element;
     typedef typename GET_PROP_TYPE(TypeTag, FVElementGeometry) FVElementGeometry;
+    typedef typename GET_PROP_TYPE(TypeTag, FluxVariables) FluxVariables;
+    typedef typename GET_PROP_TYPE(TypeTag, ElementVolumeVariables) ElementVolumeVariables;
 
     typedef typename GridView::ctype CoordScalar;
     typedef Dune::FieldVector<CoordScalar,dimWorld> GlobalPosition;
+    typedef Dune::FieldVector<CoordScalar,dimWorld> Vector;
 
 public:
     BoxSpatialParameters(const GridView &gv)
