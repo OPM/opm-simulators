22d269811fa369b7298b602e95d22b7f76049418
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2012-01-24 13:47:00 +0100

    C++ified reordering transport solver facade.

--- opm/core/transport/reorder/reordersequence.h
+++ opm/core/transport/reorder/reordersequence.h
@@ -2,11 +2,21 @@
 #ifndef REORDERSEQUENCE_H_INCLUDED
 #define REORDERSEQUENCE_H_INCLUDED
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct UnstructuredGrid;
 void compute_sequence(struct UnstructuredGrid *grid, const double *flux,
                       int *sequence, int *components, int *ncomponents);
 
+
+
+#ifdef __cplusplus
+}
 #endif
+
+#endif /* REORDERSEQUENCE_H_INCLUDED */
 
 /* Local Variables:    */
 /* c-basic-offset:4    */
