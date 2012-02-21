8c16722b84553f00aacb3f77ea38b79c2ef25e71
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2012-02-21 22:02:16 +0100

    Removed copied text accidentally left in.

--- opm/core/linalg/LinearSolverUmfpack.hpp
+++ opm/core/linalg/LinearSolverUmfpack.hpp
@@ -19,27 +19,6 @@
 
 #ifndef OPM_LINEARSOLVERUMFPACK_HEADER_INCLUDED
 #define OPM_LINEARSOLVERUMFPACK_HEADER_INCLUDED
-/*
-  Copyright 2012 SINTEF ICT, Applied Mathematics.
-
-  This file is part of the Open Porous Media project (OPM).
-
-  OPM is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  (at your option) any later version.
-
-  OPM is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-#ifndef OPM_LINEARSOLVERISTL_HEADER_INCLUDED
-#define OPM_LINEARSOLVERISTL_HEADER_INCLUDED
 
 
 #include <opm/core/linalg/LinearSolverInterface.hpp>
@@ -83,7 +62,5 @@ namespace Opm
 } // namespace Opm
 
 
-
-#endif // OPM_LINEARSOLVERISTL_HEADER_INCLUDED
 
 #endif // OPM_LINEARSOLVERUMFPACK_HEADER_INCLUDED
