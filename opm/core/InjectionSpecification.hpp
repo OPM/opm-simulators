bc1fcb831b091b69865d4cedb1f366fb4553ec32
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2012-03-28 12:38:48 +0200

    Fixed some warnings and added GCONPROD reading for the eclipse parser

--- opm/core/InjectionSpecification.hpp
+++ opm/core/InjectionSpecification.hpp
@@ -14,7 +14,6 @@ public:
     };
     
     InjectionSpecification();
-    InjectionSpecification(const InjectionSpecification& orig);
     virtual ~InjectionSpecification();
     
     Component component();
