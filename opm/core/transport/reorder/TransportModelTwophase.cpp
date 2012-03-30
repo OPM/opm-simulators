baf099cd5a562a9f17885c230cab044a0716fe11
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2012-03-30 16:11:07 +0200

    Minor modification of residual formula (equivalent to the old).

--- opm/core/transport/reorder/TransportModelTwophase.cpp
+++ opm/core/transport/reorder/TransportModelTwophase.cpp
@@ -154,7 +154,7 @@ namespace Opm
 	}
 	double operator()(double s) const
 	{
-	    return s - s0 + dtpv*(outflux*tm.fracFlow(s, cell) + influx) + dtpv*s*comp_term;
+	    return s - s0 + dtpv*(outflux*tm.fracFlow(s, cell) + influx + s*comp_term);
 	}
     };
 
