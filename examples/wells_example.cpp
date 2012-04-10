7db1027935777ddd23f1a51ec3f0ab839a14b6ad
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2012-04-10 14:47:29 +0200

    Fixed namespacing issue

--- examples/wells_example.cpp
+++ examples/wells_example.cpp
@@ -3,8 +3,11 @@
 #include <opm/core/utility/parameters/ParameterGroup.hpp>
 #include <opm/core/WellsManager.hpp>
 #include <opm/core/GridManager.hpp>
-
+#include <opm/core/pressure/IncompTpfa.hpp>
+#include <opm/core/fluid/IncompPropertiesFromDeck.hpp>
+#include <opm/core/linalg/LinearSolverUmfpack.hpp>
 #include "opm/core/newwells.h"
+#include "opm/core/grid.h"
 
 int main(int argc, char** argv) {
 
@@ -12,7 +15,7 @@ int main(int argc, char** argv) {
     using namespace Opm;
     ParameterGroup parameters( argc, argv, false );
     std::string file_name = parameters.getDefault<std::string>("inputdeck", "data.data");
-    
+
     // Read input file
     EclipseGridParser parser(file_name);
     std::cout << "Done!" << std::endl;
@@ -21,6 +24,18 @@ int main(int argc, char** argv) {
     
     // Finally handle the wells
     WellsManager wells(parser, *grid.c_grid(), NULL);
+    
+    std::vector<int> global_cells(grid.c_grid()->global_cell, grid.c_grid()->global_cell + grid.c_grid()->number_of_cells);
+
+    std::cout << "ahoi" << std::endl;
+    double gravity[3] = {0.0, 0.0, parameters.getDefault<double>("gravity", 0.0)};
+    IncompPropertiesFromDeck incomp_properties(parser, global_cells);
+        std::cout << "there" << std::endl;
+
+    LinearSolverUmfpack umfpack_solver;
+    std::cout << "here" << std::endl;
+    IncompTpfa pressure_solver(*grid.c_grid(), incomp_properties.permeability(), 
+                               gravity, umfpack_solver,  wells.c_wells());
     
     return 0;
 }
