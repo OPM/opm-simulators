8c5816586228e58f95eb0cc0cb2e66e877141240
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2012-04-13 16:33:51 +0200

    Added LinearSolverFactory class.

---
+++ opm/core/linalg/LinearSolverFactory.cpp
@@ -0,0 +1,100 @@
+/*
+  Copyright 2012 SINTEF ICT, Applied Mathematics.
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <opm/core/linalg/LinearSolverFactory.hpp>
+
+#if HAVE_SUITESPARSE_UMFPACK_H
+#include <opm/core/linalg/LinearSolverIstl.hpp>
+#endif
+
+#if HAVE_DUNE_ISTL
+#include <opm/core/linalg/LinearSolverUmfpack.hpp>
+#endif
+
+#include <opm/core/utility/parameters/ParameterGroup.hpp>
+#include <opm/core/utility/ErrorMacros.hpp>
+#include <string>
+
+
+namespace Opm
+{
+
+    LinearSolverFactory::LinearSolverFactory()
+    {
+#if HAVE_SUITESPARSE_UMFPACK_H
+        solver_.reset(new LinearSolverUmfpack);
+#elif HAVE_DUNE_ISTL
+        solver_.reset(new LinearSolverIstl);
+#else
+        THROW("No linear solver available, you must have UMFPACK or dune-istl installed to use LinearSolverFactory.");
+#endif
+    }
+
+
+
+
+    LinearSolverFactory::LinearSolverFactory(const parameter::ParameterGroup& param)
+    {
+        const std::string ls = param.getDefault<std::string>("linsolver", "umfpack");
+        if (ls == "umfpack") {
+#if HAVE_SUITESPARSE_UMFPACK_H
+            solver_.reset(new LinearSolverUmfpack);
+#else
+            THROW("Linear solver " << ls <<" is not available.");
+#endif
+        } else if (ls == "istl") {
+#if HAVE_DUNE_ISTL
+            solver_.reset(new LinearSolverIstl(param));
+#else
+            THROW("Linear solver " << ls <<" is not available.");
+#endif
+        }
+    }
+
+
+
+
+    LinearSolverFactory::~LinearSolverFactory()
+    {
+    }
+
+
+
+
+    LinearSolverInterface::LinearSolverReport
+    LinearSolverFactory::solve(const int size,
+                               const int nonzeros,
+                               const int* ia,
+                               const int* ja,
+                               const double* sa,
+                               const double* rhs,
+                               double* solution) const
+    {
+        return solver_->solve(size, nonzeros, ia, ja, sa, rhs, solution);
+    }
+
+
+
+
+} // namespace Opm
+
---
+++ opm/core/linalg/LinearSolverFactory.hpp
@@ -0,0 +1,86 @@
+/*
+  Copyright 2012 SINTEF ICT, Applied Mathematics.
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef OPM_LINEARSOLVERFACTORY_HEADER_INCLUDED
+#define OPM_LINEARSOLVERFACTORY_HEADER_INCLUDED
+
+
+#include <opm/core/linalg/LinearSolverInterface.hpp>
+#include <tr1/memory>
+
+namespace Opm
+{
+
+    namespace parameter { class ParameterGroup; }
+
+
+    /// Concrete class encapsulating any available linear solver.
+    /// For the moment, this means UMFPACK and dune-istl.
+    /// Since both are optional dependencies, either or both
+    /// may be unavailable, depending on configuration.
+    class LinearSolverFactory : public LinearSolverInterface
+    {
+    public:
+        /// Default constructor.
+        LinearSolverFactory();
+
+        /// Construct from parameters.
+        /// The accepted parameters are (default) (allowed values):
+        ///    linsolver ("umfpack")   ("umfpack", "istl")
+        /// For the umfpack solver to be available, this class must be
+        /// compiled with UMFPACK support, as indicated by the
+        /// variable HAVE_SUITESPARSE_UMFPACK_H in config.h.
+        /// For the istl solver to be available, this class must be
+        /// compiled with dune-istl support, as indicated by the
+        /// variable HAVE_DUNE_ISTL in config.h.
+        /// Any further parameters are passed on to the constructors
+        /// of the actual solver used, see LinearSolverUmfpack
+        /// and LinearSolverIstl for details.
+        LinearSolverFactory(const parameter::ParameterGroup& param);
+
+        /// Destructor.
+        virtual ~LinearSolverFactory();
+
+        using LinearSolverInterface::solve;
+
+        /// Solve a linear system, with a matrix given in compressed sparse row format.
+        /// \param[in] size        # of rows in matrix
+        /// \param[in] nonzeros    # of nonzeros elements in matrix
+        /// \param[in] ia          array of length (size + 1) containing start and end indices for each row
+        /// \param[in] ja          array of length nonzeros containing column numbers for the nonzero elements
+        /// \param[in] sa          array of length nonzeros containing the values of the nonzero elements
+        /// \param[in] rhs         array of length size containing the right hand side
+        /// \param[inout] solution array of length size to which the solution will be written, may also be used
+        ///                        as initial guess by iterative solvers.
+        virtual LinearSolverReport solve(const int size,
+                                         const int nonzeros,
+                                         const int* ia,
+                                         const int* ja,
+                                         const double* sa,
+                                         const double* rhs,
+                                         double* solution) const;
+    private:
+        std::tr1::shared_ptr<LinearSolverInterface> solver_;
+    };
+
+
+} // namespace Opm
+
+
+#endif // OPM_LINEARSOLVERFACTORY_HEADER_INCLUDED
