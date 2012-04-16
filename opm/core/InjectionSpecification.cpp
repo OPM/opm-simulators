627c3be1f957f568f6bfcc0810bced1d11d43f50
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2012-04-16 19:27:34 +0200

    InjectionSpecification: Split long lines. No functional changes.

--- opm/core/InjectionSpecification.cpp
+++ opm/core/InjectionSpecification.cpp
@@ -3,8 +3,12 @@ namespace Opm
 {
 
     InjectionSpecification::InjectionSpecification()
-    : injector_type_(WATER), control_mode_(NONE), surface_flow_max_rate_(1e100),
-    reinjection_fraction_target_(0.0), BHP_limit_(1e100), fluid_volume_max_rate_(1e100)
+    : injector_type_(WATER),
+      control_mode_(NONE),
+      surface_flow_max_rate_(1e100),
+      reinjection_fraction_target_(0.0),
+      BHP_limit_(1e100),
+      fluid_volume_max_rate_(1e100)
     {
 
     }
