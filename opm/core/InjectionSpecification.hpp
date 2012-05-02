2d7d52bec1b2ae7dfef3f2ff5fdb1ee3ce224911
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2012-05-02 09:25:19 +0200

    Refined and reordered control enum, added guide rates.

--- opm/core/InjectionSpecification.hpp
+++ opm/core/InjectionSpecification.hpp
@@ -11,7 +11,7 @@ namespace Opm
 
         enum ControlMode
         {
-            NONE, ORAT, REIN, RESV, VREP, WGRA, FLD, GRUP
+            NONE, RATE, RESV, BHP, THP, REIN, VREP, GRUP, FLD
         };
 
         enum InjectorType
@@ -19,14 +19,21 @@ namespace Opm
             WATER, OIL, GAS
         };
 
+        enum GuideRateType
+        {
+            RAT, NONE_GRT
+        };
+
         InjectionSpecification();
 
         InjectorType injector_type_;
         ControlMode control_mode_;
         double surface_flow_max_rate_;
-        double reinjection_fraction_target_;
-        double fluid_volume_max_rate_;
+        double reservoir_flow_max_rate_;
         double BHP_limit_;
+        double reinjection_fraction_target_;
+        double guide_rate_;
+        GuideRateType guide_rate_type_;
     };
 
 }
