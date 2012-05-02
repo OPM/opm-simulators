c234c0b42a79ac13147135dd807effc644a48bfd
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2012-05-02 09:25:52 +0200

    Follow changes to header file.

--- opm/core/InjectionSpecification.cpp
+++ opm/core/InjectionSpecification.cpp
@@ -5,13 +5,14 @@ namespace Opm
     InjectionSpecification::InjectionSpecification()
     : injector_type_(WATER),
       control_mode_(NONE),
-      surface_flow_max_rate_(1e100),
-      reinjection_fraction_target_(0.0),
-      fluid_volume_max_rate_(1e100),
-      BHP_limit_(1e100)
+      surface_flow_max_rate_(-1e100),
+      reservoir_flow_max_rate_(-1e100),
+      BHP_limit_(-1e100),
+      reinjection_fraction_target_(-1e100),
+      guide_rate_(1.0),
+      guide_rate_type_(NONE_GRT)
     {
 
     }
-
 
 } // namespace Opm
