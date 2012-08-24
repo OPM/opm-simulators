3e9cc7492231568263d214161754934e7099c692
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2012-08-24 16:08:26 +0200

    Initial commit of tof computation by reordering.

---
+++ opm/core/transport/reorder/TransportModelTracerTof.cpp
@@ -0,0 +1,100 @@
+/*
+  Copyright 2012 SINTEF ICT, Applied Mathematics.
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <opm/core/transport/reorder/TransportModelTracerTof.hpp>
+#include <opm/core/grid.h>
+#include <opm/core/utility/ErrorMacros.hpp>
+#include <algorithm>
+
+namespace Opm
+{
+
+
+    /// Construct solver.
+    /// \param[in] grid      A 2d or 3d grid.
+    TransportModelTracerTof::TransportModelTracerTof(const UnstructuredGrid& grid)
+        : grid_(grid)
+    {
+    }
+
+
+
+
+    /// Solve for time-of-flight at next timestep.
+    /// \param[in] darcyflux         Array of signed face fluxes.
+    /// \param[in] porevolume        Array of pore volumes.
+    /// \param[in, out] tof          Array of time-of-flight values.
+    void TransportModelTracerTof::solveTof(const double* darcyflux,
+                                           const double* porevolume,
+                                           std::vector<double>& tof)
+    {
+        darcyflux_ = darcyflux;
+        porevolume_ = porevolume;
+        tof.resize(grid_.number_of_cells);
+        std::fill(tof.begin(), tof.end(), 0.0);
+        tof_ = &tof[0];
+        reorderAndTransport(grid_, darcyflux);
+    }
+
+
+
+
+    void TransportModelTracerTof::solveSingleCell(const int cell)
+    {
+        // Compute flux terms.
+        double upwind_term = 0.0;
+        double downwind_flux = 0.0;
+        for (int i = grid_.cell_facepos[cell]; i < grid_.cell_facepos[cell+1]; ++i) {
+            int f = grid_.cell_faces[i];
+            double flux;
+            int other;
+            // Compute cell flux
+            if (cell == grid_.face_cells[2*f]) {
+                flux  = darcyflux_[f];
+                other = grid_.face_cells[2*f+1];
+            } else {
+                flux  =-darcyflux_[f];
+                other = grid_.face_cells[2*f];
+            }
+            // Add flux to upwind_term or downwind_flux, if interior.
+            if (other != -1) {
+                if (flux < 0.0) {
+                    upwind_term  += flux*tof_[other];
+                } else {
+                    downwind_flux += flux;
+                }
+            }
+        }
+
+        // Compute tof.
+        tof_[cell] = (porevolume_[cell] - upwind_term)/downwind_flux;
+    }
+
+
+
+
+    void TransportModelTracerTof::solveMultiCell(const int /*num_cells*/, const int* /*cells*/)
+    {
+        THROW("Not yet implemented.");
+    }
+
+
+
+
+} // namespace Opm
---
+++ opm/core/transport/reorder/TransportModelTracerTof.hpp
@@ -0,0 +1,63 @@
+/*
+  Copyright 2012 SINTEF ICT, Applied Mathematics.
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef OPM_TRANSPORTMODELTRACERTOF_HEADER_INCLUDED
+#define OPM_TRANSPORTMODELTRACERTOF_HEADER_INCLUDED
+
+#include <opm/core/transport/reorder/TransportModelInterface.hpp>
+#include <vector>
+#include <map>
+#include <ostream>
+struct UnstructuredGrid;
+
+namespace Opm
+{
+
+    class IncompPropertiesInterface;
+
+    /// Implements a reordering transport solver for incompressible two-phase flow.
+    class TransportModelTracerTof : public TransportModelInterface
+    {
+    public:
+        /// Construct solver.
+        /// \param[in] grid      A 2d or 3d grid.
+        TransportModelTracerTof(const UnstructuredGrid& grid);
+
+        /// Solve for time-of-flight at next timestep.
+        /// \param[in] darcyflux         Array of signed face fluxes.
+        /// \param[in] porevolume        Array of pore volumes.
+        /// \param[in, out] tof          Array of time-of-flight values.
+        void solveTof(const double* darcyflux,
+                      const double* porevolume,
+                      std::vector<double>& tof);
+
+    private:
+        virtual void solveSingleCell(const int cell);
+        virtual void solveMultiCell(const int num_cells, const int* cells);
+
+    private:
+        const UnstructuredGrid& grid_;
+        const double* darcyflux_;   // one flux per grid face
+        const double* porevolume_;  // one volume per cell
+        double* tof_;
+    };
+
+} // namespace Opm
+
+#endif // OPM_TRANSPORTMODELTRACERTOF_HEADER_INCLUDED
