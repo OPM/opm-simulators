b44f8a236b918c0b78f732401c27ca42b70c6bd1
Andreas Lauser <and@poware.org>
2012-09-19 14:54:11 +0200

    correct copyright headers
    This time hopfully for good. There were a few bugs in the script to
    update them, and the metric was slightly chanted: Now, only authors
    that contributed more than 5% of the lines of a file are listed. We
    now also properly ignore the copyright headers and the changes made in
    commit 024955b5 as the latter just moved a lot of files around.

--- examples/tutorialproblem_decoupled.hh
+++ examples/tutorialproblem_decoupled.hh
@@ -1,12 +1,11 @@
 // -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
 // vi: set et ts=4 sw=4 sts=4:
 /*****************************************************************************
- *   Copyright (C) 2010-2012 by Benjamin Faigle                              *
  *   Copyright (C) 2008-2012 by Markus Wolff                                 *
- *   Copyright (C) 2009-2012 by Andreas Lauser                               *
- *   Copyright (C) 2010-2012 by Bernd Flemisch                               *
+ *   Copyright (C) 2009-2012 by Bernd Flemisch                               *
+ *   Copyright (C) 2010-2012 by Benjamin Faigle                              *
+ *   Copyright (C) 2008-2012 by Andreas Lauser                               *
  *   Copyright (C) 2009 by Anneli Schoeniger                                 *
- *   Copyright (C) 2011 by Klaus Mosthaf                                     *
  *                                                                           *
  *   This program is free software: you can redistribute it and/or modify    *
  *   it under the terms of the GNU General Public License as published by    *
