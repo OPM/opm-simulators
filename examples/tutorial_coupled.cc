421650fef1fc2f6310bc213ccf7fc5f3adf734f8
Andreas Lauser <and@poware.org>
2012-09-26 15:55:36 +0200

    update copyright notices
    in addition to last time, don't add authors that are responsible than
    5 lines in a file. This mainly makes a difference for very short files
    like the .cc files...

--- examples/tutorial_coupled.cc
+++ examples/tutorial_coupled.cc
@@ -3,10 +3,6 @@
 /*****************************************************************************
  *   Copyright (C) 2009-2012 by Andreas Lauser                               *
  *   Copyright (C) 2010 by Melanie Darcis                                    *
- *   Copyright (C) 2011 by Benjamin Faigle                                   *
- *   Copyright (C) 2009-2012 by Bernd Flemisch                               *
- *   Copyright (C) 2010 by Klaus Mosthaf                                     *
- *   Copyright (C) 2010 by Philipp Nuske                                     *
  *                                                                           *
  *   This program is free software: you can redistribute it and/or modify    *
  *   it under the terms of the GNU General Public License as published by    *
--- examples/tutorialproblem_decoupled.hh
+++ examples/tutorialproblem_decoupled.hh
@@ -5,7 +5,6 @@
  *   Copyright (C) 2009-2012 by Bernd Flemisch                               *
  *   Copyright (C) 2010-2012 by Benjamin Faigle                              *
  *   Copyright (C) 2008-2012 by Andreas Lauser                               *
- *   Copyright (C) 2009 by Anneli Schoeniger                                 *
  *                                                                           *
  *   This program is free software: you can redistribute it and/or modify    *
  *   it under the terms of the GNU General Public License as published by    *
