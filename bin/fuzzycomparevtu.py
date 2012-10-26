b4c2aba27f69b58c7d641fa35e69d6b1d067052b
Andreas Lauser <and@poware.org>
2012-10-26 13:59:19 +0200

    implement molecular diffusion for all compositional box models

--- bin/fuzzycomparevtu.py
+++ bin/fuzzycomparevtu.py
@@ -32,12 +32,14 @@ def isFuzzyEqual(vtkFile1, vtkFile2, absTol, relTol):
         for i in range(0, len(curVals1)):
             number1 = curVals1[i]
             number2 = curVals2[i]
-            if curFieldName.startswith("saturation") and abs(number1 - number2) > 1e-3:
-                print 'Difference between %f and %f too large in data field "%s: %s"'%(number1,number2,curFieldName,abs(number1 - number2))
-                return False
-            elif curFieldName.startswith("pressure") and abs(number1 - number2) > 0.1 and abs(number1 - number2) > 1e-5*abs(number1 + number2):
-                print 'Difference between %f and %f too large in data field "%s: %s"'%(number1,number2,curFieldName,abs(number1 - number2))
-                return False
+            if curFieldName.startswith("saturation"):
+                if abs(number1 - number2) > 1e-3:
+                    print 'Difference between %f and %f too large in data field "%s: %s"'%(number1,number2,curFieldName,abs(number1 - number2))
+                    return False
+            elif curFieldName.startswith("pressure"):
+                if abs(number1 - number2) > 0.1 and abs(number1 - number2) > 1e-5*abs(number1 + number2):
+                    print 'Difference between %f and %f too large in data field "%s: %s"'%(number1,number2,curFieldName,abs(number1 - number2))
+                    return False
             elif abs(number1 - number2) > absTol and number2 != 0 and abs(number1/number2 - 1) > relTol:
                 print 'Difference between %f and %f too large (%f%%) in data field "%s"'%(number1,number2,abs(number1/number2 - 1)*100, curFieldName)
                 return False
