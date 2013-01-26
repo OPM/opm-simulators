f7e3f7bb4130104475ff350babedd88bcd8669e5
Andreas Lauser <Andreas.Lauser@iws.uni-stuttgart.de>
2013-01-26 23:30:00 +0100

    also avoid warning if mpihelper.hh is included in a .cc file

--- test/material/fluidsystems/test_fluidsystems.cc
+++ test/material/fluidsystems/test_fluidsystems.cc
@@ -26,7 +26,12 @@
 
 #include "checkfluidsystem.hh"
 
+#include <dune/common/version.hh>
+#if DUNE_VERSION_NEWER(DUNE_COMMON, 2, 3)
+#include <dune/common/parallel/mpihelper.hh>
+#else
 #include <dune/common/mpihelper.hh>
+#endif
 
 // include all fluid systems in ewoms-stable
 #include <ewoms/material/fluidsystems/1pfluidsystem.hh>
