2ad6a1b5accf8992d45ec7fc78b55ea2179d2b24
Roland Kaufmann <roland.kaufmann@uni.no>
2013-06-05 14:20:29 +0200

    Don't install debug syms for a library that isn't there

--- cmake/Modules/OpmInstall.cmake
+++ cmake/Modules/OpmInstall.cmake
@@ -42,12 +42,12 @@ macro (opm_install opm)
   endif (SYSTEM_DEBUG)
   # static libraries don't have their debug info stripped, so there is
   # only a separate file when we are building shared objects
-  if (${opm}_LIBRARY_TYPE STREQUAL "SHARED")
+  if (${opm}_LIBRARY_TYPE STREQUAL "SHARED" AND ${opm}_TARGET)
 	install (
 	  FILES ${PROJECT_BINARY_DIR}/${${opm}_DEBUG}
 	  DESTINATION ${_dbg_prefix}${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}
 	  )
-  endif (${opm}_LIBRARY_TYPE STREQUAL "SHARED")
+  endif (${opm}_LIBRARY_TYPE STREQUAL "SHARED" AND ${opm}_TARGET)
   install (
 	FILES ${PROJECT_SOURCE_DIR}/dune.module
 	DESTINATION ${CMAKE_INSTALL_LIBDIR_NOARCH}/dunecontrol/${${opm}_NAME}
