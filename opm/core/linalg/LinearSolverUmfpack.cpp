baa9bf12e07b07775b18efed59528b329683d9e0
Roland Kaufmann <roland.kaufmann@uni.no>
2013-07-05 10:59:52 +0200

    Zero-initialize structure to avoid returning undefined
    The compiler will otherwise complain that we are returning undefined
    data. There is no way for the client code to know whether this was
    the case.

--- opm/core/linalg/LinearSolverUmfpack.cpp
+++ opm/core/linalg/LinearSolverUmfpack.cpp
@@ -56,7 +56,7 @@ namespace Opm
             const_cast<double*>(sa)
         };
         call_UMFPACK(&A, rhs, solution);
-        LinearSolverReport rep;
+        LinearSolverReport rep = {0};
         rep.converged = true;
         return rep;
     }
