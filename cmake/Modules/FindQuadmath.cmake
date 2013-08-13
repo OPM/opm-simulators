687f7164d284ec3ed4df36035e5a7971d9c9711b
Andreas Lauser <and@poware.org>
2013-08-13 14:58:26 +0200

    make the build system link to the "quadmath" library if it is supported
    Thanks a lot to Roland Kaufmann for the support.

--- cmake/Modules/FindQuadmath.cmake
+++ cmake/Modules/FindQuadmath.cmake
@@ -1,8 +1,9 @@
 # Module that checks whether the compiler supports the
 # quadruple precision floating point math
 #
-# Sets the following variable:
+# Sets the following variables:
 # HAVE_QUAD
+# QUADMATH_LIBRARIES
 #
 # perform tests
 include(CheckCXXSourceCompiles)
@@ -17,3 +18,14 @@ int main(void){
     __float128 foo = sqrtq(123.456);
 }" HAVE_QUAD)
 cmake_pop_check_state()
+
+if (HAVE_QUAD)
+  set(QUADMATH_LIBRARIES "quadmath")
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(QuadMath
+  DEFAULT_MSG
+  QUADMATH_LIBRARIES
+  HAVE_QUAD
+  )
