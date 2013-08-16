b3947b0ff33809b643c4900759d319ec5fe78cad
Roland Kaufmann <roland.kaufmann@uni.no>
2013-08-16 21:48:28 +0200

    OPM-Material module must have DUNE Common and DUNE ISTL

--- cmake/Modules/opm-material-prereqs.cmake
+++ cmake/Modules/opm-material-prereqs.cmake
@@ -14,6 +14,6 @@ set (opm-material_DEPS
 	# compile with C++0x/11 support if available
 	"CXX11Features REQUIRED"
 	# DUNE dependency
-	"dune-common"
-	"dune-istl"
+	"dune-common REQUIRED"
+	"dune-istl REQUIRED"
 	)
