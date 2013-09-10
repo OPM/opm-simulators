594b3dd15be93fbc4bbbfa8df09872eb077e1b95
Andreas Lauser <and@poware.org>
2013-09-10 13:40:27 +0200

    add version.h header file and adapt version number in dune.module

---
+++ opm/material/version.h
@@ -0,0 +1,6 @@
+/**
+ * Current API level (for use with DUNE_VERSION_xxx):
+ */
+#define OPM_MATERIAL_VERSION_MAJOR    2
+#define OPM_MATERIAL_VERSION_MINOR    0
+#define OPM_MATERIAL_VERSION_REVISION 0
