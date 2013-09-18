b13f9d251ba6889e8c563b9a14a9d495ae0b545f
Roland Kaufmann <roland.kaufmann@uni.no>
2013-09-18 13:11:03 +0200

    Fix logic bug and make language explicit
    The operator precedence in CMake can be surprising, so add parenthesis
    to make in unambigious. Also, make it more explicit from which language
    we get the default options. (They are all set from the settings given
    for C++; why would have different optimization options for C and C++?)

--- cmake/Modules/UseWarnings.cmake
+++ cmake/Modules/UseWarnings.cmake
@@ -6,7 +6,7 @@ is_compiler_gcc_compatible ()
 
 if (CXX_COMPAT_GCC)
   # default warnings flags, if not set by user
-  set_default_option (_warn_flag "-Wall" "(^|\ )-W")
+  set_default_option (CXX _warn_flag "-Wall" "(^|\ )-W")
   if (_warn_flag)
 	message (STATUS "All warnings enabled: ${_warn_flag}")
 	add_options (ALL_LANGUAGES ALL_BUILDS "${_warn_flag}")
