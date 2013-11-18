1764c1fffa6e9abc0755056755e6205f12ca3e63
Joakim Hove <joakim.hove@gmail.com>
2013-11-18 14:49:38 +0100

    Updated to require opm-parser. Identical to opm-core PR:433

--- cmake/Modules/opm-core-prereqs.cmake
+++ cmake/Modules/opm-core-prereqs.cmake
@@ -3,31 +3,33 @@
 
 # defines that must be present in config.h for our headers
 set (opm-core_CONFIG_VAR
-	HAVE_ERT
-	HAVE_SUITESPARSE_UMFPACK_H
-	)
+        HAVE_ERT
+        HAVE_SUITESPARSE_UMFPACK_H
+        )
 
 # dependencies
 set (opm-core_DEPS
-	# compile with C99 support if available
-	"C99"
-	# compile with C++0x/11 support if available
-	"CXX11Features REQUIRED"
-	# various runtime library enhancements
-	"Boost 1.39.0
-		COMPONENTS date_time filesystem system unit_test_framework REQUIRED"
-	# matrix library
-	"BLAS REQUIRED"
-	"LAPACK REQUIRED"
-	# Tim Davis' SuiteSparse archive
-	"SuiteSparse COMPONENTS umfpack"
-	# solver
-	"SuperLU"
-	# xml processing (for config parsing)
-	"TinyXML"
-	# Ensembles-based Reservoir Tools (ERT)
-	"ERT"
-	# DUNE dependency
-	"dune-common"
-	"dune-istl"
-	)
+        # compile with C99 support if available
+        "C99"
+        # compile with C++0x/11 support if available
+        "CXX11Features REQUIRED"
+        # various runtime library enhancements
+        "Boost 1.44.0
+                COMPONENTS date_time filesystem system unit_test_framework REQUIRED"
+        # matrix library
+        "BLAS REQUIRED"
+        "LAPACK REQUIRED"
+        # Tim Davis' SuiteSparse archive
+        "SuiteSparse COMPONENTS umfpack"
+        # solver
+        "SuperLU"
+        # xml processing (for config parsing)
+        "TinyXML"
+        #Parser library
+        "opm-parser REQUIRED"
+        # Ensembles-based Reservoir Tools (ERT)
+        "ERT"
+        # DUNE dependency
+        "dune-common"
+        "dune-istl"
+        )
