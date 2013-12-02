f26f06bf97dd6e71871ec387e06b3658bcb4d095
Andreas Lauser <and@poware.org>
2013-12-02 16:39:42 +0100

    update the in-file copyright holder statements
    this patch is quite large as there were various bug fixes to the
    script which generates these statements

--- tests/models/problems/stokestestproblem.hh
+++ tests/models/problems/stokestestproblem.hh
@@ -1,7 +1,7 @@
 // -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
 // vi: set et ts=4 sw=4 sts=4:
 /*
-  Copyright (C) 2012 by Andreas Lauser
+  Copyright (C) 2012-2013 by Andreas Lauser
   Copyright (C) 2012 by Klaus Mosthaf
 
   This file is part of the Open Porous Media project (OPM).
