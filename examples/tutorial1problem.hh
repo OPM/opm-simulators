3c8229586f282cb10acccaac7d290d6c3639537e
Andreas Lauser <and@poware.org>
2013-12-02 16:39:42 +0100

    update the in-file copyright holder statements
    this patch is quite large as there were various bug fixes to the
    script which generates these statements

--- examples/tutorial1problem.hh
+++ examples/tutorial1problem.hh
@@ -1,7 +1,9 @@
 // -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
 // vi: set et ts=4 sw=4 sts=4:
 /*
-  Copyright (C) 2009-2012 by Andreas Lauser
+  Copyright (C) 2009-2013 by Andreas Lauser
+  Copyright (C) 2010 by Benjamin Faigle
+  Copyright (C) 2010 by Klaus Mosthaf
 
   This file is part of the Open Porous Media project (OPM).
 
