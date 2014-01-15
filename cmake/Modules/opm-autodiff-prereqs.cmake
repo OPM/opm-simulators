ea9f088126880705728a07a40531575019b7e970
Markus Blatt <markus@dr-blatt.de>
2014-01-15 13:15:13 +0100

    Corrects required version and make Eigen3 download work.
    At least on Debian requiring version 3.1 of Eigen3 is not sufficient
    according to issue #25 the minimum version is 3.1.2. This patch fixes
    the required version. In addition it skips the REQUIRED option passed
    to find_package(Eigen3) to make the git download work.
    
    Fixes #25.

--- cmake/Modules/opm-autodiff-prereqs.cmake
+++ cmake/Modules/opm-autodiff-prereqs.cmake
@@ -3,21 +3,21 @@
 
 # defines that must be present in config.h for our headers
 set (opm-autodiff_CONFIG_VAR
-        )
+	)
 
 # dependencies
 set (opm-autodiff_DEPS
-        # Compile with C99 support if available
-        "C99"
-        # Compile with C++0x/11 support if available
-        "CXX11Features"
-        # Various runtime library enhancements
-        "Boost 1.44.0
-                COMPONENTS date_time filesystem system unit_test_framework REQUIRED"
-        # DUNE prerequisites
-        "dune-common REQUIRED;
-        dune-istl REQUIRED;
-        opm-core REQUIRED"
-        # Eigen
-        "Eigen3 3.1.2 REQUIRED"
-        )
+	# Compile with C99 support if available
+	"C99"
+	# Compile with C++0x/11 support if available
+	"CXX11Features"
+	# Various runtime library enhancements
+	"Boost 1.39.0
+		COMPONENTS date_time filesystem system unit_test_framework REQUIRED"
+	# DUNE prerequisites
+	"dune-common REQUIRED;
+	dune-istl REQUIRED;
+	opm-core REQUIRED"
+	# Eigen
+	"Eigen3 3.1.2 "
+	)
