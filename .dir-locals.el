4605bd9d5593d20adb194ac78104ef5e3cf653d8
Andreas Lauser <and@poware.org>
2014-01-16 18:40:08 +0100

    remove all vim and emacs modelines
    for emacs, add a toplevel .dir-locals.el file instead...

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;; <http://www.gnu.org/software/emacs/manual/html_node/emacs/Directory-Variables.html>
+((c-mode . ((c-basic-offset . 4)
+            (tab-width . 8)
+            (indent-tabs-mode . nil))))
--- opm/material/DynamicTabulated2dFunction.hpp
+++ opm/material/DynamicTabulated2dFunction.hpp
@@ -1,5 +1,3 @@
-// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
-// vi: set et ts=4 sw=4 sts=4:
 /*
   Copyright (C) 2013 by Andreas Lauser
 
