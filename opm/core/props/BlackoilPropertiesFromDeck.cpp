42f5025c886ac6f4dd24a46b8c2a07a71602f99f
Andreas Lauser <and@poware.org>
2014-01-24 12:00:03 +0100

    add variants of all methods which take a deck of the new parser to the BlackOilPropertiesFromDeck

--- opm/core/props/BlackoilPropertiesFromDeck.cpp
+++ opm/core/props/BlackoilPropertiesFromDeck.cpp
@@ -23,9 +23,7 @@
 
 namespace Opm
 {
-
-    BlackoilPropertiesFromDeck::BlackoilPropertiesFromDeck(const EclipseGridParser& deck,
-                                                           const UnstructuredGrid& grid,
+    BlackoilPropertiesFromDeck::BlackoilPropertiesFromDeck(const EclipseGridParser& deck,                                                           const UnstructuredGrid& grid,
                                                            bool init_rock)
     {
        if (init_rock){
@@ -36,6 +34,25 @@ namespace Opm
             = new SaturationPropsFromDeck<SatFuncSimpleUniform>();
         satprops_.reset(ptr);
         ptr->init(deck, grid, 200);
+
+        if (pvt_.numPhases() != satprops_->numPhases()) {
+            OPM_THROW(std::runtime_error, "BlackoilPropertiesFromDeck::BlackoilPropertiesFromDeck() - Inconsistent number of phases in pvt data ("
+                  << pvt_.numPhases() << ") and saturation-dependent function data (" << satprops_->numPhases() << ").");
+        }
+    }
+
+    BlackoilPropertiesFromDeck::BlackoilPropertiesFromDeck(Opm::DeckConstPtr newParserDeck,
+                                                           const UnstructuredGrid& grid,
+                                                           bool init_rock)
+    {
+        if (init_rock){
+           rock_.init(newParserDeck, grid);
+        }
+        pvt_.init(newParserDeck, /*numSamples=*/200);
+        SaturationPropsFromDeck<SatFuncSimpleUniform>* ptr
+            = new SaturationPropsFromDeck<SatFuncSimpleUniform>();
+        satprops_.reset(ptr);
+        ptr->init(newParserDeck, grid, /*numSamples=*/200);
 
         if (pvt_.numPhases() != satprops_->numPhases()) {
             OPM_THROW(std::runtime_error, "BlackoilPropertiesFromDeck::BlackoilPropertiesFromDeck() - Inconsistent number of phases in pvt data ("
@@ -96,6 +113,70 @@ namespace Opm
                     = new SaturationPropsFromDeck<SatFuncGwsegNonuniform>();
                 satprops_.reset(ptr);
                 ptr->init(deck, grid, sat_samples);
+            } else {
+                OPM_THROW(std::runtime_error, "Unknown threephase_model: " << threephase_model);
+            }
+        }
+
+        if (pvt_.numPhases() != satprops_->numPhases()) {
+            OPM_THROW(std::runtime_error, "BlackoilPropertiesFromDeck::BlackoilPropertiesFromDeck() - Inconsistent number of phases in pvt data ("
+                  << pvt_.numPhases() << ") and saturation-dependent function data (" << satprops_->numPhases() << ").");
+        }
+    }
+
+    BlackoilPropertiesFromDeck::BlackoilPropertiesFromDeck(Opm::DeckConstPtr newParserDeck,
+                                                           const UnstructuredGrid& grid,
+                                                           const parameter::ParameterGroup& param,
+                                                           bool init_rock)
+    {
+        if(init_rock){
+            rock_.init(newParserDeck, grid);
+        }
+
+        const int pvt_samples = param.getDefault("pvt_tab_size", 200);
+        pvt_.init(newParserDeck, pvt_samples);
+
+        // Unfortunate lack of pointer smartness here...
+        const int sat_samples = param.getDefault("sat_tab_size", 200);
+        std::string threephase_model = param.getDefault<std::string>("threephase_model", "simple");
+        if (newParserDeck->hasKeyword("ENDSCALE") && threephase_model != "simple") {
+            OPM_THROW(std::runtime_error, "Sorry, end point scaling currently available for the 'simple' model only.");
+        }
+        if (sat_samples > 1) {
+            if (threephase_model == "stone2") {
+                SaturationPropsFromDeck<SatFuncStone2Uniform>* ptr
+                    = new SaturationPropsFromDeck<SatFuncStone2Uniform>();
+                satprops_.reset(ptr);
+                ptr->init(newParserDeck, grid, sat_samples);
+            } else if (threephase_model == "simple") {
+                SaturationPropsFromDeck<SatFuncSimpleUniform>* ptr
+                    = new SaturationPropsFromDeck<SatFuncSimpleUniform>();
+                satprops_.reset(ptr);
+                ptr->init(newParserDeck, grid, sat_samples);
+            } else if (threephase_model == "gwseg") {
+                SaturationPropsFromDeck<SatFuncGwsegUniform>* ptr
+                    = new SaturationPropsFromDeck<SatFuncGwsegUniform>();
+                satprops_.reset(ptr);
+                ptr->init(newParserDeck, grid, sat_samples);
+            } else {
+                OPM_THROW(std::runtime_error, "Unknown threephase_model: " << threephase_model);
+            }
+        } else {
+            if (threephase_model == "stone2") {
+                SaturationPropsFromDeck<SatFuncStone2Nonuniform>* ptr
+                    = new SaturationPropsFromDeck<SatFuncStone2Nonuniform>();
+                satprops_.reset(ptr);
+                ptr->init(newParserDeck, grid, sat_samples);
+            } else if (threephase_model == "simple") {
+                SaturationPropsFromDeck<SatFuncSimpleNonuniform>* ptr
+                    = new SaturationPropsFromDeck<SatFuncSimpleNonuniform>();
+                satprops_.reset(ptr);
+                ptr->init(newParserDeck, grid, sat_samples);
+            } else if (threephase_model == "gwseg") {
+                SaturationPropsFromDeck<SatFuncGwsegNonuniform>* ptr
+                    = new SaturationPropsFromDeck<SatFuncGwsegNonuniform>();
+                satprops_.reset(ptr);
+                ptr->init(newParserDeck, grid, sat_samples);
             } else {
                 OPM_THROW(std::runtime_error, "Unknown threephase_model: " << threephase_model);
             }
