9d97a8d938a67142b21e13c6ad0321ec105b2921
Andreas Lauser <and@poware.org>
2014-02-09 19:43:18 +0100

    extend the .dir-locals.el definitions for emacs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,19 @@
 ;; <http://www.gnu.org/software/emacs/manual/html_node/emacs/Directory-Variables.html>
-((c-mode . ((c-basic-offset . 4)
+((c-mode . (
+            ; indentation of blocks is 4 characters ...
+            (c-basic-offset . 4)
+            ; ... and we use SPACES, _not_ TABULATORS!
+            (indent-tabs-mode . nil)
+            ; if we encounter a tabulator anyway, this is interpreted
+            ; as being 8 character wide
             (tab-width . 8)
-            (indent-tabs-mode . nil))))
+            ; trailing whitespace is to be avoided, so let's make it
+            ; annoying.
+            (setq show-trailing-whitespace t)
+            ; we don't indent inline opens, ...
+            (c-set-offset 'inline-open 0)
+            ; ... namespaces,
+            (c-set-offset 'innamespace 0)
+            ; and case labels further than the rest of the function
+            ; body
+            (c-set-offset 'case-label 0))))
