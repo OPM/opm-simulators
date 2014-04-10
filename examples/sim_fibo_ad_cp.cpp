OPM/opm-simulators

commit fdca5404b75cddb9ad8dc9ec3c8a73babd8eedc7

Use new parser for all deck information
This commit switches to using the convenience method

    GridManager::createGrdecl()

to construct a "struct grdecl" that can be passed directly into
CpGrid::processEclipseFormat().  The practical benefit is that we
get to leverage the opm-parser that's in active development rather
than the calcifying "EclipseGridParser" that will only become less
relevant in the future.  We also don't parse the data twice.

Note: This is mostly compile tested.  In the few actual tests I ran,
I got into trouble with internal assertions in processEclipseFormat.
That problem is not yet analysed.

examples/sim_fibo_ad_cp.cpp
@@ -34,9 +34,13 @@
 #include <opm/core/pressure/FlowBCManager.hpp>
 
 #include <opm/core/grid.h>
-//#include <opm/core/grid/GridManager.hpp>
+#include <opm/core/grid/cornerpoint_grid.h>
+
+#include <opm/core/grid/GridManager.hpp>
+
 #include <dune/grid/CpGrid.hpp>
 #include <dune/grid/common/GridAdapter.hpp>
+
 #include <opm/core/wells.h>
 #include <opm/core/wells/WellsManager.hpp>
 #include <opm/core/utility/ErrorMacros.hpp>
@@ -118,13 +122,15 @@ try
 
     Opm::ParserPtr newParser(new Opm::Parser() );
     Opm::DeckConstPtr newParserDeck = newParser->parseFile( deck_filename );
-    std::shared_ptr<EclipseGridParser> deck;
-    deck.reset(new EclipseGridParser(deck_filename));
-    std::shared_ptr<const EclipseGridParser> cdeck(deck);
 
     // Grid init
     grid.reset(new Dune::CpGrid());
-    grid->processEclipseFormat(*deck, 2e-12, false);
+    {
+        grdecl g = {};
+        GridManager::createGrdecl(newParserDeck, g);
+
+        grid->processEclipseFormat(g, 2e-12, false);
+    }
 
 
     Opm::EclipseWriter outputWriter(param, newParserDeck,

commit 1cbffa09de4355e3af72728c416ea1dbbb615927

Fix memory leak
Function GridManager::createGrdecl() generally allocates memory
(using std::malloc()) for the MAPAXES keyword data output of the
'grdecl' structure.  Release those resources before leaving the
scope to prevent memory leak introduced in commit fdca540.

examples/sim_fibo_ad_cp.cpp
@@ -73,6 +73,7 @@
 
 #include <memory>
 #include <algorithm>
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 #include <numeric>
@@ -130,6 +131,8 @@ try
         GridManager::createGrdecl(newParserDeck, g);
 
         grid->processEclipseFormat(g, 2e-12, false);
+
+        std::free(const_cast<double*>(g.mapaxes));
     }
 
 