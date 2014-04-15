dc622c6a7ad0badeae6e440b59044bcbabeecd45
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2014-04-15 12:10:43 +0200

    Add forgotten #include <config.h>.

--- opm/autodiff/NewtonIterationBlackoilSimple.cpp
+++ opm/autodiff/NewtonIterationBlackoilSimple.cpp
@@ -17,6 +17,7 @@
   along with OPM.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <config.h>
 
 #include <opm/autodiff/NewtonIterationBlackoilSimple.hpp>
 #include <opm/autodiff/AutoDiffHelpers.hpp>
