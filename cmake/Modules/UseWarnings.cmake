682d8c394edd0152bcff9655f726b8a3c0b1cac0
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2014-04-15 19:46:16 +0200

    Added more warnings to suppression list, sorted list.

--- cmake/Modules/UseWarnings.cmake
+++ cmake/Modules/UseWarnings.cmake
@@ -17,10 +17,13 @@ option(SILENCE_DUNE_WARNINGS "Disable warnings from DUNE?" OFF)
 if(SILENCE_DUNE_WARNINGS AND CXX_COMPAT_GCC)
   file(WRITE ${CMAKE_BINARY_DIR}/dune_disable_pragmas.h "
 #pragma GCC diagnostic push
-#pragma GCC diagnostic ignored \"-Wshadow\"
-#pragma GCC diagnostic ignored \"-Wunused-parameter\"
+#pragma GCC diagnostic ignored \"-Wdeprecated-declarations\"
+#pragma GCC diagnostic ignored \"-Wdeprecated-register\"
 #pragma GCC diagnostic ignored \"-Wignored-qualifiers\"
-#pragma GCC diagnostic ignored \"-Wmismatched-tags\"")
+#pragma GCC diagnostic ignored \"-Wmismatched-tags\"
+#pragma GCC diagnostic ignored \"-Wshadow\"
+#pragma GCC diagnostic ignored \"-Wsign-compare\"
+#pragma GCC diagnostic ignored \"-Wunused-parameter\"")
   file(WRITE ${CMAKE_BINARY_DIR}/dune_reenable_pragmas.h "#pragma GCC diagnostic pop")
 else()
     file(WRITE ${CMAKE_BINARY_DIR}/dune_disable_pragmas.h "")
