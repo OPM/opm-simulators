b7b285f7ad75a701d6fd395350a4675b6c32480b
Andreas Lauser <and@poware.org>
2014-04-23 16:51:03 +0200

    synchronize the build system with opm-core

--- cmake/Modules/UseWarnings.cmake
+++ cmake/Modules/UseWarnings.cmake
@@ -12,3 +12,20 @@ if (CXX_COMPAT_GCC)
 	add_options (ALL_LANGUAGES ALL_BUILDS "${_warn_flag}")
   endif (_warn_flag)
 endif ()
+
+option(SILENCE_DUNE_WARNINGS "Disable warnings from DUNE?" OFF)
+if(SILENCE_EXTERNAL_WARNINGS AND CXX_COMPAT_GCC)
+  file(WRITE ${CMAKE_BINARY_DIR}/disable_warning_pragmas.h "
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored \"-Wdeprecated-declarations\"
+#pragma GCC diagnostic ignored \"-Wdeprecated-register\"
+#pragma GCC diagnostic ignored \"-Wignored-qualifiers\"
+#pragma GCC diagnostic ignored \"-Wmismatched-tags\"
+#pragma GCC diagnostic ignored \"-Wshadow\"
+#pragma GCC diagnostic ignored \"-Wsign-compare\"
+#pragma GCC diagnostic ignored \"-Wunused-parameter\"")
+  file(WRITE ${CMAKE_BINARY_DIR}/reenable_warning_pragmas.h "#pragma GCC diagnostic pop")
+else()
+    file(WRITE ${CMAKE_BINARY_DIR}/disable_warning_pragmas.h "")
+    file(WRITE ${CMAKE_BINARY_DIR}/reenable_warning_pragmas.h "")
+endif()
