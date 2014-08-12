16cee1f124a8e202bd4f79d8586861c5e821ef00
Andreas Lauser <and@poware.org>
2014-08-12 18:04:29 +0200

    build system: handle the threads library in the ERT module exactly as opm-core

--- cmake/Modules/FindERT.cmake
+++ cmake/Modules/FindERT.cmake
@@ -184,8 +184,10 @@ endif (UNIX)
 
 # if shared libraries are disabled on linux, explcitly linking to the
 # pthreads library is required by ERT
-find_package(Threads ${ERT_QUIET} REQUIRED)
-list (APPEND ERT_LIBRARIES ${CMAKE_THREAD_LIBS_INIT})
+find_package(Threads ${ERT_QUIET})
+if (CMAKE_THREAD_LIBS_INIT)
+  list (APPEND ERT_LIBRARIES ${CMAKE_THREAD_LIBS_INIT})
+endif()
 
 # Platform specific library where dlopen with friends lives
 list (APPEND ERT_LIBRARIES ${CMAKE_DL_LIBS})
