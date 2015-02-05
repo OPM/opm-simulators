771bbea6d09e6beb95e675b1eac9a3421c2a0a48
Andreas Lauser <and@poware.org>
2015-02-05 16:15:36 +0100

    synchronize ewoms-prereqs.cmake and Findewoms.cmake with the main versions from eWoms

--- cmake/Modules/ewoms-prereqs.cmake
+++ cmake/Modules/ewoms-prereqs.cmake
@@ -1,10 +1,25 @@
 # -*- mode: cmake; tab-width: 2; indent-tabs-mode: t; truncate-lines: t; compile-command: "cmake -Wdev" -*-
 # vim: set filetype=cmake autoindent tabstop=2 shiftwidth=2 noexpandtab softtabstop=2 nowrap:
 
+# this avoids an annoying deprecation warning on DUNE 2.4 (which we
+# are not interested in anyway)
+set(DUNE_AVOID_CAPABILITIES_IS_PARALLEL_DEPRECATION_WARNING 1)
+
+if (NOT DEFINED EBOS_USE_ALUGRID)
+set(EBOS_USE_ALUGRID 0)
+endif()
+
+set(EBOS_USE_ALUGRID "${EBOS_USE_ALUGRID}"
+	CACHE STRING "Use dune-alugrid for the ECL black oil simulator if a sufficient dune-alugrid is available")
+
 # defines that must be present in config.h for our headers
 set (ewoms_CONFIG_VAR
 	HAVE_QUAD
 	HAVE_VALGRIND
+	HAVE_OPENMP
+	HAVE_FINAL
+	EBOS_USE_ALUGRID
+	DUNE_AVOID_CAPABILITIES_IS_PARALLEL_DEPRECATION_WARNING
 	)
 
 # dependencies
