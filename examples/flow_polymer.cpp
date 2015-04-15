d300cb6eb6b64fad19c8a5bb72ba19405ac4f8bb
Edvin Brudevoll <edbru@statoil.com>
2015-04-15 14:04:26 +0200

    Now also flow_polymer.cpp in project opm-polymer takes the NOSIM keyword into account.

--- examples/flow_polymer.cpp
+++ examples/flow_polymer.cpp
@@ -221,7 +221,8 @@ try
         fis_solver.reset(new NewtonIterationBlackoilSimple(param));
     }
 
-    Opm::TimeMapConstPtr timeMap(eclipseState->getSchedule()->getTimeMap());
+    Opm::ScheduleConstPtr schedule = eclipseState->getSchedule();
+    Opm::TimeMapConstPtr timeMap(schedule->getTimeMap());
     SimulatorTimer simtimer;
 
     // initialize variables
@@ -262,19 +263,24 @@ try
                                              deck,
                                              threshold_pressures);
 
-    std::cout << "\n\n================ Starting main simulation loop ===============\n"
-              << std::flush;
+    if (!schedule->initOnly()){
+        std::cout << "\n\n================ Starting main simulation loop ===============\n"
+                  << std::flush;
 
-    SimulatorReport fullReport = simulator.run(simtimer, state);
+        SimulatorReport fullReport = simulator.run(simtimer, state);
 
-    std::cout << "\n\n================    End of simulation     ===============\n\n";
-    fullReport.report(std::cout);
+        std::cout << "\n\n================    End of simulation     ===============\n\n";
+        fullReport.report(std::cout);
 
-    if (output) {
-        std::string filename = output_dir + "/walltime.txt";
-        std::fstream tot_os(filename.c_str(),std::fstream::trunc | std::fstream::out);
-        fullReport.reportParam(tot_os);
-        warnIfUnusedParams(param);
+        if (output) {
+            std::string filename = output_dir + "/walltime.txt";
+            std::fstream tot_os(filename.c_str(),std::fstream::trunc | std::fstream::out);
+            fullReport.reportParam(tot_os);
+            warnIfUnusedParams(param);
+        }
+    } else {
+        outputWriter.writeInit( simtimer );
+        std::cout << "\n\n================ Simulation turned off ===============\n" << std::flush;
     }
 }
 catch (const std::exception &e) {
