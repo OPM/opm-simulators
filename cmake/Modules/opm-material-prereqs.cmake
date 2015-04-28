9bac533edc71d7d90bd16a37ecee500e6338a5f3
Andreas Lauser <and@poware.org>
2015-04-28 12:03:30 +0200

    remove opm-core from the required prerequisites of opm-material

--- cmake/Modules/opm-material-prereqs.cmake
+++ cmake/Modules/opm-material-prereqs.cmake
@@ -15,7 +15,7 @@ set (opm-material_DEPS
 	# compile with C++0x/11 support if available
 	"CXX11Features REQUIRED"
 	# prerequisite OPM modules
-	"opm-core REQUIRED"
+	#"opm-core REQUIRED"
 	# DUNE dependency
 	"dune-common REQUIRED"
 	)
