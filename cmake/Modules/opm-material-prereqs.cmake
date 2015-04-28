e5c2395f7084d8fda3547a4b249e15e65f77f995
Andreas Lauser <and@poware.org>
2015-04-28 13:30:51 +0200

    opm-material-prereqs.cmake: add opm-parser as an optional dependency

--- cmake/Modules/opm-material-prereqs.cmake
+++ cmake/Modules/opm-material-prereqs.cmake
@@ -15,7 +15,7 @@ set (opm-material_DEPS
 	# compile with C++0x/11 support if available
 	"CXX11Features REQUIRED"
 	# prerequisite OPM modules
-	#"opm-core REQUIRED"
+	"opm-parser"
 	# DUNE dependency
 	"dune-common REQUIRED"
 	)
