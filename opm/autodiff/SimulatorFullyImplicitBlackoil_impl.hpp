f0f38d7ac2b5ed2f0ca9026d1275da23755411e5
Markus Blatt <markus@dr-blatt.de>
2015-05-20 17:53:54 +0200

    Fix seting of parallel_run_ member.
    We are having a parallel only if there are more than one processes,
    and not already with only one.
    
    Kudos to @atgeirr for finding this in the review.

--- opm/autodiff/SimulatorFullyImplicitBlackoil_impl.hpp
+++ opm/autodiff/SimulatorFullyImplicitBlackoil_impl.hpp
@@ -207,7 +207,7 @@ namespace Opm
                     boost::any_cast<const ParallelISTLInformation&>(solver_.parallelInformation());
                 // Only rank 0 does print to std::cout
                 terminal_output_= (info.communicator().rank()==0);
-                is_parallel_run_ = info.communicator().size();
+                is_parallel_run_ = ( info.communicator().size() > 1 );
             }
         }
 #endif
