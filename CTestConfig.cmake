0df880ce438b436a785ff61384479377aa4f927c
Andreas Lauser <and@poware.org>
2015-11-11 16:50:25 +0100

    cdash: update dropsite

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,6 +2,6 @@
 set(CTEST_PROJECT_NAME "${${project}_NAME}")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "opm-project.org")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=${${project}_NAME}")
+set(CTEST_DROP_SITE "cdash.opm-project.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=${${project}_NAME}")
 set(CTEST_DROP_SITE_CDASH TRUE)
