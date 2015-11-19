df305468411a911ad540242147501b1d3943709a
Kai Bao <kai.bao@sintef.no>
2015-11-19 17:39:46 +0100

    not handling SHUT wells when generating wells_multisegment
    it fixed the 7th time step running for Norne.

--- opm/autodiff/SimulatorFullyImplicitBlackoilMultiSegment_impl.hpp
+++ opm/autodiff/SimulatorFullyImplicitBlackoilMultiSegment_impl.hpp
@@ -112,10 +112,13 @@ namespace Opm
             // well_state.init(wells, state, prev_well_state);
 
             const std::vector<WellConstPtr>& wells_ecl = eclipse_state_->getSchedule()->getWells(timer.currentStepNum());
-            std::vector<WellMultiSegmentConstPtr> wells_multisegment(wells_ecl.size());
-            // wells_multisegment.resize(wells_ecl.size());
-            for (size_t i = 0; i < wells_multisegment.size(); ++i) {
-                wells_multisegment[i].reset(new WellMultiSegment(wells_ecl[i], timer.currentStepNum(), wells));
+            std::vector<WellMultiSegmentConstPtr> wells_multisegment;
+            wells_multisegment.reserve(wells_ecl.size());
+            for (size_t i = 0; i < wells_ecl.size(); ++i) {
+                if (wells_ecl[i]->getStatus(timer.currentStepNum()) == WellCommon::SHUT) {
+                    continue;
+                }
+                wells_multisegment.push_back(std::make_shared<WellMultiSegment>(wells_ecl[i], timer.currentStepNum(), wells));
             }
 
 #if 0
