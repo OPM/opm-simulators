74319c19f43c80cf8e7e53bc83f47ee3fe207b26
Markus Blatt <markus@dr-blatt.de>
2015-11-24 21:45:15 +0100

    Adds a communcator that allows for sending and receiving at different times.
    The current communicators for parallel index sets in dune-istl always
    assume that sending and receiving happens at the same time and is thus
    synchronized. This is not useful in all occasions. A popular exception
    are parallel preconditioners where the result of the computation
    relies on the order of the unknowns like ILU, SOR, or SSOR. In the
    parallel dune-istl only block Jacobi preconditioners have been used so
    far (a block is the whole partition of a process), with inexact solves
    (by ILU, SOR, ...) have been used.
    
    At least for the interleaved flow version these seem to scale a bit
    suboptimal. Therefore this commit is the first step towards a more
    general parallelization of these.

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -88,6 +88,7 @@ list (APPEND TEST_SOURCE_FILES
   tests/test_span.cpp
   tests/test_syntax.cpp
   tests/test_scalar_mult.cpp
+	tests/test_sendreceive_communicator.cpp
   tests/test_transmissibilitymultipliers.cpp
   tests/test_welldensitysegmented.cpp
   tests/test_vfpproperties.cpp
@@ -199,6 +200,7 @@ list (APPEND PUBLIC_HEADER_FILES
   opm/autodiff/ParallelRestrictedAdditiveSchwarz.hpp
   opm/autodiff/RateConverter.hpp
   opm/autodiff/RedistributeDataHandles.hpp
+  opm/autodiff/SendReceiveCommunicator.hpp
   opm/autodiff/SimFIBODetails.hpp
   opm/autodiff/SimulatorBase.hpp
   opm/autodiff/SimulatorBase_impl.hpp
