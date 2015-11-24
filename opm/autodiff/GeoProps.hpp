2a13275943ce887472ba21dd50dd5db08e654ba4
Tor Harald Sandve <tor.harald.sandve@iris.no>
2015-11-24 10:06:05 +0100

    Use cell depth calculated directly from the corners of the cell

--- opm/autodiff/GeoProps.hpp
+++ opm/autodiff/GeoProps.hpp
@@ -143,7 +143,7 @@ namespace Opm
 
             // Compute z coordinates
             for (int c = 0; c<numCells; ++c){
-                z_[c] = Opm::UgGridHelpers::cellCentroidCoordinate(grid, c, 2);
+                z_[c] = Opm::UgGridHelpers::cellCenterDepth(grid, c);
             }
 
 
