e769c2768cb23fe24021d090d35fb002b3fe8af5
Andreas Lauser <and@poware.org>
2016-03-14 13:21:47 +0100

    clean up the licensing preable of source files
    the in-file lists of authors has been removed in favor of a global
    list of authors in the LICENSE file. this is done because (a)
    maintaining a list of authors at the beginning of a file is a major
    pain in the a**, (b) the list of authors was not accurate in about 85%
    of all cases where more than one person was involved and (c) this list
    is not legally binding in any way (the copyright is at the person who
    authored a given change, if these lists had any legal relevance, one
    could "aquire" the copyright of the module by forking it and removing
    the lists...)
    
    the only exception of this is the eWoms fork of dune-istl's solvers.hh
    file. This is beneficial because the authors of that file do not
    appear in the global list. Further, carrying the fork of that file is
    required because we would like to use a reasonable convergence
    criterion for the linear solver. (the solvers from dune-istl do
    neither support user-defined convergence criteria not do the
    developers want support for it. (my patch was rejected a few years
    ago.))

--- applications/ebos/alucartesianindexmapper.hh
+++ applications/ebos/alucartesianindexmapper.hh
@@ -1,13 +1,11 @@
 // -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
 // vi: set et ts=4 sw=4 sts=4:
 /*
-  Copyright 2015 IRIS AS
-
   This file is part of the Open Porous Media project (OPM).
 
   OPM is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
+  the Free Software Foundation, either version 2 of the License, or
   (at your option) any later version.
 
   OPM is distributed in the hope that it will be useful,
@@ -17,6 +15,10 @@
 
   You should have received a copy of the GNU General Public License
   along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+
+  Consult the COPYING file in the top-level source directory of this
+  module for the precise wording of the license and the list of
+  copyright holders.
 */
 #ifndef EWOMS_ALU_CARTESIANINDEXMAPPER_HH
 #define EWOMS_ALU_CARTESIANINDEXMAPPER_HH
--- applications/ebos/eclalugridmanager.hh
+++ applications/ebos/eclalugridmanager.hh
@@ -15,6 +15,10 @@
 
   You should have received a copy of the GNU General Public License
   along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+
+  Consult the COPYING file in the top-level source directory of this
+  module for the precise wording of the license and the list of
+  copyright holders.
 */
 /*!
  * \file
