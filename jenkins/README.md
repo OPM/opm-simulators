edd54ea4f42da9e555d947da794b6900bd504913
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2016-04-04 17:35:23 +0200

    add jenkins build scripts

---
+++ jenkins/README.md
@@ -0,0 +1,26 @@
+# opm-autodiff jenkins build scripts:
+
+**build-opm-autodiff.sh**:
+This is a helper script which contains functions for building,
+testing and cloning opm-autodiff and its dependencies.
+
+**build.sh**:
+This script will build dependencies, then build opm-autodiff and execute its tests.
+It is intended for post-merge builds of the master branch.
+
+**build-pr.sh**:
+This script will build dependencies, then build opm-autodiff and execute its tests.
+It inspects the $ghbPrBuildComment environmental variable to obtain a pull request
+to use for ert, opm-common, opm-parser, opm-material, opm-core and
+dune-cornerpoint (defaults to master) and then builds $sha1 of opm-autodiff.
+
+It is intended for pre-merge builds of pull requests.
+
+You can optionally specify a given pull request to use for ert, opm-common,
+opm-parser, opm-material, opm-core and dune-cornerpoint through the trigger.
+The trigger line needs to contain ert=&lt;pull request number&gt; and/or
+opm-common=&lt;pull request number&gt; and/or opm-parser=&lt;pull request number&gt;
+and/or opm-material=&lt;pull request number&gt;
+and/or opm-core=&lt;pull request number&gt;
+and/or dune-cornerpoint=&lt;pull request number&gt;
+and/or opm-output=&lt;pull request number&gt;.
---
+++ jenkins/build-opm-autodiff.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+function build_opm_autodiff {
+  # Build ERT
+  pushd .
+  mkdir -p $WORKSPACE/deps/ert
+  cd $WORKSPACE/deps/ert
+  git init .
+  git remote add origin https://github.com/Ensembles/ert
+  git fetch origin $ERT_REVISION:branch_to_build
+  test $? -eq 0 || exit 1
+  git checkout branch_to_build
+  popd
+
+  pushd .
+  mkdir -p serial/build-ert
+  cd serial/build-ert
+  cmake $WORKSPACE/deps/ert/devel -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$WORKSPACE/serial/install
+  cmake --build . --target install
+  popd
+
+  # Build opm-common
+  pushd .
+  mkdir -p $WORKSPACE/deps/opm-common
+  cd $WORKSPACE/deps/opm-common
+  git init .
+  git remote add origin https://github.com/OPM/opm-common
+  git fetch origin $OPM_COMMON_REVISION:branch_to_build
+  test $? -eq 0 || exit 1
+  git checkout branch_to_build
+  popd
+  source $WORKSPACE/deps/opm-common/jenkins/build-opm-module.sh
+
+  pushd .
+  mkdir serial/build-opm-common
+  cd serial/build-opm-common
+  build_module "-DCMAKE_INSTALL_PREFIX=$WORKSPACE/serial/install" 0 $WORKSPACE/deps/opm-common
+  popd
+
+  # Build opm-parser
+  clone_and_build_module opm-parser "-DCMAKE_PREFIX_PATH=$WORKSPACE/serial/install -DCMAKE_INSTALL_PREFIX=$WORKSPACE/serial/install" $OPM_PARSER_REVISION $WORKSPACE/serial
+
+  # Build opm-material
+  clone_and_build_module opm-material "-DCMAKE_PREFIX_PATH=$WORKSPACE/serial/install -DCMAKE_INSTALL_PREFIX=$WORKSPACE/serial/install" $OPM_MATERIAL_REVISION $WORKSPACE/serial
+
+  # Build opm-core
+  clone_and_build_module opm-core "-DCMAKE_PREFIX_PATH=$WORKSPACE/serial/install -DCMAKE_INSTALL_PREFIX=$WORKSPACE/serial/install" $OPM_CORE_REVISION $WORKSPACE/serial
+
+  # Build dune-cornerpoint
+  clone_and_build_module dune-cornerpoint "-DCMAKE_PREFIX_PATH=$WORKSPACE/serial/install -DCMAKE_INSTALL_PREFIX=$WORKSPACE/serial/install" $DUNE_CORNERPOINT_REVISION $WORKSPACE/serial
+
+  # Build opm-output
+  clone_and_build_module opm-output "-DCMAKE_PREFIX_PATH=$WORKSPACE/serial/install -DCMAKE_INSTALL_PREFIX=$WORKSPACE/serial/install" $OPM_OUTPUT_REVISION $WORKSPACE/serial
+
+  # Build opm-autodiff
+  pushd .
+  mkdir serial/build-opm-autodiff
+  cd serial/build-opm-autodiff
+  build_module "-DCMAKE_PREFIX_PATH=$WORKSPACE/serial/install" 1 $WORKSPACE
+  popd
+}
