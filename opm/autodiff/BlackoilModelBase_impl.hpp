b76887ca0b0e7827c2d26d13605d4448ee822473
Markus Blatt <markus@dr-blatt.de>
2016-06-27 15:42:07 +0200

    Prevent signed-unsigned-comparison warning.

--- opm/autodiff/BlackoilModelBase_impl.hpp
+++ opm/autodiff/BlackoilModelBase_impl.hpp
@@ -1159,7 +1159,7 @@ namespace detail {
                 {
                     auto& maskContainer = info.getOwnerMask();
                     auto mask = maskContainer.begin();
-                    assert(maskContainer.size() == size_per_component);
+                    assert(static_cast<int>(maskContainer.size()) == size_per_component);
 
                     for(int cell = 0; cell < size_per_component; ++cell, ++mask)
                     {
