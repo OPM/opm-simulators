a02d39f3375cb029988db2ae105b8a4578e56043
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2016-08-23 13:56:32 +0200

    add multiconfiguration support to jenkins build script
    also unifies build.sh and build-pr.sh and gets rid of
    build-opm-material.sh

--- jenkins/README.md
+++ jenkins/README.md
@@ -1,20 +1,14 @@
 # opm-material jenkins build scripts:
 
-**build-opm-material.sh**:
-This is a helper script which contains a function for building,
-testing and cloning opm-material and its dependencies.
-
 **build.sh**:
 This script will build dependencies, then build opm-material and execute its tests.
-It is intended for post-merge builds of the master branch.
+It also inspects the $ghbPrBuildComment environmental variable and builds
+downstreams if requested. It inspects the $ghbPrBuildComment
+environmental variable to obtain a pull request to use for the modules.
 
-**build-pr.sh**:
-This script will build dependencies, then build opm-material and execute its tests.
-It inspects the $ghbPrBuildComment environmental variable to obtain a pull request
-to use for ert, opm-common and opm-parser (defaults to master)
-and then builds $sha1 of opm-material. It is intended for pre-merge builds of pull requests.
+It is intended for pre-merge builds of pull requests.
 
-You can optionally specify a given pull request to use for ert, opm-common
-and opm-parser through the trigger. The trigger line needs to contain
-ert=&lt;pull request number&gt; and/or opm-common=&lt;pull request number&gt;
-and/or opm-parser=&lt;pull request number&gt;.
+To specify a given pull request to use for upstreams and downstreams,
+trigger line needs to contain &lt;module-name&gt;=&lt;pull request number&gt;.
+
+To build with downstreams the trigger line needs to contain 'with downstreams'.
--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,18 +1,57 @@
 #!/bin/bash
 
-source `dirname $0`/build-opm-material.sh
-
 declare -a upstreams
-upstreams=(ert
+upstreams=(opm-common
+           ert
            opm-parser)
 
 declare -A upstreamRev
+upstreamRev[opm-common]=master
 upstreamRev[ert]=master
 upstreamRev[opm-parser]=master
 
-OPM_COMMON_REVISION=master
+if grep -q "opm-common=" <<< $ghprbCommentBody
+then
+  upstreamRev[opm-common]=pull/`echo $ghprbCommentBody | sed -r 's/.*opm-common=([0-9]+).*/\1/g'`/merge
+fi
 
-build_opm_material
+# Downstream revisions
+declare -a downstreams
+downstreams=(opm-output
+             opm-core
+             opm-grid
+             opm-simulators
+             opm-upscaling
+             ewoms)
+
+declare -A downstreamRev
+downstreamRev[opm-output]=master
+downstreamRev[opm-core]=master
+downstreamRev[opm-grid]=master
+downstreamRev[opm-simulators]=master
+downstreamRev[opm-upscaling]=master
+downstreamRev[ewoms]=master
+
+# Clone opm-common
+pushd .
+mkdir -p $WORKSPACE/deps/opm-common
+cd $WORKSPACE/deps/opm-common
+git init .
+git remote add origin https://github.com/OPM/opm-common
+git fetch --depth 1 origin ${upstreamRev[opm-common]}:branch_to_build
 test $? -eq 0 || exit 1
+git checkout branch_to_build
+popd
 
-cp serial/build-opm-material/testoutput.xml .
+source $WORKSPACE/deps/opm-common/jenkins/build-opm-module.sh
+
+parseRevisions
+printHeader opm-material
+
+# Setup opm-data
+if grep -q "with downstreams" <<< $ghprbCommentBody
+then
+  source $WORKSPACE/deps/opm-common/jenkins/setup-opm-data.sh
+fi
+
+build_module_full opm-material
