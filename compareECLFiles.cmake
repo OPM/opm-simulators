9cae20d6f3404a04a9e30aa9fc3ad0a0586c57c2
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2016-11-22 13:26:46 +0100

    added: new test class: init
    this compares the init file from a simulation run to a reference
    in opm-data

--- compareECLFiles.cmake
+++ compareECLFiles.cmake
@@ -15,7 +15,6 @@ set(BASE_RESULT_PATH ${PROJECT_BINARY_DIR}/tests/results)
 macro (add_test_compareECLFiles casename filename simulator)
 
   set(RESULT_PATH ${BASE_RESULT_PATH}/${simulator}+${casename})
-  # Add test that runs flow and outputs the results to file
   opm_add_test(compareECLFiles_${simulator}+${filename} NO_COMPILE
                EXE_NAME ${simulator}
                DRIVER_ARGS ${OPM_DATA_ROOT}/${casename} ${RESULT_PATH}
@@ -37,7 +36,6 @@ endmacro (add_test_compareECLFiles)
 macro (add_test_compareECLRestartFiles casename filename simulator)
 
   set(RESULT_PATH ${BASE_RESULT_PATH}/restart/${simulator}+${casename})
-  # Add test that runs flow and outputs the results to file
   opm_add_test(compareECLRestartFiles_${simulator}+${filename} NO_COMPILE
                EXE_NAME ${simulator}
                DRIVER_ARGS ${OPM_DATA_ROOT}/${casename} ${RESULT_PATH}
@@ -48,6 +46,27 @@ macro (add_test_compareECLRestartFiles casename filename simulator)
                            ${COMPARE_ECL_COMMAND}
                TEST_ARGS ${OPM_DATA_ROOT}/${casename}/${filename})
 endmacro (add_test_compareECLRestartFiles)
+
+###########################################################################
+# TEST: compareECLInitFiles
+###########################################################################
+
+# Input:
+#   - casename: basename (no extension)
+#
+macro (add_test_compareECLInitFiles casename filename simulator)
+
+  set(RESULT_PATH ${BASE_RESULT_PATH}/init/${simulator}+${casename})
+  opm_add_test(compareECLInitFiles_${simulator}+${filename} NO_COMPILE
+               EXE_NAME ${simulator}
+               DRIVER_ARGS ${OPM_DATA_ROOT}/${casename} ${RESULT_PATH}
+                           ${CMAKE_BINARY_DIR}/bin
+                           ${filename}
+                           ${abs_tol} ${rel_tol}
+                           ${COMPARE_SUMMARY_COMMAND}
+                           ${COMPARE_ECL_COMMAND}
+               TEST_ARGS ${OPM_DATA_ROOT}/${casename}/${filename})
+endmacro (add_test_compareECLInitFiles)
 
 ###########################################################################
 # TEST: parallelECLFiles
@@ -90,6 +109,11 @@ opm_set_test_driver(${PROJECT_SOURCE_DIR}/tests/run-restart-regressionTest.sh ""
 
 add_test_compareECLRestartFiles(spe1 SPE1CASE2_ACTNUM flow)
 add_test_compareECLRestartFiles(spe9 SPE9_CP_SHORT flow)
+
+# Init tests
+opm_set_test_driver(${PROJECT_SOURCE_DIR}/tests/run-init-regressionTest.sh "")
+
+add_test_compareECLInitFiles(norne NORNE_ATW2013 flow)
 
 # Parallel tests
 if(MPI_FOUND)
