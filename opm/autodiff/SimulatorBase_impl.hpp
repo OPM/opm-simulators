28c36ef949c340755b0f38ff25b592f627f05c0c
Jørgen Kvalsvik <jokva@statoil.com>
2016-11-23 16:36:56 +0100

    Mini deck is reference, not pointer

--- opm/autodiff/SimulatorBase_impl.hpp
+++ opm/autodiff/SimulatorBase_impl.hpp
@@ -264,7 +264,7 @@ namespace Opm
                 //
                 // TODO (?): handle the parallel case (maybe this works out of the box)
                 const auto& miniDeck = schedule.getModifierDeck(nextTimeStepIdx);
-                eclipse_state_->applyModifierDeck(*miniDeck);
+                eclipse_state_->applyModifierDeck(miniDeck);
                 geo_.update(grid_, props_, *eclipse_state_, gravity_);
             }
 
