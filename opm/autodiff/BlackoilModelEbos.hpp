e6acf888ccab9114fff06d162ac9172f73927b09
Andreas Lauser <and@poware.org>
2016-11-30 13:50:47 +0100

    flow_ebos: tell the ebos in ourselves to not handle SWATINIT
    because the flow part also wants to do this. (and it is quite a bit
    more stubborn!)

--- opm/autodiff/BlackoilModelEbos.hpp
+++ opm/autodiff/BlackoilModelEbos.hpp
@@ -84,6 +84,10 @@ namespace Properties {
 NEW_TYPE_TAG(EclFlowProblem, INHERITS_FROM(BlackOilModel, EclBaseProblem));
 SET_BOOL_PROP(EclFlowProblem, DisableWells, true);
 SET_BOOL_PROP(EclFlowProblem, EnableDebuggingChecks, false);
+
+// SWATINIT is done by the flow part of flow_ebos. this can be removed once the legacy
+// code for fluid and satfunc handling gets fully retired.
+SET_BOOL_PROP(EclFlowProblem, EnableSwatinit, false);
 }}
 
 namespace Opm {
