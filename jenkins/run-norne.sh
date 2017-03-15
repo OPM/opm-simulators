33042a6f751a4445dec7f7b0c26976281ab18d3d
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2017-03-15 11:25:44 +0100

    added: generate a plot without ecl reference for norne runs
    sometimes it is easier to compare without the extra data in the graphs

--- jenkins/run-norne.sh
+++ jenkins/run-norne.sh
@@ -9,6 +9,7 @@ cd norne
 mkdir $SIM
 $WORKSPACE/$configuration/build-opm-simulators/bin/$SIM deck_filename=NORNE_ATW2013.DATA output_dir=$SIM
 test $? -eq 0 || exit 1
-./plotwells.sh $WORKSPACE/$configuration/install/bin
+./plotwells.sh $WORKSPACE/$configuration/install/bin "ECL.2014.2 opm-simulation-reference" norne-wells
+./plotwells.sh $WORKSPACE/$configuration/install/bin "opm-simulation-reference" norne-wells-noecl
 
 popd
