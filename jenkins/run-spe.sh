79aec24f33b45d429a81e38edf14fc83cba91c7f
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2017-04-10 12:27:19 +0200

    Add executable name to reference data locations.

--- jenkins/run-spe.sh
+++ jenkins/run-spe.sh
@@ -12,17 +12,19 @@ function dotest {
 pushd .
 cd deps/opm-data
 
+EXE=flow_legacy
+
 # Run the SPE1/3/9 cases
 cd spe1
-$WORKSPACE/$configuration/build-opm-simulators/bin/flow_legacy deck_filename=SPE1CASE2.DATA
+$WORKSPACE/$configuration/build-opm-simulators/bin/${EXE} deck_filename=SPE1CASE2.DATA
 test $? -eq 0 || exit 1
 cd ..
 cd spe3
-$WORKSPACE/$configuration/build-opm-simulators/bin/flow_legacy max_iter=50 deck_filename=SPE3CASE1.DATA
+$WORKSPACE/$configuration/build-opm-simulators/bin/${EXE} max_iter=50 deck_filename=SPE3CASE1.DATA
 test $? -eq 0 || exit 1
 cd ..
 cd spe9
-$WORKSPACE/$configuration/build-opm-simulators/bin/flow_legacy max_iter=50 deck_filename=SPE9_CP.DATA
+$WORKSPACE/$configuration/build-opm-simulators/bin/${EXE} max_iter=50 deck_filename=SPE9_CP.DATA
 test $? -eq 0 || exit 1
 cd ..
 
@@ -32,7 +34,6 @@ compareECL=$WORKSPACE/$configuration/install/bin/compareECL
 reffile=spe1/eclipse-simulation/SPE1CASE2
 opmfile=spe1/SPE1CASE2
 dotest 0.01 0.01
-refffile=spe1/opm-simulation-reference/SPE1CASE2
 test $? -eq 0 || exit 1
 
 reffile=spe3/eclipse-simulation/SPE3CASE1
@@ -46,17 +47,17 @@ dotest 0.002 0.001
 test $? -eq 0 || exit 1
 
 # Compare OPM with OPM reference
-reffile=spe1/opm-simulation-reference/SPE1CASE2
+reffile=spe1/opm-simulation-reference/${EXE}/SPE1CASE2
 opmfile=spe1/SPE1CASE2
 dotest 0.001 0.001
 test $? -eq 0 || exit 1
 
-reffile=spe3/opm-simulation-reference/SPE3CASE1
+reffile=spe3/opm-simulation-reference/${EXE}/SPE3CASE1
 opmfile=spe3/SPE3CASE1
 dotest 0.001 0.001
 test $? -eq 0 || exit 1
 
-reffile=spe9/opm-simulation-reference/SPE9_CP
+reffile=spe9/opm-simulation-reference/${EXE}/SPE9_CP
 opmfile=spe9/SPE9_CP
 dotest 0.002 0.007
 test $? -eq 0 || exit 1
