a2831966c226757b8f052f2c12d3ab6e10f59489
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2017-06-09 10:11:42 +0200

    update polymer integration test for new data file path

--- jenkins/run-polymer.sh
+++ jenkins/run-polymer.sh
@@ -4,15 +4,15 @@ pushd .
 cd deps/opm-data
 
 # Run the simple2D polymer case
-cd polymer_test_suite/simple2D
+cd polymer_simple2D
 $WORKSPACE/$configuration/build-opm-simulators/bin/flow_polymer run.param
 test $? -eq 0 || exit 1
-cd ../..
+cd ..
 
 # Compare OPM with eclipse reference
 compareECL=$WORKSPACE/$configuration/install/bin/compareECL
-reffile=polymer_test_suite/simple2D/eclipse-simulation/2D_THREEPHASE_POLY_HETER
-opmfile=polymer_test_suite/simple2D/opm-simulation/2D_THREEPHASE_POLY_HETER
+reffile=polymer_simple2D/eclipse-simulation/2D_THREEPHASE_POLY_HETER
+opmfile=polymer_simple2D/opm-simulation-reference/flow_polymer/2D_THREEPHASE_POLY_HETER
 $compareECL $reffile $opmfile 1.0 0.004 -k SGAS
 test $? -eq 0 || exit 1
 $compareECL $reffile $opmfile 1.0 0.004 -k SWAT
