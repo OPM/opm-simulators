e0a7ced2562f6b65e1db63d7ffdff6d1bf3e8e96
Markus Blatt <markus@dr-blatt.de>
2017-06-19 17:47:24 +0200

    Only create step_timing.txt file if it gets written to.
    If output=false is requested we created an empty file.
    Now that file will only be created if output was requested
    and it will only be opened on one rank in a parallel run.

--- opm/autodiff/SimulatorFullyImplicitBlackoilEbos.hpp
+++ opm/autodiff/SimulatorFullyImplicitBlackoilEbos.hpp
@@ -163,7 +163,14 @@ public:
         Opm::time::StopWatch total_timer;
         total_timer.start();
         std::string tstep_filename = output_writer_.outputDirectory() + "/step_timing.txt";
-        std::ofstream tstep_os(tstep_filename.c_str());
+        std::ofstream tstep_os;
+
+        if ( output_writer_.output() ) {
+            if ( output_writer_.isIORank() )
+            {
+                tstep_os.open(tstep_filename.c_str());
+            }
+        }
 
         const auto& schedule = eclState().getSchedule();
 
@@ -351,11 +358,8 @@ public:
             // update timing.
             report.solver_time += solver_timer.secsSinceStart();
 
-            if ( output_writer_.output() ) {
-                if ( output_writer_.isIORank() )
-                {
-                    stepReport.reportParam(tstep_os);
-                }
+            if ( tstep_os.is_open() ) {
+                stepReport.reportParam(tstep_os);
             }
 
             // Increment timer, remember well state.
