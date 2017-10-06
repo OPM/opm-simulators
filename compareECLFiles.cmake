597021084f5ea4a7edee2f0551f37a08ec67d060
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2017-10-06 11:49:54 +0200

    Further slacken coarse test tolerance for the benefit of test 27.

--- compareECLFiles.cmake
+++ compareECLFiles.cmake
@@ -112,7 +112,7 @@ opm_set_test_driver(${PROJECT_SOURCE_DIR}/tests/run-regressionTest.sh "")
 # Set absolute tolerance to be used passed to the macros in the following tests
 set(abs_tol 2e-2)
 set(rel_tol 1e-5)
-set(coarse_rel_tol 5e-3)
+set(coarse_rel_tol 1e-2)
 
 foreach(SIM flow flow_ebos flow_legacy)
   add_test_compareECLFiles(CASENAME spe1
