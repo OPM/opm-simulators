425c7c2d666de2f40529d38fda0429d38dd0052c
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2017-11-28 09:50:32 +0100

    fixed: run polymer integration test with flow, not recently removed flow_polymer

--- jenkins/run-polymer.sh
+++ jenkins/run-polymer.sh
@@ -5,14 +5,14 @@ cd deps/opm-data
 
 # Run the simple2D polymer case
 cd polymer_simple2D
-$WORKSPACE/$configuration/build-opm-simulators/bin/flow_polymer run.param
+$WORKSPACE/$configuration/build-opm-simulators/bin/flow run.param
 test $? -eq 0 || exit 1
 cd ..
 
 # Compare OPM with eclipse reference
 compareECL=$WORKSPACE/$configuration/install/bin/compareECL
 reffile=polymer_simple2D/eclipse-simulation/2D_THREEPHASE_POLY_HETER
-opmfile=polymer_simple2D/opm-simulation-reference/flow_polymer/2D_THREEPHASE_POLY_HETER
+opmfile=polymer_simple2D/opm-simulation-reference/flow/2D_THREEPHASE_POLY_HETER
 $compareECL $reffile $opmfile 1.0 0.004 -k SGAS
 test $? -eq 0 || exit 1
 $compareECL $reffile $opmfile 1.0 0.004 -k SWAT
