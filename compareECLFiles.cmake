c583e3d2ebde5bc6869ff8b7f6df83ffdbbb5cbb
Tor Harald Sandve <tor.harald.sandve@iris.no>
2018-01-05 14:54:07 +0100

    start using parallelRestartTest

--- compareECLFiles.cmake
+++ compareECLFiles.cmake
@@ -251,6 +251,13 @@ foreach(sim flow flow_legacy)
                                         REL_TOL ${rel_tol_restart})
 endforeach()
 
+  add_test_compare_parallel_restarted_simulation(CASENAME spe1
+                                        FILENAME SPE1CASE2_ACTNUM
+                                        SIMULATOR flow
+                                        ABS_TOL ${abs_tol_restart}
+                                        REL_TOL ${rel_tol_restart})
+
+
 # Init tests
 opm_set_test_driver(${PROJECT_SOURCE_DIR}/tests/run-init-regressionTest.sh "")
 
