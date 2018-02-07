58a1b7df1fc5d97abcbb1fb761be448080aa4d47
Andreas Lauser <and@poware.org>
2018-02-07 10:27:32 +0100

    adapt to the move of infrastructure from opm-common to opm-material

--- examples/flow.cpp
+++ examples/flow.cpp
@@ -28,7 +28,7 @@
 
 #include <opm/autodiff/MissingFeatures.hpp>
 #include <opm/common/utility/parameters/ParameterGroup.hpp>
-#include <opm/common/utility/ResetLocale.hpp>
+#include <opm/material/common/ResetLocale.hpp>
 
 #include <opm/parser/eclipse/Deck/Deck.hpp>
 #include <opm/parser/eclipse/Parser/Parser.hpp>
