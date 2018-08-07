d28e5d1ea204ea3f1407e60864d7c508a11473c7
Andreas Lauser <and@poware.org>
2018-08-07 15:49:49 +0200

    flow: remove the Newton-Raphson "update stabilization"
    for the tests which I tested (Norne, Model 2 base case), this only
    lead to worse performance while it did not improve convergence
    behaviour.

--- opm/autodiff/BlackoilModelEbos.hpp
+++ opm/autodiff/BlackoilModelEbos.hpp
@@ -299,23 +299,6 @@ namespace Opm {
                 // there is no theorectical explanation which way is better for sure.
                 wellModel().recoverWellSolutionAndUpdateWellState(x);
 
-                if (param_.use_update_stabilization_) {
-                    // Stabilize the nonlinear update.
-                    bool isOscillate = false;
-                    bool isStagnate = false;
-                    nonlinear_solver.detectOscillations(residual_norms_history_, iteration, isOscillate, isStagnate);
-                    if (isOscillate) {
-                        current_relaxation_ -= nonlinear_solver.relaxIncrement();
-                        current_relaxation_ = std::max(current_relaxation_, nonlinear_solver.relaxMax());
-                        if (terminalOutputEnabled()) {
-                            std::string msg = "    Oscillating behavior detected: Relaxation set to "
-                                    + std::to_string(current_relaxation_);
-                            OpmLog::info(msg);
-                        }
-                    }
-                    nonlinear_solver.stabilizeNonlinearUpdate(x, dx_old_, current_relaxation_);
-                }
-
                 // Apply the update, with considering model-dependent limitations and
                 // chopping of the update.
                 updateState(x);
