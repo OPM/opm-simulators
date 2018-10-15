e958ee1139cbc357458f3312caa1b5dd2a616132
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2018-10-15 16:41:10 +0200

    WellState FIBO: Initialize 'nseg_' in init()
    Needed to guarantee that numSegments(wellID) returns sensible values
    for wellID = numWells()-1 in the case of no multi-segment wells.

--- opm/autodiff/WellStateFullyImplicitBlackoil.hpp
+++ opm/autodiff/WellStateFullyImplicitBlackoil.hpp
@@ -280,6 +280,7 @@ namespace Opm
             {
                 // we need to create a trival segment related values to avoid there will be some
                 // multi-segment wells added later.
+                nseg_ = nw;
                 top_segment_index_.reserve(nw);
                 for (int w = 0; w < nw; ++w) {
                     top_segment_index_.push_back(w);
