d6f1f1bd3db25c76881542e77ad528f1b3b3f180
WesselZ <wessel.zeeuw@tno.nl>
2018-10-16 15:59:16 +0200

    Implementation of Fetkovich Aquifers. Makes changes to the files for BlackOilAquiferModel(_impl) and AquiferFetkovich file.

--- opm/autodiff/BlackoilAquiferModel_impl.hpp
+++ opm/autodiff/BlackoilAquiferModel_impl.hpp
@@ -1,123 +1,176 @@
 #include <opm/grid/utility/cartesianToCompressed.hpp>
-
 namespace Opm {
 
+  template<typename TypeTag>
+  BlackoilAquiferModel<TypeTag>::
+  BlackoilAquiferModel(Simulator& simulator)
+  : simulator_(simulator)
+  {
+    init();
+  }
 
-    template<typename TypeTag>
-    BlackoilAquiferModel<TypeTag>::
-    BlackoilAquiferModel(Simulator& simulator)
-        : simulator_(simulator)
+  template<typename TypeTag>
+  void
+  BlackoilAquiferModel<TypeTag>::initialSolutionApplied()
+  {
+    if(aquiferCarterTracyActive())
     {
-        init();
+      for (auto aquifer = aquifers_CarterTracy.begin(); aquifer != aquifers_CarterTracy.end(); ++aquifer)
+      {
+        aquifer->initialSolutionApplied();
+      }
     }
-
-    template<typename TypeTag>
-    void
-    BlackoilAquiferModel<TypeTag>::initialSolutionApplied()
+    if(aquiferFetkovichActive())
     {
-        for (auto aquifer = aquifers_.begin(); aquifer != aquifers_.end(); ++aquifer) {
-            aquifer->initialSolutionApplied();
-        }
+      for (auto aquifer = aquifers_Fetkovich.begin(); aquifer != aquifers_Fetkovich.end(); ++aquifer)
+      {
+        aquifer->initialSolutionApplied();
+      }
     }
+  }
 
-    template<typename TypeTag>
-    void
-    BlackoilAquiferModel<TypeTag>::beginEpisode()
-    { }
+  template<typename TypeTag>
+  void
+  BlackoilAquiferModel<TypeTag>::beginEpisode()
+  { }
 
-    template<typename TypeTag>
-    void
-    BlackoilAquiferModel<TypeTag>::beginTimeStep()
+  template<typename TypeTag>
+  void
+  BlackoilAquiferModel<TypeTag>::beginIteration()
+  { }
+
+  template<typename TypeTag>
+  void BlackoilAquiferModel<TypeTag>:: beginTimeStep()
+  {
+    if(aquiferCarterTracyActive())
     {
-        for (auto aquifer = aquifers_.begin(); aquifer != aquifers_.end(); ++aquifer) {
-            aquifer->beginTimeStep();
-        }
+      for (auto aquifer = aquifers_CarterTracy.begin(); aquifer != aquifers_CarterTracy.end(); ++aquifer)
+      {
+        aquifer->beginTimeStep();
+      }
     }
-
-    template<typename TypeTag>
-    void
-    BlackoilAquiferModel<TypeTag>::beginIteration()
-    { }
-
-    template<typename TypeTag>
-    template <class Context>
-    void
-    BlackoilAquiferModel<TypeTag>::addToSource(RateVector& rates,
-                                               const Context& context,
-                                               unsigned spaceIdx,
-                                               unsigned timeIdx) const
+    if(aquiferFetkovichActive())
     {
-        for (auto& aquifer: aquifers_) {
-            aquifer.addToSource(rates, context, spaceIdx, timeIdx);
-        }
+      for (auto aquifer = aquifers_Fetkovich.begin(); aquifer != aquifers_Fetkovich.end(); ++aquifer)
+      {
+        aquifer->beginTimeStep();
+      }
     }
+  }
 
-    template<typename TypeTag>
-    void
-    BlackoilAquiferModel<TypeTag>::endIteration()
-    { }
-
-    template<typename TypeTag>
-    void
-    BlackoilAquiferModel<TypeTag>::endTimeStep()
+  template<typename TypeTag>
+  template<class Context>
+  void BlackoilAquiferModel<TypeTag>:: addToSource(RateVector& rates, const Context& context, unsigned spaceIdx, unsigned timeIdx) const
+  {
+    if(aquiferCarterTracyActive())
     {
-        for (auto aquifer = aquifers_.begin(); aquifer != aquifers_.end(); ++aquifer) {
-            aquifer->endTimeStep();
-        }
+      for (auto& aquifer : aquifers_CarterTracy)
+      {
+        aquifer.addToSource(rates, context, spaceIdx, timeIdx);
+      }
     }
-
-    template<typename TypeTag>
-    void
-    BlackoilAquiferModel<TypeTag>::endEpisode()
-    { }
-
-    // Initialize the aquifers in the deck
-    template<typename TypeTag>
-    void
-    BlackoilAquiferModel<TypeTag>:: init()
+    if(aquiferFetkovichActive())
     {
-        const auto& deck = this->simulator_.vanguard().deck();
-
-        if ( !deck.hasKeyword("AQUCT") ) {
-            return ;
-        }
-
-        //updateConnectionIntensiveQuantities();
-        const auto& eclState = this->simulator_.vanguard().eclState();
-
-        // Get all the carter tracy aquifer properties data and put it in aquifers vector
-        const AquiferCT aquiferct = AquiferCT(eclState,deck);
-        const Aquancon aquifer_connect = Aquancon(eclState.getInputGrid(), deck);
-
-        std::vector<AquiferCT::AQUCT_data> aquifersData = aquiferct.getAquifers();
-        std::vector<Aquancon::AquanconOutput> aquifer_connection = aquifer_connect.getAquOutput();
-
-        assert( aquifersData.size() == aquifer_connection.size() );
-
-        // generate the mapping from Cartesian coordinates to the index in the actual grid
-        const auto& ugrid = simulator_.vanguard().grid();
-        const auto& gridView = simulator_.gridView();
-        const int number_of_cells = gridView.size(0);
-
-        cartesian_to_compressed_ = cartesianToCompressed(number_of_cells,
-                                                         Opm::UgGridHelpers::globalCell(ugrid));
-
-        for (size_t i = 0; i < aquifersData.size(); ++i)
-        {
-            aquifers_.push_back(
-               AquiferCarterTracy<TypeTag> (aquifersData.at(i),
-                    aquifer_connection.at(i), cartesian_to_compressed_, this->simulator_)
-                               );
-        }
-
-
+      for (auto& aquifer : aquifers_Fetkovich)
+      {
+        aquifer.addToSource(rates, context, spaceIdx, timeIdx);
+      }
     }
+  }
 
-    template<typename TypeTag>
-    bool
-    BlackoilAquiferModel<TypeTag>:: aquiferActive() const
+  template<typename TypeTag>
+  void
+  BlackoilAquiferModel<TypeTag>::endIteration()
+  { }
+
+  template<typename TypeTag>
+  void BlackoilAquiferModel<TypeTag>:: endTimeStep()
+  {
+    if(aquiferCarterTracyActive())
     {
-        return !aquifers_.empty();
+      for (auto aquifer = aquifers_CarterTracy.begin(); aquifer != aquifers_CarterTracy.end(); ++aquifer)
+      {
+        aquifer->endTimeStep();
+      }
     }
+    if(aquiferFetkovichActive())
+    {
+      for (auto aquifer = aquifers_Fetkovich.begin(); aquifer != aquifers_Fetkovich.end(); ++aquifer)
+      {
+        aquifer->endTimeStep();
+      }
+    }
+  }
+  template<typename TypeTag>
+  void
+  BlackoilAquiferModel<TypeTag>::endEpisode()
+  { }
 
+  // Initialize the aquifers in the deck
+  template<typename TypeTag>
+  void
+  BlackoilAquiferModel<TypeTag>:: init()
+  {
+    const auto& deck = this->simulator_.vanguard().deck();
+    if (deck.hasKeyword("AQUCT")) {
+      //updateConnectionIntensiveQuantities();
+      const auto& eclState = this->simulator_.vanguard().eclState();
+
+      // Get all the carter tracy aquifer properties data and put it in aquifers vector
+      const AquiferCT aquiferct = AquiferCT(eclState,deck);
+      const Aquancon aquifer_connect = Aquancon(eclState.getInputGrid(), deck);
+
+      std::vector<AquiferCT::AQUCT_data> aquifersData = aquiferct.getAquifers();
+      std::vector<Aquancon::AquanconOutput> aquifer_connection = aquifer_connect.getAquOutput();
+
+      assert( aquifersData.size() == aquifer_connection.size() );
+      cartesian_to_compressed_ = cartesianToCompressed(number_of_cells,
+                                                       Opm::UgGridHelpers::globalCell(ugrid));
+
+      for (size_t i = 0; i < aquifersData.size(); ++i)
+      {
+        aquifers_CarterTracy.push_back(
+          AquiferCarterTracy<TypeTag> (aquifersData.at(i), aquifer_connection.at(i), cartesian_to_compressed_, this->simulator_)
+        );
+      }
+    }
+    if(deck.hasKeyword("AQUFETP"))
+    {
+      //updateConnectionIntensiveQuantities();
+      const auto& eclState = this->simulator_.vanguard().eclState();
+
+      // Get all the carter tracy aquifer properties data and put it in aquifers vector
+      const Aquifetp aquifetp = Aquifetp(deck);
+      const Aquancon aquifer_connect = Aquancon(eclState.getInputGrid(), deck);
+
+      std::vector<Aquifetp::AQUFETP_data> aquifersData = aquifetp.getAquifers();
+      std::vector<Aquancon::AquanconOutput> aquifer_connection = aquifer_connect.getAquOutput();
+
+      assert( aquifersData.size() == aquifer_connection.size() );
+      for (size_t i = 0; i < aquifersData.size(); ++i)
+      {
+        aquifers_Fetkovich.push_back(
+          AquiferFetkovich<TypeTag> (aquifersData.at(i), aquifer_connection.at(i), this->simulator_)
+        );
+      }
+    }
+  }
+  template<typename TypeTag>
+  bool
+  BlackoilAquiferModel<TypeTag>:: aquiferActive() const
+  {
+    return (aquiferCarterTracyActive() || aquiferFetkovichActive());
+  }
+  template<typename TypeTag>
+  bool
+  BlackoilAquiferModel<TypeTag>:: aquiferCarterTracyActive() const
+  {
+    return !aquifers_CarterTracy.empty();
+  }
+  template<typename TypeTag>
+  bool
+  BlackoilAquiferModel<TypeTag>:: aquiferFetkovichActive() const
+  {
+    return !aquifers_Fetkovich.empty();
+  }
 } // namespace Opm
