79ec104a4f907301ad8dd1501b4e0943b129daa1
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2018-12-05 19:19:53 +0100

    Restart Case Runner: Support Conditional ECL Compatiblity
    This commit extends the restart-related regression test runner to
    support the client conditionally using restart output files that are
    strictly compatible with ECLIPSE (don't contain OPM-specific restart
    vectors and whose solution data vectors are output in single (float)
    precision).
    
    While here, also correct misuse of the special variable "$@" and
    split long lines for readability.

--- tests/run-restart-regressionTest.sh
+++ tests/run-restart-regressionTest.sh
@@ -1,57 +1,82 @@
 #!/bin/bash
 
-# This runs a simulator from start to end, then a restarted
-# run of the simulator, before comparing the output from the two runs.
-# This is meant to track regressions in the restart support.
+# This runs a simulator from start to end, then a restarted run of the
+# simulator, before comparing the output from the two runs.  This is
+# meant to track regressions in the restart support.
 
-INPUT_DATA_PATH="$1"
-RESULT_PATH="$2"
-BINPATH="$3"
-FILENAME="$4"
-ABS_TOL="$5"
-REL_TOL="$6"
-COMPARE_ECL_COMMAND="$7"
-OPM_PACK_COMMAND="$8"
-PARALLEL="${9}"
-EXE_NAME="${10}"
-shift 10
-TEST_ARGS="$@"
+INPUT_DATA_PATH="${1}";       shift
+RESULT_PATH="${1}";           shift
+BINPATH="${1}";               shift
+FILENAME="${1}";              shift
+ABS_TOL="${1}";               shift
+REL_TOL="${1}";               shift
+COMPARE_ECL_COMMAND="${1}";   shift
+OPM_PACK_COMMAND="${1}";      shift
+PARALLEL="${1}";              shift
+EXE_NAME="${1}";              shift
+CASENAME="${1}";              shift
+OPM_RSTFILE=$(echo "${1:-true}" | tr "[:upper:]" "[:lower:]");
 
-BASE_NAME=`basename ${TEST_ARGS}_RESTART.DATA`
-
-rm -Rf ${RESULT_PATH}
-mkdir -p ${RESULT_PATH}
-cd ${RESULT_PATH}
-if test $PARALLEL -eq 1
+if [ $# -gt 0 ]
 then
-  CMD_PREFIX="mpirun -np 4 "
-else
-  CMD_PREFIX=""
+    # Caller provided OPM_RSTFILE.  Shift away that setting to enable
+    # passing all other parameters on to ${EXE_NAME}.
+    shift
 fi
- ${CMD_PREFIX} ${BINPATH}/${EXE_NAME} ${TEST_ARGS}.DATA --enable-adaptive-time-stepping=false --output-dir=${RESULT_PATH}
 
+BASE_NAME=`basename "${CASENAME}_RESTART.DATA"`
+
+rm -Rf "${RESULT_PATH}"
+mkdir -p "${RESULT_PATH}"
+
+cd "${RESULT_PATH}"
+if test "$PARALLEL" -eq 1
+then
+    CMD_PREFIX="mpirun -np 4 "
+else
+    CMD_PREFIX=""
+fi
+
+${CMD_PREFIX} "${BINPATH}/${EXE_NAME}" "${CASENAME}.DATA" \
+    --enable-adaptive-time-stepping=false \
+    --enable-opm-rst-file="${OPM_RSTFILE}" \
+    --output-dir="${RESULT_PATH}" "$@"
 test $? -eq 0 || exit 1
 
-${OPM_PACK_COMMAND} -o ${BASE_NAME} ${TEST_ARGS}_RESTART.DATA
+${OPM_PACK_COMMAND} -o "${BASE_NAME}" "${CASENAME}_RESTART.DATA"
 
-${CMD_PREFIX} ${BINPATH}/${EXE_NAME} ${BASE_NAME} --enable-adaptive-time-stepping=false --output-dir=${RESULT_PATH}
+    --enable-adaptive-time-stepping=false \
+    --enable-opm-rst-file="${OPM_RSTFILE}" \
+    --output-dir="${RESULT_PATH}" "$@"
 test $? -eq 0 || exit 1
 
 ecode=0
 echo "=== Executing comparison for summary file ==="
-${COMPARE_ECL_COMMAND} -R -t SMRY ${RESULT_PATH}/${FILENAME} ${RESULT_PATH}/${FILENAME}_RESTART ${ABS_TOL} ${REL_TOL}
+${COMPARE_ECL_COMMAND} -R -t SMRY \
+    "${RESULT_PATH}/${FILENAME}" \
+    "${RESULT_PATH}/${FILENAME}_RESTART" \
+    ${ABS_TOL} ${REL_TOL}
 if [ $? -ne 0 ]
 then
-  ecode=1
-  ${COMPARE_ECL_COMMAND} -a -R -t SMRY ${RESULT_PATH}/${FILENAME} ${RESULT_PATH}/${FILENAME}_RESTART ${ABS_TOL} ${REL_TOL}
+    ecode=1
+    ${COMPARE_ECL_COMMAND} -a -R -t SMRY \
+        "${RESULT_PATH}/${FILENAME}" \
+        "${RESULT_PATH}/${FILENAME}_RESTART" \
+        ${ABS_TOL} ${REL_TOL}
 fi
 
 echo "=== Executing comparison for restart file ==="
-${COMPARE_ECL_COMMAND} -l -t UNRST ${RESULT_PATH}/${FILENAME} ${RESULT_PATH}/${FILENAME}_RESTART ${ABS_TOL} ${REL_TOL}
+${COMPARE_ECL_COMMAND} -l -t UNRST \
+    "${RESULT_PATH}/${FILENAME}" \
+    "${RESULT_PATH}/${FILENAME}_RESTART" \
+    ${ABS_TOL} ${REL_TOL}
 if [ $? -ne 0 ]
 then
-  ecode=1
-  ${COMPARE_ECL_COMMAND} -a -l -t UNRST ${RESULT_PATH}/${FILENAME} ${RESULT_PATH}/${FILENAME}_RESTART ${ABS_TOL} ${REL_TOL}
+    ecode=1
+    ${COMPARE_ECL_COMMAND} -a -l -t UNRST \
+        "${RESULT_PATH}/${FILENAME}" \
+        "${RESULT_PATH}/${FILENAME}_RESTART" \
+        ${ABS_TOL} ${REL_TOL}
 fi
 
 exit $ecode
