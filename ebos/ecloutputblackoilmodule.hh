af0d02b03dfaf645899d657e2a0f58da6bb8a45f
Andreas Lauser <and@poware.org>
2018-12-17 11:37:07 +0100

    use `enableEnergy` consistently
    thanks to [at]totto82 for noticing.

--- ebos/ecloutputblackoilmodule.hh
+++ ebos/ecloutputblackoilmodule.hh
@@ -217,6 +217,9 @@ public:
             }
         }
 
+        // always allocate memory for temperature
+        temperature_.resize(bufferSize, 0.0);
+
         // Only provide restart on restart steps
         if (!restartConfig.getWriteRestartFile(reportStepNum, log) || substep)
             return;
@@ -230,7 +233,6 @@ public:
         }
         // and oil pressure
         oilPressure_.resize(bufferSize, 0.0);
-        temperature_.resize(bufferSize, 0.0);
 
         if (FluidSystem::enableDissolvedGas())
             rs_.resize(bufferSize, 0.0);
@@ -385,7 +387,7 @@ public:
                 Opm::Valgrind::CheckDefined(oilPressure_[globalDofIdx]);
             }
 
-            if (temperature_.size() > 0) {
+            if (enableEnergy) {
                 temperature_[globalDofIdx] = Opm::getValue(fs.temperature(oilPhaseIdx));
                 Opm::Valgrind::CheckDefined(temperature_[globalDofIdx]);
             }
@@ -961,7 +963,7 @@ public:
 
         if (oilPressure_.size() > 0 && sol.has("PRESSURE"))
             oilPressure_[elemIdx] = sol.data("PRESSURE")[globalDofIndex];
-        if (temperature_.size() > 0 && sol.has("TEMP"))
+        if (enableEnergy && sol.has("TEMP"))
             temperature_[elemIdx] = sol.data("TEMP")[globalDofIndex];
         if (rs_.size() > 0 && sol.has("RS"))
             rs_[elemIdx] = sol.data("RS")[globalDofIndex];
@@ -1019,7 +1021,7 @@ public:
             }
         }
 
-        if (temperature_.size() > 0)
+        if (enableEnergy)
             fs.setTemperature(temperature_[elemIdx]);
         if (rs_.size() > 0)
            fs.setRs(rs_[elemIdx]);
