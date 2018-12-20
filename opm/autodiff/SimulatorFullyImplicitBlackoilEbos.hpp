eb04d8c4a7ec967b085a7a3aa186fce606c87169
hnil <Halvor.M.Nilsen@sintef.no>
2018-12-20 13:37:43 +0100

    changed default setting of adjoint to 0

--- opm/autodiff/SimulatorFullyImplicitBlackoilEbos.hpp
+++ opm/autodiff/SimulatorFullyImplicitBlackoilEbos.hpp
@@ -46,7 +46,7 @@ SET_BOOL_PROP(EclFlowProblem, EnableTerminalOutput, true);
 SET_BOOL_PROP(EclFlowProblem, EnableAdaptiveTimeStepping, true);
 SET_BOOL_PROP(EclFlowProblem, EnableTuning, false);
 SET_STRING_PROP(EclFlowProblem, AdjointResultsFile, "adjoint_results.txt");
-SET_INT_PROP(EclFlowProblem,NumWellAdjoint,1);
+SET_INT_PROP(EclFlowProblem,NumWellAdjoint,0);
 END_PROPERTIES
 
 
