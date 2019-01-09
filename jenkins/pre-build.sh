d3720b8d4820c536a1eee813393ccbd688ba9ed1
Andreas Lauser <and@poware.org>
2019-01-09 14:29:58 +0100

    add a pre build check to ensure that the Evaluation code is identical with the one produced by the code generator

---
+++ jenkins/pre-build.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+./bin/genEvalSpecializations.py
+
+if test -n "$(git diff)"; then
+    echo "The generated source files have been manually edited or the "
+    echo "code generator has been modified but not been run before "
+    echo "proposing the branch for merging."
+    exit 1
+else
+    exit 0
+fi
