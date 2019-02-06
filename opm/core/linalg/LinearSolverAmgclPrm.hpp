89c1a93aa288c06d4b9fb4ee0f7bbea92baf865a
hnil <Halvor.M.Nilsen@sintef.no>
2019-02-06 23:19:24 +0100

    tried to correct

--- opm/core/linalg/LinearSolverAmgclPrm.hpp
+++ opm/core/linalg/LinearSolverAmgclPrm.hpp
@@ -18,8 +18,8 @@
   along with OPM.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef OPM_LINEARSOLVERAMGCLNEW_HEADER_INCLUDED
-#define OPM_LINEARSOLVERAMGCLNEW_HEADER_INCLUDED
+#ifndef OPM_LINEARSOLVERAMGCLCPR_HEADER_INCLUDED
+#define OPM_LINEARSOLVERAMGCLCPR_HEADER_INCLUDED
 
 #include <vector>
 #include <memory>
@@ -50,47 +50,52 @@
 
 namespace Opm
 {
-    class LinearSolverAmgclNew
+    class LinearSolverAmgclCpr
     {
     public:
-      LinearSolverAmgclNew(const boost::property_tree::ptree& prm):
+      LinearSolverAmgclCpr(const boost::property_tree::ptree& prm):
 	prm_(prm),
 	solve_time_(1e99),
 	setup_time_(0.0)
-        {
+      {
+	
+      }
+      void init(const int sz,
+		const std::vector<int>& ptr,
+		const std::vector<int>& col,
+		const std::vector<double>& val,
+		const std::vector<double>& rhs,
+		const double tolerance,
+		const int maxiter){
+	auto x = new DebugTimeReport("setup all");
+	time::StopWatch clock;
+	boost::property_tree::ptree prm = prm_;
+	bool use_drs = prm.get<bool>("use_drs");
+	prm.erase("solver_type");
+	prm.erase("use_drs");
+	prm.erase("block_size");
+	prm.erase("verbose");
+	if(use_drs){	    
+	  solve_cpr_drs_.reset(new solver_cpr_drs_t(std::tie(sz, ptr, col, val), prm));
+	  std::ofstream file("amg_setup_cpr_drs.json");
+	  boost::property_tree::json_parser::write_json(file, prm);
+	}else{
+	  solve_cpr_.reset(new solver_cpr_drs_t(std::tie(sz, ptr, col, val), prm)); 
+	  std::ofstream file("amg_setup_cpr.json");
+	  boost::property_tree::json_parser::write_json(file, prm);
+	}
+	delete x;
+      }
 
-        }
-        void init(const int sz,
-                          const std::vector<int>& ptr,
-                          const std::vector<int>& col,
-                          const std::vector<double>& val,
-                          const std::vector<double>& rhs,
-                          const double tolerance,
-                          const int maxiter){
-            auto x = new DebugTimeReport("setup all");
-            time::StopWatch clock;
-            clock.start();
-	    int block_size = prm_.get<int>("block_size"):
-            prm_.put("precond.block_size", block_size);
-            prm_.put("precond.active_rows", sz);
-            //prm_.put("solver.tol", tolerance);
-            //prm_.put("solver.maxiter", maxiter);
-            solver_ = std::make_shared<solver_t>(std::tie(sz, ptr, col, val), prm_);
-            std::ofstream file("amg_setup_cpr_drs_debug.json");
-            setup_time_ = clock.secsSinceStart();
-            delete x;
-            std::property_tree::json_parser::write_json(file, prm_);
-        }
-
-        void updatePre(const int sz,
-                        const std::vector<int>& ptr,
-                        const std::vector<int>& col,
-                        const std::vector<double>& val){
-            auto x = new DebugTimeReport("update smoother");
-            solver_->precond().updateSmoother(std::tie(sz, ptr, col, val));
-            //solver_->precond()
-            delete x;
-        }
+      // void updatePre(const int sz,
+      // 		     const std::vector<int>& ptr,
+      // 		     const std::vector<int>& col,
+      // 		     const std::vector<double>& val){
+      // 	auto x = new DebugTimeReport("update smoother");
+      // 	solver_->precond().updateSmoother(std::tie(sz, ptr, col, val));
+      // 	//solver_->precond()
+      // 	delete x;
+      // }
 
         void solve( const std::vector<double>& rhs,
                     std::vector<double>& sol,
@@ -100,40 +105,30 @@ namespace Opm
                 auto y = new DebugTimeReport("solution");
                 time::StopWatch clock;
                 clock.start();
-                std::tie(iters, error) = (*solver_)(rhs, sol);
+		ifif(use_drs_){
+		  std::tie(iters, error) = (*solver_cpr_drs_)(rhs, sol);
+		}else{
+		  std::tie(iters, error) = (*solver_cpr_drs_)(rhs, sol);
+		}
                 std::cout << (*solver_) << std::endl;
                 std::cout << "Iterations" << iters << std::endl;
                 std::cout << "Errors " << error << std::endl;
                 solve_time_ = clock.secsSinceStart();
                 delete y;            
         }
-        void hackScalingFactors(const int sz,
-                                const std::vector<int>& ptr,
-                                std::vector<double>& val,
-                                std::vector<double>& rhs){
-            //const int block_size = 2;
-	    int block_size = prm_.get<double>("block_size"):
-            const double b_gas = 100.0;
-            const int n = sz / block_size;
-            assert(n*block_size_ == sz);
-            for (int blockrow = 0; blockrow < n; ++blockrow) {
-                const int gasrow = block_size_*blockrow + 2;
-                for (int jj = ptr[gasrow]; jj < ptr[gasrow + 1]; ++jj) {
-                    val[jj] /= b_gas;
-                }
-                rhs[gasrow] /= b_gas;
-            }
-        }
+      
     private:
- 
       using Backend = amgcl::backend::builtin<double>;
       using PPrecond = amgcl::amg<Backend, amgcl::runtime::coarsening::wrapper, amgcl::runtime::relaxation::wrapper>;
       using SPrecond = amgcl::relaxation::as_preconditioner<Backend, amgcl::runtime::relaxation::wrapper>;
-      using Precond = amgcl::preconditioner::cpr_drs<PPrecond, SPrecond>;
+      using Precond_cpr_drs = amgcl::preconditioner::cpr_drs<PPrecond, SPrecond>;
       using IterativeSolver = amgcl::runtime::solver::wrapper<Backend>;
-      using solver_t = amgcl::make_solver<Precond, IterativeSolver>;
+      using solver_t_cpr_drs = amgcl::make_solver<Precond_cpr_drs, IterativeSolver>;
+      using Precond_cpr = amgcl::preconditioner::cpr<PPrecond, SPrecond>;
       //using solver_t = amgcl:<Precond, IterativeSolver>;
-      std::shared_ptr<solver_t> solver_;
+      using solver_t_cpr = amgcl::make_solver<Precond_cpr, IterativeSolver>;
+      std::shared_ptr<solver_t_cpr_drs> solver_cpr_drs_;
+      std::shared_ptr<solver_t_cpr> solver_cpr_;
       boost::property_tree::ptree prm_;
       double solve_time_;
       double setup_time_;
