883193b0e36460510cd8e54c8592b3eabc24248d
Andreas Lauser <and@poware.org>
2019-03-01 10:46:51 +0100

    Newton: Do not fiddle around with the user specified tolerance anymore
    As a consquence, the property which sets this tolerance is renamed
    from NewtonRawTolerance to NewtonTolerance.

--- examples/co2injection_flash_ni_ecfv.cc
+++ examples/co2injection_flash_ni_ecfv.cc
@@ -61,7 +61,7 @@ SET_TYPE_PROP(Co2InjectionFlashNiEcfvProblem, Scalar, quad);
 // precision scalars... (this seems to only apply to Dune >= 2.4)
 SET_TAG_PROP(Co2InjectionFlashNiEcfvProblem, LinearSolverSplice, ParallelBiCGStabLinearSolver);
 #else
-SET_SCALAR_PROP(Co2InjectionFlashNiEcfvProblem, NewtonRawTolerance, 1e-5);
+SET_SCALAR_PROP(Co2InjectionFlashNiEcfvProblem, NewtonTolerance, 1e-5);
 #endif
 
 END_PROPERTIES
