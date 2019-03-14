13cd05fc1973843edce87eb1e4e27b8aa10fef5f
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2019-03-14 09:56:12 +0100

    Add weights argument to test for BlackoilAmg.

--- tests/test_blackoil_amg.cpp
+++ tests/test_blackoil_amg.cpp
@@ -303,7 +303,13 @@ void runBlackoilAmgLaplace()
     smootherArgs.iterations = 1;
     Opm::CPRParameter param;
 
+    Vector weights(b.size());
+    for (auto& elem : weights) {
+        elem = 1.0;
+    }
+
     Opm::BlackoilAmg<Operator,ParSmoother,Criterion,Communication,0> amg(param,
+                                                                         weights,
                                                                          fop, criterion,
                                                                          smootherArgs,
                                                                          comm);
