c0a2fdf5c67c12ec5498a11af136cdedf1f45fdf
Andreas Lauser <and@poware.org>
2019-06-03 11:11:41 +0200

    fix a few harmless compiler warnings

--- ebos/eclthresholdpressure.hh
+++ ebos/eclthresholdpressure.hh
@@ -168,8 +168,8 @@ public:
                 int cartElem1Idx = vanguard.cartesianIndex(elem1Idx);
                 int cartElem2Idx = vanguard.cartesianIndex(elem2Idx);
 
-                assert(0 <= cartElem1Idx && cartElemFaultIdx_.size() > 0U + cartElem1Idx);
-                assert(0 <= cartElem2Idx && cartElemFaultIdx_.size() > 0U + cartElem2Idx);
+                assert(0 <= cartElem1Idx && static_cast<int>(cartElemFaultIdx_.size()) > cartElem1Idx);
+                assert(0 <= cartElem2Idx && static_cast<int>(cartElemFaultIdx_.size()) > cartElem2Idx);
 
                 int fault1Idx = cartElemFaultIdx_[cartElem1Idx];
                 int fault2Idx = cartElemFaultIdx_[cartElem2Idx];
