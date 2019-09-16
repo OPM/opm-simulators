17e8fa6574d48fab638539c75fb8cdcc2f3b958e
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2019-09-16 12:36:57 +0200

    changed: rename ewoms/models/common -> opm/models/common

---
+++ opm/models/common/flux.hh
@@ -0,0 +1,35 @@
+// -*- mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+// vi: set et ts=4 sw=4 sts=4:
+/*
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+
+  Consult the COPYING file in the top-level source directory of this
+  module for the precise wording of the license and the list of
+  copyright holders.
+*/
+/*!
+ * \file
+ *
+ * \brief This file contains the necessary classes to calculate the
+ *        velocity out of a pressure potential gradient.
+ */
+#ifndef EWOMS_VELOCITY_MODULES_HH
+#define EWOMS_VELOCITY_MODULES_HH
+
+#include <opm/models/common/darcyfluxmodule.hh>
+#include <opm/models/common/forchheimerfluxmodule.hh>
+
+#endif
