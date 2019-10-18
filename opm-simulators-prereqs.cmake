2c903dfafbf7d1e3d7c8055c1047addda0441c3c
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2019-10-18 09:27:29 +0200

    remove conditionals HAVE_ECL_INPUT and HAVE_ECL_OUTPUT

--- opm-simulators-prereqs.cmake
+++ opm-simulators-prereqs.cmake
@@ -42,7 +42,3 @@ set (opm-simulators_DEPS
   )
 
 find_package_deps(opm-simulators)
-
-if(NOT HAVE_ECL_INPUT OR NOT HAVE_ECL_OUTPUT)
-  message(FATAL_ERROR "Eclipse input/output support required in opm-common")
-endif()
