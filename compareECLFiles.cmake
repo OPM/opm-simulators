65da51691a6c06ee1e09ea46c166da48f631a2a3
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2019-10-22 08:46:09 +0200

    silly changes to force failing tests

--- compareECLFiles.cmake
+++ compareECLFiles.cmake
@@ -145,7 +145,7 @@ opm_set_test_driver(${PROJECT_SOURCE_DIR}/tests/run-regressionTest.sh "")
 
 # Set absolute tolerance to be used passed to the macros in the following tests
 set(abs_tol 2e-2)
-set(rel_tol 1e-5)
+set(rel_tol 1e-16)
 set(coarse_rel_tol 1e-2)
 
 add_test_compareECLFiles(CASENAME spe1
@@ -215,7 +215,7 @@ add_test_compareECLFiles(CASENAME spe3
                          SIMULATOR flow
                          ABS_TOL ${abs_tol}
                          REL_TOL ${coarse_rel_tol}
-                         TEST_ARGS --tolerance-wells=1e-6 --flow-newton-max-iterations=20)
+                         TEST_ARGS --tolerance-wells=1e-2 --flow-newton-max-iterations=20)
 
 add_test_compareECLFiles(CASENAME spe9
                          FILENAME SPE9_CP_SHORT
