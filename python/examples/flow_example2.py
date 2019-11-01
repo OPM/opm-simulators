adf638c1df1bd776562429eb74b86d20fe867314
Michael Sargado <michael.sargado@gmail.com>
2019-11-01 14:22:29 +0100

    Python examples.

---
+++ python/examples/flow_example2.py
@@ -0,0 +1,19 @@
+import opm.io
+from opm.io.parser import Parser, ParseContext
+from opm.io.ecl_state import EclipseState
+from opm.io.schedule import Schedule
+from opm.io.summary import SummaryConfig
+from simulators import simulators
+
+parse_context = ParseContext( [("PARSE_RANDOM_SLASH", opm.io.action.ignore)] )
+deck = Parser().parse( '../../../opm-data/spe1/SPE1CASE1.DATA' , parse_context)
+state = EclipseState(deck)
+schedule = Schedule(deck, state)
+summary_config = SummaryConfig(deck, state, schedule)
+
+p = simulators.BlackOilSimulator()
+p.setDeck(deck)
+p.setEclipseState(state)
+p.setSchedule(schedule)
+p.setSummaryConfig(summary_config)
+p.run()
