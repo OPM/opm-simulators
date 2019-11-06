1fc68d635999177542cb4567948fcb2433496c2d
Michael Sargado <michael.sargado@gmail.com>
2019-11-06 18:18:10 +0100

    Updated example files.

---
+++ python/examples/NORNE_example2.py
@@ -0,0 +1,4 @@
+from simulators import BlackOilSimulator
+
+p = BlackOilSimulator('../../../opm-data/norne/NORNE_ATW2013.DATA')
+p.run()
---
+++ python/examples/SPE1_example2.py
@@ -0,0 +1,4 @@
+from simulators import BlackOilSimulator
+
+p = BlackOilSimulator('../../../opm-data/spe1/SPE1CASE1.DATA')
+p.run()
