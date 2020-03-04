ccb23d88e847092c771017f2cd7b7ff9530a9232
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2020-03-04 09:59:59 +0100

    changed: no need to serialize PolymerModule any longer

--- ebos/eclproblem.hh
+++ ebos/eclproblem.hh
@@ -610,14 +610,13 @@ public:
         const auto& vanguard = simulator.vanguard();
         const auto& comm = this->gridView().comm();
         SolventModule::initFromState(vanguard.eclState(), vanguard.schedule());
+        PolymerModule::initFromState(vanguard.eclState());
         if (comm.rank() == 0) {
-            PolymerModule::initFromDeck(vanguard.deck(), vanguard.eclState());
             FoamModule::initFromDeck(vanguard.deck(), vanguard.eclState());
             BrineModule::initFromDeck(vanguard.deck(), vanguard.eclState());
         }
 
         EclMpiSerializer ser(comm);
-        ser.staticBroadcast<PolymerModule>();
         ser.staticBroadcast<FoamModule>();
         ser.staticBroadcast<BrineModule>();
 
