0126243f027d2713f896afc7b7a21c3c86d66244
Markus Blatt <markus@dr-blatt.de>
2020-11-27 18:24:35 +0100

    Utility to communicate above value of a perf of distributed well
    This adds an utility that creates a vector of all above values for
    the local perforations. For distributed wells this is needed as the
    perforation above might live on another processor. We use the parallel
    index sets together with the global index of the cells that are
    perforated.

--- opm/simulators/wells/ParallelWellInfo.hpp
+++ opm/simulators/wells/ParallelWellInfo.hpp
@@ -21,6 +21,8 @@
 #define OPM_PARALLELWELLINFO_HEADER_INCLUDED
 #include <dune/common/version.hh>
 #include <dune/common/parallel/mpihelper.hh>
+#include <dune/common/parallel/plocalindex.hh>
+#include <dune/istl/owneroverlapcopy.hh>
 
 #include <opm/parser/eclipse/EclipseState/Schedule/Well/Well.hpp>
 
@@ -28,6 +30,73 @@
 
 namespace Opm
 {
+
+/// \brief Class to facilitate getting values associated with the above perforation
+///
+class CommunicateAbove
+{
+    enum Attribute {
+      owner=1, overlap=2
+    };
+    using MPIComm = typename Dune::MPIHelper::MPICommunicator;
+#if DUNE_VERSION_NEWER(DUNE_COMMON, 2, 7)
+    using Communication = Dune::Communication<MPIComm>;
+#else
+    using Communication = Dune::CollectiveCommunication<MPIComm>;
+#endif
+    using LocalIndex = Dune::ParallelLocalIndex<Attribute>;
+#if HAVE_MPI
+    using IndexSet = Dune::ParallelIndexSet<std::size_t,LocalIndex,50>;
+    using RI = Dune::RemoteIndices<IndexSet>;
+#endif
+
+public:
+    CommunicateAbove(const Communication& comm);
+    /// \brief Adds information about the ecl indices of the perforations.
+    ///
+    /// \warning Theses indices need to be push in the same order as they
+    ///          appear in the ECL well specifiation. Use -1 if there is
+    ///          no perforation above.
+    /// \param above The ECL index of the next open perforation above.
+    /// \param current The ECL index of the current open perforation.
+    void pushBackEclIndex(int above, int current, bool owner=true);
+
+    /// \brief Clear all the parallel information
+    void clear();
+
+    /// \brief Indicates that we will add the index information
+    /// \see pushBackEclIndex
+    void beginReset();
+
+    /// \brief Indicates that the index information is complete.
+    ///
+    /// Sets up the commmunication structures to be used by
+    /// communicate()
+    void endReset();
+
+    /// \brief Creates an array of values for the perforation above.
+    /// \param first_value Value to use for above of the first perforation
+    /// \param current C-array of the values at the perforations
+    /// \param size The size of the C-array and the returned vector
+    /// \return a vector containing the values for the perforation above.
+    std::vector<double> communicate(double first_value,
+                                    const double* current,
+                                    std::size_t size);
+
+private:
+#if HAVE_MPI
+    Communication comm_;
+    /// \brief Mapping of the local well index to ecl index
+    IndexSet current_indices_;
+    /// \brief Mapping of the above well index to ecl index
+    IndexSet above_indices_;
+    RI remote_indices_;
+    Dune::Interface interface_;
+    Dune::BufferedCommunicator communicator_;
+#endif
+    std::size_t count_{};
+};
+
 
 /// \brief Class encapsulating some information about parallel wells
 ///
@@ -72,6 +141,29 @@ public:
         return res;
     }
 
+    /// \brief Creates an array of values for the perforation above.
+    /// \param first_value Value to use for above of the first perforation
+    /// \param current C-array of the values at the perforations
+    /// \param size The size of the C-array and the returned vector
+    /// \return a vector containing the values for the perforation above.
+    std::vector<double> communicateAboveValues(double zero_value,
+                                               const double* current,
+                                               std::size_t size) const;
+
+    /// \brief Creates an array of values for the perforation above.
+    /// \param first_value Value to use for above of the first perforation
+    /// \param current vector of current values
+    std::vector<double> communicateAboveValues(double zero_value,
+                                               const std::vector<double>& current) const;
+    /// \brief Adds information about the ecl indices of the perforations.
+    ///
+    /// \warning Theses indices need to be push in the same order as they
+    ///          appear in the ECL well specifiation. Use -1 if there is
+    ///          no perforation above.
+    /// \param above The ECL index of the next open perforation above.
+    /// \param current The ECL index of the current open perforation.
+    void pushBackEclIndex(int above, int current);
+
     /// \brief Name of the well.
     const std::string& name() const
     {
@@ -88,6 +180,16 @@ public:
         return isOwner_;
     }
 
+    /// \brief Inidicate that we will reset the ecl index information
+    ///
+    /// \see pushBackEclIndex;
+    void beginReset();
+
+    /// \brief Inidicate completion of reset of the ecl index information
+    void endReset();
+
+    /// \brief Free data of communication data structures.
+    void clearCommunicateAbove();
 private:
 
     /// \brief Deleter that also frees custom MPI communicators
@@ -109,6 +211,9 @@ private:
     ///
     /// Contains only ranks where this well will perforate local cells.
     std::unique_ptr<Communication, DestroyComm> comm_;
+
+    /// \brief used to communicate the values for the perforation above.
+    std::unique_ptr<CommunicateAbove> commAbove_;
 };
 
 /// \brief Class checking that all connections are on active cells
