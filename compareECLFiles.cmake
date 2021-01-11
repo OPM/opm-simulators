b0446dfecd417e395749c365d47f200394cf82bd
Tor Harald Sandve <tosa@norceresearch.no>
2021-01-11 08:59:14 +0100

    Use stricter mass balance tolerance for SPE1CASE1_BRINE MPI test

--- compareECLFiles.cmake
+++ compareECLFiles.cmake
@@ -951,7 +951,7 @@ if(MPI_FOUND)
                                        SIMULATOR flow
                                        ABS_TOL ${abs_tol_parallel}
                                        REL_TOL ${rel_tol_parallel}
-                                       TEST_ARGS --linear-solver-reduction=1e-7 --tolerance-cnv=5e-6 --tolerance-mb=1e-6)
+                                       TEST_ARGS --linear-solver-reduction=1e-7 --tolerance-cnv=5e-6 --tolerance-mb=1e-8)
 
   add_test_compare_parallel_simulation(CASENAME fetkovich_2d
                                        FILENAME 2D_FETKOVICHAQUIFER
