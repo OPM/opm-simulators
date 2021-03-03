d2409f86a1cbaa374627ceb3e25bb5b06f28be9d
Joakim Hove <joakim.hove@gmail.com>
2021-03-03 20:04:09 +0100

    Ensure that controls are set for wells before WTEST reopen attempt

--- opm/simulators/wells/BlackoilWellModel_impl.hpp
+++ opm/simulators/wells/BlackoilWellModel_impl.hpp
@@ -279,16 +279,16 @@ namespace Opm {
                                                  + ScheduleEvents::INJECTION_UPDATE
                                                  + ScheduleEvents::NEW_WELL;
 
-            if(!schedule()[timeStepIdx].wellgroup_events().hasEvent(well.name(), effective_events_mask))
-                continue;
-
-            if (well.isProducer()) {
-                const auto controls = well.productionControls(summaryState);
-                well_state_.currentProductionControls()[w] = controls.cmode;
-            }
-            else {
-                const auto controls = well.injectionControls(summaryState);
-                well_state_.currentInjectionControls()[w] = controls.cmode;
+            const auto& events = schedule()[timeStepIdx].wellgroup_events();
+            if(events.hasEvent(well.name(), effective_events_mask) || this->wellTestState_.hasWellClosed(well.name())) {
+                if (well.isProducer()) {
+                    const auto controls = well.productionControls(summaryState);
+                    well_state_.currentProductionControls()[w] = controls.cmode;
+                }
+                else {
+                    const auto controls = well.injectionControls(summaryState);
+                    well_state_.currentInjectionControls()[w] = controls.cmode;
+                }
             }
         }
         const Group& fieldGroup = schedule().getGroup("FIELD", timeStepIdx);
