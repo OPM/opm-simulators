0cb097167302b1a052b8d009ef4eaafe6f65acbb
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2021-04-29 09:01:03 +0200

    ParallelFileMerger: move to separate compile unit

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -39,6 +39,7 @@ list (APPEND MAIN_SOURCE_FILES
   opm/simulators/timestepping/gatherConvergenceReport.cpp
   opm/simulators/utils/DeferredLogger.cpp
   opm/simulators/utils/gatherDeferredLogger.cpp
+  opm/simulators/utils/ParallelFileMerger.cpp
   opm/simulators/utils/ParallelRestart.cpp
   opm/simulators/wells/GroupState.cpp
   opm/simulators/wells/WGState.cpp
