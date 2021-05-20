0dca68f5a9ce0e3d64f53df2277b19aa1158ba21
Joakim Hove <joakim.hove@gmail.com>
2021-05-20 11:20:32 +0200

    Do not install wellstate.hpp

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -200,7 +200,6 @@ list (APPEND PUBLIC_HEADER_FILES
   opm/core/props/satfunc/RelpermDiagnostics.hpp
   opm/simulators/timestepping/SimulatorReport.hpp
   opm/simulators/wells/WellContainer.hpp
-  opm/simulators/wells/WellState.hpp
   opm/simulators/aquifers/AquiferInterface.hpp
   opm/simulators/aquifers/AquiferCarterTracy.hpp
   opm/simulators/aquifers/AquiferFetkovich.hpp
