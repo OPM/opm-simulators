039d6dfdfe3d7b311cf5e8999195dfc2918a7bbc
Joakim Hove <joakim.hove@gmail.com>
2021-05-20 10:32:04 +0200

    Move reportConnections

--- opm/simulators/wells/WellStateFullyImplicitBlackoil.cpp
+++ opm/simulators/wells/WellStateFullyImplicitBlackoil.cpp
@@ -550,7 +550,24 @@ void WellStateFullyImplicitBlackoil::reportConnections(data::Well& well,
                                                        const int* globalCellIdxMap) const
 {
     using rt = data::Rates::opt;
-    WellState::reportConnections(well, pu, wt, globalCellIdxMap);
+    const auto well_index = wt.second[ 0 ];
+    const auto& pd = this->well_perf_data_[well_index];
+    const int num_perf_well = pd.size();
+    well.connections.resize(num_perf_well);
+
+    const auto& perf_rates = this->perfRates(well_index);
+    const auto& perf_pressure = this->perfPress(well_index);
+    for( int i = 0; i < num_perf_well; ++i ) {
+        const auto active_index = this->well_perf_data_[well_index][i].cell_index;
+        auto& connection = well.connections[ i ];
+        connection.index = globalCellIdxMap[active_index];
+        connection.pressure = perf_pressure[i];
+        connection.reservoir_rate = perf_rates[i];
+        connection.trans_factor = pd[i].connection_transmissibility_factor;
+    }
+    assert(num_perf_well == int(well.connections.size()));
+
+
     const int np = pu.num_phases;
     size_t local_comp_index = 0;
     std::vector< rt > phs( np );
