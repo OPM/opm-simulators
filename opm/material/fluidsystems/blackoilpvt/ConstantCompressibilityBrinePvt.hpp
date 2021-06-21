OPM/opm-simulators

commit 43776b5d3a4ad185de8b1a894361427950f0eb76

Add a co2brine pvt model
The CO2BRINE model is activated by setting CO2STOR in the RUNSPEC section

The CO2 and brine pvt properties are computed based on pvt models in opm-material
- CO2 density is from Span and Wager (1996) as given in co2table.inc
- CO2 viscosity is from Fenhour et al (1998)
- Brine density and viscosity is based on H20 + correction based on Batzle and Wang (1992)
- H20 density is from Hu et al (2007)
- H20 viscosity is computed from density based on correlation given in IAPWS 97

At the current stage the oil-phase is used to model the brine. If a proper gas-water
simulator is made, the Brine PVT should be moved to the water phase.

Known limitations:
- Currently the viscosity of the Brine does not depend on the composition
- Salinity is assumed to be constant and given by SALINITY [mol/kg].

opm/material/fluidsystems/blackoilpvt/ConstantCompressibilityBrinePvt.hpp
@@ -202,8 +202,8 @@ public:
 
     }
 
-    const std::vector<Scalar>& waterReferenceDensity() const
-    { return waterReferenceDensity_; }
+    const Scalar waterReferenceDensity(unsigned regionIdx) const
+    { return waterReferenceDensity_[regionIdx]; }
 
     const std::vector<Scalar>& referencePressure() const
     { return referencePressure_; }

opm/material/fluidsystems/blackoilpvt/ConstantCompressibilityOilPvt.hpp
@@ -272,11 +272,8 @@ public:
                                   const Evaluation& /*Rs*/) const
     { return 0.0; /* this is dead oil, so there isn't any meaningful saturation pressure! */ }
 
-    const std::vector<Scalar>& oilReferenceDensity() const
-    { return oilReferenceDensity_; }
-
-    const std::vector<Scalar>& oilReferencePressure() const
-    { return oilReferencePressure_; }
+    const Scalar oilReferenceDensity(unsigned regionIdx) const
+    { return oilReferenceDensity_[regionIdx]; }
 
     const std::vector<Scalar>& oilReferenceFormationVolumeFactor() const
     { return oilReferenceFormationVolumeFactor_; }

opm/material/fluidsystems/blackoilpvt/ConstantCompressibilityWaterPvt.hpp
@@ -210,8 +210,8 @@ public:
         return (1.0 + X*(1.0 + X/2.0))/BwRef;
     }
 
-    const std::vector<Scalar>& waterReferenceDensity() const
-    { return waterReferenceDensity_; }
+    const Scalar waterReferenceDensity(unsigned regionIdx) const
+    { return waterReferenceDensity_[regionIdx]; }
 
     const std::vector<Scalar>& waterReferencePressure() const
     { return waterReferencePressure_; }

opm/material/fluidsystems/blackoilpvt/DeadOilPvt.hpp
@@ -278,8 +278,8 @@ public:
                                         const Evaluation& /*pressure*/) const
     { return 0.0; /* this is dead oil! */ }
 
-    const std::vector<Scalar>& oilReferenceDensity() const
-    { return oilReferenceDensity_; }
+    const Scalar oilReferenceDensity(unsigned regionIdx) const
+    { return oilReferenceDensity_[regionIdx]; }
 
     const std::vector<TabulatedOneDFunction>& inverseOilB() const
     { return inverseOilB_; }

opm/material/fluidsystems/blackoilpvt/LiveOilPvt.hpp
@@ -102,7 +102,7 @@ public:
 
         for (unsigned regionIdx = 0; regionIdx < numRegions; ++ regionIdx) {
             Scalar rhoRefO = densityTable[regionIdx].oil;
-            Scalar rhoRefG = densityTable[regionIdx].gas;;
+            Scalar rhoRefG = densityTable[regionIdx].gas;
             Scalar rhoRefW = densityTable[regionIdx].water;
 
             setReferenceDensities(regionIdx, rhoRefO, rhoRefG, rhoRefW);
@@ -602,11 +602,11 @@ public:
         throw NumericalIssue(errlog.str());
     }
 
-    const std::vector<Scalar>& gasReferenceDensity() const
-    { return gasReferenceDensity_; }
+    const Scalar gasReferenceDensity(unsigned regionIdx) const
+    { return gasReferenceDensity_[regionIdx]; }
 
-    const std::vector<Scalar>& oilReferenceDensity() const
-    { return oilReferenceDensity_; }
+    const Scalar oilReferenceDensity(unsigned regionIdx) const
+    { return oilReferenceDensity_[regionIdx]; }
 
     const std::vector<TabulatedTwoDFunction>& inverseOilBTable() const
     { return inverseOilBTable_; }

opm/material/fluidsystems/blackoilpvt/WetGasPvt.hpp
@@ -628,13 +628,11 @@ public:
         throw NumericalIssue(errlog.str());
     }
 
-    const std::vector<Scalar>& gasReferenceDensity() const {
-        return gasReferenceDensity_;
-    }
+    const Scalar gasReferenceDensity(unsigned regionIdx) const
+    { return gasReferenceDensity_[regionIdx]; }
 
-    const std::vector<Scalar>& oilReferenceDensity() const {
-        return oilReferenceDensity_;
-    }
+    const Scalar oilReferenceDensity(unsigned regionIdx) const
+    { return oilReferenceDensity_[regionIdx]; }
 
     const std::vector<TabulatedTwoDFunction>& inverseGasB() const {
         return inverseGasB_;

CMakeLists.txt
@@ -103,6 +103,7 @@ opm_add_test(test_ConditionalStorage)
 opm_add_test(test_eclblackoilfluidsystem CONDITION HAVE_ECL_INPUT)
 opm_add_test(test_eclblackoilpvt CONDITION HAVE_ECL_INPUT)
 opm_add_test(test_eclmateriallawmanager CONDITION HAVE_ECL_INPUT)
+opm_add_test(test_co2brinepvt CONDITION HAVE_ECL_INPUT)
 opm_add_test(test_fluidmatrixinteractions)
 opm_add_test(test_pengrobinson)
 opm_add_test(test_densead)

opm/material/binarycoefficients/Brine_CO2.hpp
@@ -28,9 +28,6 @@
 #ifndef OPM_BINARY_COEFF_BRINE_CO2_HPP
 #define OPM_BINARY_COEFF_BRINE_CO2_HPP
 
-#include <opm/material/components/Brine.hpp>
-#include <opm/material/components/H2O.hpp>
-#include <opm/material/components/CO2.hpp>
 #include <opm/material/IdealGas.hpp>
 
 namespace Opm {
@@ -40,10 +37,8 @@ namespace BinaryCoeff {
  * \ingroup Binarycoefficients
  * \brief Binary coefficients for brine and CO2.
  */
-template<class Scalar, class CO2Tables, bool verbose = true>
+template<class Scalar, class H2O, class CO2, bool verbose = true>
 class Brine_CO2 {
-    typedef Opm::H2O<Scalar> H2O;
-    typedef Opm::CO2<Scalar, CO2Tables> CO2;
     typedef Opm::IdealGas<Scalar> IdealGas;
     static const int liquidPhaseIdx = 0; // index of the liquid phase
     static const int gasPhaseIdx = 1; // index of the gas phase

commit ee5bb6ff9c5c02721d491e84def8c9dc63003a54

fix some issues in pvt classes
comparison operators were broken by 43776b5d3a4ad185de8b1a894361427950f0eb76

opm/material/fluidsystems/blackoilpvt/ConstantCompressibilityBrinePvt.hpp
@@ -222,7 +222,7 @@ public:
 
     bool operator==(const ConstantCompressibilityBrinePvt<Scalar>& data) const
     {
-        return this->waterReferenceDensity() == data.waterReferenceDensity() &&
+        return this->waterReferenceDensity_ == data.waterReferenceDensity_ &&
                this->referencePressure() == data.referencePressure() &&
                this->formationVolumeTables() == data.formationVolumeTables() &&
                this->compressibilityTables() == data.compressibilityTables() &&

opm/material/fluidsystems/blackoilpvt/ConstantCompressibilityOilPvt.hpp
@@ -297,8 +297,8 @@ public:
 
     bool operator==(const ConstantCompressibilityOilPvt<Scalar>& data) const
     {
-        return this->oilReferenceDensity() == data.oilReferenceDensity() &&
-               this->oilReferencePressure() == data.oilReferencePressure() &&
+        return this->oilReferenceDensity_ == data.oilReferenceDensity_ &&
+               this->oilReferencePressure_ == data.oilReferencePressure_ &&
                this->oilReferenceFormationVolumeFactor() == data.oilReferenceFormationVolumeFactor() &&
                this->oilCompressibility() == data.oilCompressibility() &&
                this->oilViscosity() == data.oilViscosity() &&

opm/material/fluidsystems/blackoilpvt/ConstantCompressibilityWaterPvt.hpp
@@ -230,7 +230,7 @@ public:
 
     bool operator==(const ConstantCompressibilityWaterPvt<Scalar>& data) const
     {
-        return this->waterReferenceDensity() == data.waterReferenceDensity() &&
+        return this->waterReferenceDensity_ == data.waterReferenceDensity_ &&
                this->waterReferencePressure() == data.waterReferencePressure() &&
                this->waterReferenceFormationVolumeFactor() == data.waterReferenceFormationVolumeFactor() &&
                this->waterCompressibility() == data.waterCompressibility() &&

opm/material/fluidsystems/blackoilpvt/DeadOilPvt.hpp
@@ -300,7 +300,7 @@ public:
 
     bool operator==(const DeadOilPvt<Scalar>& data) const
     {
-        return this->oilReferenceDensity() == data.oilReferenceDensity() &&
+        return this->oilReferenceDensity_ == data.oilReferenceDensity_ &&
                this->inverseOilB() == data.inverseOilB() &&
                this->oilMu() == data.oilMu() &&
                this->inverseOilBMu() == data.inverseOilBMu();

opm/material/fluidsystems/blackoilpvt/LiveOilPvt.hpp
@@ -645,8 +645,8 @@ public:
 
     bool operator==(const LiveOilPvt<Scalar>& data) const
     {
-        return this->gasReferenceDensity() == data.gasReferenceDensity() &&
-               this->oilReferenceDensity() == data.oilReferenceDensity() &&
+        return this->gasReferenceDensity_ == data.gasReferenceDensity_ &&
+               this->oilReferenceDensity_ == data.oilReferenceDensity_ &&
                this->inverseOilBTable() == data.inverseOilBTable() &&
                this->oilMuTable() == data.oilMuTable() &&
                this->inverseOilBMuTable() == data.inverseOilBMuTable() &&

opm/material/fluidsystems/blackoilpvt/WetGasPvt.hpp
@@ -680,8 +680,8 @@ public:
 
     bool operator==(const WetGasPvt<Scalar>& data) const
     {
-        return this->gasReferenceDensity() == data.gasReferenceDensity() &&
-               this->oilReferenceDensity() == data.oilReferenceDensity() &&
+        return this->gasReferenceDensity_ == data.gasReferenceDensity_ &&
+               this->oilReferenceDensity_ == data.oilReferenceDensity_ &&
                this->inverseGasB() == data.inverseGasB() &&
                this->inverseSaturatedGasB() == data.inverseSaturatedGasB() &&
                this->gasMu() == data.gasMu() &&