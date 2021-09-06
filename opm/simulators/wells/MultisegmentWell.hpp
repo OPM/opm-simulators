d2fb4f326686051cf74707c957ff6b5f7b4a93fc
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2021-09-06 12:58:16 +0200

    drop using statement for flowPhaseToEbosCompIdx
    rather qualify member function calls with this->

--- opm/simulators/wells/MultisegmentWell.hpp
+++ opm/simulators/wells/MultisegmentWell.hpp
@@ -181,7 +181,6 @@ namespace Opm
         WellSegments::MultiPhaseModel multiphaseModel() const;
 
         // protected functions from the Base class
-        using Base::flowPhaseToEbosCompIdx;
         using Base::flowPhaseToEbosPhaseIdx;
         using Base::ebosCompIdxToFlowCompIdx;
         using Base::getAllowCrossFlow;
