5dcc58484859573f1e596dd44a9280e49ab34f24
Tor Harald Sandve <tosa@norceresearch.no>
2021-10-19 14:09:58 +0200

    Add micp test case

--- compareECLFiles.cmake
+++ compareECLFiles.cmake
@@ -978,6 +978,13 @@ add_test_compareECLFiles(CASENAME actionx_wefac
                          REL_TOL ${rel_tol}
                          DIR actionx)
 
+add_test_compareECLFiles(CASENAME micp
+                         FILENAME MICP
+                         SIMULATOR flow
+                         ABS_TOL ${abs_tol}
+                         REL_TOL ${rel_tol}
+                         DIR micp)
+
 # Restart tests
 opm_set_test_driver(${PROJECT_SOURCE_DIR}/tests/run-restart-regressionTest.sh "")
 
