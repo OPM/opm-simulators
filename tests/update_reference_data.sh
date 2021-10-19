ad8e7095500084f9d31afcf082098d40aa48d5a2
jilocode <jilo@equinor.com>
2021-10-19 19:28:14 +0200

    Regression tests added

--- tests/update_reference_data.sh
+++ tests/update_reference_data.sh
@@ -163,6 +163,8 @@ tests[actionx_wconinje]="flow actionx ACTIONX_WCONINJE"
 tests[actionx_wefac]="flow actionx ACTIONX_WEFAC"
 tests[micp]="flow micp MICP"
 tests[0_base_model6]="flow model6 0_BASE_MODEL6"
+tests[0a_aquct_model6]="flow model6 0A_AQUCT_MODEL6"
+tests[0b_rocktab_model6]="flow model6 0B_ROCKTAB_MODEL6"
 
 changed_tests=""
 
