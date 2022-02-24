005241a13babab7f5ea714bfc266215549d3a5f5
Torbjørn Skille <tskille@equinor.com>
2022-02-24 08:20:31 +0100

    adding regression testing of RXFT summary cases

--- compareECLFiles.cmake
+++ compareECLFiles.cmake
@@ -1173,6 +1173,13 @@ add_test_compareECLFiles(CASENAME max_wgr_1
                          REL_TOL ${rel_tol}
                          DIR wtest/wecon_wgr_max )
 
+add_test_compareECLFiles(CASENAME rxft_smry
+                         FILENAME TEST_RXFT
+                         SIMULATOR flow
+                         ABS_TOL ${abs_tol}
+                         REL_TOL ${rel_tol}
+                         DIR rxft_smry )
+
 # Restart tests
 opm_set_test_driver(${PROJECT_SOURCE_DIR}/tests/run-restart-regressionTest.sh "")
 # Cruder tolerances for the restarted tests
