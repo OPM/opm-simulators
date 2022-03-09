367e26a719fe1140362139807d0dc53b2e90dbf4
Håkon Hægland <hakon.hagland@gmail.com>
2022-03-09 09:11:10 +0100

    Use new constructor
    Refer to opm-common/pull #2971. In order to use the new constructor
    proposed in that PR, the constructor must now be passed a boolean
    variable signalling if gaslift is active.

--- tests/test_vfpproperties.cpp
+++ tests/test_vfpproperties.cpp
@@ -593,7 +593,8 @@ VFPPROD \n\
     auto units = Opm::UnitSystem::newFIELD();
     Opm::Parser parser;
     auto deck = parser.parseString(table_str);
-    Opm::VFPProdTable table(deck["VFPPROD"].front(), units);
+    bool gaslift_active = false;
+    Opm::VFPProdTable table(deck["VFPPROD"].front(), gaslift_active, units);
     Opm::VFPProdProperties properties;
     properties.addTable( table );
 
@@ -653,7 +654,8 @@ BOOST_AUTO_TEST_CASE(ParseInterpolateRealisticVFPPROD)
     BOOST_REQUIRE(deck.hasKeyword("VFPPROD"));
     BOOST_CHECK_EQUAL(deck.count("VFPPROD"), 1);
 
-    Opm::VFPProdTable table(deck["VFPPROD"].front(), units);
+    bool gaslift_active = false;
+    Opm::VFPProdTable table(deck["VFPPROD"].front(), gaslift_active, units);
     Opm::VFPProdProperties properties;
     properties.addTable(table);
 
