8ca52a4d35579d7933b03b4bb2c707bd5d0fb320
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2022-08-03 12:07:48 +0200

    DryHumidGasPvt: use as a private template

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -46,6 +46,7 @@ list (APPEND MAIN_SOURCE_FILES
   opm/material/fluidsystems/blackoilpvt/ConstantCompressibilityWaterPvt.cpp
   opm/material/fluidsystems/blackoilpvt/DeadOilPvt.cpp
   opm/material/fluidsystems/blackoilpvt/DryGasPvt.cpp
+  opm/material/fluidsystems/blackoilpvt/DryHumidGasPvt.cpp
   opm/simulators/timestepping/SimulatorReport.cpp
   opm/simulators/flow/countGlobalCells.cpp
   opm/simulators/flow/KeywordValidation.cpp
