0a91b773d83ced9ca3afb0b80ee05f75893c9668
goncalvesmachadoc <cintia.machado@tno.nl>
2022-09-16 14:17:53 +0200

    test also with live oil

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -212,9 +212,9 @@ list (APPEND TEST_DATA_FILES
   tests/deadfluids.DATA
   tests/equil_wetgas.DATA
   tests/equil_liveoil.DATA
-  tests/equil_rsvd_and_rvvd.DATA
-  tests/equil_rvvd_and_rvwvd.DATA
   tests/equil_humidwetgas.DATA
+  tests/equil_rsvd_and_rvvd.DATA
+  tests/equil_rsvd_rvvd_and_rvwvd.DATA
   tests/wetgas.DATA
   tests/satfuncEPS_B.DATA
   tests/wells_manager_data.data
