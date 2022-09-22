24f8f7c85769100d892d18e70bde55d3bbb350a9
Tong Dong Qiu <tongdong.qiu@bigdataccelerate.com>
2022-09-22 10:16:32 +0200

    Use exact instead of approximate inverted diagonal blocks for ChowPatel

--- opm/simulators/linalg/bda/opencl/ChowPatelIlu.cpp
+++ opm/simulators/linalg/bda/opencl/ChowPatelIlu.cpp
@@ -756,9 +756,7 @@ void ChowPatelIlu<block_size>::decomposition(
     for(int i = 0; i < Nb; ++i) {
         for(int k = Ut->rowPointers[i]; k < Ut->rowPointers[i+1]; ++k) {
             int j = Ut->colIndices[k];
-            if (j == i) {
-                inverter(Ut->nnzValues + k * bs * bs, invDiagVals + i * bs * bs);
-            } else {
+            if (j != i) {
                 ++ptr[j+1];
             }
         }
