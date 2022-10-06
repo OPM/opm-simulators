7afb12ff6e0d77bd9f38c64aa2a1e8cf4979a138
Torbjørn Skille <tskille@equinor.com>
2022-10-06 14:30:46 +0200

    add regression test for model WVFPEXT-02

--- compareECLFiles.cmake
+++ compareECLFiles.cmake
@@ -1212,6 +1212,13 @@ add_test_compareECLFiles(CASENAME actionx_wpimult
                          REL_TOL ${rel_tol}
                          DIR actionx)
 
+add_test_compareECLFiles(CASENAME wvfpexp_02
+                         FILENAME WVFPEXP-02
+                         SIMULATOR flow
+                         ABS_TOL ${abs_tol}
+                         REL_TOL ${rel_tol}
+                         DIR wvfpexp)
+
 # Restart tests
 opm_set_test_driver(${PROJECT_SOURCE_DIR}/tests/run-restart-regressionTest.sh "")
 # Cruder tolerances for the restarted tests
