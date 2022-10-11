99974a308739906c01a69855af213f5ac594b8ba
hnil <Halvor.M.Nilsen@sintef.no>
2022-10-11 16:10:00 +0200

    modification to to well assembly form well side to avoid asking for wells on all cells

--- opm/simulators/wells/BlackoilWellModel_impl.hpp
+++ opm/simulators/wells/BlackoilWellModel_impl.hpp
@@ -1279,6 +1279,37 @@ namespace Opm {
         }
     }
 
+    template <typename TypeTag>
+    void BlackoilWellModel<TypeTag>::void
+    addReseroirSourceTerms(GlobalEqVector& residual, SparseMatrixAdapter& jacobian) const
+    {
+        // NB this loop may write to same element if a cell has more than one perforation
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+        for (size_t i = 0; i < well_container_.size(); i++) { // to be sure open understand
+            const auto& well = well_container_[i];
+            if (!well->isOperableAndSolvable() && !well->wellIsStopped())
+                continue;
+
+            const auto& cells = well->cells();
+            const auto& rates = well->connectionRates();
+            for (unsigned perfIdx = 0; perfIdx < rates.size(); ++perfIdx) {
+                unsigned cellIdx = cells[perfIdx];
+                auto rate = rates[perfIdx];
+                // Scalar volume =  ebosSimulator_.problem().volume(cellIdx,0);
+                rate *= -1.0;
+                VectorBlockType res(0.0);
+		using  MatrixBlockType = Opm::MatrixBlock<Scalar, numEq, numEq >;
+                MatrixBlockType bMat(0.0);
+                ebosSimulator_.model().linearizer().setResAndJacobi(res, bMat, rate);
+                residual[cellIdx] += res;
+                jacobian.addToBlock(cellIdx, cellIdx, bMat);
+            }
+        }
+    }
+
+
     template<typename TypeTag>
     int
     BlackoilWellModel<TypeTag>::
