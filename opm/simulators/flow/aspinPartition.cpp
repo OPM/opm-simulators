5ef0917c5bb497498b2e2db775d6df83f56365ad
Atgeirr Flø Rasmussen <atgeirr@sintef.no>
2023-01-16 09:01:28 +0100

    [WIP] Use 25 partitions, i.e. for the layer 10 test.

--- opm/simulators/flow/aspinPartition.cpp
+++ opm/simulators/flow/aspinPartition.cpp
@@ -82,7 +82,7 @@ partitionCells(const int num_cells)
     const std::string method = "simple";
     // const std::string method = "file";
     if (method == "simple") {
-        return partitionCellsSimple(num_cells, 1);
+        return partitionCellsSimple(num_cells, 25);
     } else if (method == "file") {
         return partitionCellsFromFile(num_cells);
     } else {
