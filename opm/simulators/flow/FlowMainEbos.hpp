OPM/opm-simulators

commit 82d2146c90ce74677bd8f04c745c31cd08b430de

Main: move code for reading deck to compile unit

opm/simulators/flow/Main.cpp
@@ -23,6 +23,8 @@
 #include <config.h>
 #include <opm/simulators/flow/Main.hpp>
 
+#include <opm/simulators/utils/readDeck.hpp>
+
 namespace Opm {
 
 Main::Main(int argc, char** argv)
@@ -152,6 +154,48 @@ void Main::handleTestSplitCommunicatorCmdLine_()
         argv_[1] = argv_[0]; // What used to be the first proper argument now becomes the command argument.
         ++argv_;             // Pretend this is what it always was.
     }
+}
+
+void Main::readDeck(const std::string& deckFilename,
+                    const std::string& outputDir,
+                    const std::string& outputMode,
+                    const bool init_from_restart_file,
+                    const bool allRanksDbgPrtLog,
+                    const bool strictParsing,
+                    const int mpiRank,
+                    const int output_param)
+{
+    auto omode = setupLogging(mpiRank,
+                              deckFilename,
+                              outputDir,
+                              outputMode,
+                              outputCout_, "STDOUT_LOGGER", allRanksDbgPrtLog);
+
+    if (outputCout_) {
+        OpmLog::info("Reading deck file '" + deckFilename + "'");
+    }
+
+    std::optional<int> outputInterval;
+    if (output_param >= 0)
+        outputInterval = output_param;
+
+    Opm::readDeck(EclGenericVanguard::comm(),
+                  deckFilename,
+                  eclipseState_,
+                  schedule_,
+                  udqState_,
+                  actionState_,
+                  wtestState_,
+                  summaryConfig_,
+                  std::make_shared<Python>(),
+                  strictParsing,
+                  init_from_restart_file,
+                  outputCout_,
+                  outputInterval);
+
+    verifyValidCellGeometry(EclGenericVanguard::comm(), *this->eclipseState_);
+
+    outputFiles_ = (omode != FileOutputMode::OUTPUT_NONE);
 }
 
 } // namespace Opm

opm/simulators/flow/Main.hpp
@@ -59,7 +59,6 @@
 #include <opm/models/utils/parametersystem.hh>
 
 #include <opm/simulators/flow/FlowMainEbos.hpp>
-#include <opm/simulators/utils/readDeck.hpp>
 
 #if HAVE_DUNE_FEM
 #include <dune/fem/misc/mpimanager.hh>
@@ -369,7 +368,6 @@ private:
 #endif // HAVE_DAMARIS
 
         int mpiRank = EclGenericVanguard::comm().rank();
-        FileOutputMode outputMode = FileOutputMode::OUTPUT_NONE;
         outputCout_ = false;
         if (mpiRank == 0)
             outputCout_ = EWOMS_GET_PARAM(PreTypeTag, bool, EnableTerminalOutput);
@@ -400,39 +398,18 @@ private:
         if (outputCout_) {
             FlowMainEbos<PreTypeTag>::printBanner(EclGenericVanguard::comm());
         }
+
         // Create Deck and EclipseState.
         try {
-            auto python = std::make_shared<Python>();
-            const bool init_from_restart_file = !EWOMS_GET_PARAM(PreTypeTag, bool, SchedRestart);
-            const bool allRanksDbgPrtLog = EWOMS_GET_PARAM(PreTypeTag, bool,
-                                                      EnableLoggingFalloutWarning);
-            outputMode = setupLogging(mpiRank,
-                                      deckFilename,
-                                      outputDir,
-                                      EWOMS_GET_PARAM(PreTypeTag, std::string, OutputMode),
-                                      outputCout_, "STDOUT_LOGGER", allRanksDbgPrtLog);
-            const bool strictParsing = EWOMS_GET_PARAM(PreTypeTag, bool, EclStrictParsing);
-
-            FlowMainEbos<PreTypeTag>::printPRTHeader(outputCout_);
-
-            if (outputCout_) {
-                OpmLog::info("Reading deck file '" + deckFilename + "'");
-            }
-
-            std::optional<int> outputInterval;
-            int output_param = EWOMS_GET_PARAM(PreTypeTag, int, EclOutputInterval);
-            if (output_param >= 0)
-                outputInterval = output_param;
-
-            readDeck(EclGenericVanguard::comm(), deckFilename, eclipseState_,
-                     schedule_, udqState_, actionState_, wtestState_,
-                     summaryConfig_, python, strictParsing,
-                     init_from_restart_file, outputCout_, outputInterval);
-
-            verifyValidCellGeometry(EclGenericVanguard::comm(), *this->eclipseState_);
-
+            this->readDeck(deckFilename,
+                           outputDir,
+                           EWOMS_GET_PARAM(PreTypeTag, std::string, OutputMode),
+                           !EWOMS_GET_PARAM(PreTypeTag, bool, SchedRestart),
+                           EWOMS_GET_PARAM(PreTypeTag, bool,  EnableLoggingFalloutWarning),
+                           EWOMS_GET_PARAM(PreTypeTag, bool, EclStrictParsing),
+                           mpiRank,
+                           EWOMS_GET_PARAM(PreTypeTag, int, EclOutputInterval));
             setupTime_ = externalSetupTimer.elapsed();
-            outputFiles_ = (outputMode != FileOutputMode::OUTPUT_NONE);
         }
         catch (const std::invalid_argument& e)
         {
@@ -666,6 +643,15 @@ private:
             return flowEbosBlackoilTpfaMain(argc_, argv_, outputCout_, outputFiles_);
         }
     }
+
+    void readDeck(const std::string& deckFilename,
+                  const std::string& outputDir,
+                  const std::string& outputMode,
+                  const bool init_from_restart_file,
+                  const bool allRanksDbgPrtLog,
+                  const bool strictParsing,
+                  const int mpiRank,
+                  const int output_param);
 
     int argc_{0};
     char** argv_{nullptr};

commit 385f526d77507b07496f6b4d9f67c5b32f5326d0

fixed: regression from 82d2146c90ce74677bd8f04c745c31cd08b430de
missing PRT header

opm/simulators/flow/FlowMainEbos.hpp
@@ -343,7 +343,6 @@ namespace Opm
         }
 
         // Print an ASCII-art header to the PRT and DEBUG files.
-        // \return Whether unkown keywords were seen during parsing.
         static void printPRTHeader(bool output_cout)
         {
           if (output_cout) {

opm/simulators/flow/Main.cpp
@@ -171,13 +171,16 @@ void Main::readDeck(const std::string& deckFilename,
                     const bool allRanksDbgPrtLog,
                     const bool strictParsing,
                     const int mpiRank,
-                    const int output_param)
+                    const int output_param,
+                    std::function<void(bool)> outputPrt)
 {
     auto omode = setupLogging(mpiRank,
                               deckFilename,
                               outputDir,
                               outputMode,
                               outputCout_, "STDOUT_LOGGER", allRanksDbgPrtLog);
+
+    outputPrt(outputCout_);
 
     if (outputCout_) {
         OpmLog::info("Reading deck file '" + deckFilename + "'");

opm/simulators/flow/Main.hpp
@@ -68,6 +68,7 @@
 
 #include <cassert>
 #include <cstdlib>
+#include <functional>
 #include <iostream>
 #include <memory>
 #include <stdexcept>
@@ -366,7 +367,8 @@ private:
                            EWOMS_GET_PARAM(PreTypeTag, bool,  EnableLoggingFalloutWarning),
                            EWOMS_GET_PARAM(PreTypeTag, bool, EclStrictParsing),
                            mpiRank,
-                           EWOMS_GET_PARAM(PreTypeTag, int, EclOutputInterval));
+                           EWOMS_GET_PARAM(PreTypeTag, int, EclOutputInterval),
+                           FlowMainEbos<PreTypeTag>::printPRTHeader);
             setupTime_ = externalSetupTimer.elapsed();
         }
         catch (const std::invalid_argument& e)
@@ -609,7 +611,8 @@ private:
                   const bool allRanksDbgPrtLog,
                   const bool strictParsing,
                   const int mpiRank,
-                  const int output_param);
+                  const int output_param,
+                  std::function<void(bool)> outputPrt);
 
     void setupVanguard();
 