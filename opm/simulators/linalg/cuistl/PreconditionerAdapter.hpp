b30e6d79d5bcda438f6e7f28589d778635f74c66
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2023-03-31 13:36:27 +0200

    Added CuSeqILU0 with the preconditioner adapter.

---
+++ opm/simulators/linalg/cuistl/PreconditionerAdapter.hpp
@@ -0,0 +1,126 @@
+/*
+  Copyright 2022-2023 SINTEF AS
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef OPM_PRECONDITIONERADAPTER_HPP
+#define OPM_PRECONDITIONERADAPTER_HPP
+#include <cusparse.h>
+#include <dune/istl/preconditioner.hh>
+#include <opm/simulators/linalg/PreconditionerWithUpdate.hpp>
+#include <opm/simulators/linalg/cuistl/CuVector.hpp>
+#include <opm/simulators/linalg/cuistl/detail/preconditioner_should_call_post_pre.hpp>
+
+
+namespace Opm::cuistl
+{
+//!\brief Makes a CUDA preconditioner available to a CPU simulator.
+//!
+//! The use case for this adapter is to use a CUDA preconditioner during a linear
+//! solver that works on the CPU. The motivation for this is benchmarking new preconditioners on the GPU.
+//!
+//! \tparam X the domain type (should be on the CPU). Typicall a Dune::BlockVector
+//! \tparam Y the range type (should be on the CPU). Typicall a Dune::BlockVector
+//! \tparam CudaPreconditionerType the preconditioner taking CuVector<real_type> as arguments to apply
+template <class X, class Y, class CudaPreconditionerType>
+class PreconditionerAdapter : public Dune::PreconditionerWithUpdate<X, Y>
+{
+public:
+    //! \brief The domain type of the preconditioner.
+    using domain_type = X;
+    //! \brief The range type of the preconditioner.
+    using range_type = Y;
+    //! \brief The field type of the preconditioner.
+    using field_type = typename X::field_type;
+
+    //! \brief Constructor.
+    //!
+    //! Constructor gets all parameters to operate the prec.
+    //!    \param A The matrix to operate on.
+    //!    \param w The relaxation factor.
+    //!
+    explicit PreconditionerAdapter(std::shared_ptr<CudaPreconditionerType> preconditioner)
+        : m_underlyingPreconditioner(preconditioner)
+    {
+    }
+
+
+    //! \brief Prepare the preconditioner.
+    //!
+    //! Currently not supported.
+    virtual void pre([[maybe_unused]] X& x, [[maybe_unused]] Y& b) override
+    {
+        static_assert(!detail::shouldCallPreconditionerPre<CudaPreconditionerType>(),
+                      "We currently do not support Preconditioner::pre().");
+    }
+
+
+    //! \brief Apply the preconditoner.
+    //!
+    //! \copydoc Preconditioner::apply(X&,const Y&)
+    virtual void apply(X& v, const Y& d) override
+    {
+        if (!m_inputBuffer) {
+            m_inputBuffer.reset(new CuVector<field_type>(v.dim()));
+            m_outputBuffer.reset(new CuVector<field_type>(v.dim()));
+        }
+        m_inputBuffer->copyFromHost(d);
+        m_underlyingPreconditioner->apply(*m_outputBuffer, *m_inputBuffer);
+        m_outputBuffer->copyToHost(v);
+    }
+
+
+    //! \brief Clean up.
+    //!
+    //! Currently not supported.
+    virtual void post([[maybe_unused]] X& x) override
+    {
+        static_assert(!detail::shouldCallPreconditionerPost<CudaPreconditionerType>(),
+                      "We currently do not support Preconditioner::post().");
+    }
+
+
+    //! Category of the preconditioner (see SolverCategory::Category)
+    virtual Dune::SolverCategory::Category category() const
+    {
+        return m_underlyingPreconditioner->category();
+    }
+
+    //! Calls update on the underlying CUDA preconditioner
+    virtual void update() override
+    {
+        m_underlyingPreconditioner->update();
+    }
+
+    static constexpr bool shouldCallPre()
+    {
+        return detail::shouldCallPreconditionerPost<CudaPreconditionerType>();
+    }
+    static constexpr bool shouldCallPost()
+    {
+        return detail::shouldCallPreconditionerPre<CudaPreconditionerType>();
+    }
+
+private:
+    //! \brief the underlying preconditioner to use
+    std::shared_ptr<CudaPreconditionerType> m_underlyingPreconditioner;
+
+    std::unique_ptr<CuVector<field_type>> m_inputBuffer;
+    std::unique_ptr<CuVector<field_type>> m_outputBuffer;
+};
+} // end namespace Opm::cuistl
+
+#endif
---
+++ opm/simulators/linalg/cuistl/detail/has_function.hpp
@@ -0,0 +1,88 @@
+/*
+  Copyright 2022-2023 SINTEF AS
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef OPM_CUISTL_DETAIL_HAS_FUNCTION_HPP
+#define OPM_CUISTL_DETAIL_HAS_FUNCTION_HPP
+#include <type_traits>
+/**
+ * Simple utility structs to test for the existence of functions in types.
+ *
+ * Note that there are alternatives to this, see for instance
+ * https://stackoverflow.com/questions/257288/templated-check-for-the-existence-of-a-class-member-function , however,
+ * this is by far the cleanest approach for where this is going to be used for now.
+ *
+ * TODO: Use the requires-keyword once C++20 becomes availble (https://en.cppreference.com/w/cpp/language/constraints ).
+ * With C++20 this file can be removed.
+ */
+namespace Opm::cuistl::detail
+{
+
+/**
+ * @brief The has_should_call_pre class detects the presence of the method shouldCallPre
+ *
+ * Usage:
+ *
+ * @code{.cpp}
+ * if constexpr (has_should_call_pre<decltype(preconditioner)>::value) {
+ *     // We know that the function shouldCallPre is present:
+ *     auto shouldCallPre = preconditioner.shouldCallPre();
+ * }
+ * @endcode
+ *
+ * @note This is mainly done in the GPU preconditioner to avoid having to copy data in the pre step.
+ */
+template <typename T>
+class has_should_call_pre
+{
+    template <typename U>
+    static std::true_type test(decltype(&U::shouldCallPre));
+    template <typename U>
+    static std::false_type test(...);
+
+public:
+    static constexpr bool value = std::is_same_v<decltype(test<T>(0)), std::true_type>;
+};
+
+/**
+ * @brief The has_should_call_post class detects the presence of the method shouldCallPost
+ *
+ * Usage:
+ *
+ * @code{.cpp}
+ * if constexpr (has_should_call_post<decltype(preconditioner)>::value) {
+ *     // We know that the function shouldCallPost is present:
+ *     auto shouldCallPost = preconditioner.shouldCallPost();
+ * }
+ * @endcode
+ *
+ * @note This is mainly done in the GPU preconditioner to avoid having to copy data in the post step.
+ */
+template <typename T>
+class has_should_call_post
+{
+    template <typename U>
+    static std::true_type test(decltype(&U::shouldCallPost));
+    template <typename U>
+    static std::false_type test(...);
+
+public:
+    static constexpr bool value = std::is_same_v<decltype(test<T>(0)), std::true_type>;
+};
+
+} // namespace Opm::cuistl::detail
+#endif
