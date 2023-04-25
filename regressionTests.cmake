05795c0bb14c8cb3130e620de9b555a5d95f7d91
Kai Bao <kai.bao@sintef.no>
2023-04-25 12:00:10 +0200

    adding regression test for network modeling

--- regressionTests.cmake
+++ regressionTests.cmake
@@ -92,6 +92,14 @@ add_test_compareECLFiles(CASENAME spe1_precsalt
                          REL_TOL ${rel_tol}
                          DIR spe1_precsalt)
 
+add_test_compareECLFiles(CASENAME network_balance_01
+		FILENAME NETWORK-01
+		SIMULATOR flow
+		ABS_TOL ${abs_tol}
+		REL_TOL ${rel_tol}
+		DIR network
+		TEST_ARGS --enable-tuning=true)
+
 add_test_compareECLFiles(CASENAME gas_precsalt
                          FILENAME GASWATER_VAPWAT_PRECSALT
                          SIMULATOR flow
