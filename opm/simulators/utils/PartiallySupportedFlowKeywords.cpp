d4743ed4a5264bf7ad434515a1b6570344be2e73
Paul <paul.egberts@tno.nl>
2023-08-21 10:03:18 +0200

    item 7 added to partially supported keywords

--- opm/simulators/utils/PartiallySupportedFlowKeywords.cpp
+++ opm/simulators/utils/PartiallySupportedFlowKeywords.cpp
@@ -121,6 +121,7 @@ partiallySupported()
             {
                {5,{true, allow_values<std::string> {"NO"}, "GRUPNET(SUBSEAMANIFOLD): only option NO is supported"}}, // SUB_SEA_MANIFOLD
                {6,{true, allow_values<std::string> {"NO"}, "GRUPNET(LIFTGAS): only option NO is supported"}}, // LIFT_GAS_FLOW_THROUGH
+               {7,{true, allow_values<std::string> {"NONE"}, "GRUPNET(ALQ-DEN): only option NONE is supported"}}, // ALQ_SURFACE_DENSITY
             }
          },
          {
