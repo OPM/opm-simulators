696b31137578fd0e47a386123a7a553b772e5141
Letian Jiang <1584191130@qq.com>
2023-10-16 12:01:23 +0000

    Code needed to build the new simulators for python bindings

---
+++ opm/simulators/flow/python/PyFlowSimulator.hpp
@@ -0,0 +1,78 @@
+
+
+#ifndef OPM_PY_FLOW_SIMULATOR_HEADER_INCLUDED
+#define OPM_PY_FLOW_SIMULATOR_HEADER_INCLUDED
+
+#include <opm/simulators/flow/Main.hpp>
+#include <opm/simulators/flow/FlowMainEbos.hpp>
+#include <opm/models/utils/propertysystem.hh>
+#include <opm/models/utils/parametersystem.hh>
+#include <opm/simulators/flow/python/Pybind11Exporter.hpp>
+#include <opm/simulators/flow/python/PyFluidState.hpp>
+#include <opm/simulators/flow/python/PyMaterialState.hpp>
+#include <opm/input/eclipse/EclipseState/EclipseState.hpp>
+#include <opm/input/eclipse/Schedule/Schedule.hpp>
+#include <opm/input/eclipse/EclipseState/SummaryConfig/SummaryConfig.hpp>
+
+#include <map>
+
+namespace Opm::Pybind {
+class PyFlowSimulator
+{
+private:
+    using TypeTag = ::Opm::Properties::TTag::EclFlowProblemTPFA;
+    using Simulator = ::Opm::GetPropType<TypeTag, ::Opm::Properties::Simulator>;
+
+public:
+    PyFlowSimulator( const std::string& deckFilename);
+    PyFlowSimulator(
+        std::shared_ptr<::Opm::Deck> deck,
+        std::shared_ptr<::Opm::EclipseState> state,
+        std::shared_ptr<::Opm::Schedule> schedule,
+        std::shared_ptr<::Opm::SummaryConfig> summary_config);
+    void advance(int report_step);
+    bool checkSimulationFinished() const;
+    int currentStep() const;
+    py::array_t<double> getCellVolumes() const;
+    double getDT() const;
+    py::array_t<double> getFluidStateVariable(const std::string &name) const;
+    py::array_t<double> getPorosity() const;
+    py::array_t<double> getPrimaryVariable(const std::string &variable) const;
+    py::array_t<int> getPrimaryVarMeaning(const std::string &variable) const;
+    std::map<std::string, int> getPrimaryVarMeaningMap(const std::string &variable) const;
+    int run();
+    void setPorosity(py::array_t<double, py::array::c_style | py::array::forcecast> array);
+    void setPrimaryVariable(
+        const std::string &idx_name,
+        py::array_t<double,
+        py::array::c_style | py::array::forcecast> array);
+    int step();
+    int stepCleanup();
+    int stepInit();
+
+private:
+    ::Opm::FlowMainEbos<TypeTag>& getFlowMainEbos() const;
+    PyFluidState<TypeTag>& getFluidState() const;
+    PyMaterialState<TypeTag>& getMaterialState() const;
+
+    const std::string deck_filename_;
+    bool has_run_init_ = false;
+    bool has_run_cleanup_ = false;
+    //bool debug_ = false;
+    // This *must* be declared before other pointers
+    // to simulator objects. This in order to deinitialize
+    // MPI at the correct time (ie after the other objects).
+    std::unique_ptr<::Opm::Main> main_;
+
+    std::unique_ptr<::Opm::FlowMainEbos<TypeTag>> main_ebos_;
+    Simulator *ebos_simulator_;
+    std::unique_ptr<PyFluidState<TypeTag>> fluid_state_;
+    std::unique_ptr<PyMaterialState<TypeTag>> material_state_;
+    std::shared_ptr<::Opm::Deck> deck_;
+    std::shared_ptr<::Opm::EclipseState> eclipse_state_;
+    std::shared_ptr<::Opm::Schedule> schedule_;
+    std::shared_ptr<::Opm::SummaryConfig> summary_config_;
+};
+
+} // namespace Opm::Pybind
+#endif // OPM_PY_BLACKOIL_SIMULATOR_HEADER_INCLUDED
---
+++ python/simulators/PyFlowSimulator.cpp
@@ -0,0 +1,309 @@
+
+#include "config.h"
+#include <opm/input/eclipse/Deck/Deck.hpp>
+#include <opm/input/eclipse/EclipseState/EclipseState.hpp>
+#include <opm/input/eclipse/Schedule/Schedule.hpp>
+#include <opm/input/eclipse/EclipseState/SummaryConfig/SummaryConfig.hpp>
+#include <opm/simulators/flow/Main.hpp>
+#include <opm/simulators/flow/FlowMainEbos.hpp>
+#include <opm/simulators/flow/python/PyFlowSimulator.hpp>
+#include <opm/simulators/flow/python/PyFluidState.hpp>
+#include <opm/simulators/flow/python/PyMaterialState.hpp>
+
+// NOTE: EXIT_SUCCESS, EXIT_FAILURE is defined in cstdlib
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+namespace py = pybind11;
+
+namespace Opm::Pybind {
+PyFlowSimulator::
+PyFlowSimulator( const std::string &deck_filename)
+    : deck_filename_{deck_filename}
+{
+}
+
+PyFlowSimulator::
+PyFlowSimulator(
+    std::shared_ptr<Opm::Deck> deck,
+    std::shared_ptr<Opm::EclipseState> state,
+    std::shared_ptr<Opm::Schedule> schedule,
+    std::shared_ptr<Opm::SummaryConfig> summary_config
+)
+    : deck_{std::move(deck)}
+    , eclipse_state_{std::move(state)}
+    , schedule_{std::move(schedule)}
+    , summary_config_{std::move(summary_config)}
+{
+}
+
+// Public methods alphabetically sorted
+// ------------------------------------
+
+//used to advance the state of the simulator until the specified reporting step is reached.
+void
+PyFlowSimulator::
+advance(int report_step)
+{
+    while (currentStep() < report_step) {
+        step();
+    }
+}
+
+bool
+PyFlowSimulator::
+checkSimulationFinished() const
+{
+    return getFlowMainEbos().getSimTimer()->done();
+}
+
+// This returns the report step number that will be executed next time step()
+//   is called.
+int
+PyFlowSimulator::
+currentStep() const
+{
+    return getFlowMainEbos().getSimTimer()->currentStepNum();
+    // NOTE: this->ebos_simulator_->episodeIndex() would also return the current
+    // report step number, but this number is always delayed by 1 step relative
+    // to this->main_ebos_->getSimTimer()->currentStepNum()
+    // See details in runStep() in file SimulatorFullyImplicitBlackoilEbos.hpp
+}
+
+py::array_t<double>
+PyFlowSimulator::
+getCellVolumes() const
+{
+    std::size_t len;
+    auto array = getMaterialState().getCellVolumes(&len);
+    return py::array(len, array.get());
+}
+
+double
+PyFlowSimulator::
+getDT() const
+{
+    return getFlowMainEbos().getPreviousReportStepSize();
+}
+
+py::array_t<double>
+PyFlowSimulator::
+getPorosity() const
+{
+    std::size_t len;
+    auto array = getMaterialState().getPorosity(&len);
+    return py::array(len, array.get());
+}
+
+py::array_t<double>
+PyFlowSimulator::
+getFluidStateVariable(const std::string &name) const
+{
+    std::size_t len;  //size_t unsigned int
+    auto array = getFluidState().getFluidStateVariable(name, &len);
+    return py::array(len, array.get());
+}
+
+py::array_t<double>
+PyFlowSimulator::
+getPrimaryVariable(const std::string &variable) const
+{
+    std::size_t len;
+    auto array = getFluidState().getPrimaryVariable(variable, &len);
+    return py::array(len, array.get());
+}
+
+py::array_t<int>
+PyFlowSimulator::
+getPrimaryVarMeaning(const std::string &variable) const
+{
+    std::size_t len;
+    auto array = getFluidState().getPrimaryVarMeaning(variable, &len);
+    return py::array(len, array.get());
+}
+
+std::map<std::string, int>
+PyFlowSimulator::
+getPrimaryVarMeaningMap(const std::string &variable) const
+{
+
+    return getFluidState().getPrimaryVarMeaningMap(variable);
+}
+
+int
+PyFlowSimulator::
+run()
+{
+    auto main_object = ::Opm::Main( this->deck_filename_ );
+    //return main_object.runStatic<::Opm::Properties::TTag::EclFlowProblemTPFA>();
+    return main_object.runDynamic();
+}
+
+void
+PyFlowSimulator::
+setPorosity( py::array_t<double, py::array::c_style | py::array::forcecast> array)
+{
+    std::size_t size_ = array.size();
+    const double *poro = array.data();
+    getMaterialState().setPorosity(poro, size_);
+}
+
+void
+PyFlowSimulator::
+setPrimaryVariable(
+    const std::string &idx_name,
+    py::array_t<double,py::array::c_style | py::array::forcecast> array
+)
+{
+    std::size_t size_ = array.size();
+    const double *data = array.data();
+    getFluidState().setPrimaryVariable(idx_name, data, size_);
+}
+
+int
+PyFlowSimulator::
+step()
+{
+    if (!this->has_run_init_) {
+        throw std::logic_error("step() called before step_init()");
+    }
+    if (this->has_run_cleanup_) {
+        throw std::logic_error("step() called after step_cleanup()");
+    }
+    if(checkSimulationFinished()) {
+        throw std::logic_error("step() called, but simulation is done");
+    }
+    //if (this->debug_)
+    //    this->mainEbos_->getSimTimer()->report(std::cout);
+    auto result = getFlowMainEbos().executeStep();
+    return result;
+}
+
+int
+PyFlowSimulator::
+stepCleanup()
+{
+    this->has_run_cleanup_ = true;
+    return getFlowMainEbos().executeStepsCleanup();
+}
+
+int
+PyFlowSimulator::
+stepInit()
+{
+
+    if (this->has_run_init_) {
+        // Running step_init() multiple times is not implemented yet,
+        if (this->has_run_cleanup_) {
+            throw std::logic_error("step_init() called again");
+        }
+        else {
+            return EXIT_SUCCESS;
+        }
+    }
+    if (this->deck_) {
+        this->main_ = std::make_unique<::Opm::Main>(
+            this->deck_->getDataFile(),
+            this->eclipse_state_,
+            this->schedule_,
+            this->summary_config_
+        );
+    }
+    else {
+        this->main_ = std::make_unique<::Opm::Main>( this->deck_filename_ );
+    }
+    int exit_code = EXIT_SUCCESS;
+    this->main_ebos_ = this->main_->initFlowEbosBlackoil(exit_code);
+    if (this->main_ebos_) {
+        int result = this->main_ebos_->executeInitStep();
+        this->has_run_init_ = true;
+        this->ebos_simulator_ = this->main_ebos_->getSimulatorPtr();
+        this->fluid_state_ = std::make_unique<PyFluidState<TypeTag>>(this->ebos_simulator_);
+        this->material_state_ = std::make_unique<PyMaterialState<TypeTag>>(this->ebos_simulator_);
+        return result;
+    }
+    else {
+        return exit_code;
+    }
+}
+
+// Private methods alphabetically sorted
+// ------------------------------------
+
+::Opm::FlowMainEbos<typename PyFlowSimulator::TypeTag>&
+PyFlowSimulator::
+getFlowMainEbos() const
+{
+    if (this->main_ebos_) {
+        return *this->main_ebos_;
+    }
+    else {
+        throw std::runtime_error("FlowSimulator not initialized: "
+            "Cannot get reference to FlowMainEbos object" );
+    }
+}
+
+PyFluidState<typename PyFlowSimulator::TypeTag>&
+PyFlowSimulator::
+getFluidState() const
+{
+    if (this->fluid_state_) {
+        return *this->fluid_state_;
+    }
+    else {
+        throw std::runtime_error("FlowSimulator not initialized: "
+            "Cannot get reference to FlowMainEbos object" );
+    }
+}
+
+PyMaterialState<typename Opm::Pybind::PyFlowSimulator::TypeTag>&
+PyFlowSimulator::
+getMaterialState() const
+{
+    if (this->material_state_) {
+        return *this->material_state_;
+    }
+    else {
+        throw std::runtime_error("FlowSimulator not initialized: "
+            "Cannot get reference to FlowMainEbos object" );
+    }
+}
+
+// Exported functions
+void export_PyFlowSimulator(py::module& m)
+{
+    py::class_<PyFlowSimulator>(m, "FlowSimulator")
+        .def(py::init< const std::string& >())
+        .def(py::init<
+            std::shared_ptr<Opm::Deck>,
+            std::shared_ptr<Opm::EclipseState>,
+            std::shared_ptr<Opm::Schedule>,
+            std::shared_ptr<Opm::SummaryConfig> >())
+        .def("advance", &PyFlowSimulator::advance, py::arg("report_step"))
+        .def("current_step", &PyFlowSimulator::currentStep)
+        .def("get_cell_volumes", &PyFlowSimulator::getCellVolumes,
+            py::return_value_policy::copy)
+        .def("get_dt", &PyFlowSimulator::getDT)
+        .def("get_fluidstate_variable", &PyFlowSimulator::getFluidStateVariable,
+            py::return_value_policy::copy, py::arg("name"))
+        .def("get_porosity", &PyFlowSimulator::getPorosity,
+            py::return_value_policy::copy)
+        .def("get_primary_variable_meaning", &PyFlowSimulator::getPrimaryVarMeaning,
+            py::return_value_policy::copy, py::arg("variable"))
+        .def("get_primary_variable_meaning_map", &PyFlowSimulator::getPrimaryVarMeaningMap,
+            py::return_value_policy::copy, py::arg("variable"))
+        .def("get_primary_variable", &PyFlowSimulator::getPrimaryVariable,
+            py::return_value_policy::copy, py::arg("variable"))
+        .def("run", &PyFlowSimulator::run)
+        .def("set_porosity", &PyFlowSimulator::setPorosity)
+        .def("set_primary_variable", &PyFlowSimulator::setPrimaryVariable,
+            py::arg("idx_name"), py::arg("value"))
+        .def("step", &PyFlowSimulator::step)
+        .def("step_cleanup", &PyFlowSimulator::stepCleanup)
+        .def("step_init", &PyFlowSimulator::stepInit);
+}
+
+} // namespace Opm::Pybind
+
