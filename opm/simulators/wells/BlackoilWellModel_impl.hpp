0acb148ba7ac7f872e20e4883ad443f9697fad61
Vegard Kippe <vkip@equinor.com>
2023-11-30 16:13:40 +0100

    Using std::exception

--- opm/simulators/wells/BlackoilWellModel_impl.hpp
+++ opm/simulators/wells/BlackoilWellModel_impl.hpp
@@ -586,7 +586,7 @@ namespace Opm {
 
             try {
                 well->wellTesting(ebosSimulator_, simulationTime, this->wellState(), this->groupState(), wellTestState(), deferred_logger);
-            } catch (std::runtime_error& e) {
+            } catch (std::exception& e) {
                 const std::string msg = fmt::format("Exception during testing of well: {}. The well will not open.\n Exception message: {}", wellEcl.name(), e.what());
                 deferred_logger.warning("WELL_TESTING_FAILED", msg);
             }
