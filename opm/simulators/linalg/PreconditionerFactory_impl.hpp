71d58afc0e715c8bd74fec7f0a194713dd7c4ff0
Tobias Meyer Andersen <tobiasmeyer.andersen@sintef.no>
2023-12-19 15:33:16 +0100

    Add a valid wrapper through OwningBlockPreconditioner that rebuiilds the preconditioner on updates. Still has excessive matrix copy and misses a wrapper function

--- opm/simulators/linalg/PreconditionerFactory_impl.hpp
+++ opm/simulators/linalg/PreconditionerFactory_impl.hpp
@@ -177,10 +177,10 @@ struct StandardPreconditioners
           return createParILU(op, prm, comm, prm.get<int>("ilulevel", 0));
         });
         F::addCreator("DuneILU", [](const O& op, const P& prm, const std::function<V()>&, std::size_t, const C& comm) {
-            const double w = prm.get<double>("relaxation", 1.0);
             const int n = prm.get<int>("ilulevel", 0);
+            const double w = prm.get<double>("relaxation", 1.0);
             const bool resort = prm.get<bool>("resort", false);
-            return wrapBlockPreconditioner<DummyUpdatePreconditioner<Dune::SeqILU<M, V, V>>>(comm, op.getmat(), n, w, resort);
+            return wrapBlockPreconditioner<RebuildOnUpdatePreconditioner<Dune::SeqILU<M, V, V>, M>>(comm, op.getmat(), n, w, resort);
         });
         F::addCreator("DILU", [](const O& op, const P& prm, const std::function<V()>&, std::size_t, const C& comm) {
           DUNE_UNUSED_PARAMETER(prm);
