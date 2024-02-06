aaaf2307243805a6b9db64116937bf9c6edb848b
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2024-02-06 10:11:57 +0100

    BlackoilModel: rename ebosNewtonMethod to newtonMethod

--- opm/simulators/flow/BlackoilModel.hpp
+++ opm/simulators/flow/BlackoilModel.hpp
@@ -690,15 +690,15 @@ namespace Opm {
         void updateSolution(const BVector& dx)
         {
             OPM_TIMEBLOCK(updateSolution);
-            auto& ebosNewtonMethod = simulator_.model().newtonMethod();
+            auto& newtonMethod = simulator_.model().newtonMethod();
             SolutionVector& solution = simulator_.model().solution(/*timeIdx=*/0);
 
-            ebosNewtonMethod.update_(/*nextSolution=*/solution,
-                                     /*curSolution=*/solution,
-                                     /*update=*/dx,
-                                     /*resid=*/dx); // the update routines of the black
-                                                    // oil model do not care about the
-                                                    // residual
+            newtonMethod.update_(/*nextSolution=*/solution,
+                                 /*curSolution=*/solution,
+                                 /*update=*/dx,
+                                 /*resid=*/dx); // the update routines of the black
+                                                // oil model do not care about the
+                                                // residual
 
             // if the solution is updated, the intensive quantities need to be recalculated
             {
