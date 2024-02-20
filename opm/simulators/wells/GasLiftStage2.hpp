ababeb79c41b4efca66274ec380eb7ec2dd57d2b
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2024-02-20 08:24:50 +0100

    GasLiftStage2: template Scalar type

--- opm/simulators/wells/GasLiftStage2.hpp
+++ opm/simulators/wells/GasLiftStage2.hpp
@@ -30,8 +30,7 @@
 #include <tuple>
 #include <vector>
 
-namespace Opm
-{
+namespace Opm {
 
 class DeferredLogger;
 class GasLiftOpt;
@@ -42,86 +41,113 @@ class Schedule;
 class WellInterfaceGeneric;
 template<class Scalar> class WellState;
 
-class GasLiftStage2 : public GasLiftCommon<double> {
-    using GasLiftSingleWell = GasLiftSingleWellGeneric<double>;
+template<class Scalar>
+class GasLiftStage2 : public GasLiftCommon<Scalar>
+{
+    using GasLiftSingleWell = GasLiftSingleWellGeneric<Scalar>;
     using GLiftOptWells = std::map<std::string,std::unique_ptr<GasLiftSingleWell>>;
     using GLiftProdWells = std::map<std::string,const WellInterfaceGeneric*>;
-    using GLiftWellStateMap = std::map<std::string,std::unique_ptr<GasLiftWellState<double>>>;
-    using GradPair = std::pair<std::string, double>;
-    using GradPairItr = std::vector<GradPair>::iterator;
-    using GradInfo = typename GasLiftSingleWellGeneric<double>::GradInfo;
+    using GLiftWellStateMap = std::map<std::string,std::unique_ptr<GasLiftWellState<Scalar>>>;
+    using GradPair = std::pair<std::string, Scalar>;
+    using GradPairItr = typename std::vector<GradPair>::iterator;
+    using GradInfo = typename GasLiftSingleWellGeneric<Scalar>::GradInfo;
     using GradMap = std::map<std::string, GradInfo>;
-    using MPIComm = typename Dune::MPIHelper::MPICommunicator;
+    using MessageType = typename GasLiftCommon<Scalar>::MessageType;
+
     static const int Water = BlackoilPhases::Aqua;
     static const int Oil = BlackoilPhases::Liquid;
     static const int Gas = BlackoilPhases::Vapour;
+
 public:
-    GasLiftStage2(
-        const int report_step_idx,
-        const Parallel::Communication& comm,
-        const Schedule& schedule,
-        const SummaryState& summary_state,
-        DeferredLogger& deferred_logger,
-        WellState<double>& well_state,
-        const GroupState<double>& group_state,
-        GLiftProdWells& prod_wells,
-        GLiftOptWells& glift_wells,
-        GasLiftGroupInfo<double>& group_info,
-        GLiftWellStateMap& state_map,
-        bool glift_debug
-    );
+    GasLiftStage2(const int report_step_idx,
+                  const Parallel::Communication& comm,
+                  const Schedule& schedule,
+                  const SummaryState& summary_state,
+                  DeferredLogger& deferred_logger,
+                  WellState<Scalar>& well_state,
+                  const GroupState<Scalar>& group_state,
+                  GLiftProdWells& prod_wells,
+                  GLiftOptWells& glift_wells,
+                  GasLiftGroupInfo<Scalar>& group_info,
+                  GLiftWellStateMap& state_map,
+                  bool glift_debug);
+
     void runOptimize();
+
 protected:
-    void addOrRemoveALQincrement_(
-        GradMap& grad_map, const std::string& well_name, bool add);
-    std::optional<GradInfo> calcIncOrDecGrad_(
-        const std::string name, const GasLiftSingleWell& gs_well, const std::string& gr_name_dont_limit, bool increase);
+    void addOrRemoveALQincrement_(GradMap& grad_map,
+                                  const std::string& well_name,
+                                  bool add);
+
+    std::optional<GradInfo> calcIncOrDecGrad_(const std::string name,
+                                              const GasLiftSingleWell& gs_well,
+                                              const std::string& gr_name_dont_limit,
+                                              bool increase);
+
     bool checkRateAlreadyLimited_(const std::string& well_name,
-                                  GasLiftWellState<double>& state,
+                                  GasLiftWellState<Scalar>& state,
                                   bool increase);
+
     GradInfo deleteDecGradItem_(const std::string& name);
     GradInfo deleteIncGradItem_(const std::string& name);
     GradInfo deleteGrad_(const std::string& name, bool increase);
+
     void displayDebugMessage_(const std::string& msg) const override;
     void displayDebugMessage2B_(const std::string& msg);
-    void displayDebugMessage_(const std::string& msg, const std::string& group_name);
-    void displayWarning_(const std::string& msg, const std::string& group_name);
+    void displayDebugMessage_(const std::string& msg,
+                              const std::string& group_name);
+    void displayWarning_(const std::string& msg,
+                         const std::string& group_name);
     void displayWarning_(const std::string& msg);
-    std::tuple<double, double, double, double> getCurrentGroupRates_(const Group& group);
-    std::optional<double> getGroupMaxALQ_(const Group &group);
-    std::optional<double> getGroupMaxTotalGas_(const Group &group);
-    std::vector<GasLiftSingleWell *> getGroupGliftWells_(
-        const Group& group);
-    void getGroupGliftWellsRecursive_(
-        const Group& group, std::vector<GasLiftSingleWell *>& wells);
+
+    std::tuple<Scalar, Scalar, Scalar, Scalar>
+    getCurrentGroupRates_(const Group& group);
+
+    std::optional<Scalar> getGroupMaxALQ_(const Group& group);
+    std::optional<Scalar> getGroupMaxTotalGas_(const Group& group);
+
+    std::vector<GasLiftSingleWell*> getGroupGliftWells_(const Group& group);
+
+    void getGroupGliftWellsRecursive_(const Group& group,
+                                      std::vector<GasLiftSingleWell*>& wells);
+
     void optimizeGroup_(const Group& group);
     void optimizeGroupsRecursive_(const Group& group);
-    void recalculateGradientAndUpdateData_(
-        GradPairItr& grad_itr, const std::string& gr_name_dont_limit, bool increase,
-        std::vector<GradPair>& grads, std::vector<GradPair>& other_grads);
-    void redistributeALQ_(
-        std::vector<GasLiftSingleWell *>& wells,  const Group& group,
-        std::vector<GradPair>& inc_grads, std::vector<GradPair>& dec_grads);
-    void removeSurplusALQ_(
-        const Group& group,
-        std::vector<GradPair>& inc_grads, std::vector<GradPair>& dec_grads);
+
+    void recalculateGradientAndUpdateData_(GradPairItr& grad_itr,
+                                           const std::string& gr_name_dont_limit,
+                                           bool increase,
+                                           std::vector<GradPair>& grads,
+                                           std::vector<GradPair>& other_grads);
+
+    void redistributeALQ_(std::vector<GasLiftSingleWell*>& wells,
+                          const Group& group,
+                          std::vector<GradPair>& inc_grads,
+                          std::vector<GradPair>& dec_grads);
+
+    void removeSurplusALQ_(const Group& group,
+                           std::vector<GradPair>& inc_grads,
+                           std::vector<GradPair>& dec_grads);
+
     void saveGrad_(GradMap& map, const std::string& name, GradInfo& grad);
     void saveDecGrad_(const std::string& name, GradInfo& grad);
     void saveIncGrad_(const std::string& name, GradInfo& grad);
     void sortGradients_(std::vector<GradPair>& grads);
-    std::optional<GradInfo> updateGrad_(
-        const std::string& name, GradInfo& grad, bool increase);
-    void updateGradVector_(
-        const std::string& name, std::vector<GradPair>& grads, double grad);
-    void mpiSyncGlobalGradVector_(std::vector<GradPair>& grads_global) const;
-    void mpiSyncLocalToGlobalGradVector_(
-        const std::vector<GradPair>& grads_local,
-        std::vector<GradPair>& grads_global) const;
 
+    std::optional<GradInfo> updateGrad_(const std::string& name,
+                                        GradInfo& grad, bool increase);
+
+    void updateGradVector_(const std::string& name,
+                           std::vector<GradPair>& grads,
+                           Scalar grad);
+
+    void mpiSyncGlobalGradVector_(std::vector<GradPair>& grads_global) const;
+    void mpiSyncLocalToGlobalGradVector_(const std::vector<GradPair>& grads_local,
+                                         std::vector<GradPair>& grads_global) const;
 
     GLiftProdWells& prod_wells_;
     GLiftOptWells& stage1_wells_;
-    GasLiftGroupInfo<double>& group_info_;
+    GasLiftGroupInfo<Scalar>& group_info_;
     GLiftWellStateMap& well_state_map_;
 
     int report_step_idx_;
@@ -133,12 +159,14 @@ protected:
     int max_iterations_ = 1000;
     //int time_step_idx_;
 
-    struct OptimizeState {
-        OptimizeState(GasLiftStage2& parent_, const Group& group_ ) :
-            parent{parent_},
-            group{group_},
-            it{0}
+    struct OptimizeState
+    {
+        OptimizeState(GasLiftStage2& parent_, const Group& group_)
+            : parent{parent_}
+            , group{group_}
+            , it{0}
         {}
+
         GasLiftStage2& parent;
         const Group& group;
         int it;
@@ -147,70 +175,90 @@ protected:
         using GradPair = typename GasLiftStage2::GradPair;
         using GradPairItr = typename GasLiftStage2::GradPairItr;
         using GradMap = typename GasLiftStage2::GradMap;
-        void calculateEcoGradients(std::vector<GasLiftSingleWell *>& wells,
-            std::vector<GradPair>& inc_grads, std::vector<GradPair>& dec_grads);
-        bool checkAtLeastTwoWells(std::vector<GasLiftSingleWell *>& wells);
+
+        void calculateEcoGradients(std::vector<GasLiftSingleWell*>& wells,
+                                   std::vector<GradPair>& inc_grads,
+                                   std::vector<GradPair>& dec_grads);
+
+        bool checkAtLeastTwoWells(std::vector<GasLiftSingleWell*>& wells);
+
         void debugShowIterationInfo();
+
         std::pair<std::optional<GradPairItr>,std::optional<GradPairItr>>
-        getEcoGradients(
-            std::vector<GradPair>& inc_grads, std::vector<GradPair>& dec_grads);
-        void recalculateGradients(
-            std::vector<GradPair>& inc_grads, std::vector<GradPair>& dec_grads,
-            GradPairItr& min_dec_grad_itr, GradPairItr &max_inc_grad_itr);
-        void redistributeALQ( GradPairItr& min_dec_grad, GradPairItr& max_inc_grad);
+        getEcoGradients(std::vector<GradPair>& inc_grads,
+                        std::vector<GradPair>& dec_grads);
+
+        void recalculateGradients(std::vector<GradPair>& inc_grads,
+                                  std::vector<GradPair>& dec_grads,
+                                  GradPairItr& min_dec_grad_itr,
+                                  GradPairItr &max_inc_grad_itr);
+
+        void redistributeALQ( GradPairItr& min_dec_grad,
+                             GradPairItr& max_inc_grad);
 
     private:
         void displayDebugMessage_(const std::string& msg);
         void displayWarning_(const std::string& msg);
-
     };
 
-    struct SurplusState {
-        SurplusState( GasLiftStage2& parent_, const Group& group_,
-              double oil_rate_, double gas_rate_, double water_rate_, double alq_,
-              double min_eco_grad_,
-              double oil_target_, double gas_target_, double water_target_, double liquid_target_,
-              std::optional<double> max_glift_, std::optional<double> max_total_gas_) :
-            parent{parent_},
-            group{group_},
-            oil_rate{oil_rate_},
-            gas_rate{gas_rate_},
-            water_rate{water_rate_},
-            alq{alq_},
-            min_eco_grad{min_eco_grad_},
-            oil_target{oil_target_},
-            gas_target{gas_target_},
-            water_target(water_target_),
-            liquid_target{liquid_target_},
-            max_glift{max_glift_},
-            max_total_gas{max_total_gas_},
-            it{0}
+    struct SurplusState
+    {
+        SurplusState(GasLiftStage2& parent_,
+                     const Group& group_,
+                     Scalar oil_rate_,
+                     Scalar gas_rate_,
+                     Scalar water_rate_,
+                     Scalar alq_,
+                     Scalar min_eco_grad_,
+                     Scalar oil_target_,
+                     Scalar gas_target_,
+                     Scalar water_target_,
+                     Scalar liquid_target_,
+                     std::optional<Scalar> max_glift_,
+                     std::optional<Scalar> max_total_gas_)
+            : parent{parent_}
+            , group{group_}
+            , oil_rate{oil_rate_}
+            , gas_rate{gas_rate_}
+            , water_rate{water_rate_}
+            , alq{alq_}
+            , min_eco_grad{min_eco_grad_}
+            , oil_target{oil_target_}
+            , gas_target{gas_target_}
+            , water_target(water_target_)
+            , liquid_target{liquid_target_}
+            , max_glift{max_glift_}
+            , max_total_gas{max_total_gas_}
+            , it{0}
         {}
+
         GasLiftStage2 &parent;
         const Group &group;
-        double oil_rate;
-        double gas_rate;
-        double water_rate;
-        double alq;
-        const double min_eco_grad;
-        const double oil_target;
-        const double gas_target;
-        const double water_target;
-        const double liquid_target;
-        std::optional<double> max_glift;
-        std::optional<double> max_total_gas;
+        Scalar oil_rate;
+        Scalar gas_rate;
+        Scalar water_rate;
+        Scalar alq;
+        const Scalar min_eco_grad;
+        const Scalar oil_target;
+        const Scalar gas_target;
+        const Scalar water_target;
+        const Scalar liquid_target;
+        std::optional<Scalar> max_glift;
+        std::optional<Scalar> max_total_gas;
         int it;
 
-        void addOrRemoveALQincrement(
-            GradMap &grad_map, const std::string& well_name, bool add);
+        void addOrRemoveALQincrement(GradMap &grad_map,
+                                     const std::string& well_name,
+                                     bool add);
+
         bool checkALQlimit();
-        bool checkEcoGradient(const std::string& well_name, double eco_grad);
-        bool checkGasTarget(double delta_gas);
-        bool checkLiquidTarget(double delta_liquid);
-        bool checkOilTarget(double delta_oil);
-        bool checkWaterTarget(double delta_water);
-        std::array<double, 4> computeDelta(const std::string& name);
-        void updateRates(const std::array<double, 4>& delta);
+        bool checkEcoGradient(const std::string& well_name, Scalar eco_grad);
+        bool checkGasTarget(Scalar delta_gas);
+        bool checkLiquidTarget(Scalar delta_liquid);
+        bool checkOilTarget(Scalar delta_oil);
+        bool checkWaterTarget(Scalar delta_water);
+        std::array<Scalar, 4> computeDelta(const std::string& name);
+        void updateRates(const std::array<Scalar, 4>& delta);
     };
 };
 
