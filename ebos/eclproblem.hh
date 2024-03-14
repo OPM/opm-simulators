979232a1a666c1de5ced13eb8ba73be8abe594c0
Antonella Ritorto <anto.ritorto@gmail.com>
2024-03-14 16:40:18 +0100

    Skip evalSummaryState for CpGrid with LGRs

--- ebos/eclproblem.hh
+++ ebos/eclproblem.hh
@@ -671,7 +671,12 @@ public:
         }
         bool isSubStep = !EWOMS_GET_PARAM(TypeTag, bool, EnableWriteAllSolutions) && !this->simulator().episodeWillBeOver();
         // For CpGrid with LGRs, ecl/vtk output is not supported yet.
-        if (enableEclOutput_) {
+        const auto& grid =  this->simulator().vanguard().gridView().grid();
+        using GridType =  std::remove_cv_t< typename std::remove_reference<decltype(grid)>::type>;
+        bool isCpGrid = std::is_same_v<GridType, Dune::CpGrid>;
+        if ( isCpGrid && (this->simulator().vanguard().gridView().grid().maxLevel()>0)) {
+        }
+        else {
             eclWriter_->evalSummaryState(isSubStep);
         }
 
