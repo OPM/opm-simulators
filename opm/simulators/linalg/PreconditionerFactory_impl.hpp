78aa0d4b51551809792dbd88f9697733071ff33b
Tobias Meyer Andersen <tobiasmeyer.andersen@sintef.no>
2024-06-19 13:14:59 +0200

    Add an OPM implementation of ILU0 Clean up cuistl

--- opm/simulators/linalg/PreconditionerFactory_impl.hpp
+++ opm/simulators/linalg/PreconditionerFactory_impl.hpp
@@ -50,6 +50,15 @@
 #include <opm/simulators/linalg/PreconditionerFactoryGPUIncludeWrapper.hpp>
 
 
+
+#include <opm/simulators/linalg/PreconditionerWithUpdate.hpp>
+#include <opm/simulators/linalg/hipistl/CuSparseMatrix.hpp>
+#include <opm/simulators/linalg/hipistl/detail/CuMatrixDescription.hpp>
+#include <opm/simulators/linalg/hipistl/detail/CuSparseHandle.hpp>
+#include <opm/simulators/linalg/hipistl/detail/CuSparseResource.hpp>
+#include <dune/istl/ilu.hh>
+
+
 namespace Opm {
 
 template <class Smoother>
