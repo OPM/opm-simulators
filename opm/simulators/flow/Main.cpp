97ad881c0419b3156ce2a074758417a07a429de3
Håkon Hægland <hakon.hagland@gmail.com>
2024-08-12 15:17:38 +0200

    Rebased, and fixed command line parsing
    Create one log file for each slave subprocess. Redirect both
    stdout and stderr to this file

--- opm/simulators/flow/Main.cpp
+++ opm/simulators/flow/Main.cpp
@@ -39,6 +39,10 @@
 #include <fcntl.h>  // for open()
 #include <unistd.h> // for dup2(), close()
 
+#include <iostream>
+#include <fcntl.h>  // for open()
+#include <unistd.h> // for dup2(), close()
+
 namespace Opm {
 
 Main::Main(int argc, char** argv, bool ownMPI)
