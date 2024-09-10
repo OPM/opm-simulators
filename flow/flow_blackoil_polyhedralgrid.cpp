9cc6196708135515d5aaff09265d0153c866d23d
Kai Bao <kai.bao@sintef.no>
2024-09-10 14:20:46 +0200

    removing GridHelpers.hpp from flow_blackoil_polyhedralgrid
    it is not necessary anymore.

--- flow/flow_blackoil_polyhedralgrid.cpp
+++ flow/flow_blackoil_polyhedralgrid.cpp
@@ -19,8 +19,6 @@
 #include <config.h>
 
 #include <opm/grid/polyhedralgrid.hh>
-// TODO: temporarily fixing the compilation of flow_blackoil_polyhedralgrid
-#include <opm/grid/cpgrid/GridHelpers.hpp>
 
 #include <opm/models/blackoil/blackoillocalresidualtpfa.hh>
 #include <opm/models/discretization/common/tpfalinearizer.hh>
