046dfeaccc61b4dcc380af46f213aeab42a6a586
Lisa Julia Nebel <lisa.nebel@dr-blatt.de>
2024-09-30 08:06:29 +0200

    Calculate the segment rates of the well state correctly, i.e. sum when the well is distributed over multiple processes

--- opm/simulators/wells/WellState.cpp
+++ opm/simulators/wells/WellState.cpp
@@ -747,14 +747,15 @@ void WellState<Scalar>::initWellStateMSWell(const std::vector<Well>& wells_ecl,
                 // TODO: to see if this strategy can benefit StandardWell too
                 // TODO: it might cause big problem for gas rate control or if there is a gas rate limit
                 // maybe the best way is to initialize the fractions first then get the rates
-                for (int perf = 0; perf < n_activeperf; perf++)
+                for (std::size_t perf = 0; perf < perf_data.size(); perf++)
                     perf_rates[perf*np + gaspos] *= 100;
             }
 
             const auto& perf_rates = perf_data.phase_rates;
             std::vector<Scalar> perforation_rates(perf_rates.begin(), perf_rates.end());
 
-            calculateSegmentRates(segment_inlets, segment_perforations, perforation_rates, np, 0 /* top segment */, ws.segments.rates);
+            calculateSegmentRates(ws, segment_inlets, segment_perforations, perforation_rates, np, 0 /* top segment */, ws.segments.rates);
+
             // for the segment pressure, the segment pressure is the same with the first perforation belongs to the segment
             // if there is no perforation associated with this segment, it uses the pressure from the outlet segment
             // which requres the ordering is successful
@@ -765,16 +766,38 @@ void WellState<Scalar>::initWellStateMSWell(const std::vector<Well>& wells_ecl,
                 auto& segment_pressure = ws.segments.pressure;
                 segment_pressure[0] = ws.bhp;
                 const auto& perf_press = perf_data.pressure;
+                // The indices_to_combine contain the indices that are only availabe on one process, we reduce the segment pressure vector
+                // later for these indices.
+                std::vector<int> indices_to_combine;
                 for (int seg = 1; seg < well_nseg; ++seg) {
+                    // todo: this is not the ideal solution yet, but ok
+                    // the problem is: segment_pressure has 25 entries but perf_press only has 5 or 7 (in total 12)
+                    // -> but that is exactly what the array segment_perforations is doing, it gets the perforations for each segment, right?
                     if (!segment_perforations[seg].empty()) {
-                        const int first_perf = segment_perforations[seg][0];
-                        segment_pressure[seg] = perf_press[first_perf];
+                        const int first_perf = ws.parallel_info.get().globalToLocal(segment_perforations[seg][0]);
+                        if (first_perf > -1) { //-1 indicates that the global id is not on this process
+                            segment_pressure[seg] = perf_press[first_perf];
+                        }
+                        indices_to_combine.push_back(seg);
                     } else {
                         // seg_press_.push_back(bhp); // may not be a good decision
                         // using the outlet segment pressure // it needs the ordering is correct
                         const int outlet_seg = segment_set[seg].outletSegment();
                         segment_pressure[seg] = segment_pressure[segment_set.segmentNumberToIndex(outlet_seg)];
                     }
+                }
+
+                // Commuinicate the segment_pressure values
+                std::vector<Scalar> values_to_combine(indices_to_combine.size(), 0.0);
+
+                for (size_t i = 0; i < indices_to_combine.size(); ++i) {
+                    values_to_combine[i] = segment_pressure[indices_to_combine[i]];
+                }
+                ws.parallel_info.get().communication().sum(values_to_combine.data(), values_to_combine.size());
+
+                // Now make segment_pressure equal across all processes
+                for (size_t i = 0; i < indices_to_combine.size(); ++i) {
+                    segment_pressure[indices_to_combine[i]] = values_to_combine[i];
                 }
             }
         }
@@ -810,11 +833,12 @@ void WellState<Scalar>::initWellStateMSWell(const std::vector<Well>& wells_ecl,
 
 template<class Scalar>
 void WellState<Scalar>::
-calculateSegmentRates(const std::vector<std::vector<int>>& segment_inlets,
-                      const std::vector<std::vector<int>>& segment_perforations,
-                      const std::vector<Scalar>& perforation_rates,
-                      const int np, const int segment,
-                      std::vector<Scalar>& segment_rates)
+calculateSegmentRatesBeforeSum(const SingleWellState<Scalar>& ws,
+                               const std::vector<std::vector<int>>& segment_inlets,
+                               const std::vector<std::vector<int>>& segment_perforations,
+                               const std::vector<Scalar>& perforation_rates,
+                               const int np, const int segment,
+                               std::vector<Scalar>& segment_rates)
 {
     // the rate of the segment equals to the sum of the contribution from the perforations and inlet segment rates.
     // the first segment is always the top segment, its rates should be equal to the well rates.
@@ -825,16 +849,33 @@ calculateSegmentRates(const std::vector<std::vector<int>>& segment_inlets,
     }
     // contributions from the perforations belong to this segment
     for (const int& perf : segment_perforations[segment]) {
-        for (int p = 0; p < np; ++p) {
-            segment_rates[np * segment + p] += perforation_rates[np * perf + p];
+        auto local_perf = ws.parallel_info.get().globalToLocal(perf);
+        if (local_perf > -1) {
+            // if local_perf == -1, then the perforation is not on this process, therefore we do not sum that here
+            // but at the end of the function
+            for (int p = 0; p < np; ++p) {
+                segment_rates[np * segment + p] += perforation_rates[np * local_perf + p];
+            }
         }
     }
     for (const int& inlet_seg : segment_inlets[segment]) {
-        calculateSegmentRates(segment_inlets, segment_perforations, perforation_rates, np, inlet_seg, segment_rates);
+        calculateSegmentRatesBeforeSum(ws, segment_inlets, segment_perforations, perforation_rates, np, inlet_seg, segment_rates);
         for (int p = 0; p < np; ++p) {
             segment_rates[np * segment + p] += segment_rates[np * inlet_seg + p];
         }
     }
+}
+template<class Scalar>
+void WellState<Scalar>::
+calculateSegmentRates(const SingleWellState<Scalar>& ws,
+                      const std::vector<std::vector<int>>& segment_inlets,
+                      const std::vector<std::vector<int>>& segment_perforations,
+                      const std::vector<Scalar>& perforation_rates,
+                      const int np, const int segment,
+                      std::vector<Scalar>& segment_rates)
+{
+    calculateSegmentRatesBeforeSum(ws, segment_inlets, segment_perforations, perforation_rates, np, segment, segment_rates);
+    ws.parallel_info.get().communication().sum(segment_rates.data(), segment_rates.size());
 }
 
 template<class Scalar>
