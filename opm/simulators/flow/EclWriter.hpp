aee9cf3140f2fc2f78ad07d4e9332dddbe544862
Kai Bao <kai.bao@sintef.no>
2024-10-17 13:05:39 +0200

    WIP in recovering EclWriter related

--- opm/simulators/flow/EclWriter.hpp
+++ opm/simulators/flow/EclWriter.hpp
@@ -745,26 +745,26 @@ private:
             this->outputModule_->accumulateDensityParallel();
         }
 
-//        if constexpr (enableMech) {
-//            if (simulator_.vanguard().eclState().runspec().mech()) {
-//                OPM_TIMEBLOCK(prepareMechData);
-//                for (const auto& elem : elements(gridView, Dune::Partitions::interior)) {
-//                    elemCtx.updatePrimaryStencil(elem);
-//                    elemCtx.updatePrimaryIntensiveQuantities(/*timeIdx=*/0);
-//                    outputModule_->processElementMech(elemCtx);
-//                }
-//            }
-//        }
+        if constexpr (enableMech) {
+            if (simulator_.vanguard().eclState().runspec().mech()) {
+                OPM_TIMEBLOCK(prepareMechData);
+                for (const auto& elem : elements(gridView, Dune::Partitions::interior)) {
+                    elemCtx.updatePrimaryStencil(elem);
+                    elemCtx.updatePrimaryIntensiveQuantities(/*timeIdx=*/0);
+                    outputModule_->processElementMech(elemCtx);
+                }
+            }
+        }
 
-//        if (! this->simulator_.model().linearizer().getFlowsInfo().empty()) {
-//            OPM_TIMEBLOCK(prepareFlowsData);
-//            for (const auto& elem : elements(gridView, Dune::Partitions::interior)) {
-//                elemCtx.updatePrimaryStencil(elem);
-//                elemCtx.updatePrimaryIntensiveQuantities(/*timeIdx=*/0);
-//
-//                this->outputModule_->processElementFlows(elemCtx);
-//            }
-//        }
+        if (! this->simulator_.model().linearizer().getFlowsInfo().empty()) {
+            OPM_TIMEBLOCK(prepareFlowsData);
+            for (const auto& elem : elements(gridView, Dune::Partitions::interior)) {
+                elemCtx.updatePrimaryStencil(elem);
+                elemCtx.updatePrimaryIntensiveQuantities(/*timeIdx=*/0);
+
+                this->outputModule_->processElementFlows(elemCtx);
+            }
+        }
 
         {
             OPM_TIMEBLOCK(prepareBlockData);
