1cf57db1d1eb56f673be976330c2a6fb5baa0586
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2024-11-06 13:19:12 +0100

    move spe9 tests to json

--- regressionTests.cmake
+++ regressionTests.cmake
@@ -6,13 +6,6 @@ set(abs_tol 2e-2)
 set(rel_tol 1e-5)
 set(coarse_rel_tol 1e-2)
 
-add_test_compareECLFiles(CASENAME spe9
-                         FILENAME SPE9_CP_SHORT
-                         SIMULATOR flow
-                         ABS_TOL ${abs_tol}
-                         REL_TOL ${rel_tol}
-                         RESTART_STEP 10)
-
 add_test_compareECLFiles(CASENAME spe9group
                          FILENAME SPE9_CP_GROUP
                          SIMULATOR flow
@@ -306,6 +299,7 @@ if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.19)
         "tests/definitions/regression/spe1_precsalt.json"
         "tests/definitions/regression/spe1_solvent.json"
         "tests/definitions/regression/spe3.json"
+        "tests/definitions/regression/spe9.json"
         "tests/definitions/regression/tracer.json"
         "tests/definitions/regression/udq_actionx.json"
         "tests/definitions/regression/udt.json"
