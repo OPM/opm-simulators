3fa34ed51f1636733cd55925739e424538352382
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2024-11-06 13:19:12 +0100

    move wsegvalv tests to json

--- regressionTests.cmake
+++ regressionTests.cmake
@@ -16,12 +16,6 @@ if (opm-common_EMBEDDED_PYTHON)
                            TEST_ARGS --solver-max-time-step-in-days=10)
 endif()
 
-add_test_compareECLFiles(CASENAME wsegvalv
-			  FILENAME BASE_MSW_WSEGVALV
-			  SIMULATOR flow
-			  ABS_TOL ${abs_tol}
-			  REL_TOL ${rel_tol})
-
 add_test_compareECLFiles(CASENAME wsegvalv_2d_vert
                          FILENAME  MSW-2D-VERT-02
                          SIMULATOR flow
@@ -229,6 +223,7 @@ if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.19)
         "tests/definitions/regression/winjmult.json"
         "tests/definitions/regression/wsegaicd.json"
         "tests/definitions/regression/wsegsicd.json"
+        "tests/definitions/regression/wsegvalv.json"
         "tests/definitions/regression/wtest_bhp.json"
         "tests/definitions/regression/wtest_thp.json"
      )
