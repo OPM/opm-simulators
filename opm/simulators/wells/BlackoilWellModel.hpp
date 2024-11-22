977f66913464b09ee561f3453e74c6b3a1c854eb
Tor Harald Sandve <tosa@norceresearch.no>
2024-11-22 10:27:23 +0100

    check if updateThpWellGroupIsUpdated

--- opm/simulators/wells/BlackoilWellModel.hpp
+++ opm/simulators/wells/BlackoilWellModel.hpp
@@ -480,7 +480,7 @@ template<class Scalar> class WellContributions;
                                               const double dt,
                                               DeferredLogger& local_deferredLogger);
 
-            void computeWellGroupThp(const double dt, DeferredLogger& local_deferredLogger);
+            bool computeWellGroupThp(const double dt, DeferredLogger& local_deferredLogger);
 
             /// Update rank's notion of intersecting wells and their
             /// associate solution variables.
