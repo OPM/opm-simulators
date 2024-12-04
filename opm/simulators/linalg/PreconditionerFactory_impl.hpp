3b67d6dc54cb195b3a1395157fa6b77e1a3475d2
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2024-12-04 11:31:04 +0100

    Small fixes for hypre integration (#1)
    * use appropriate hypre init function
    
    * properly handle hypre without device support
    
    * don't add hypre preconditioner to factory if scalar type does not match

--- opm/simulators/linalg/PreconditionerFactory_impl.hpp
+++ opm/simulators/linalg/PreconditionerFactory_impl.hpp
@@ -549,7 +549,8 @@ struct StandardPreconditioners<Operator, Dune::Amg::SequentialInformation> {
             });
 #if HAVE_HYPRE
             // Only add Hypre for scalar matrices
-            if constexpr (M::block_type::rows == 1 && M::block_type::cols == 1) {
+            if constexpr (M::block_type::rows == 1 && M::block_type::cols == 1 &&
+                          std::is_same_v<HYPRE_Real, typename V::field_type>) {
                 F::addCreator("hypre", [](const O& op, const P& prm, const std::function<V()>&, std::size_t) {
                     return std::make_shared<Hypre::HyprePreconditioner<M, V, V>>(op.getmat(), prm);
                 });
