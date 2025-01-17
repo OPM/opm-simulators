eea55ce619e0cb26da99d1b4f1078308654cbf2f
jakobtorben <Jakob.Torben@sintef.no>
2025-01-17 18:43:59 +0100

    Add tests for partitionCells with wells

--- tests/test_partitionCells.cpp
+++ tests/test_partitionCells.cpp
@@ -24,11 +24,80 @@
 #define BOOST_TEST_NO_MAIN
 #include <boost/test/unit_test.hpp>
 
-#include <opm/simulators/flow/partitionCells.hpp>
 #include <dune/grid/common/gridview.hh>
-#include <opm/grid/CpGrid.hpp>
 
+#include <opm/grid/common/WellConnections.hpp>
+#include <opm/grid/CpGrid.hpp>
+#include <opm/grid/utility/OpmWellType.hpp>
+
+#include <opm/input/eclipse/Schedule/Well/Connection.hpp>
 #include <opm/input/eclipse/Schedule/Well/Well.hpp>
+#include <opm/input/eclipse/Schedule/Well/WellConnections.hpp>
+
+#include <opm/simulators/flow/partitionCells.hpp>
+
+// Helper functions
+namespace {
+    Opm::Connection createConnection(int i, int j, int k) {
+        // Calculate global index from i,j,k coordinates
+        // For this 1D test grid, global_index is just i since j=k=0
+        std::size_t global_index = i;
+        return Opm::Connection(i, j, k,                           // i,j,k
+                             global_index,                        // global_index
+                             0,                                   // complnum
+                             Opm::Connection::State::OPEN,        // state
+                             Opm::Connection::Direction::Z,       // direction
+                             Opm::Connection::CTFKind::DeckValue, // kind
+                             0,                                   // satTableId
+                             0.0,                                 // depth
+                             Opm::Connection::CTFProperties(),    // properties
+                             0,                                   // sort_value
+                             false);                             // defaultSatTabId
+    }
+
+    Dune::cpgrid::OpmWellType createWell(const std::string& name) {
+        using namespace Opm;
+        return Dune::cpgrid::OpmWellType(name, name, 0, 0, 0, 0, 0.0, WellType(),
+                                        Well::ProducerCMode(), Connection::Order::TRACK,
+                                        UnitSystem::newMETRIC(),
+                                        0.0, 0.0, false, false, 0, Well::GasInflowEquation());
+    }
+
+    std::vector<Dune::cpgrid::OpmWellType> createWellsWithConnections(
+        const std::vector<std::pair<std::string, std::vector<int>>>& well_specs) {
+        std::vector<Dune::cpgrid::OpmWellType> wells;
+        for (const auto& [well_name, cell_indices] : well_specs) {
+            auto well_conn = std::make_shared<Opm::WellConnections>();
+            for (int idx : cell_indices) {
+                well_conn->add(createConnection(idx, 0, 0));
+            }
+            auto well = createWell(well_name);
+            well.updateConnections(well_conn, true);
+            wells.push_back(well);
+        }
+        return wells;
+    }
+
+    Dune::CpGrid createTestGrid(const std::array<int, 3>& dims, 
+                               const std::array<double, 3>& size) {
+        Dune::CpGrid grid;
+        grid.createCartesian(dims, size);
+        return grid;
+    }
+
+    template<typename Entity>
+    Opm::ZoltanPartitioningControl<Entity> createZoltanControl(const Dune::CpGrid& grid) {
+        Opm::ZoltanPartitioningControl<Entity> zoltan_ctrl;
+        zoltan_ctrl.domain_imbalance = 1.1;
+        zoltan_ctrl.index = [](const auto& element) {
+            return element.index();
+        };
+        zoltan_ctrl.local_to_global = [&grid](const int local_idx) {
+            return grid.globalCell()[local_idx];
+        };
+        return zoltan_ctrl;
+    }
+}
 
 BOOST_AUTO_TEST_CASE(FileBased)
 {
@@ -61,34 +130,19 @@ BOOST_AUTO_TEST_CASE(Simple2)
     BOOST_CHECK_EQUAL_COLLECTIONS(expected.begin(), expected.end(), part.begin(), part.end());
 }
 
-BOOST_AUTO_TEST_CASE(PartitionCellsTestSimple)
+
+BOOST_AUTO_TEST_CASE(PartitionCellsTest)
 {
-    // Create a 3x2x2 grid (12 cells total)
-    Dune::CpGrid grid;
-    std::array<int, 3> dims{{3, 4, 1}};
-    std::array<double, 3> size{{3.0, 4.0, 1.0}};
-    grid.createCartesian(dims, size);
-
-    // Setup Zoltan control parameters
+    auto grid = createTestGrid({3, 4, 1}, {3.0, 4.0, 1.0});
     using Entity = typename Dune::CpGrid::LeafGridView::template Codim<0>::Entity;
-    Opm::ZoltanPartitioningControl<Entity> zoltan_ctrl;
-    zoltan_ctrl.domain_imbalance = 1.1;  // Allow 10% imbalance
+    auto zoltan_ctrl = createZoltanControl<Entity>(grid);
 
-    // Initialize index function to return entity index
-    std::function<int(const Entity&)> index_func = [](const Entity& e) { return e.index(); };
-    zoltan_ctrl.index = index_func;
-
-    // Initialize local_to_global function to use index as global ID for this test
-    std::function<int(int)> l2g_func = [](int idx) { return idx; };
-    zoltan_ctrl.local_to_global = l2g_func;
-
-    // Test simple partitioning first
+    // Test simple partitioning
     {
         auto [part, num_part] = Opm::partitionCells("simple", 3, grid.leafGridView(),
                                                    std::vector<Opm::Well>{},
                                                    std::unordered_map<std::string, std::set<int>>{},
                                                    zoltan_ctrl);
-
         BOOST_CHECK_EQUAL(num_part, 3);
         BOOST_CHECK_EQUAL(part.size(), 12);
 
@@ -97,28 +151,6 @@ BOOST_AUTO_TEST_CASE(PartitionCellsTestSimple)
             BOOST_CHECK(p >= 0 && p < 3);
         }
     }
-}
-
-BOOST_AUTO_TEST_CASE(PartitionCellsTestZoltan)
-{
-    // Create a 3x2x2 grid (12 cells total)
-    Dune::CpGrid grid;
-    std::array<int, 3> dims{{3, 4, 1}};
-    std::array<double, 3> size{{3.0, 4.0, 1.0}};
-    grid.createCartesian(dims, size);
-
-    // Setup Zoltan control parameters
-    using Entity = typename Dune::CpGrid::LeafGridView::template Codim<0>::Entity;
-    Opm::ZoltanPartitioningControl<Entity> zoltan_ctrl;
-    zoltan_ctrl.domain_imbalance = 1.1;  // Allow 10% imbalance
-
-    // Initialize index function to return entity index
-    std::function<int(const Entity&)> index_func = [](const Entity& e) { return e.index(); };
-    zoltan_ctrl.index = index_func;
-
-    // Initialize local_to_global function to use index as global ID for this test
-    std::function<int(int)> l2g_func = [](int idx) { return idx; };
-    zoltan_ctrl.local_to_global = l2g_func;
 
 #if HAVE_MPI && HAVE_ZOLTAN
     // Test Zoltan partitioning
@@ -127,7 +159,6 @@ BOOST_AUTO_TEST_CASE(PartitionCellsTestZoltan)
                                                    std::vector<Opm::Well>{},
                                                    std::unordered_map<std::string, std::set<int>>{},
                                                    zoltan_ctrl);
-
         BOOST_CHECK_EQUAL(num_part, 3);
         BOOST_CHECK_EQUAL(part.size(), 12);
 
@@ -136,6 +167,98 @@ BOOST_AUTO_TEST_CASE(PartitionCellsTestZoltan)
             BOOST_CHECK(p >= 0 && p < 3);
         }
     }
+#endif
+}
+
+BOOST_AUTO_TEST_CASE(PartitionCellsWithWellMergeTest)
+{
+    // Create a 1D grid with 7 cells
+    // Visual representation of the grid and wells:
+    // Cell indices:    0    1    2    3    4    5    6
+    // Well layout:     W1---W1   |    W2---W2   |    |
+    // Expected:        Cell 0 and 1 should be merged
+    //                  Cell 3 and 4 should be merged
+    //                  Cells 2, 5, and 6 can be in any partition
+    auto grid = createTestGrid({7, 1, 1}, {7.0, 1.0, 1.0});
+
+    // Setup Zoltan control parameters
+    using Entity = typename Dune::CpGrid::LeafGridView::template Codim<0>::Entity;
+    auto zoltan_ctrl = createZoltanControl<Entity>(grid);
+
+    auto wells = createWellsWithConnections({
+        {"TESTW1", {0, 1}},    // Well 1 connects cells 0,1
+        {"TESTW2", {3, 4}}     // Well 2 connects cells 3,4
+    });
+
+    // Create well connections object
+    Dune::cpgrid::WellConnections wellConnections(wells, std::unordered_map<std::string, std::set<int>>{}, grid);
+
+#if HAVE_MPI && HAVE_ZOLTAN
+    // Test Zoltan partitioning with well cells
+    auto [part, num_part] = Opm::partitionCells("zoltan", 4, grid.leafGridView(),
+                                               wells,
+                                               std::unordered_map<std::string, std::set<int>>{},
+                                               zoltan_ctrl);
+
+    // Verify number of partitions
+    BOOST_CHECK_EQUAL(num_part, 4);
+    BOOST_CHECK_EQUAL(part.size(), 7);
+
+    // The key tests:
+    // 1. Cells 0 and 1 (well1) should be in the same partition
+    BOOST_CHECK_EQUAL(part[0], part[1]);
+
+    // 2. Cells 3 and 4 (well2) should be in the same partition
+    BOOST_CHECK_EQUAL(part[3], part[4]);
+
+    // 3. Cells 2, 5, and 6 can be in any partition, but well1 and well2 cells
+    // don't need to be in the same partition
+#endif
+}
+
+BOOST_AUTO_TEST_CASE(PartitionCellsWithOverlappingWellsTest)
+{
+    // Create a 1D grid with 7 cells
+    // Visual representation of the grid and wells:
+    // Cell indices:    0    1    2    3    4    5    6
+    // Well layout:          W1---W1---W1   |    |    |
+    //                                W2---W2
+    // Expected:        Cells 1,2,3 should be in same partition (Well 1)
+    //                  Cells 3,4 should be in same partition (Well 2)
+    //                  Therefore cells 1,2,3,4 should all end up in same partition
+
+    auto grid = createTestGrid({7, 1, 1}, {7.0, 1.0, 1.0});
+    using Entity = typename Dune::CpGrid::LeafGridView::template Codim<0>::Entity;
+    auto zoltan_ctrl = createZoltanControl<Entity>(grid);
+
+    auto wells = createWellsWithConnections({
+        {"TESTW1", {1, 2, 3}}, // Well 1 connects cells 1,2,3
+        {"TESTW2", {3, 4}}     // Well 2 connects cells 3,4
+    });
+
+#if HAVE_MPI && HAVE_ZOLTAN
+    // Test Zoltan partitioning with overlapping well cells
+    auto [part, num_part] = Opm::partitionCells("zoltan", 4, grid.leafGridView(),
+                                               wells,
+                                               std::unordered_map<std::string, std::set<int>>{},
+                                               zoltan_ctrl);
+
+    // Verify number of partitions
+    BOOST_CHECK_EQUAL(num_part, 4);
+    BOOST_CHECK_EQUAL(part.size(), 7);
+
+    // The key tests:
+    // 1. All cells connected by Well 1 should be in the same partition
+    BOOST_CHECK_EQUAL(part[1], part[2]);
+    BOOST_CHECK_EQUAL(part[2], part[3]);
+
+    // 2. All cells connected by Well 2 should be in the same partition
+    BOOST_CHECK_EQUAL(part[3], part[4]);
+
+    // 3. Due to the overlap at cell 3, all well-connected cells should be in the same partition
+    BOOST_CHECK_EQUAL(part[1], part[4]);
+
+    // 4. Cells 0, 5, and 6 can be in any partition
 #endif
 }
 
