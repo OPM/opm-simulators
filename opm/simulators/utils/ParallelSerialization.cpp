fef6cfcf38de0bd8834f50e7b163514b2edd1fa5
Tor Harald Sandve <tosa@norceresearch.no>
2025-01-17 09:35:16 +0100

    Fix parallel gsatprod

--- opm/simulators/utils/ParallelSerialization.cpp
+++ opm/simulators/utils/ParallelSerialization.cpp
@@ -34,6 +34,7 @@
 #include <opm/input/eclipse/Schedule/Group/GroupEconProductionLimits.hpp>
 #include <opm/input/eclipse/Schedule/Group/GConSale.hpp>
 #include <opm/input/eclipse/Schedule/Group/GConSump.hpp>
+#include <opm/input/eclipse/Schedule/Group/GSatProd.hpp>
 #include <opm/input/eclipse/Schedule/Group/GuideRateConfig.hpp>
 #include <opm/input/eclipse/Schedule/MSW/SICD.hpp>
 #include <opm/input/eclipse/Schedule/MSW/Valve.hpp>
