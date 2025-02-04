5a3373f93f9a180ab59f2c857b3962ec3028130a
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2025-02-04 17:05:25 +0100

    added: voigt array
    this is a container for vector data stored using voigt notation

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -160,6 +160,7 @@ list (APPEND MAIN_SOURCE_FILES
   opm/simulators/utils/PressureAverage.cpp
   opm/simulators/utils/SerializationPackers.cpp
   opm/simulators/utils/UnsupportedFlowKeywords.cpp
+  opm/simulators/utils/VoigtArray.cpp
   opm/simulators/utils/compressPartition.cpp
   opm/simulators/utils/gatherDeferredLogger.cpp
   opm/simulators/utils/phaseUsageFromDeck.cpp
@@ -419,6 +420,7 @@ list (APPEND TEST_SOURCE_FILES
   tests/test_stoppedwells.cpp
   tests/test_timer.cpp
   tests/test_vfpproperties.cpp
+  tests/test_VoigtArray.cpp
   tests/test_wellmodel.cpp
   tests/test_wellprodindexcalculator.cpp
   tests/test_wellstate.cpp
@@ -985,6 +987,7 @@ list (APPEND PUBLIC_HEADER_FILES
   opm/simulators/utils/ParallelSerialization.hpp
   opm/simulators/utils/readDeck.hpp
   opm/simulators/utils/satfunc/RelpermDiagnostics.hpp
+  opm/simulators/utils/VoigtArray.hpp
   opm/simulators/wells/ALQState.hpp
   opm/simulators/wells/BlackoilWellModel.hpp
   opm/simulators/wells/BlackoilWellModel_impl.hpp
