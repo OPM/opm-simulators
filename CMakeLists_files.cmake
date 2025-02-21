3ac18e733463fb2d23accb4fbbdadc0f362094a2
Tobias Meyer Andersen <tobiasmeyer.andersen@sintef.no>
2025-02-21 10:31:05 +0100

    copy_to_gpu test works

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -488,6 +488,7 @@ if (HAVE_CUDA)
   ADD_CUDA_OR_HIP_FILE(TEST_SOURCE_FILES tests test_gpu_resources.cu)
   ADD_CUDA_OR_HIP_FILE(TEST_SOURCE_FILES tests test_is_gpu_pointer.cpp)
   ADD_CUDA_OR_HIP_FILE(TEST_SOURCE_FILES tests test_throw_macros_on_gpu.cu)
+  ADD_CUDA_OR_HIP_FILE(TEST_SOURCE_FILES tests test_gpuBlackOilFluidSystem.cu)
 
 
   # for loop providing the flag --expt-relaxed-constexpr to fix some cuda issues with constexpr
