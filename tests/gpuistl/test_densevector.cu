3ddb185462ea8d29474b81b9baa98ba3599ec476
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2025-02-26 20:43:26 +0100

    More tests for dense vector

--- tests/gpuistl/test_densevector.cu
+++ tests/gpuistl/test_densevector.cu
@@ -26,6 +26,19 @@
 #include <opm/simulators/linalg/gpuistl/detail/gpu_safe_call.hpp>
 #include <opm/simulators/linalg/gpuistl/dense/DenseVector.hpp>
 #include <opm/simulators/linalg/gpuistl/dense/FieldVector.hpp>
+#include <opm/simulators/linalg/gpuistl/gpu_smart_pointer.hpp>
+
+namespace {
+  __global__ void writeToGlobalFieldVector(Opm::gpuistl::dense::FieldVector<double, 10> *vec) {
+    (*vec)[2] = 1.0;
+  }
+
+  __global__ void makeFieldVector(double* value) {
+    Opm::gpuistl::dense::FieldVector<double, 10> vec;
+    vec[0] = 10;
+    *value = vec[0];
+  }
+}
 
 BOOST_AUTO_TEST_CASE(TestDenseCreation) 
 {
@@ -34,4 +47,21 @@ BOOST_AUTO_TEST_CASE(TestDenseCreation)
     BOOST_CHECK_EQUAL(someVec.size(), 10);
     someVec[9] = 1.0;
     BOOST_CHECK_EQUAL(someVec[9], 1.0);
+}
+
+
+BOOST_AUTO_TEST_CASE(TestDenseVector) 
+{
+    Opm::gpuistl::dense::FieldVector<double, 10> vec;
+    auto gpuVec = Opm::gpuistl::make_gpu_shared_ptr(vec);
+    writeToGlobalFieldVector<<<1, 1>>>(gpuVec.get());
+    OPM_GPU_SAFE_CALL(cudaDeviceSynchronize());
+    auto vecFromGPU = Opm::gpuistl::copyFromGPU(gpuVec);
+    BOOST_CHECK_EQUAL(vecFromGPU[2], 1.0);
+
+    auto valueOnGPU = Opm::gpuistl::make_gpu_shared_ptr(0.0);
+    makeFieldVector<<<1, 1>>>(valueOnGPU.get());
+    OPM_GPU_SAFE_CALL(cudaDeviceSynchronize());
+    auto value = Opm::gpuistl::copyFromGPU(valueOnGPU);
+    BOOST_CHECK_EQUAL(value, 10.0);
 }
