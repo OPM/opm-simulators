ff8243a92d0cb2232b88ec0674f4e4f2d5c00de2
Tobias Meyer Andersen <tobiasmeyer.andersen@sintef.no>
2025-03-06 15:16:11 +0100

    Add tets for make_view of BOFS

--- opm/simulators/linalg/gpuistl/GpuView.cpp
+++ opm/simulators/linalg/gpuistl/GpuView.cpp
@@ -78,5 +78,9 @@ GpuView<T>::copyToHost(std::vector<T>& data) const
 template class GpuView<double>;
 template class GpuView<float>;
 template class GpuView<int>;
+template class GpuView<std::array<double, 3>>;
+template class GpuView<std::array<float, 3>>;
+template class GpuView<std::array<double, 9>>;
+template class GpuView<std::array<float, 9>>;
 
 } // namespace Opm::gpuistl
--- tests/gpuistl/test_throw_macros_on_gpu.cu
+++ tests/gpuistl/test_throw_macros_on_gpu.cu
@@ -26,8 +26,8 @@
 #include <opm/simulators/linalg/gpuistl/detail/gpu_safe_call.hpp>
 
 namespace {
-// NOTE: We have to split this into a separate function due 
-// to some weirdness of hipcc. Note however that this is 
+// NOTE: We have to split this into a separate function due
+// to some weirdness of hipcc. Note however that this is
 // the realistic use case of the macro.
 __device__ __host__ void functionThatContainsMacros(bool call) {
     if (call) {
@@ -44,19 +44,20 @@ __global__ void codeThatContainsMacros(bool call) {
 
 BOOST_AUTO_TEST_CASE(TestKernel)
 {
-    // OPM_GPU_SAFE_CALL(cudaDeviceSynchronize());
-    // OPM_GPU_SAFE_CALL(cudaGetLastError());
-    // codeThatContainsMacros<<<1, 1>>>(false);
-    // OPM_GPU_SAFE_CALL(cudaDeviceSynchronize());
-    // OPM_GPU_SAFE_CALL(cudaGetLastError());
+    // TODO: Figure out why this test halts when run in debug mode on AMD GPUs
+    OPM_GPU_SAFE_CALL(cudaDeviceSynchronize());
+    OPM_GPU_SAFE_CALL(cudaGetLastError());
+    codeThatContainsMacros<<<1, 1>>>(false);
+    OPM_GPU_SAFE_CALL(cudaDeviceSynchronize());
+    OPM_GPU_SAFE_CALL(cudaGetLastError());
 }
 
-BOOST_AUTO_TEST_CASE(TestOutsideKernel) 
+BOOST_AUTO_TEST_CASE(TestOutsideKernel)
 {
     // This is to make sure that the macros work outside of kernels but inside a .cu file
     // ie. inside a file compiled by nvcc/hipcc.
-    // BOOST_CHECK_THROW(OPM_THROW(std::runtime_error, "THROW"), std::runtime_error);
-    // BOOST_CHECK_THROW(OPM_THROW_NOLOG(std::runtime_error, "THROW_NOLOG"), std::runtime_error);
-    // BOOST_CHECK_THROW(OPM_THROW_PROBLEM(std::runtime_error, "THROW_PROBLEM"), std::runtime_error);
-    // BOOST_CHECK_THROW(OPM_ERROR_IF(true, "ERROR_IF"), std::logic_error);
+    BOOST_CHECK_THROW(OPM_THROW(std::runtime_error, "THROW"), std::runtime_error);
+    BOOST_CHECK_THROW(OPM_THROW_NOLOG(std::runtime_error, "THROW_NOLOG"), std::runtime_error);
+    BOOST_CHECK_THROW(OPM_THROW_PROBLEM(std::runtime_error, "THROW_PROBLEM"), std::runtime_error);
+    BOOST_CHECK_THROW(OPM_ERROR_IF(true, "ERROR_IF"), std::logic_error);
 }
