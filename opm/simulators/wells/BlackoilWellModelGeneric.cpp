fd04e7f150d85d2d4ec675b85886c3fff36d5d24
Tor Harald Sandve <tosa@norceresearch.no>
2025-04-14 07:34:43 +0200

    Only update wells in network interations where thp has changed more than the tolerance

--- opm/simulators/wells/BlackoilWellModelGeneric.cpp
+++ opm/simulators/wells/BlackoilWellModelGeneric.cpp
@@ -1658,7 +1658,7 @@ inferLocalShutWells()
 template<typename Scalar, typename IndexTraits>
 Scalar
 BlackoilWellModelGeneric<Scalar, IndexTraits>::
-updateNetworkPressures(const int reportStepIdx, const Scalar damping_factor, const Scalar upper_update_bound)
+updateNetworkPressures(const int reportStepIdx, const Scalar damping_factor, const Scalar upper_update_bound, std::vector<std::string>& updated_wells)
 {
     OPM_TIMEFUNCTION();
     // Get the network and return if inactive (no wells in network at this time)
@@ -1709,6 +1709,7 @@ updateNetworkPressures(const int reportStepIdx, const Scalar damping_factor, con
             }
         }
     }
+    const auto& balance = this->schedule()[reportStepIdx].network_balance();
 
     for (auto& well : well_container_generic_) {
 
@@ -1723,8 +1724,14 @@ updateNetworkPressures(const int reportStepIdx, const Scalar damping_factor, con
                 const Scalar new_limit = it->second;
                 well->setDynamicThpLimit(new_limit);
                 SingleWellState<Scalar, IndexTraits>& ws = this->wellState()[well->indexOfWell()];
+                const Scalar pressure_tolerance = balance.pressure_tolerance();
+                // We only re-compute the solution if the thp has changed more than
+                // the pressure_tolerance
+                bool needs_update = std::abs(new_limit - ws.thp) > pressure_tolerance;
+                if (needs_update) {
+                    updated_wells.push_back(well->name());
+                }
                 const bool thp_is_limit = ws.production_cmode == Well::ProducerCMode::THP;
-                // TODO: not sure why the thp is NOT updated properly elsewhere
                 if (thp_is_limit) {
                     ws.thp = well->getTHPConstraint(summaryState_);
                 }
