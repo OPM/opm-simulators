082e3557d7925138d85f9d909ff1c09abd782f63
David Landa Marban <dmar@norceresearch.no>
2025-04-30 18:02:43 +0200

    Check for systems with water

--- opm/simulators/wells/MultisegmentWell_impl.hpp
+++ opm/simulators/wells/MultisegmentWell_impl.hpp
@@ -1764,9 +1764,11 @@ namespace Opm
         const bool allow_cf = this->getAllowCrossFlow() || openCrossFlowAvoidSingularity(simulator);
 
         const int nseg = this->numberOfSegments();
-
-        const Scalar rhow = FluidSystem::referenceDensity( FluidSystem::waterPhaseIdx, Base::pvtRegionIdx() );
-        const unsigned watCompIdx = Indices::canonicalToActiveComponentIndex(FluidSystem::waterCompIdx);
+        
+        const Scalar rhow = FluidSystem::phaseIsActive(FluidSystem::waterPhaseIdx) ?
+            FluidSystem::referenceDensity( FluidSystem::waterPhaseIdx, Base::pvtRegionIdx() ) : 0.0;
+        const unsigned watCompIdx = FluidSystem::phaseIsActive(FluidSystem::waterPhaseIdx) ?
+            Indices::canonicalToActiveComponentIndex(FluidSystem::waterCompIdx) : 0;
 
         for (int seg = 0; seg < nseg; ++seg) {
             // calculating the perforation rate for each perforation that belongs to this segment
@@ -1806,7 +1808,9 @@ namespace Opm
                 perf_press_state[local_perf_index] = perf_press.value();
 
                 // mass rates, for now only water
-                perf_data.wat_mass_rates[local_perf_index] = cq_s[watCompIdx].value() * rhow;
+                if (FluidSystem::phaseIsActive(FluidSystem::waterPhaseIdx)) {
+                    perf_data.wat_mass_rates[local_perf_index] = cq_s[watCompIdx].value() * rhow;
+                }
 
                 for (int comp_idx = 0; comp_idx < this->num_components_; ++comp_idx) {
                     // the cq_s entering mass balance equations need to consider the efficiency factors.
