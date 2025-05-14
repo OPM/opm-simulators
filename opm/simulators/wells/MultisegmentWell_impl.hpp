f912f6f8a442256161dece532227585be90c2c93
Lisa Julia Nebel <lisa.nebel@dr-blatt.de>
2025-05-14 10:15:25 +0200

    Don't throw on a misalignment of the WellConvergence in MultisegmentWell_impl.hpp but warn and align convergence

--- opm/simulators/wells/MultisegmentWell_impl.hpp
+++ opm/simulators/wells/MultisegmentWell_impl.hpp
@@ -1678,8 +1678,17 @@ namespace Opm
             const auto report = getWellConvergence(simulator, well_state, Base::B_avg_, deferred_logger, relax_convergence);
             converged = report.converged();
             if (this->parallel_well_info_.communication().size() > 1 && converged != this->parallel_well_info_.communication().min(converged)) {
-                OPM_THROW(std::runtime_error, fmt::format("Misalignment of the parallel simulation run in iterateWellEqWithSwitching - the well calculation succeeded on rank {} but failed on other ranks.", this->parallel_well_info_.communication().rank()));
+                std::cout << fmt::format("Misalignment of the parallel simulation run in iterateWellEqWithSwitching - the well calculation for well {} succeeded on rank {} but failed on other ranks.", this->name(), this->parallel_well_info_.communication().rank()) << std::endl;
+                OpmLog::warning(fmt::format("Misalignment of the parallel simulation run in iterateWellEqWithSwitching - the well calculation  for well {} succeeded on rank {} but failed on other ranks.", this->name(), this->parallel_well_info_.communication().rank()));
             }
+            if (this->parallel_well_info_.communication().size() > 1 && converged != this->parallel_well_info_.communication().max(converged)) {
+                std::cout << fmt::format("Misalignment of the parallel simulation run in iterateWellEqWithSwitching - the well calculation  for well {} failed on rank {} but succeeded on other ranks.", this->name(), this->parallel_well_info_.communication().rank()) << std::endl;
+                OpmLog::warning(fmt::format("Misalignment of the parallel simulation run in iterateWellEqWithSwitching - the well calculation  for well {} failed on rank {} but succeeded on other ranks.", this->name(), this->parallel_well_info_.communication().rank()));
+            }
+            if (this->parallel_well_info_.communication().size() > 1 && (converged != this->parallel_well_info_.communication().min(converged) || converged != this->parallel_well_info_.communication().max(converged))) {
+                converged = this->parallel_well_info_.communication().min(converged) == 0;
+            }
+
             if (converged) {
                 // if equations are sufficiently linear they might converge in less than min_its_after_switch
                 // in this case, make sure all constraints are satisfied before returning
