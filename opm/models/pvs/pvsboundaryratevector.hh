f020f2a4ff4bd6b688840806dfe3129161a4ae00
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2025-05-15 13:22:24 +0200

    pvsboundaryratevector: use standard algorithm

--- opm/models/pvs/pvsboundaryratevector.hh
+++ opm/models/pvs/pvsboundaryratevector.hh
@@ -35,6 +35,8 @@
 #include <opm/models/common/multiphasebaseproperties.hh>
 #include <opm/models/discretization/common/fvbaseproperties.hh>
 
+#include <algorithm>
+
 namespace Opm {
 
 /*!
@@ -182,10 +184,8 @@ public:
         this->setFreeFlow(context, bfIdx, timeIdx, fluidState);
 
         // we only allow fluxes in the direction opposite to the outer unit normal
-        for (unsigned eqIdx = 0; eqIdx < numEq; ++eqIdx) {
-            Evaluation& val = this->operator[](eqIdx);
-            val = Toolbox::min(0.0, val);
-        }
+        std::for_each(this->begin(), this->end(),
+                      [](auto& val) { val = Toolbox::min(0.0, val); });
     }
 
     /*!
@@ -200,10 +200,8 @@ public:
         this->setFreeFlow(context, bfIdx, timeIdx, fluidState);
 
         // we only allow fluxes in the same direction as the outer unit normal
-        for (unsigned eqIdx = 0; eqIdx < numEq; ++eqIdx) {
-            Evaluation& val = this->operator[](eqIdx);
-            val = Toolbox::max(0.0, val);
-        }
+        std::for_each(this->begin(), this->end(),
+                      [](auto& val) { val = Toolbox::max(0.0, val); });
     }
 
     /*!
