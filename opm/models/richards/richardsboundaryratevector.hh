ec5876e06d1ff94de3ed930b454752520ce73855
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2025-05-16 09:02:55 +0200

    richardsboundaryratevector: use standard algorithm

--- opm/models/richards/richardsboundaryratevector.hh
+++ opm/models/richards/richardsboundaryratevector.hh
@@ -35,6 +35,8 @@
 #include <opm/models/discretization/common/fvbaseproperties.hh>
 #include <opm/models/richards/richardsproperties.hh>
 
+#include <algorithm>
+
 namespace Opm {
 
 /*!
@@ -135,10 +137,8 @@ public:
         this->setFreeFlow(context, bfIdx, timeIdx, fluidState);
 
         // we only allow fluxes in the direction opposite to the outer unit normal
-        for (unsigned eqIdx = 0; eqIdx < numEq; ++eqIdx) {
-            Evaluation& val = this->operator[](eqIdx);
-            val = Toolbox::min(0.0, val);
-        }
+        std::for_each(this->begin(), this->end(),
+                      [](auto& val) { val = Toolbox::min(0.0, val); });
     }
 
     /*!
@@ -153,10 +153,8 @@ public:
         this->setFreeFlow(context, bfIdx, timeIdx, fluidState);
 
         // we only allow fluxes in the same direction as the outer unit normal
-        for (unsigned eqIdx = 0; eqIdx < numEq; ++eqIdx) {
-            Evaluation& val = this->operator[](eqIdx);
-            val = Toolbox::max(0.0, val);
-        }
+        std::for_each(this->begin(), this->end(),
+                      [](auto& val) { val = Toolbox::max(0.0, val); });
     }
 
     /*!
