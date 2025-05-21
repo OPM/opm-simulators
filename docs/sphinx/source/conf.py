b40e4f66599ff5fd10d6b9ee55d31e0f56130bcb
Antonella Ritorto <anto.ritorto@gmail.com>
2025-05-21 15:28:59 +0200

    Add some files

---
+++ docs/sphinx/source/conf.py
@@ -0,0 +1,14 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+extensions = [
+    'breathe', 
+]
+
+breathe_projects = {
+	"OPM": "../../doxygen/xml"
+}
+breathe_default_project = "OPM"
+
+html_theme = 'furo' # or ‘sphinc_rtd_theme’
---
+++ docs/sphinx/source/index.rst
@@ -0,0 +1,12 @@
+opm-simulators documentation
+============================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+API Reference
+=============
+
+.. doxygenindex::
+   :project: OPM
