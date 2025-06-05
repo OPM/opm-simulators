59a5e9728128cdf8159ea83257bff256ec3e1348
Halvor M Nilsen <Halvor.M.Nilsen@sintef.no>
2025-06-05 16:57:03 +0200

    Selected Fixes for Geo-Mechanical Runs
    In particular:
    
      - fixes for pinched out connections
      - for getting "action like behavoir" ok

--- opm/simulators/wells/BlackoilWellModelGeneric.hpp
+++ opm/simulators/wells/BlackoilWellModelGeneric.hpp
@@ -392,7 +392,7 @@ protected:
     createLocalParallelWellInfo(const std::vector<Well>& wells);
 
     void initializeWellProdIndCalculators();
-    void initializeWellPerfData();
+    void initializeWellPerfData(const int report_step);
 
     bool wasDynamicallyShutThisTimeStep(const int well_index) const;
 
