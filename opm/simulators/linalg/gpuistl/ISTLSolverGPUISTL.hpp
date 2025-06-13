850771543c25bdf96d560b63baabc7eec3cc6eab
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2025-06-13 10:00:41 +0200

    Added protected checkConverged method

--- opm/simulators/linalg/gpuistl/ISTLSolverGPUISTL.hpp
+++ opm/simulators/linalg/gpuistl/ISTLSolverGPUISTL.hpp
@@ -191,22 +191,7 @@ public:
 private:
     bool checkConvergence(const Dune::InverseOperatorResult& result) const
     {
-        if (!result.converged) {
-            if (result.reduction < m_parameters.relaxed_linear_solver_reduction_) {
-                std::stringstream ss;
-                ss << "Full linear solver tolerance not achieved. The reduction is:" << result.reduction << " after "
-                   << result.iterations << " iterations ";
-                OpmLog::warning(ss.str());
-                return true;
-            }
-        }
-        // Check for failure of linear solver.
-        if (!m_parameters.ignoreConvergenceFailure_ && !result.converged) {
-            const std::string msg("Convergence failure for linear solver.");
-            OPM_THROW_NOLOG(NumericalProblem, msg);
-        }
-
-        return result.converged;
+        return AbstractISTLSolver<TypeTag>::checkConvergence(result, m_parameters);
     }
 
     void updateMatrix(const Matrix& M)
