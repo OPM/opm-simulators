96918b840797166ab9a7394b77598a9271d742b8
Halvor M Nilsen <Halvor.M.Nilsen@sintef.no>
2025-07-17 14:27:51 +0200

    This introduce parallel hypre preconditioner

--- opm/simulators/linalg/StandardPreconditioners_serial.hpp
+++ opm/simulators/linalg/StandardPreconditioners_serial.hpp
@@ -174,8 +174,8 @@ struct StandardPreconditioners<Operator, Dune::Amg::SequentialInformation, typen
             // Only add Hypre for scalar matrices
             if constexpr (M::block_type::rows == 1 && M::block_type::cols == 1 &&
                           std::is_same_v<HYPRE_Real, typename V::field_type>) {
-                F::addCreator("hypre", [](const O& op, const P& prm, const std::function<V()>&, std::size_t) {
-                    return std::make_shared<Hypre::HyprePreconditioner<M, V, V>>(op.getmat(), prm);
+              F::addCreator("hypre", [](const O& op, const P& prm, const std::function<V()>&, std::size_t) {
+                return std::make_shared<Hypre::HyprePreconditioner<M, V, V, Dune::Amg::SequentialInformation>>(op.getmat(), prm, Dune::Amg::SequentialInformation());
                 });
             }
 #endif
