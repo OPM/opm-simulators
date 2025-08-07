2eb98dea7c38581317e210ba1697b08e879e91c0
Tor Harald Sandve <tosa@norceresearch.no>
2025-08-07 10:59:21 +0200

    code clean-up based on review

--- opm/simulators/wells/GroupState.cpp
+++ opm/simulators/wells/GroupState.cpp
@@ -54,8 +54,8 @@ GroupState<Scalar> GroupState<Scalar>::serializationTestObject()
     result.injection_controls = {{{Phase::FOAM, "test12"}, Group::InjectionCMode::REIN}};
     result.gpmaint_state.add("foo", GPMaint::State::serializationTestObject());
     result.m_gconsump_rates = {{"testA", {0.2, 0.1}}};
-    result.m_number_of_wells_under_this_control = {{"test13", 3}};
-    result.m_number_of_wells_under_this_inj_control = {{ {Phase::WATER, "test14"}, 3}};
+    result.m_number_of_wells_under_group_control = {{"test13", 3}};
+    result.m_number_of_wells_under_inj_group_control = {{ {Phase::WATER, "test14"}, 3}};
 
     return result;
 }
@@ -74,8 +74,8 @@ bool GroupState<Scalar>::operator==(const GroupState& other) const
            this->inj_surface_rates == other.inj_surface_rates &&
            this->m_grat_sales_target == other.m_grat_sales_target &&
            this->injection_controls == other.injection_controls &&
-           this->m_number_of_wells_under_this_control == m_number_of_wells_under_this_control &&
-           this->m_number_of_wells_under_this_inj_control == m_number_of_wells_under_this_inj_control &&
+           this->m_number_of_wells_under_group_control == m_number_of_wells_under_group_control &&
+           this->m_number_of_wells_under_inj_group_control == m_number_of_wells_under_inj_group_control &&
            this->gpmaint_state == other.gpmaint_state &&
            this->m_gconsump_rates == other.m_gconsump_rates;
 }
@@ -426,26 +426,19 @@ injection_control(const std::string& gname, Phase phase) const
 
 template<class Scalar>
 void GroupState<Scalar>::
-update_number_of_wells_under_this_control(const std::string& gname, int number)
+update_number_of_wells_under_group_control(const std::string& gname, int number)
 {
-    this->m_number_of_wells_under_this_control[gname] = number;
+    this->m_number_of_wells_under_group_control[gname] = number;
 }
 template<class Scalar>
 int GroupState<Scalar>::
-number_of_wells_under_this_control(const std::string& gname) const
+number_of_wells_under_group_control(const std::string& gname) const
 {
-    auto group_iter = this->m_number_of_wells_under_this_control.find(gname);
-    if (group_iter == this->m_number_of_wells_under_this_control.end())
+    auto group_iter = this->m_number_of_wells_under_group_control.find(gname);
+    if (group_iter == this->m_number_of_wells_under_group_control.end())
         throw std::logic_error("No such group");
 
     return group_iter->second;
-}
-
-template<class Scalar>
-bool GroupState<Scalar>::
-has_number_of_wells_under_this_control(const std::string& gname) const
-{
-    return (this->m_number_of_wells_under_this_control.count(gname) > 0);
 }
 
 //-------------------------------------------------------------------------
@@ -453,30 +446,21 @@ has_number_of_wells_under_this_control(const std::string& gname) const
 
 template<class Scalar>
 void GroupState<Scalar>::
-update_number_of_wells_under_this_inj_control(const std::string& gname, Phase phase, int number)
+update_number_of_wells_under_inj_group_control(const std::string& gname, Phase phase, int number)
 {
-    this->m_number_of_wells_under_this_inj_control[{phase, gname}] = number;
+    this->m_number_of_wells_under_inj_group_control[{phase, gname}] = number;
 }
 
 template<class Scalar>
 int GroupState<Scalar>::
-number_of_wells_under_this_inj_control(const std::string& gname, Phase phase) const
+number_of_wells_under_inj_group_control(const std::string& gname, Phase phase) const
 {
-    auto group_iter = this->m_number_of_wells_under_this_inj_control.find({phase, gname});
-    if (group_iter == this->m_number_of_wells_under_this_inj_control.end())
+    auto group_iter = this->m_number_of_wells_under_inj_group_control.find({phase, gname});
+    if (group_iter == this->m_number_of_wells_under_inj_group_control.end())
         throw std::logic_error("No such group");
 
     return group_iter->second;
 }
-
-template<class Scalar>
-bool GroupState<Scalar>::
-has_number_of_wells_under_this_inj_control(const std::string& gname, Phase phase) const
-{
-    return (this->m_number_of_wells_under_this_inj_control.count({phase, gname}) > 0);
-}
-
-//-------------------------------------------------------------------------
 
 //-------------------------------------------------------------------------
 
