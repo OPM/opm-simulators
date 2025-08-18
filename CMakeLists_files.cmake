9e1de783ccbc390f7d6674e33e49591dccd70192
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2025-08-18 11:16:46 +0200

    Added --expt-relaxed-constexpr to test_gpuBlackOilFluidSystem

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -535,6 +535,7 @@ if (HAVE_CUDA)
       tests/gpuistl/test_gpu_ad.cu
       tests/gpuistl/test_gpu_linear_two_phase_material.cu
       tests/gpuistl/test_gpuPvt.cu
+      tests/gpuistl/test_gpuBlackOilFluidSystem
     )
 
     foreach(file ${CU_FILES_NEEDING_RELAXED_CONSTEXPR})
