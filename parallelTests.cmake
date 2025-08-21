a2af7f906943903218f0e481fe6f17faa42529ee
David Landa Marban <dmar@norceresearch.no>
2025-08-21 14:58:15 +0200

    TUNING: XXX and TRG for MBE and CNV

--- parallelTests.cmake
+++ parallelTests.cmake
@@ -184,7 +184,7 @@ add_test_compare_parallel_simulation(CASENAME spe1_thermal
                                      FILENAME SPE1CASE2_THERMAL
                                      SIMULATOR flow
                                      ABS_TOL ${abs_tol}
-                                     REL_TOL ${coarse_rel_tol_parallel}}
+                                     REL_TOL ${coarse_rel_tol_parallel}
                                      DIR spe1
                                      TEST_ARGS --linear-solver-reduction=1e-7 --tolerance-cnv=5e-6 --tolerance-mb=1e-8)
 
@@ -316,7 +316,7 @@ add_test_compare_parallel_simulation(CASENAME reg_smry_in_fld_udq
                                      ABS_TOL ${abs_tol_parallel}
                                      REL_TOL ${coarse_rel_tol_parallel}
                                      DIR udq_actionx
-                                     TEST_ARGS --enable-tuning=true)
+                                     TEST_ARGS --enable-tuning=true --time-step-control=pid)
 
 add_test_compare_parallel_simulation(CASENAME winjmult_msw
                                      FILENAME WINJMULT_MSW
