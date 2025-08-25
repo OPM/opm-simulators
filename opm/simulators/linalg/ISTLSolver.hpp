ca3700fe832baa82e289bd1906b94e0af63686be
Halvor M Nilsen <Halvor.M.Nilsen@sintef.no>
2025-08-25 13:57:22 +0200

    added: cg + small changes

--- opm/simulators/linalg/ISTLSolver.hpp
+++ opm/simulators/linalg/ISTLSolver.hpp
@@ -536,6 +536,7 @@ std::unique_ptr<Matrix> blockJacobiAdjacency(const Grid& grid,
             if (flexibleSolver_.empty()) {
                 return true;
             }
+
             if (!flexibleSolver_[activeSolverNum_].solver_) {
                 return true;
             }
