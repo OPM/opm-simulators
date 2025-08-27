ce9e19c943f60daf6469a9add61d3a01b17b0c4e
Svenn Tveit <svtv@norceresearch.no>
2025-08-27 12:50:30 +0200

    Clarify ordering and clean up doc in rest of the tests

--- python/test/test_schedule.py
+++ python/test/test_schedule.py
@@ -13,9 +13,6 @@ from .pytest_common import pushd
 class TestBasic(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        # NOTE: See comment in test_basic.py for the reason why we are
-        #   only using a single test_all() function instead of splitting
-        #   it up in multiple test functions
         test_dir = Path(os.path.dirname(__file__))
         cls.data_dir = test_dir.parent.joinpath("test_data/SPE1CASE1b")
 
