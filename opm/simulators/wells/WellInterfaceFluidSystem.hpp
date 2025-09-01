1b8649bb5d3faf4840f62b7345f63fa4cdcf0591
Kai Bao <kai.bao@sintef.no>
2025-09-01 15:30:07 +0200

    changing numComponents in wells to be numConservationQuantities
    numComponents has been a very misleading name and something causes
    confusion when coming to phase/component indices.
    
    For example, mobility is not way to be associated with compoent.

--- opm/simulators/wells/WellInterfaceFluidSystem.hpp
+++ opm/simulators/wells/WellInterfaceFluidSystem.hpp
@@ -74,7 +74,7 @@ protected:
                              const ModelParameters& param,
                              const RateConverterType& rate_converter,
                              const int pvtRegionIdx,
-                             const int num_components,
+                             const int num_conservation_quantities,
                              const int num_phases,
                              const int index_of_well,
                              const std::vector<PerforationData<Scalar>>& perf_data);
