13bf78d7b6456c3b28c5447d83f1fdea608304f5
kjetil <kjetil@haugenlabs.com>
2025-09-08 12:28:11 -0500

    solve: compiling version of mixed precision solver

--- opm/simulators/linalg/HaugenLabs.c
+++ opm/simulators/linalg/HaugenLabs.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#include <math.h>
 #include <immintrin.h>
 
 void bsr_hello()
@@ -23,6 +24,7 @@ bsr_matrix* bsr_new()
     A->rowptr = NULL;
     A->colidx = NULL;
     A->dbl    = NULL;
+    A->flt    = NULL;
 
     return A;
 }
@@ -51,6 +53,54 @@ void bsr_info(bsr_matrix *A)
 
     printf("\n");
 }
+
+void bsr_vmspmv3(bsr_matrix *A, const double *x, double *y)
+{
+    int nrows = A->nrows;
+    int *rowptr=A->rowptr;
+    int *colidx=A->colidx;
+    const float *data=A->flt;
+
+    const int b=3;
+
+    __m256d mm_zeros =_mm256_setzero_pd();
+    for(int i=0;i<nrows;i++)
+    {
+        __m256d vA[3];
+        for(int k=0;k<3;k++) vA[k] = mm_zeros;
+        for(int k=rowptr[i];k<rowptr[i+1];k++)
+        {
+            const float *AA=data+9*k;
+
+            int j = colidx[k];
+            __m256d vx = _mm256_loadu_pd(x+b*j);
+
+            vA[0] += _mm256_cvtps_pd(_mm_loadu_ps(AA+0))*_mm256_permute4x64_pd(vx,0b00000000); //0b01010101
+            vA[1] += _mm256_cvtps_pd(_mm_loadu_ps(AA+3))*_mm256_permute4x64_pd(vx,0b01010101); //0b01010101
+            vA[2] += _mm256_cvtps_pd(_mm_loadu_ps(AA+6))*_mm256_permute4x64_pd(vx,0b10101010); //0b01010101
+        }
+
+        // sum over columns
+        __m256d vy, vz;
+        vz = vA[0] + vA[1] + vA[2];
+
+        double *y_i = y+b*i;
+        vy = _mm256_loadu_pd(y_i);       // optional blend to keep
+        vz =_mm256_blend_pd(vy,vz,0x7);  // 4th element unchanged
+        _mm256_storeu_pd(y_i,vz);
+    }
+}
+
+void bsr_downcast(bsr_matrix *M)
+{
+    int nnz = M->nnz;
+    int b = M->b;
+
+    if(M->flt==NULL) posix_memalign((void**)&(M->flt),64,b*b*nnz*sizeof(float));
+    for(int i=0;i<b*b*nnz;i++) M->flt[i]=M->dbl[i];
+}
+
+
 
 void bsr_sparsity(const bsr_matrix *A, const char *name)
 {
@@ -109,7 +159,7 @@ bildu_prec *bildu_new()
     return P;
 }
 
-void bildu_init(bildu_prec *P, bsr_matrix *A)
+void bildu_init(bildu_prec *P, bsr_matrix const *A)
 {
     int b     = A->b;
     int nrows = A->nrows;
@@ -163,6 +213,11 @@ void bildu_init(bildu_prec *P, bsr_matrix *A)
 void vec_copy(double *y, double const * x, int n)
 {
     for(int i=0;i<n;i++) y[i]=x[i];
+}
+
+void vec_fill(double *y, double x, int n)
+{
+    for(int i=0;i<n;i++) y[i]=x;
 }
 
 void mat3_view(double const *M, char const *name)
@@ -240,7 +295,7 @@ void mat3_inv(double *invA, const double *A)
     for(int k=0;k<9;k++) invA[k]=M[k]/detA;
 }
 
-void inline vec_copy9(double *y, double const *x)
+inline void vec_copy9(double *y, double const *x)
 {
     for(int i=0;i<9;i++) y[i]=x[i];
 }
@@ -466,6 +521,96 @@ void bildu_apply3(bildu_prec *restrict P, double *x)
     }
 }
 
+void bildu_mapply3c(bildu_prec *restrict P, double *x)
+{
+    bsr_matrix *L  = P->L;
+    bsr_matrix *D  = P->D;
+    bsr_matrix *U  = P->U;
+
+    int b=L->b;
+    int bb=b*b;
+
+    __m256d mm256_zero_pd =_mm256_setzero_pd();
+
+    // Lower triangular solve assuming ones on diagonal
+    for(int i=0;i<L->ncols;i++)
+    {
+        __m256d vA[3], vx[3];
+
+        double *xi = x+b*i;
+        __m256d vxi = _mm256_loadu_pd(xi);
+
+        vx[0] = _mm256_permute4x64_pd(vxi,0b00000000); //0b01010101
+        vx[1] = _mm256_permute4x64_pd(vxi,0b01010101); //0b01010101
+        vx[2] = _mm256_permute4x64_pd(vxi,0b10101010); //0b01010101
+        for(int k=L->rowptr[i];k<L->rowptr[i+1];k++)
+        {
+            const float *A = L->flt+k*bb;
+            int j=U->colidx[k]; // should be L, but does not matter die to structural symmetry?
+            vA[0] = _mm256_cvtps_pd(_mm_loadu_ps(A+0))*vx[0];
+            vA[1] = _mm256_cvtps_pd(_mm_loadu_ps(A+3))*vx[1];
+            vA[2] = _mm256_cvtps_pd(_mm_loadu_ps(A+6))*vx[2];
+
+            double *xj = x+b*j;
+            __m256d vxj = _mm256_loadu_pd(xj);
+            __m256d vz = (vxj - vA[0]) - (vA[1] + vA[2]);
+
+            //vz =_mm256_blend_pd(vxj,vz,0x7);  // 4th element unchanged
+            //_mm256_storeu_pd(xj,vz);
+            double z[4];
+            _mm256_store_pd(z,vz);
+            for(int n=0;n<3;n++) xj[n]=z[n];
+        }
+
+        // Muliply by (inverse) diagonal block
+        const float *A = D->flt+i*bb;
+        vA[0] = _mm256_cvtps_pd(_mm_loadu_ps(A+0))*vx[0]; //0b01010101
+        vA[1] = _mm256_cvtps_pd(_mm_loadu_ps(A+3))*vx[1]; //0b01010101
+        vA[2] = _mm256_cvtps_pd(_mm_loadu_ps(A+6))*vx[2]; //0b01010101
+        __m256d vz = vA[0] + vA[1] + vA[2];
+
+        //vz =_mm256_blend_pd(vxi,vz,0x7);  // 4th element unchanged
+        //_mm256_storeu_pd(xi,vz);
+        double z[4];
+        _mm256_store_pd(z,vz);
+        for(int k=0;k<3;k++) xi[k]=z[k];
+    }
+
+    // Upper triangular solve assuming nonzeros stored in original order
+    for(int i=U->ncols;i>0;i--)
+    {
+        __m256d vA[3];
+        for(int k=0;k<3;k++) vA[k]=mm256_zero_pd;
+        for(int k=U->rowptr[i]-1;k>U->rowptr[i-1]-1;k--)
+        {
+            const float *A = U->flt+k*bb;
+            int j=U->colidx[k];
+            __m256d vxj = _mm256_loadu_pd(x+b*j);
+            vA[0] += _mm256_cvtps_pd(_mm_loadu_ps(A+0))*_mm256_permute4x64_pd(vxj,0b00000000); //0b01010101
+            vA[1] += _mm256_cvtps_pd(_mm_loadu_ps(A+3))*_mm256_permute4x64_pd(vxj,0b01010101); //0b01010101
+            vA[2] += _mm256_cvtps_pd(_mm_loadu_ps(A+6))*_mm256_permute4x64_pd(vxj,0b10101010); //0b01010101
+        }
+        double *xi = x+b*(i-1);
+        __m256d vxi = _mm256_loadu_pd(xi);
+        __m256d vz = (vxi - vA[0]) - (vA[1] + vA[2]);
+        vz =_mm256_blend_pd(vxi,vz,0x7);  // 4th element unchanged
+        _mm256_storeu_pd(xi,vz);
+
+        //double z[4];
+        //_mm256_store_pd(z,vz);
+        //for(int k=0;k<3;k++) xi[k]=z[k];
+    }
+}
+
+void bildu_downcast(bildu_prec *P)
+{
+    bsr_downcast(P->L);
+    bsr_downcast(P->D);
+    bsr_downcast(P->U);
+}
+
+
+
 void bildu_info(bildu_prec *P)
 {
     bsr_info(P->L);
@@ -534,6 +679,106 @@ void bslv_init(bslv_memory *mem, double tol, int max_iter, bsr_matrix const *A)
 
 }
 
+void bslv_pbicgstab3(bslv_memory *mem, bsr_matrix *A, const double *b, double *x)
+{
+
+    double tol = mem->tol;
+    int max_iter = mem->max_iter;
+    int n = mem->n;
+
+    double * restrict e = mem->e;
+    //const double *r0 = b;
+    const double * restrict r0  = mem->dtmp[0]; //access randomly initialized one-dimensional shadow space
+          double * restrict p_j = mem->dtmp[1];
+          double * restrict q_j = mem->dtmp[2];
+          double * restrict r_j = mem->dtmp[3];
+          double * restrict s_j = mem->dtmp[4];
+          double * restrict t_j = mem->dtmp[5];
+          double * restrict v_j = mem->dtmp[6];
+          double * restrict x_j = x;
+
+    bildu_prec * restrict P = mem->P;
+    bildu_factorize(P,A);
+    bildu_downcast(P);
+
+    vec_fill(x_j,0.0,n);
+    vec_copy(r_j,b,n);
+    vec_copy(p_j,b,n);
+
+    vec_copy(q_j,p_j,n);
+    //double norm_0 = sqrt(vec_inner(r_j,r_j,n));
+    double norm_0 = sqrt(vec_inner2(r_j,r_j,n));
+
+    //double rho_j = vec_inner(r0,r_j,n);
+    double rho_j = vec_inner2(r0,r_j,n);
+    int j;
+    for(j=0;j<max_iter;j++)
+    {
+        //vec_copy(q_j,p_j,n);                                        //q_j=p_j
+        bildu_mapply3c(P,q_j);                                          //q_j=P.q_j;
+        bsr_vmspmv3(A,q_j,v_j);                                        //v_j= A.q_j
+
+        //double alpha_j = rho_j/vec_inner(r0,v_j,n);
+        double alpha_j = rho_j/vec_inner2(r0,v_j,n);
+        for (int k=0;k<n;k++) q_j[k] = s_j[k] = r_j[k]-alpha_j*v_j[k];       // r_j and s_j can overwrite each other
+        //vec_axpy(-alpha_j,v_j,r_j,s_j,n);
+
+        //vec_copy(q_j,s_j,n);                                        //q_j=s_j
+        bildu_mapply3c(P,q_j);                                          //q_j=P.q_j;
+        bsr_vmspmv3(A,q_j,t_j);                                        //t_j= A.q_j
+
+        //double w_j = vec_inner(s_j,t_j,n)/vec_inner(t_j,t_j,n);
+        double w_j = vec_inner2(s_j,t_j,n)/vec_inner2(t_j,t_j,n);
+        for (int k=0;k<n;k++) x_j[k] += alpha_j*p_j[k] + w_j*s_j[k];
+        for (int k=0;k<n;k++) r_j[k]  =         s_j[k] - w_j*t_j[k];
+        //vec_axpy(-w_j,t_j,s_j,r_j,n);
+        //double norm_e = sqrt(vec_inner(r_j,r_j,n));
+        double norm_e = sqrt(vec_inner2(r_j,r_j,n));
+        e[j+1]=norm_e/norm_0;
+
+        if (norm_e<tol*norm_0) break;                               //convergence check
+
+        double rho_0 =rho_j;
+        //rho_j=vec_inner(r0,r_j,n);
+        rho_j=vec_inner2(r0,r_j,n);
+
+        double beta_j = (alpha_j/w_j)*(rho_j/rho_0);
+        for (int k=0;k<n;k++) q_j[k] = p_j[k] = r_j[k] + beta_j*(p_j[k] - w_j*v_j[k]);
+    }
+    bildu_mapply3c(P,x_j);                                       //x_j=P.x_j;
+
+    printf("pbicgstab: iterations=%d residual=%.2e\n",j,e[j+1]);
+
+}
+
+
+
+
+
+
+
+
+
+double __attribute__((noinline)) vec_inner2(const double *a, const double *b, int n)
+{
+    const double *x = __builtin_assume_aligned(a,64);
+    const double *y = __builtin_assume_aligned(b,64);
+
+    int const N=8;
+    double agg[N];
+    for(int i=0;i<N;i++) agg[i]=0.0;
+    for(int i=0;i<n;i+=N)
+    {
+        for(int j=0;j<N;j++) agg[j]+=x[i+j]*y[i+j];
+    }
+    //for(int j=0;j<8;j++) agg[j]+=agg[j+8];
+    for(int j=0;j<4;j++) agg[j]+=agg[j+4];
+    for(int j=0;j<2;j++) agg[j]+=agg[j+2];
+    for(int j=0;j<1;j++) agg[j]+=agg[j+1];
+
+    return agg[0];
+
+}
 
 
 
