fa4a00cb6ac1dc166548e7addb91b759135882bd
Michal Tóth <michal.toth@opm-op.com>
2025-09-10 11:46:05 +0200

    Update getRefDensity description

--- opm/simulators/wells/StandardWell.hpp
+++ opm/simulators/wells/StandardWell.hpp
@@ -321,7 +321,8 @@ namespace Opm
                                            std::vector<Scalar>& well_potentials,
                                            DeferredLogger& deferred_logger) const;               
 
-        // communicate and return the density at the perforation[0] of the rank owning this well
+        // return the density at the perforation[0] of the rank owning this well,
+        // value is cached to minimize the number of broadcasts
         Scalar getRefDensity() const override;
 
         // get the mobility for specific perforation
