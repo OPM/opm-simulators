6b3f14684de0caba7e883f4b5144610093de4769
Tor Harald Sandve <tosa@norceresearch.no>
2025-09-15 08:44:23 +0200

    Dont include temporary stopped wells among the wells considered for group control

--- opm/simulators/wells/WellState.cpp
+++ opm/simulators/wells/WellState.cpp
@@ -1021,16 +1021,19 @@ void WellState<Scalar, IndexTraits>::communicateGroupRates(const Parallel::Commu
 }
 
 template<typename Scalar, typename IndexTraits>
-void WellState<Scalar, IndexTraits>::updateGlobalIsGrup(const Parallel::Communication& comm)
+void WellState<Scalar, IndexTraits>::updateGlobalIsGrup(const Parallel::Communication& comm,  const std::vector<WellStatus>& well_status)
 {
     this->global_well_info.value().clear();
     for (std::size_t well_index = 0; well_index < this->size(); well_index++) {
         const auto& ws = this->well(well_index);
+        // We cannot use the well status directly from the well state here as well, may
+        // be temporarily stopped due to convergence or operability issues
+        const auto& this_well_status = well_status[well_index];
         this->global_well_info.value().update_efficiency_scaling_factor(well_index, ws.efficiency_scaling_factor);
         if (ws.producer)
-            this->global_well_info.value().update_producer(well_index, ws.status, ws.production_cmode);
+            this->global_well_info.value().update_producer(well_index, this_well_status, ws.production_cmode);
         else
-            this->global_well_info.value().update_injector(well_index, ws.status, ws.injection_cmode);
+            this->global_well_info.value().update_injector(well_index, this_well_status, ws.injection_cmode);
     }
     this->global_well_info.value().communicate(comm);
 }
