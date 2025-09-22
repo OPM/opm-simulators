665891b229bb6b2ee29e0bae68d32bd26ccadaf7
kjetil <kjetil@haugenlabs.com>
2025-09-22 12:35:24 -0500

    solve: support for full block ILDU0

--- opm/simulators/linalg/HaugenLabs.h
+++ opm/simulators/linalg/HaugenLabs.h
@@ -35,12 +35,16 @@ struct bildu_prec
     bsr_matrix *L;
     bsr_matrix *D;
     bsr_matrix *U;
+    int noffsets;
+    int(*offsets)[3];
 }
 bildu_prec;
 
 bildu_prec *bildu_new();
 void bildu_init(bildu_prec *P, bsr_matrix const *A);
+int  bildu_analyze(bsr_matrix *M, int (*offsets)[3]);
 void bildu_factorize(bildu_prec *P, bsr_matrix *A);
+void bildu_factorize2(bildu_prec *P, bsr_matrix *A);
 void bildu_apply3(bildu_prec *P, double *x);
 void bildu_apply3c(bildu_prec *P, double *x);
 void bildu_mapply3c(bildu_prec *P, double *x);
