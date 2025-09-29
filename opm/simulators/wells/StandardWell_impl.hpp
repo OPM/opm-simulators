7412db2451d48224e8eb29a0f43dfaa96fac7d2d
Michal Tóth <michal.toth@opm-op.com>
2025-09-29 10:06:58 +0200

    Fix formatting

--- opm/simulators/wells/StandardWell_impl.hpp
+++ opm/simulators/wells/StandardWell_impl.hpp
@@ -1346,7 +1346,7 @@ namespace Opm
                                prop_func, props, deferred_logger);
         // density was updated
         cachedRefDensity = this->connections_.rho(0);
-        if (this->parallel_well_info_.communication().size()>1) {
+        if (this->parallel_well_info_.communication().size() > 1) {
             cachedRefDensity = this->parallel_well_info_.broadcastFirstPerforationValue(cachedRefDensity);
         }
     }
@@ -2248,8 +2248,9 @@ namespace Opm
             max_pressure = std::max(max_pressure, pressure_cell);
         }
         const auto& comm = this->parallel_well_info_.communication();
-        if (comm.size()>1)
+        if (comm.size() > 1) {
             max_pressure = comm.max(max_pressure);
+        }
         auto bhpAtLimit = WellBhpThpCalculator(*this).computeBhpAtThpLimitProd(frates,
                                                                                summary_state,
                                                                                max_pressure,
