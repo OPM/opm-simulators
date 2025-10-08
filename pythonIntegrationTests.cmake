dc0cf9589311d3d0912e47b446253bbbecbc475c
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2025-10-08 14:44:42 +0200

    move HybridNewton to a python integration test

---
+++ pythonIntegrationTests.cmake
@@ -0,0 +1,20 @@
+file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/tests/integration/HybridNewton)
+
+add_test(NAME
+          HybridNewton
+         COMMAND
+          pytest ${PROJECT_SOURCE_DIR}/python/integration_tests/ml/hybrid_newton
+        WORKING_DIRECTORY
+          ${PROJECT_BINARY_DIR}/tests/integration/HybridNewton
+  )
+if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.22)
+  set(modifications
+      "PYTHONPATH=path_list_append:${opm-common_DIR}/python"
+      "PYTHONPATH=path_list_append:${CMAKE_CURRENT_BINARY_DIR}/python"
+      "FLOW_BINARY=set:$<TARGET_FILE:flow>"
+  )
+  set_tests_properties(HybridNewton PROPERTIES
+                       ENVIRONMENT_MODIFICATION
+                         "${modifications}"
+  )
+endif()
