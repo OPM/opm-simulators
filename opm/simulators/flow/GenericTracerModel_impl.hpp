fe81bd3a1a37a1fa31a572f2addc4fde2b565fdb
David Landa Marban <dmar@norceresearch.no>
2025-10-14 19:27:32 +0200

    UDQ support for WTRACER

--- opm/simulators/flow/GenericTracerModel_impl.hpp
+++ opm/simulators/flow/GenericTracerModel_impl.hpp
@@ -208,9 +208,9 @@ enableSolTracers() const
 
 template<class Grid, class GridView, class DofMapper, class Stencil, class FluidSystem, class Scalar>
 Scalar GenericTracerModel<Grid,GridView,DofMapper,Stencil,FluidSystem,Scalar>::
-currentConcentration_(const Well& eclWell, const std::string& name) const
+currentConcentration_(const Well& eclWell, const std::string& name, const SummaryState& summaryState) const
 {
-    return eclWell.getTracerProperties().getConcentration(name);
+    return eclWell.getTracerProperties().getConcentration(eclWell.name(), name, summaryState);
 }
 
 template<class Grid, class GridView, class DofMapper, class Stencil, class FluidSystem, class Scalar>
