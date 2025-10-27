fae43ceee0db77abcc867c245a99cf45a4119b61
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2025-10-27 11:14:40 +0100

    changed: move generic network functionality to a separate class
    and model as has-a in BlackoilWellModelGeneric

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -206,6 +206,7 @@ list (APPEND MAIN_SOURCE_FILES
   opm/simulators/wells/BlackoilWellModelGasLift.cpp
   opm/simulators/wells/BlackoilWellModelGeneric.cpp
   opm/simulators/wells/BlackoilWellModelGuideRates.cpp
+  opm/simulators/wells/BlackoilWellModelNetworkGeneric.cpp
   opm/simulators/wells/BlackoilWellModelNldd.cpp
   opm/simulators/wells/BlackoilWellModelRestart.cpp
   opm/simulators/wells/BlackoilWellModelWBP.cpp
@@ -1121,6 +1122,7 @@ list (APPEND PUBLIC_HEADER_FILES
   opm/simulators/wells/BlackoilWellModelGasLift_impl.hpp
   opm/simulators/wells/BlackoilWellModelGeneric.hpp
   opm/simulators/wells/BlackoilWellModelGuideRates.hpp
+  opm/simulators/wells/BlackoilWellModelNetworkGeneric.hpp
   opm/simulators/wells/BlackoilWellModelNldd.hpp
   opm/simulators/wells/BlackoilWellModelNldd_impl.hpp
   opm/simulators/wells/BlackoilWellModelRestart.hpp
---
+++ opm/simulators/wells/BlackoilWellModelNetworkGeneric.hpp
@@ -0,0 +1,106 @@
+/*
+  Copyright 2016 SINTEF ICT, Applied Mathematics.
+  Copyright 2016 - 2017 Statoil ASA.
+  Copyright 2017 Dr. Blatt - HPC-Simulation-Software & Services
+  Copyright 2016 - 2018 IRIS AS
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef OPM_BLACKOILWELLMODEL_NETWORK_GENERIC_HEADER_INCLUDED
+#define OPM_BLACKOILWELLMODEL_NETWORK_GENERIC_HEADER_INCLUDED
+
+#include <opm/output/data/Groups.hpp>
+#include <opm/simulators/utils/ParallelCommunication.hpp>
+
+#include <map>
+#include <optional>
+#include <string>
+
+namespace Opm {
+    class Schedule;
+    template<class Scalar, class IndexTraits> class BlackoilWellModelGeneric;
+}
+
+namespace Opm {
+
+/// Class for handling the blackoil well network model.
+template<typename Scalar, typename IndexTraits>
+class BlackoilWellModelNetworkGeneric
+{
+public:
+    BlackoilWellModelNetworkGeneric(BlackoilWellModelGeneric<Scalar,IndexTraits>& well_model,
+                                    const std::optional<std::map<std::string, double>>& restart_pressures);
+
+    virtual ~BlackoilWellModelNetworkGeneric() = default;
+
+    /// return true if network is active (at least one network well in prediction mode)
+    bool active() const
+    { return active_; }
+
+    const std::map<std::string, Scalar>&
+    nodePressures() const { return node_pressures_; }
+
+    // do not use, only needed for serialization testing
+    void setNodePressures(const std::map<std::string, Scalar>& values)
+    { node_pressures_ = values; }
+
+    /// Checks if network is active (at least one network well on prediction).
+    void updateActiveState(const int report_step);
+
+    /// Checks if there are reasons to perform a pre-step network re-balance.
+    /// (Currently, the only reasons are network well status changes.)
+    /// (TODO: Consider if adding network change events would be helpful.)
+    bool needPreStepRebalance(const int report_step) const;
+
+    bool shouldBalance(const int reportStepIndex,
+                       const int iterationIdx) const;
+
+    Scalar updatePressures(const int reportStepIdx,
+                           const Scalar damping_factor,
+                           const Scalar update_upper_bound);
+
+    void assignNodeValues(std::map<std::string, data::NodeData>& nodevalues,
+                          const int reportStepIdx) const;
+
+    void commitState()
+    { this->last_valid_node_pressures_ = this->node_pressures_; }
+
+    void resetState()
+    { this->node_pressures_ = this->last_valid_node_pressures_; }
+
+    template<class Serializer>
+    void serializeOp(Serializer& serializer)
+    {
+        serializer(node_pressures_);
+        serializer(last_valid_node_pressures_);
+    }
+
+    bool operator==(const BlackoilWellModelNetworkGeneric<Scalar,IndexTraits>& rhs) const;
+
+protected:
+    bool active_{false};
+    BlackoilWellModelGeneric<Scalar,IndexTraits>& well_model_;
+
+    // Network pressures for output and initialization
+    std::map<std::string, Scalar> node_pressures_;
+    // Valid network pressures for output and initialization for safe restart after failed iterations
+    std::map<std::string, Scalar> last_valid_node_pressures_;
+};
+
+} // namespace Opm
+
+#endif
