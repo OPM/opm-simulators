4a514588c692269e4921797a1d5e786f8dacec0b
Kjetil Olsen Lye <kjetil.olsen.lye@sintef.no>
2025-11-04 12:37:40 +0000

    fixed license header

--- opm/models/blackoil/blackoilmeanings.hh
+++ opm/models/blackoil/blackoilmeanings.hh
@@ -1,8 +1,10 @@
 /*
+  Copyright 2025 Equinor ASA
+
   This file is part of the Open Porous Media project (OPM).
   OPM is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 2 of the License, or
+  the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
   OPM is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -10,9 +12,6 @@
   GNU General Public License for more details.
   You should have received a copy of the GNU General Public License
   along with OPM.  If not, see <http://www.gnu.org/licenses/>.
-  Consult the COPYING file in the top-level source directory of this
-  module for the precise wording of the license and the list of
-  copyright holders.
 */
 #ifndef OPM_BLACK_OIL_MEANINGS_HH
 #define OPM_BLACK_OIL_MEANINGS_HH
