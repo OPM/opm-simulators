hakonhagland/opm-simulators-1

Refactor TargetCalculator
Refactor TargetCalculator::groupTarget() to
WellGroupHelper::getProductionGroupTarget(), and
don't pass Group::ProductionControls as a parameter. Since
TargetCalculator only works for a given group, passing control as a
parameter is confusing and can be error prone if being passed a control
for a different group than the TargetCalculator was constructed for.

opm/simulators/wells/BlackoilWellModel_impl.hpp
@@ -514,9 +514,9 @@ namespace Opm {
                 if (event || dyn_status_change) {
                     try {
                         well->scaleSegmentRatesAndPressure(this->wellState());
-                        well->calculateExplicitQuantities(simulator_, this->wellState(), local_deferredLogger);
+                        well->calculateExplicitQuantities(simulator_, this->wgHelper(), local_deferredLogger);
                         well->updateWellStateWithTarget(simulator_, this->wgHelper(), this->wellState(), local_deferredLogger);
-                        well->updatePrimaryVariables(simulator_, this->wellState(), local_deferredLogger);
+                        well->updatePrimaryVariables(this->wgHelper(), local_deferredLogger);
                         well->solveWellEquation(
                             simulator_, this->wgHelper(), this->wellState(), local_deferredLogger
                         );
@@ -1169,7 +1169,7 @@ namespace Opm {
 
         OPM_BEGIN_PARALLEL_TRY_CATCH();
         for (auto& well : this->well_container_) {
-            well->solveEqAndUpdateWellState(simulator_, well_state, deferred_logger);
+            well->solveEqAndUpdateWellState(simulator_, this->wgHelper(), well_state, deferred_logger);
         }
         OPM_END_PARALLEL_TRY_CATCH("BlackoilWellModel::doPreStepNetworkRebalance() failed: ",
                                     this->simulator_.vanguard().grid().comm());
@@ -1403,9 +1403,6 @@ namespace Opm {
 
                 //TODO: Auto choke combined with RESV control is not supported
                 std::vector<Scalar> resv_coeff(Indices::numPhases, 1.0);
-                Scalar gratTargetFromSales = 0.0;
-                if (group_state.has_grat_sales_target(group.name()))
-                    gratTargetFromSales = group_state.grat_sales_target(group.name());
 
                 const auto ctrl = group.productionControls(summary_state);
                 auto cmode_tmp = ctrl.cmode;
@@ -1418,7 +1415,7 @@ namespace Opm {
                     const Scalar efficiencyFactor = 1.0;
                     const Group& parentGroup = this->schedule().getGroup(group.parent(), reportStepIdx);
                     auto target = WellGroupControls<Scalar, IndexTraits>::getAutoChokeGroupProductionTargetRate(
-                                                            group.name(),
+                                                            group,
                                                             parentGroup,
                                                             this->wgHelper(),
                                                             this->schedule(),
@@ -1431,15 +1428,12 @@ namespace Opm {
                     target_tmp = target.first;
                     cmode_tmp = target.second;
                 }
-                const auto cmode = cmode_tmp;
                 using TargetCalculatorType =  WGHelpers::TargetCalculator<Scalar, IndexTraits>;
-                TargetCalculatorType tcalc(cmode, FluidSystem::phaseUsage(), resv_coeff,
-                                           gratTargetFromSales, nodeName, group_state,
-                                           group.has_gpmaint_control(cmode));
+                TargetCalculatorType tcalc{this->wgHelper(), resv_coeff, group};
                 if (!fld_none)
                 {
                     // Target is set for the autochoke group itself
-                    target_tmp = tcalc.groupTarget(ctrl, local_deferredLogger);
+                    target_tmp = tcalc.groupTarget(local_deferredLogger);
                 }
 
                 const Scalar orig_target = target_tmp;
@@ -1599,8 +1593,7 @@ namespace Opm {
         OPM_BEGIN_PARALLEL_TRY_CATCH();
 
         for (auto& well: well_container_) {
-            well->assembleWellEqWithoutIteration(simulator_, dt, this->wellState(), this->groupState(),
-                                                 deferred_logger);
+            well->assembleWellEqWithoutIteration(simulator_, dt, this->wgHelper(), this->wellState(), deferred_logger);
         }
         OPM_END_PARALLEL_TRY_CATCH_LOG(deferred_logger, "BlackoilWellModel::assembleWellEqWithoutIteration failed: ",
                                        this->terminal_output_, grid().comm());
@@ -1773,7 +1766,7 @@ namespace Opm {
                     x_local_[i] = x[cells[i]];
                 }
                 well->recoverWellSolutionAndUpdateWellState(simulator_, x_local_,
-                                                            this->wellState(), local_deferredLogger);
+                                                            this->wgHelper(), this->wellState(), local_deferredLogger);
             }
         }
         OPM_END_PARALLEL_TRY_CATCH_LOG(local_deferredLogger,
@@ -1808,7 +1801,7 @@ namespace Opm {
         for (const auto& well : well_container_) {
             if (well->isOperableAndSolvable() || well->wellIsStopped()) {
                 local_report += well->getWellConvergence(
-                        simulator_, this->wellState(), B_avg, local_deferredLogger,
+                        this->wgHelper(), B_avg, local_deferredLogger,
                         iterationIdx > param_.strict_outer_iter_wells_);
             } else {
                 ConvergenceReport report;
@@ -1854,7 +1847,7 @@ namespace Opm {
     {
         // TODO: checking isOperableAndSolvable() ?
         for (auto& well : well_container_) {
-            well->calculateExplicitQuantities(simulator_, this->wellState(), deferred_logger);
+            well->calculateExplicitQuantities(simulator_, this->wgHelper(), deferred_logger);
         }
     }
 
@@ -2097,9 +2090,7 @@ namespace Opm {
                                        this->wgHelper(),
                                        local_deferredLogger);
             const bool under_zero_target =
-                well->wellUnderZeroGroupRateTarget(this->simulator_,
-                                                   this->wellState(),
-                                                   local_deferredLogger);
+                well->wellUnderZeroGroupRateTarget(this->wgHelper(), local_deferredLogger);
             well->updateWellTestState(this->wellState().well(wname),
                                       simulationTime,
                                       /*writeMessageToOPMLog=*/ true,
@@ -2276,7 +2267,7 @@ namespace Opm {
                 well->updateWellStateWithTarget(
                     simulator_, this->wgHelper(), this->wellState(), deferred_logger
                 );
-                well->updatePrimaryVariables(simulator_, this->wellState(), deferred_logger);
+                well->updatePrimaryVariables(this->wgHelper(), deferred_logger);
                 // There is no new well control change input within a report step,
                 // so next time step, the well does not consider to have effective events anymore.
                 events.clearEvent(WellState<Scalar, IndexTraits>::event_mask);
@@ -2360,7 +2351,7 @@ namespace Opm {
     updatePrimaryVariables(DeferredLogger& deferred_logger)
     {
         for (const auto& well : well_container_) {
-            well->updatePrimaryVariables(simulator_, this->wellState(), deferred_logger);
+            well->updatePrimaryVariables(this->wgHelper(), deferred_logger);
         }
     }
 

opm/simulators/wells/WellInterface.hpp
@@ -157,13 +157,13 @@ public:
                       const std::vector<Scalar>& B_avg,
                       const bool changed_to_open_this_step);
 
-    virtual ConvergenceReport getWellConvergence(const Simulator& simulator,
-                                                 const WellStateType& well_state,
+    virtual ConvergenceReport getWellConvergence(const WellGroupHelperType& wgHelper,
                                                  const std::vector<Scalar>& B_avg,
                                                  DeferredLogger& deferred_logger,
                                                  const bool relax_tolerance) const = 0;
 
     virtual void solveEqAndUpdateWellState(const Simulator& simulator,
+                                           const WellGroupHelperType& wgHelper,
                                            WellStateType& well_state,
                                            DeferredLogger& deferred_logger) = 0;
 
@@ -175,8 +175,8 @@ public:
 
     void assembleWellEqWithoutIteration(const Simulator& simulator,
                                         const double dt,
+                                        const WellGroupHelperType& wgHelper,
                                         WellStateType& well_state,
-                                        const GroupState<Scalar>& group_state,
                                         DeferredLogger& deferred_logger);
 
     // TODO: better name or further refactoring the function to make it more clear
@@ -203,6 +203,7 @@ public:
     /// xw to update Well State
     virtual void recoverWellSolutionAndUpdateWellState(const Simulator& simulator,
                                                        const BVector& x,
+                                                       const WellGroupHelperType& wgHelper,
                                                        WellStateType& well_state,
                                                        DeferredLogger& deferred_logger) = 0;
 
@@ -232,23 +233,20 @@ public:
                                                    std::vector<Scalar>& well_flux,
                                                    DeferredLogger& deferred_logger) const = 0;
 
-    bool wellUnderZeroRateTarget(const Simulator& simulator,
-                                 const WellStateType& well_state,
+    bool wellUnderZeroRateTarget(const WellGroupHelperType& wgHelper,
                                  DeferredLogger& deferred_logger) const;
 
-    bool wellUnderZeroGroupRateTarget(const Simulator& simulator,
-                                      const WellStateType& well_state,
-                                      DeferredLogger& deferred_logger,
-                                      std::optional<bool> group_control = std::nullopt) const;
-
-    bool stoppedOrZeroRateTarget(const Simulator& simulator,
-                                 const WellStateType& well_state,
+    bool stoppedOrZeroRateTarget(const WellGroupHelperType& wgHelper,
                                  DeferredLogger& deferred_logger) const;
 
     bool updateWellStateWithTHPTargetProd(const Simulator& simulator,
                                           WellStateType& well_state,
                                           const WellGroupHelperType& wgHelper,
                                           DeferredLogger& deferred_logger) const;
+
+    bool wellUnderZeroGroupRateTarget(const WellGroupHelperType& wgHelper,
+                                      DeferredLogger& deferred_logger,
+                                      const std::optional<bool> group_control = std::nullopt) const;
 
     enum class IndividualOrGroup { Individual, Group, Both };
     bool updateWellControl(const Simulator& simulator,
@@ -267,12 +265,11 @@ public:
                                                   const bool fixed_control = false,
                                                   const bool fixed_status = false);
 
-    virtual void updatePrimaryVariables(const Simulator& simulator,
-                                        const WellStateType& well_state,
+    virtual void updatePrimaryVariables(const WellGroupHelperType& wgHelper,
                                         DeferredLogger& deferred_logger) = 0;
 
     virtual void calculateExplicitQuantities(const Simulator& simulator,
-                                             const WellStateType& well_state,
+                                             const WellGroupHelperType& wgHelper,
                                              DeferredLogger& deferred_logger) = 0; // should be const?
 
     virtual void updateProductivityIndex(const Simulator& simulator,
@@ -407,8 +404,8 @@ protected:
                                                 const double dt,
                                                 const WellInjectionControls& inj_controls,
                                                 const WellProductionControls& prod_controls,
+                                                const WellGroupHelperType& wgHelper,
                                                 WellStateType& well_state,
-                                                const GroupState<Scalar>& group_state,
                                                 DeferredLogger& deferred_logger) = 0;
 
     // iterate well equations with the specified control until converged
@@ -422,6 +419,7 @@ protected:
 
     virtual void updateIPRImplicit(const Simulator& simulator,
                                    WellStateType& well_state,
+                                   const WellGroupHelperType& wgHelper,
                                    DeferredLogger& deferred_logger) = 0;
 
     bool iterateWellEquations(const Simulator& simulator,

OPM/opm-simulators

opm/simulators/wells/BlackoilWellModel_impl.hpp
@@ -513,9 +513,9 @@ namespace Opm {
                 if (event || dyn_status_change) {
                     try {
                         well->scaleSegmentRatesAndPressure(this->wellState());
-                        well->calculateExplicitQuantities(simulator_, this->wellState(), local_deferredLogger);
+                        well->calculateExplicitQuantities(simulator_, this->groupStateHelper(), local_deferredLogger);
                         well->updateWellStateWithTarget(simulator_, this->groupStateHelper(), this->wellState(), local_deferredLogger);
-                        well->updatePrimaryVariables(simulator_, this->wellState(), local_deferredLogger);
+                        well->updatePrimaryVariables(this->groupStateHelper(), local_deferredLogger);
                         well->solveWellEquation(
                             simulator_, this->groupStateHelper(), this->wellState(), local_deferredLogger
                         );
@@ -1168,7 +1168,7 @@ namespace Opm {
 
         OPM_BEGIN_PARALLEL_TRY_CATCH();
         for (auto& well : this->well_container_) {
-            well->solveEqAndUpdateWellState(simulator_, well_state, deferred_logger);
+            well->solveEqAndUpdateWellState(simulator_, this->groupStateHelper(), well_state, deferred_logger);
         }
         OPM_END_PARALLEL_TRY_CATCH("BlackoilWellModel::doPreStepNetworkRebalance() failed: ",
                                     this->simulator_.vanguard().grid().comm());
@@ -1402,9 +1402,6 @@ namespace Opm {
 
                 //TODO: Auto choke combined with RESV control is not supported
                 std::vector<Scalar> resv_coeff(Indices::numPhases, 1.0);
-                Scalar gratTargetFromSales = 0.0;
-                if (group_state.has_grat_sales_target(group.name()))
-                    gratTargetFromSales = group_state.grat_sales_target(group.name());
 
                 const auto ctrl = group.productionControls(summary_state);
                 auto cmode_tmp = ctrl.cmode;
@@ -1417,7 +1414,7 @@ namespace Opm {
                     const Scalar efficiencyFactor = 1.0;
                     const Group& parentGroup = this->schedule().getGroup(group.parent(), reportStepIdx);
                     auto target = WellGroupControls<Scalar, IndexTraits>::getAutoChokeGroupProductionTargetRate(
-                                                            group.name(),
+                                                            group,
                                                             parentGroup,
                                                             this->groupStateHelper(),
                                                             this->schedule(),
@@ -1430,16 +1427,12 @@ namespace Opm {
                     target_tmp = target.first;
                     cmode_tmp = target.second;
                 }
-                const auto cmode = cmode_tmp;
-                using TargetCalculatorType =  GroupStateHelpers
-::TargetCalculator<Scalar, IndexTraits>;
-                TargetCalculatorType tcalc(cmode, FluidSystem::phaseUsage(), resv_coeff,
-                                           gratTargetFromSales, nodeName, group_state,
-                                           group.has_gpmaint_control(cmode));
+                using TargetCalculatorType =  GroupStateHelpers::TargetCalculator<Scalar, IndexTraits>;
+                TargetCalculatorType tcalc{this->groupStateHelper(), resv_coeff, group};
                 if (!fld_none)
                 {
                     // Target is set for the autochoke group itself
-                    target_tmp = tcalc.groupTarget(ctrl, local_deferredLogger);
+                    target_tmp = tcalc.groupTarget(local_deferredLogger);
                 }
 
                 const Scalar orig_target = target_tmp;
@@ -1777,7 +1770,7 @@ namespace Opm {
                     x_local_[i] = x[cells[i]];
                 }
                 well->recoverWellSolutionAndUpdateWellState(simulator_, x_local_,
-                                                            this->wellState(), local_deferredLogger);
+                                                            this->groupStateHelper(), this->wellState(), local_deferredLogger);
             }
         }
         OPM_END_PARALLEL_TRY_CATCH_LOG(local_deferredLogger,
@@ -1812,7 +1805,7 @@ namespace Opm {
         for (const auto& well : well_container_) {
             if (well->isOperableAndSolvable() || well->wellIsStopped()) {
                 local_report += well->getWellConvergence(
-                        simulator_, this->wellState(), B_avg, local_deferredLogger,
+                        this->groupStateHelper(), B_avg, local_deferredLogger,
                         iterationIdx > param_.strict_outer_iter_wells_);
             } else {
                 ConvergenceReport report;
@@ -1858,7 +1851,7 @@ namespace Opm {
     {
         // TODO: checking isOperableAndSolvable() ?
         for (auto& well : well_container_) {
-            well->calculateExplicitQuantities(simulator_, this->wellState(), deferred_logger);
+            well->calculateExplicitQuantities(simulator_, this->groupStateHelper(), deferred_logger);
         }
     }
 
@@ -2101,9 +2094,7 @@ namespace Opm {
                                        this->groupStateHelper(),
                                        local_deferredLogger);
             const bool under_zero_target =
-                well->wellUnderZeroGroupRateTarget(this->simulator_,
-                                                   this->wellState(),
-                                                   local_deferredLogger);
+                well->wellUnderZeroGroupRateTarget(this->groupStateHelper(), local_deferredLogger);
             well->updateWellTestState(this->wellState().well(wname),
                                       simulationTime,
                                       /*writeMessageToOPMLog=*/ true,
@@ -2280,7 +2271,7 @@ namespace Opm {
                 well->updateWellStateWithTarget(
                     simulator_, this->groupStateHelper(), this->wellState(), deferred_logger
                 );
-                well->updatePrimaryVariables(simulator_, this->wellState(), deferred_logger);
+                well->updatePrimaryVariables(this->groupStateHelper(), deferred_logger);
                 // There is no new well control change input within a report step,
                 // so next time step, the well does not consider to have effective events anymore.
                 events.clearEvent(WellState<Scalar, IndexTraits>::event_mask);
@@ -2364,7 +2355,7 @@ namespace Opm {
     updatePrimaryVariables(DeferredLogger& deferred_logger)
     {
         for (const auto& well : well_container_) {
-            well->updatePrimaryVariables(simulator_, this->wellState(), deferred_logger);
+            well->updatePrimaryVariables(this->groupStateHelper(), deferred_logger);
         }
     }
 

opm/simulators/wells/WellInterface.hpp
@@ -157,13 +157,13 @@ public:
                       const std::vector<Scalar>& B_avg,
                       const bool changed_to_open_this_step);
 
-    virtual ConvergenceReport getWellConvergence(const Simulator& simulator,
-                                                 const WellStateType& well_state,
+    virtual ConvergenceReport getWellConvergence(const GroupStateHelperType& groupStateHelper,
                                                  const std::vector<Scalar>& B_avg,
                                                  DeferredLogger& deferred_logger,
                                                  const bool relax_tolerance) const = 0;
 
     virtual void solveEqAndUpdateWellState(const Simulator& simulator,
+                                           const GroupStateHelperType& groupStateHelper,
                                            WellStateType& well_state,
                                            DeferredLogger& deferred_logger) = 0;
 
@@ -204,6 +204,7 @@ public:
     /// xw to update Well State
     virtual void recoverWellSolutionAndUpdateWellState(const Simulator& simulator,
                                                        const BVector& x,
+                                                       const GroupStateHelperType& groupStateHelper,
                                                        WellStateType& well_state,
                                                        DeferredLogger& deferred_logger) = 0;
 
@@ -233,23 +234,20 @@ public:
                                                    std::vector<Scalar>& well_flux,
                                                    DeferredLogger& deferred_logger) const = 0;
 
-    bool wellUnderZeroRateTarget(const Simulator& simulator,
-                                 const WellStateType& well_state,
+    bool wellUnderZeroRateTarget(const GroupStateHelperType& groupStateHelper,
                                  DeferredLogger& deferred_logger) const;
 
-    bool wellUnderZeroGroupRateTarget(const Simulator& simulator,
-                                      const WellStateType& well_state,
-                                      DeferredLogger& deferred_logger,
-                                      std::optional<bool> group_control = std::nullopt) const;
-
-    bool stoppedOrZeroRateTarget(const Simulator& simulator,
-                                 const WellStateType& well_state,
+    bool stoppedOrZeroRateTarget(const GroupStateHelperType& groupStateHelper,
                                  DeferredLogger& deferred_logger) const;
 
     bool updateWellStateWithTHPTargetProd(const Simulator& simulator,
                                           WellStateType& well_state,
                                           const GroupStateHelperType& groupStateHelper,
                                           DeferredLogger& deferred_logger) const;
+
+    bool wellUnderZeroGroupRateTarget(const GroupStateHelperType& groupStateHelper,
+                                      DeferredLogger& deferred_logger,
+                                      const std::optional<bool> group_control = std::nullopt) const;
 
     enum class IndividualOrGroup { Individual, Group, Both };
     bool updateWellControl(const Simulator& simulator,
@@ -269,12 +267,11 @@ public:
                                                   const bool fixed_status,
                                                   const bool solving_with_zero_rate);
 
-    virtual void updatePrimaryVariables(const Simulator& simulator,
-                                        const WellStateType& well_state,
+    virtual void updatePrimaryVariables(const GroupStateHelperType& groupStateHelper,
                                         DeferredLogger& deferred_logger) = 0;
 
     virtual void calculateExplicitQuantities(const Simulator& simulator,
-                                             const WellStateType& well_state,
+                                             const GroupStateHelperType& groupStateHelper,
                                              DeferredLogger& deferred_logger) = 0; // should be const?
 
     virtual void updateProductivityIndex(const Simulator& simulator,