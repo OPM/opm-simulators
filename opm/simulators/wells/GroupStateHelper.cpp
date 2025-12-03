7e6527e6763b29ad5c921a196f8b4d2e16eb2596
Kai Bao <kai.bao@sintef.no>
2025-12-03 11:14:02 +0100

    introducing GroupTarget struct for SingleWellState
    so it can record the group name that results the group target, the group
    control mode (either injecting or producing) of the group, and also the
    target value enforced to this well.

--- opm/simulators/wells/GroupStateHelper.cpp
+++ opm/simulators/wells/GroupStateHelper.cpp
@@ -198,7 +198,8 @@ GroupStateHelper<Scalar, IndexTraits>::checkGroupConstraintsInj(const std::strin
     if (this->schedule_.hasWell(name)
         && this->wellState().well(name).group_target) { // for wells we already have computed the target
         Scalar scale = 1.0;
-        const Scalar group_target_rate_available = *this->wellState().well(name).group_target;
+        const auto& group_target = this->wellState().well(name).group_target;
+        const Scalar group_target_rate_available = group_target->target_value;
         const Scalar current_well_rate_available
             = tcalc.calcModeRateFromRates(rates); // Switch sign since 'rates' are negative for producers.
         if (current_well_rate_available > 1e-12) {
@@ -371,7 +372,8 @@ GroupStateHelper<Scalar, IndexTraits>::checkGroupConstraintsProd(const std::stri
 
         // Switch sign since 'rates' are negative for producers.
         const Scalar current_well_rate_available = -tcalc.calcModeRateFromRates(rates);
-        const Scalar group_target_rate_available = *this->wellState().well(name).group_target;
+        const auto& group_target = this->wellState().well(name).group_target;
+        const Scalar group_target_rate_available = group_target->target_value;
         Scalar scale = 1.0;
         if (current_well_rate_available > 1e-12) {
             scale = group_target_rate_available / current_well_rate_available;
@@ -483,15 +485,15 @@ GroupStateHelper<Scalar, IndexTraits>::getProductionGroupRateVector(const std::s
 }
 
 template <typename Scalar, typename IndexTraits>
-std::optional<Scalar>
+std::optional<typename SingleWellState<Scalar, IndexTraits>::GroupTarget>
 GroupStateHelper<Scalar, IndexTraits>::getWellGroupTargetInjector(const std::string& name,
-                                                                 const std::string& parent,
-                                                                 const Group& group,
-                                                                 const Scalar* rates,
-                                                                 Phase injection_phase,
-                                                                 const Scalar efficiency_factor,
-                                                                 const std::vector<Scalar>& resv_coeff,
-                                                                 DeferredLogger& deferred_logger) const
+                                                                  const std::string& parent,
+                                                                  const Group& group,
+                                                                  const Scalar* rates,
+                                                                  Phase injection_phase,
+                                                                  const Scalar efficiency_factor,
+                                                                  const std::vector<Scalar>& resv_coeff,
+                                                                  DeferredLogger& deferred_logger) const
 {
     // This function computes a wells group target.
     // 'parent' will be the name of 'group'. But if we recurse, 'name' and
@@ -612,19 +614,20 @@ GroupStateHelper<Scalar, IndexTraits>::getWellGroupTargetInjector(const std::str
             target *= local_fraction_lambda(chain[ii + 1], chain[ii + 1]);
         }
     }
-    // Avoid negative target rates comming from too large local reductions.
-    return std::max(Scalar(0.0), target / efficiency_factor);
+    // Avoid negative target rates coming from too large local reductions.
+    const auto target_value = std::max(Scalar(0.0), target / efficiency_factor);
+    return GroupTarget::injectionGroupTarget(group.name(), current_group_control, target_value);
 }
 
 template <typename Scalar, typename IndexTraits>
-std::optional<Scalar>
+std::optional<typename SingleWellState<Scalar, IndexTraits>::GroupTarget>
 GroupStateHelper<Scalar, IndexTraits>::getWellGroupTargetProducer(const std::string& name,
-                                                                 const std::string& parent,
-                                                                 const Group& group,
-                                                                 const Scalar* rates,
-                                                                 const Scalar efficiency_factor,
-                                                                 const std::vector<Scalar>& resv_coeff,
-                                                                 DeferredLogger& deferred_logger) const
+                                                                  const std::string& parent,
+                                                                  const Group& group,
+                                                                  const Scalar* rates,
+                                                                  const Scalar efficiency_factor,
+                                                                  const std::vector<Scalar>& resv_coeff,
+                                                                  DeferredLogger& deferred_logger) const
 {
     // This function computes a wells group target.
     // 'parent' will be the name of 'group'. But if we recurse, 'name' and
@@ -741,7 +744,8 @@ GroupStateHelper<Scalar, IndexTraits>::getWellGroupTargetProducer(const std::str
         }
     }
     // Avoid negative target rates coming from too large local reductions.
-    return std::max(Scalar(0.0), target / efficiency_factor);
+    const auto target_value = std::max(Scalar(0.0), target / efficiency_factor);
+    return GroupTarget::productionGroupTarget(group.name(), current_group_control, target_value);
 }
 
 template <typename Scalar, typename IndexTraits>
