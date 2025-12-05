38e23b82bd69e8920f1a4d08c8f262cd9268f76b
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2025-12-05 11:26:52 +0100

    failure_report: add parallel execution

--- tests/make_failure_report.sh
+++ tests/make_failure_report.sh
@@ -6,6 +6,7 @@ OPM_TESTS_ROOT=$1
 BUILD_DIR=$2
 RESULT_DIR=$3
 SOURCE_DIR=`dirname "$0"`
+JOBS=${BUILDTHREADS:-16}
 
 FAILED_TESTS=`cat $BUILD_DIR/Testing/Temporary/LastTestsFailed*.log`
 
@@ -13,6 +14,7 @@ mkdir -p $BUILD_DIR/failure_report
 cd $BUILD_DIR/failure_report
 rm -f *
 
+JOBLIST=""
 for failed_test in $FAILED_TESTS
 do
   if grep -q -E "compareECLFiles" <<< $failed_test
@@ -23,8 +25,7 @@ do
     dir_name=$(awk -v search="set_tests_properties\\\($failed_test\$" -v prop="DIRNAME" -f ${SOURCE_DIR}/getprop.awk $RESULT_DIR/CTestTestfile.cmake)
     file_name=$(awk -v search="set_tests_properties\\\($failed_test\$" -v prop="FILENAME" -f ${SOURCE_DIR}/getprop.awk $RESULT_DIR/CTestTestfile.cmake)
     test_name=$(awk -v search="set_tests_properties\\\($failed_test\$" -v prop="TESTNAME" -f ${SOURCE_DIR}/getprop.awk $RESULT_DIR/CTestTestfile.cmake)
-    echo "Processing ${test_name}"
-    $SOURCE_DIR/plot_well_comparison.py -r $OPM_TESTS_ROOT/$dir_name/opm-simulation-reference/$binary/$file_name -s $RESULT_DIR/tests/results/$binary+$test_name/$file_name -c $test_name -o plot
+    JOBLIST+="-r $OPM_TESTS_ROOT/$dir_name/opm-simulation-reference/$binary/$file_name -s $RESULT_DIR/tests/results/$binary+$test_name/$file_name -c $test_name -o plot\\n"
   elif grep -q -E "compareSeparateECLFiles" <<< $failed_test
   then
     failed_test=`echo $failed_test | sed -e 's/.*://g' -e 's/\+/./g'`
@@ -34,10 +35,11 @@ do
     file_name1=$(awk -v search="set_tests_properties\\\($failed_test\$" -v prop="FILENAME1" -f ${SOURCE_DIR}/getprop.awk $RESULT_DIR/CTestTestfile.cmake)
     file_name2=$(awk -v search="set_tests_properties\\\($failed_test\$" -v prop="FILENAME2" -f ${SOURCE_DIR}/getprop.awk $RESULT_DIR/CTestTestfile.cmake)
     test_name=$(awk -v search="set_tests_properties\\\($failed_test\$" -v prop="TESTNAME" -f ${SOURCE_DIR}/getprop.awk $RESULT_DIR/CTestTestfile.cmake)
-    echo "Processing ${test_name}"
-    $SOURCE_DIR/plot_well_comparison.py -r $RESULT_DIR/tests/results/$binary+$test_name/$file_name1 -s $RESULT_DIR/tests/results/$binary+$test_name/$file_name2 -c $test_name -o plot -t "$file_name1" -u "$file_name2"
+    JOBLIST+="-r $RESULT_DIR/tests/results/$binary+$test_name/$file_name1 -s $RESULT_DIR/tests/results/$binary+$test_name/$file_name2 -c $test_name -o plot -t $file_name1 -u $file_name2\\n"
   fi
 done
+
+echo -e $JOBLIST | xargs -L1 -P${JOBS} $SOURCE_DIR/plot_well_comparison.py
 
 if test -n "$FAILED_TESTS"
 then
