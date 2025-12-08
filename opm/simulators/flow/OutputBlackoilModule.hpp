4a181d4259cfe5983eb3c13d9a26c7cf2329c39a
Halvor M Nilsen <Halvor.M.Nilsen@sintef.no>
2025-12-08 14:17:05 +0100

    fixed error in BSTRESS implementation

--- opm/simulators/flow/OutputBlackoilModule.hpp
+++ opm/simulators/flow/OutputBlackoilModule.hpp
@@ -2986,7 +2986,7 @@ private:
                          }
                     },
                 };
-                handlers.insert(mech_handlers.begin(), mech_handlers.end(), handlers.end());
+                handlers.insert(handlers.end(),mech_handlers.begin(), mech_handlers.end());
             }
         }
 
