7ca8ea9c954c9dfe35fadbe28db036b37681174e
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2025-12-09 11:16:07 +0100

    adjust to run trigger handling in jenkins build script

--- jenkins/run-norne.sh
+++ jenkins/run-norne.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-pushd .
-cd deps/opm-tests
-test -z $SIM && SIM=flow
+configuration=$1
+procs=$2
+
+cd $WORKSPACE/deps/opm-tests/norne
 
 # Run the norne case
-cd norne
-if test -n "$1"
+if test -n "$procs"
 then
-  mpirun -np $1 $WORKSPACE/$configuration/build-opm-simulators/bin/$SIM --output-dir=${SIM}_${1}_proc NORNE_ATW2013.DATA
+  mpirun -np $procs $WORKSPACE/$configuration/build-opm-simulators/bin/flow --output-dir=flow_${procs}_proc NORNE_ATW2013.DATA
 else
-  $WORKSPACE/$configuration/build-opm-simulators/bin/$SIM --output-dir=$SIM NORNE_ATW2013.DATA
+  $WORKSPACE/$configuration/build-opm-simulators/bin/flow --output-dir=flow NORNE_ATW2013.DATA
 fi
 test $? -eq 0 || exit 1
 ./plotwells.sh $WORKSPACE/$configuration/install/bin "ECL.2014.2 opm-simulation-reference/flow_legacy" norne-wells
 ./plotwells.sh $WORKSPACE/$configuration/install/bin "opm-simulation-reference/flow_legacy" norne-wells-noecl
-
-popd
