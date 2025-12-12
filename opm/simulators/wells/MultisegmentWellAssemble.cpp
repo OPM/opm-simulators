hakonhagland/opm-simulators-1

Refactor TargetCalculator
Refactor TargetCalculator::groupTarget() to
WellGroupHelper::getProductionGroupTarget(), and
don't pass Group::ProductionControls as a parameter. Since
TargetCalculator only works for a given group, passing control as a
parameter is confusing and can be error prone if being passed a control
for a different group than the TargetCalculator was constructed for.

opm/simulators/wells/MultisegmentWellAssemble.cpp
@@ -33,6 +33,7 @@
 #include <opm/simulators/wells/MultisegmentWellPrimaryVariables.hpp>
 #include <opm/simulators/wells/WellAssemble.hpp>
 #include <opm/simulators/wells/WellBhpThpCalculator.hpp>
+#include <opm/simulators/wells/WellGroupHelper.hpp>
 #include <opm/simulators/wells/WellHelpers.hpp>
 #include <opm/simulators/wells/WellInterfaceIndices.hpp>
 #include <opm/simulators/wells/WellState.hpp>
@@ -82,10 +83,7 @@ private:
 
 template<class FluidSystem, class Indices>
 void MultisegmentWellAssemble<FluidSystem,Indices>::
-assembleControlEq(const WellState<Scalar, IndexTraits>& well_state,
-                  const GroupState<Scalar>& group_state,
-                  const Schedule& schedule,
-                  const SummaryState& summaryState,
+assembleControlEq(const WellGroupHelperType& wgHelper,
                   const Well::InjectionControls& inj_controls,
                   const Well::ProductionControls& prod_controls,
                   const Scalar rho,
@@ -94,6 +92,8 @@ assembleControlEq(const WellState<Scalar, IndexTraits>& well_state,
                   const bool stopped_or_zero_target,
                   DeferredLogger& deferred_logger) const
 {
+    const auto& well_state = wgHelper.wellState();
+    const auto& summary_state = wgHelper.summaryState();
     /*
         This function assembles the control equation, similar as for StandardWells.
         It does *not* need communication.
@@ -155,21 +155,18 @@ assembleControlEq(const WellState<Scalar, IndexTraits>& well_state,
             return WellBhpThpCalculator(well_).calculateBhpFromThp(well_state,
                                                                    rates,
                                                                    well,
-                                                                   summaryState,
+                                                                   summary_state,
                                                                    rho,
                                                                    deferred_logger);
         };
         // Call generic implementation.
-        WellAssemble(well_).assembleControlEqInj(well_state,
-                                                 group_state,
-                                                 schedule,
-                                                 summaryState,
-                                                 inj_controls,
-                                                 primary_variables.getBhp(),
-                                                 injection_rate,
-                                                 bhp_from_thp,
-                                                 control_eq,
-                                                 deferred_logger);
+        WellAssemble(well_).template assembleControlEqInj<EvalWell>(wgHelper,
+                                                                   inj_controls,
+                                                                   primary_variables.getBhp(),
+                                                                   injection_rate,
+                                                                   bhp_from_thp,
+                                                                   control_eq,
+                                                                   deferred_logger);
     } else {
         // Find rates.
         const auto rates = getRates();
@@ -178,21 +175,18 @@ assembleControlEq(const WellState<Scalar, IndexTraits>& well_state,
             return WellBhpThpCalculator(well_).calculateBhpFromThp(well_state,
                                                                    rates,
                                                                    well,
-                                                                   summaryState,
+                                                                   summary_state,
                                                                    rho,
                                                                    deferred_logger);
         };
         // Call generic implementation.
-        WellAssemble(well_).assembleControlEqProd(well_state,
-                                                  group_state,
-                                                  schedule,
-                                                  summaryState,
-                                                  prod_controls,
-                                                  primary_variables.getBhp(),
-                                                  rates,
-                                                  bhp_from_thp,
-                                                  control_eq,
-                                                  deferred_logger);
+        WellAssemble(well_).template assembleControlEqProd<EvalWell>(wgHelper,
+                                                                     prod_controls,
+                                                                     primary_variables.getBhp(),
+                                                                     rates,
+                                                                     bhp_from_thp,
+                                                                     control_eq,
+                                                                     deferred_logger);
     }
 
     MultisegmentWellEquationAccess<Scalar, IndexTraits, numWellEq,Indices::numEq> eqns(eqns1);

opm/simulators/wells/StandardWellAssemble.hpp
@@ -36,6 +36,7 @@ template<class FluidSystem, class Indices> class StandardWellPrimaryVariables;
 class SummaryState;
 template<class FluidSystem> class WellInterfaceFluidSystem;
 template<typename Scalar, typename IndexTraits> class WellState;
+template<typename Scalar, typename IndexTraits> class WellGroupHelper;
 
 //! \brief Class handling assemble of the equation system for StandardWell.
 template<class FluidSystem, class Indices>
@@ -47,6 +48,7 @@ public:
     using EvalWell = typename PrimaryVariables::EvalWell;
     using IndexTraits = typename FluidSystem::IndexTraitsType;
     using StandardWellEquationsType = StandardWellEquations<Scalar, IndexTraits, Indices::numEq>;
+    using WellGroupHelperType = WellGroupHelper<Scalar, IndexTraits>;
 
     //! \brief Constructor initializes reference to well.
     explicit StandardWellAssemble(const WellInterfaceFluidSystem<FluidSystem>& well)
@@ -54,10 +56,7 @@ public:
     {}
 
     //! \brief Assemble control equation.
-    void assembleControlEq(const WellState<Scalar, IndexTraits>& well_state,
-                           const GroupState<Scalar>& group_state,
-                           const Schedule& schedule,
-                           const SummaryState& summaryState,
+    void assembleControlEq(const WellGroupHelperType& wgHelper,
                            const Well::InjectionControls& inj_controls,
                            const Well::ProductionControls& prod_controls,
                            const PrimaryVariables& primary_variables,

opm/simulators/wells/WellAssemble.hpp
@@ -39,6 +39,7 @@ class Schedule;
 class SummaryState;
 template<typename FluidSystem> class WellInterfaceFluidSystem;
 template<typename Scalar, typename IndexTraits> class WellState;
+template<typename Scalar, typename IndexTraits> class WellGroupHelper;
 struct WellInjectionControls;
 struct WellProductionControls;
 
@@ -49,15 +50,13 @@ class WellAssemble {
     static constexpr int Gas = FluidSystem::gasPhaseIdx;
     using Scalar = typename FluidSystem::Scalar;
     using IndexTraits = typename FluidSystem::IndexTraitsType;
+    using WellGroupHelperType = WellGroupHelper<Scalar, IndexTraits>;
 
 public:
     explicit WellAssemble(const WellInterfaceFluidSystem<FluidSystem>& well);
 
     template<class EvalWell>
-    void assembleControlEqProd(const WellState<Scalar, IndexTraits>& well_state,
-                               const GroupState<Scalar>& group_state,
-                               const Schedule& schedule,
-                               const SummaryState& summaryState,
+    void assembleControlEqProd(const WellGroupHelperType& wgHelper,
                                const WellProductionControls& controls,
                                const EvalWell& bhp,
                                const std::vector<EvalWell>& rates, // Always 3 canonical rates.
@@ -66,10 +65,7 @@ public:
                                DeferredLogger& deferred_logger) const;
 
     template<class EvalWell>
-    void assembleControlEqInj(const WellState<Scalar, IndexTraits>& well_state,
-                              const GroupState<Scalar>& group_state,
-                              const Schedule& schedule,
-                              const SummaryState& summaryState,
+    void assembleControlEqInj(const WellGroupHelperType& wgHelper,
                               const WellInjectionControls& controls,
                               const EvalWell& bhp,
                               const EvalWell& injection_rate,

opm/simulators/wells/WellInterface.hpp
@@ -157,13 +157,13 @@ public:
                       const std::vector<Scalar>& B_avg,
                       const bool changed_to_open_this_step);
 
-    virtual ConvergenceReport getWellConvergence(const Simulator& simulator,
-                                                 const WellStateType& well_state,
+    virtual ConvergenceReport getWellConvergence(const WellGroupHelperType& wgHelper,
                                                  const std::vector<Scalar>& B_avg,
                                                  DeferredLogger& deferred_logger,
                                                  const bool relax_tolerance) const = 0;
 
     virtual void solveEqAndUpdateWellState(const Simulator& simulator,
+                                           const WellGroupHelperType& wgHelper,
                                            WellStateType& well_state,
                                            DeferredLogger& deferred_logger) = 0;
 
@@ -175,8 +175,8 @@ public:
 
     void assembleWellEqWithoutIteration(const Simulator& simulator,
                                         const double dt,
+                                        const WellGroupHelperType& wgHelper,
                                         WellStateType& well_state,
-                                        const GroupState<Scalar>& group_state,
                                         DeferredLogger& deferred_logger);
 
     // TODO: better name or further refactoring the function to make it more clear
@@ -203,6 +203,7 @@ public:
     /// xw to update Well State
     virtual void recoverWellSolutionAndUpdateWellState(const Simulator& simulator,
                                                        const BVector& x,
+                                                       const WellGroupHelperType& wgHelper,
                                                        WellStateType& well_state,
                                                        DeferredLogger& deferred_logger) = 0;
 
@@ -232,23 +233,20 @@ public:
                                                    std::vector<Scalar>& well_flux,
                                                    DeferredLogger& deferred_logger) const = 0;
 
-    bool wellUnderZeroRateTarget(const Simulator& simulator,
-                                 const WellStateType& well_state,
+    bool wellUnderZeroRateTarget(const WellGroupHelperType& wgHelper,
                                  DeferredLogger& deferred_logger) const;
 
-    bool wellUnderZeroGroupRateTarget(const Simulator& simulator,
-                                      const WellStateType& well_state,
-                                      DeferredLogger& deferred_logger,
-                                      std::optional<bool> group_control = std::nullopt) const;
-
-    bool stoppedOrZeroRateTarget(const Simulator& simulator,
-                                 const WellStateType& well_state,
+    bool stoppedOrZeroRateTarget(const WellGroupHelperType& wgHelper,
                                  DeferredLogger& deferred_logger) const;
 
     bool updateWellStateWithTHPTargetProd(const Simulator& simulator,
                                           WellStateType& well_state,
                                           const WellGroupHelperType& wgHelper,
                                           DeferredLogger& deferred_logger) const;
+
+    bool wellUnderZeroGroupRateTarget(const WellGroupHelperType& wgHelper,
+                                      DeferredLogger& deferred_logger,
+                                      const std::optional<bool> group_control = std::nullopt) const;
 
     enum class IndividualOrGroup { Individual, Group, Both };
     bool updateWellControl(const Simulator& simulator,
@@ -267,12 +265,11 @@ public:
                                                   const bool fixed_control = false,
                                                   const bool fixed_status = false);
 
-    virtual void updatePrimaryVariables(const Simulator& simulator,
-                                        const WellStateType& well_state,
+    virtual void updatePrimaryVariables(const WellGroupHelperType& wgHelper,
                                         DeferredLogger& deferred_logger) = 0;
 
     virtual void calculateExplicitQuantities(const Simulator& simulator,
-                                             const WellStateType& well_state,
+                                             const WellGroupHelperType& wgHelper,
                                              DeferredLogger& deferred_logger) = 0; // should be const?
 
     virtual void updateProductivityIndex(const Simulator& simulator,
@@ -407,8 +404,8 @@ protected:
                                                 const double dt,
                                                 const WellInjectionControls& inj_controls,
                                                 const WellProductionControls& prod_controls,
+                                                const WellGroupHelperType& wgHelper,
                                                 WellStateType& well_state,
-                                                const GroupState<Scalar>& group_state,
                                                 DeferredLogger& deferred_logger) = 0;
 
     // iterate well equations with the specified control until converged
@@ -422,6 +419,7 @@ protected:
 
     virtual void updateIPRImplicit(const Simulator& simulator,
                                    WellStateType& well_state,
+                                   const WellGroupHelperType& wgHelper,
                                    DeferredLogger& deferred_logger) = 0;
 
     bool iterateWellEquations(const Simulator& simulator,

OPM/opm-simulators

opm/simulators/wells/MultisegmentWellAssemble.cpp
@@ -33,6 +33,7 @@
 #include <opm/simulators/wells/MultisegmentWellPrimaryVariables.hpp>
 #include <opm/simulators/wells/WellAssemble.hpp>
 #include <opm/simulators/wells/WellBhpThpCalculator.hpp>
+#include <opm/simulators/wells/GroupStateHelper.hpp>
 #include <opm/simulators/wells/WellHelpers.hpp>
 #include <opm/simulators/wells/WellInterfaceIndices.hpp>
 #include <opm/simulators/wells/WellState.hpp>
@@ -82,10 +83,7 @@ private:
 
 template<class FluidSystem, class Indices>
 void MultisegmentWellAssemble<FluidSystem,Indices>::
-assembleControlEq(const WellState<Scalar, IndexTraits>& well_state,
-                  const GroupState<Scalar>& group_state,
-                  const Schedule& schedule,
-                  const SummaryState& summaryState,
+assembleControlEq(const GroupStateHelperType& groupStateHelper,
                   const Well::InjectionControls& inj_controls,
                   const Well::ProductionControls& prod_controls,
                   const Scalar rho,
@@ -94,6 +92,8 @@ assembleControlEq(const WellState<Scalar, IndexTraits>& well_state,
                   const bool stopped_or_zero_target,
                   DeferredLogger& deferred_logger) const
 {
+    const auto& well_state = groupStateHelper.wellState();
+    const auto& summary_state = groupStateHelper.summaryState();
     /*
         This function assembles the control equation, similar as for StandardWells.
         It does *not* need communication.
@@ -155,21 +155,18 @@ assembleControlEq(const WellState<Scalar, IndexTraits>& well_state,
             return WellBhpThpCalculator(well_).calculateBhpFromThp(well_state,
                                                                    rates,
                                                                    well,
-                                                                   summaryState,
+                                                                   summary_state,
                                                                    rho,
                                                                    deferred_logger);
         };
         // Call generic implementation.
-        WellAssemble(well_).assembleControlEqInj(well_state,
-                                                 group_state,
-                                                 schedule,
-                                                 summaryState,
-                                                 inj_controls,
-                                                 primary_variables.getBhp(),
-                                                 injection_rate,
-                                                 bhp_from_thp,
-                                                 control_eq,
-                                                 deferred_logger);
+        WellAssemble(well_).template assembleControlEqInj<EvalWell>(groupStateHelper,
+                                                                   inj_controls,
+                                                                   primary_variables.getBhp(),
+                                                                   injection_rate,
+                                                                   bhp_from_thp,
+                                                                   control_eq,
+                                                                   deferred_logger);
     } else {
         // Find rates.
         const auto rates = getRates();
@@ -178,21 +175,18 @@ assembleControlEq(const WellState<Scalar, IndexTraits>& well_state,
             return WellBhpThpCalculator(well_).calculateBhpFromThp(well_state,
                                                                    rates,
                                                                    well,
-                                                                   summaryState,
+                                                                   summary_state,
                                                                    rho,
                                                                    deferred_logger);
         };
         // Call generic implementation.
-        WellAssemble(well_).assembleControlEqProd(well_state,
-                                                  group_state,
-                                                  schedule,
-                                                  summaryState,
-                                                  prod_controls,
-                                                  primary_variables.getBhp(),
-                                                  rates,
-                                                  bhp_from_thp,
-                                                  control_eq,
-                                                  deferred_logger);
+            WellAssemble(well_).template assembleControlEqProd<EvalWell>(groupStateHelper,
+                                                                     prod_controls,
+                                                                     primary_variables.getBhp(),
+                                                                     rates,
+                                                                     bhp_from_thp,
+                                                                     control_eq,
+                                                                     deferred_logger);
     }
 
     MultisegmentWellEquationAccess<Scalar, IndexTraits, numWellEq,Indices::numEq> eqns(eqns1);

opm/simulators/wells/StandardWellAssemble.hpp
@@ -36,6 +36,7 @@ template<class FluidSystem, class Indices> class StandardWellPrimaryVariables;
 class SummaryState;
 template<class FluidSystem> class WellInterfaceFluidSystem;
 template<typename Scalar, typename IndexTraits> class WellState;
+template<typename Scalar, typename IndexTraits> class GroupStateHelper;
 
 //! \brief Class handling assemble of the equation system for StandardWell.
 template<class FluidSystem, class Indices>
@@ -47,6 +48,7 @@ public:
     using EvalWell = typename PrimaryVariables::EvalWell;
     using IndexTraits = typename FluidSystem::IndexTraitsType;
     using StandardWellEquationsType = StandardWellEquations<Scalar, IndexTraits, Indices::numEq>;
+    using GroupStateHelperType = GroupStateHelper<Scalar, IndexTraits>;
 
     //! \brief Constructor initializes reference to well.
     explicit StandardWellAssemble(const WellInterfaceFluidSystem<FluidSystem>& well)
@@ -54,10 +56,7 @@ public:
     {}
 
     //! \brief Assemble control equation.
-    void assembleControlEq(const WellState<Scalar, IndexTraits>& well_state,
-                           const GroupState<Scalar>& group_state,
-                           const Schedule& schedule,
-                           const SummaryState& summaryState,
+    void assembleControlEq(const GroupStateHelperType& groupStateHelper,
                            const Well::InjectionControls& inj_controls,
                            const Well::ProductionControls& prod_controls,
                            const PrimaryVariables& primary_variables,

opm/simulators/wells/WellAssemble.hpp
@@ -39,6 +39,7 @@ class Schedule;
 class SummaryState;
 template<typename FluidSystem> class WellInterfaceFluidSystem;
 template<typename Scalar, typename IndexTraits> class WellState;
+template<typename Scalar, typename IndexTraits> class GroupStateHelper;
 struct WellInjectionControls;
 struct WellProductionControls;
 
@@ -49,15 +50,13 @@ class WellAssemble {
     static constexpr int Gas = FluidSystem::gasPhaseIdx;
     using Scalar = typename FluidSystem::Scalar;
     using IndexTraits = typename FluidSystem::IndexTraitsType;
+    using GroupStateHelperType = GroupStateHelper<Scalar, IndexTraits>;
 
 public:
     explicit WellAssemble(const WellInterfaceFluidSystem<FluidSystem>& well);
 
     template<class EvalWell>
-    void assembleControlEqProd(const WellState<Scalar, IndexTraits>& well_state,
-                               const GroupState<Scalar>& group_state,
-                               const Schedule& schedule,
-                               const SummaryState& summaryState,
+    void assembleControlEqProd(const GroupStateHelperType& groupStateHelper,
                                const WellProductionControls& controls,
                                const EvalWell& bhp,
                                const std::vector<EvalWell>& rates, // Always 3 canonical rates.
@@ -66,10 +65,7 @@ public:
                                DeferredLogger& deferred_logger) const;
 
     template<class EvalWell>
-    void assembleControlEqInj(const WellState<Scalar, IndexTraits>& well_state,
-                              const GroupState<Scalar>& group_state,
-                              const Schedule& schedule,
-                              const SummaryState& summaryState,
+    void assembleControlEqInj(const GroupStateHelperType& groupStateHelper,
                               const WellInjectionControls& controls,
                               const EvalWell& bhp,
                               const EvalWell& injection_rate,

opm/simulators/wells/WellInterface.hpp
@@ -157,13 +157,13 @@ public:
                       const std::vector<Scalar>& B_avg,
                       const bool changed_to_open_this_step);
 
-    virtual ConvergenceReport getWellConvergence(const Simulator& simulator,
-                                                 const WellStateType& well_state,
+    virtual ConvergenceReport getWellConvergence(const GroupStateHelperType& groupStateHelper,
                                                  const std::vector<Scalar>& B_avg,
                                                  DeferredLogger& deferred_logger,
                                                  const bool relax_tolerance) const = 0;
 
     virtual void solveEqAndUpdateWellState(const Simulator& simulator,
+                                           const GroupStateHelperType& groupStateHelper,
                                            WellStateType& well_state,
                                            DeferredLogger& deferred_logger) = 0;
 
@@ -211,6 +211,7 @@ public:
     /// xw to update Well State
     virtual void recoverWellSolutionAndUpdateWellState(const Simulator& simulator,
                                                        const BVector& x,
+                                                       const GroupStateHelperType& groupStateHelper,
                                                        WellStateType& well_state,
                                                        DeferredLogger& deferred_logger) = 0;
 
@@ -240,23 +241,20 @@ public:
                                                    std::vector<Scalar>& well_flux,
                                                    DeferredLogger& deferred_logger) const = 0;
 
-    bool wellUnderZeroRateTarget(const Simulator& simulator,
-                                 const WellStateType& well_state,
+    bool wellUnderZeroRateTarget(const GroupStateHelperType& groupStateHelper,
                                  DeferredLogger& deferred_logger) const;
 
-    bool wellUnderZeroGroupRateTarget(const Simulator& simulator,
-                                      const WellStateType& well_state,
-                                      DeferredLogger& deferred_logger,
-                                      std::optional<bool> group_control = std::nullopt) const;
-
-    bool stoppedOrZeroRateTarget(const Simulator& simulator,
-                                 const WellStateType& well_state,
+    bool stoppedOrZeroRateTarget(const GroupStateHelperType& groupStateHelper,
                                  DeferredLogger& deferred_logger) const;
 
     bool updateWellStateWithTHPTargetProd(const Simulator& simulator,
                                           WellStateType& well_state,
                                           const GroupStateHelperType& groupStateHelper,
                                           DeferredLogger& deferred_logger) const;
+
+    bool wellUnderZeroGroupRateTarget(const GroupStateHelperType& groupStateHelper,
+                                      DeferredLogger& deferred_logger,
+                                      const std::optional<bool> group_control = std::nullopt) const;
 
     enum class IndividualOrGroup { Individual, Group, Both };
     bool updateWellControl(const Simulator& simulator,
@@ -276,12 +274,11 @@ public:
                                                   const bool fixed_status,
                                                   const bool solving_with_zero_rate);
 
-    virtual void updatePrimaryVariables(const Simulator& simulator,
-                                        const WellStateType& well_state,
+    virtual void updatePrimaryVariables(const GroupStateHelperType& groupStateHelper,
                                         DeferredLogger& deferred_logger) = 0;
 
     virtual void calculateExplicitQuantities(const Simulator& simulator,
-                                             const WellStateType& well_state,
+                                             const GroupStateHelperType& groupStateHelper,
                                              DeferredLogger& deferred_logger) = 0; // should be const?
 
     virtual void updateProductivityIndex(const Simulator& simulator,