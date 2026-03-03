deb8b7fc2256843948db020ab80cf29a45576502
Kai Bao <kai.bao@sintef.no>
2026-03-03 15:06:47 +0100

    recovering the simulation without energy enabled

--- opm/simulators/wells/MultisegmentWellAssemble.cpp
+++ opm/simulators/wells/MultisegmentWellAssemble.cpp
@@ -224,7 +224,7 @@ assembleAccelerationTerm(const int seg_target,
     if constexpr (has_gfrac_variable) {
         eqns.D()[seg_target][seg_upwind][SPres][GFrac] -= accelerationTerm.derivative(GFrac + Indices::numEq);
     }
-    assert(std::abs(accelerationTerm.derivative(Temperature + Indices::numEq)) < 1.e-14); // TODO: for debugging
+    assert(!enable_energy || std::abs(accelerationTerm.derivative(Temperature + Indices::numEq)) < 1.e-14); // TODO: for debugging
 }
 
 template<class FluidSystem, class Indices>
@@ -262,7 +262,7 @@ assemblePressureEqExtraDerivatives(const int seg,
     // Frac - derivatives are zero (they belong to upwind^2)
     eqns.D()[seg][seg_upwind][SPres][SPres] += extra_derivatives.derivative(SPres + Indices::numEq);
     eqns.D()[seg][seg_upwind][SPres][WQTotal] += extra_derivatives.derivative(WQTotal + Indices::numEq);
-    assert(std::abs(extra_derivatives.derivative(Temperature + Indices::numEq)) < 1.e-14); // TODO: for debugging
+    assert(!enable_energy || std::abs(extra_derivatives.derivative(Temperature + Indices::numEq)) < 1.e-14); // TODO: for debugging
 }
 
 
@@ -288,7 +288,7 @@ assemblePressureEq(const int seg,
     if constexpr (has_gfrac_variable) {
         eqns.D()[seg][seg_upwind][SPres][GFrac] += pressure_equation.derivative(GFrac + Indices::numEq);
     }
-    assert(std::abs(pressure_equation.derivative(Temperature + Indices::numEq)) < 1.e-14); // TODO: for debugging
+    assert(!enable_energy || std::abs(pressure_equation.derivative(Temperature + Indices::numEq)) < 1.e-14); // TODO: for debugging
 
     // contribution from the outlet segment
     eqns.residual()[seg][SPres] -= outlet_pressure.value();
@@ -310,7 +310,6 @@ assembleTrivialEq(const int seg,
         and assembleICDPressureEq is responsible for the remaining segments.
         This method does *not* need communication.
     */
-    assert(false); // TODO: for debugging
     MultisegmentWellEquationAccess<Scalar,IndexTraits,numWellEq,Indices::numEq> eqns(eqns1);
     eqns.residual()[seg][SPres] = value;
     eqns.D()[seg][seg][SPres][WQTotal] = 1.;
