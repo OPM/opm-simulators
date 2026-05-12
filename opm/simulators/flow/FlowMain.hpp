ebf4431005bd7978e9c4ad4009b54606aab4c443
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2026-05-12 10:57:46 +0200

    Integration Fixes for VEM/DDM Based Geomechanics

--- opm/simulators/flow/FlowMain.hpp
+++ opm/simulators/flow/FlowMain.hpp
@@ -45,8 +45,18 @@
 #endif
 
 #include <charconv>
+#include <concepts>
 #include <cstddef>
 #include <memory>
+
+namespace Opm::GeomechChecks {
+    template <typename Problem>
+    concept HasGeomechFinalTiming = requires(Problem problem)
+    {
+        problem.geoMechModel();
+        problem.geoMechModel().finalTimingSummary();
+    };
+}
 
 namespace Opm::Parameters {
 
