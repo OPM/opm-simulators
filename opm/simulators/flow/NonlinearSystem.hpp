2dfe30e16e7c0c5a5748e54c510287cdc6b7aa0f
hnil <Halvor.M.Nilsen@sintef.no>
2026-05-26 15:09:40 +0200

    added need GPL tings + a missing header

--- opm/simulators/flow/NonlinearSystem.hpp
+++ opm/simulators/flow/NonlinearSystem.hpp
@@ -1,6 +1,23 @@
 #ifndef OPM_FLOW_NONLINEAR_SYSTEM_HEADER_INCLUDED
 #define OPM_FLOW_NONLINEAR_SYSTEM_HEADER_INCLUDED
+/*
+  Copyright 2026, SINTEF Digital
 
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <cstddef>
 
 #include <opm/input/eclipse/Schedule/Tuning.hpp>
@@ -15,7 +32,7 @@
 #include <opm/simulators/timestepping/SimulatorTimerInterface.hpp>
 #include <opm/simulators/utils/ComponentName.hpp>
 #include <opm/simulators/utils/ParallelCommunication.hpp>
-
+#include <opm/input/eclipse/Schedule/Tuning.hpp>
 #include <span>
 #include <string_view>
 #include <tuple>
