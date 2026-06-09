cd0c75fb3ff24f3d4a49b586221ab1df048b6838
Håkon Hægland <hakon.hagland@gmail.com>
2026-06-09 18:47:12 +0200

    Add license headers to rescoup test files
    Add the standard OPM GPLv3 copyright header to the reservoir coupling
    slave parse error test files that were missing one: the CTest
    registration file and the two runner scripts.

--- tests/rescoup/slave_parse_error/run_ctest.sh
+++ tests/rescoup/slave_parse_error/run_ctest.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+# Copyright 2026 Equinor
+#
+# This file is part of the Open Porous Media project (OPM).
+#
+# OPM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OPM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+
 # CTest wrapper for the reservoir coupling slave parse error test.
 # Exits 0 on expected (currently unfixed) behavior, 1 on unexpected behavior.
 #
--- tests/rescoup/slave_parse_error/run_test.sh
+++ tests/rescoup/slave_parse_error/run_test.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+# Copyright 2026 Equinor
+#
+# This file is part of the Open Porous Media project (OPM).
+#
+# OPM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OPM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+
 # Manual test runner: Reservoir coupling slave deck parse error
 #
 # For automated testing use run_ctest.sh (registered as CTest target
