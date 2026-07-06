e74db14ff735b529278899599fe8b5a715b689f3
Kai Bao <kai.bao@sintef.no>
2026-07-07 00:13:51 +0200

    Add compositional equilibration with EQUIL and ZMFVD

--- opm/simulators/utils/UnsupportedFlowKeywords.cpp
+++ opm/simulators/utils/UnsupportedFlowKeywords.cpp
@@ -729,7 +729,6 @@ const KeywordValidation::UnsupportedKeywords& unsupportedKeywords()
         {"WTHPMAX", {true, std::nullopt}},
         {"ZIPPY2", {false, std::nullopt}},
         {"ZIPP2OFF", {false, std::nullopt}},
-        {"ZMFVD", {false, std::nullopt}},
     };
 
     return unsupported_keywords;
