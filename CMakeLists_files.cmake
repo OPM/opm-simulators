0f008b4accf933b9869daedf743c78748ec9bb75
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2026-07-13 17:22:37 +0200

    Add Parallel Support for Region Variable Values
    This commit introduces an MPI-aware version of class
    
        Opm::data::RegionVariableValues
    
    The primary extension to the base class is that this version
    implements a non-trivial communicateIncrement() member function that
    incorporates cross-rank value contributions to all variables for all
    regions in all region sets.  The same features and restrictions that
    apply to the base class also apply to this derived type.

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -188,6 +188,7 @@ list (APPEND MAIN_SOURCE_FILES
   opm/simulators/utils/FullySupportedFlowKeywords.cpp
   opm/simulators/utils/ParallelFileMerger.cpp
   opm/simulators/utils/ParallelRegionsetVariableDescriptor.cpp
+  opm/simulators/utils/ParallelRegionVariableValues.cpp
   opm/simulators/utils/ParallelRestart.cpp
   opm/simulators/utils/PartiallySupportedFlowKeywords.cpp
   opm/simulators/utils/PressureAverage.cpp
@@ -1207,6 +1208,7 @@ list (APPEND PUBLIC_HEADER_FILES
   opm/simulators/utils/moduleVersion.hpp
   opm/simulators/utils/ParallelCommunication.hpp
   opm/simulators/utils/ParallelRegionsetVariableDescriptor.hpp
+  opm/simulators/utils/ParallelRegionVariableValues.hpp
   opm/simulators/utils/ParallelSerialization.hpp
   opm/simulators/utils/readDeck.hpp
   opm/simulators/utils/satfunc/GasPhaseConsistencyChecks.hpp
