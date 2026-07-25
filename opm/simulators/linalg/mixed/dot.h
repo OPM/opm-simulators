e6f22b11e436e3138e093ff5182acee6b64868a6
Kjetil B Haugen <kjetil@haugenlabs.com>
2026-07-25 11:02:43 -0500

    mixed: move scalar product to c for avx2 support

---
+++ opm/simulators/linalg/mixed/dot.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+double vec_dot (double const *x, double const *y, int NN);
+double vec_bdot (double const *x, double const *y, int NN);
+
+#ifdef __cplusplus
+}
+#endif
