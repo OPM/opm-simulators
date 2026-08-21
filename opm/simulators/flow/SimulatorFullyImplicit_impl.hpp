663d824ea025956b7e63872b5b0f6e6815676b04
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2026-08-21 15:23:05 +0200

    flow: rebuild current storage cache on restart

--- opm/simulators/flow/SimulatorFullyImplicit_impl.hpp
+++ opm/simulators/flow/SimulatorFullyImplicit_impl.hpp
@@ -329,6 +329,7 @@ runStep(SimulatorTimer& timer)
         wellModel_().prepareDeserialize(serializer_.loadStep() - 1);
         serializer_.loadState();
         simulator_.model().invalidateAndUpdateIntensiveQuantities(/*timeIdx=*/0);
+        simulator_.model().rebuildStorageCache(/*timeIdx=*/0);
         if (simulator_.problem().intensiveQuantityHistorySize() > 1) {
             simulator_.model().invalidateAndUpdateIntensiveQuantities(/*timeIdx=*/1);
             simulator_.model().rebuildStorageCache(/*timeIdx=*/1);
