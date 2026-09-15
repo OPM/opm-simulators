41047b76362cdea12b47a39301393c004b080b8c
havahol <havard.heitlo.holm@sintef.no>
2026-09-15 20:15:16 +0000

    Test of async memcpy, partial copying, and (in debug) wrong pointer attributes to memcpy

--- tests/gpuistl/test_GpuBuffer.cu
+++ tests/gpuistl/test_GpuBuffer.cu
@@ -283,3 +283,63 @@ BOOST_AUTO_TEST_CASE(TestMakeView)
     auto gpubufOnCpu2 = gpubuf2.asStdVector();
     BOOST_CHECK_EQUAL_COLLECTIONS(gpubufOnCpu2.begin(), gpubufOnCpu2.end(), buf2.begin(), buf2.end());
 }
+
+BOOST_AUTO_TEST_CASE(TestCopyFromAndToHostAsyncRoundTrip)
+{
+    std::vector<double> data {{1, 2, 3, 4, 5, 6, 7}};
+    auto bufferOnGPU = Opm::gpuistl::GpuBuffer<double>(data.size());
+    cudaStream_t stream = nullptr;
+    OPM_GPU_SAFE_CALL(cudaStreamCreate(&stream));
+    bufferOnGPU.copyFromHostAsync(data.data(), data.size(), stream);
+    OPM_GPU_SAFE_CALL(cudaStreamSynchronize(stream));
+    std::vector<double> hostBuffer(data.size(), 0.0);
+    bufferOnGPU.copyToHostAsync(hostBuffer.data(), hostBuffer.size(), stream);
+    OPM_GPU_SAFE_CALL(cudaStreamSynchronize(stream));
+    OPM_GPU_SAFE_CALL(cudaStreamDestroy(stream));
+    BOOST_CHECK_EQUAL_COLLECTIONS(hostBuffer.begin(), hostBuffer.end(), data.begin(), data.end());
+}
+BOOST_AUTO_TEST_CASE(TestCopyFromHostAsyncPartialCopy)
+{
+    // copyFromHostAsync allows numberOfElements <= size() (same as sync copyFromHost)
+    std::vector<double> data {{10, 20, 30}};
+    auto bufferOnGPU = Opm::gpuistl::GpuBuffer<double>(5);
+    cudaStream_t stream = nullptr;
+    OPM_GPU_SAFE_CALL(cudaStreamCreate(&stream));
+    bufferOnGPU.copyFromHostAsync(data.data(), data.size(), stream);
+    OPM_GPU_SAFE_CALL(cudaStreamSynchronize(stream));
+    OPM_GPU_SAFE_CALL(cudaStreamDestroy(stream));
+    std::vector<double> hostBuffer(bufferOnGPU.size(), -1.0);
+    bufferOnGPU.copyToHost(hostBuffer.data(), hostBuffer.size());
+    BOOST_CHECK_EQUAL_COLLECTIONS(hostBuffer.begin(), hostBuffer.begin() + data.size(), data.begin(), data.end());
+}
+BOOST_AUTO_TEST_CASE(TestCopyFromHostAsyncTooManyElementsThrows)
+{
+    std::vector<double> data {{1, 2, 3, 4, 5}};
+    auto bufferOnGPU = Opm::gpuistl::GpuBuffer<double>(3);
+    BOOST_CHECK_THROW(bufferOnGPU.copyFromHostAsync(data.data(), data.size()), std::runtime_error);
+}
+BOOST_AUTO_TEST_CASE(TestCopyToHostAsyncWrongSizeThrows)
+{
+    std::vector<double> data {{1, 2, 3, 4}};
+    auto bufferOnGPU = Opm::gpuistl::GpuBuffer<double>(data);
+    std::vector<double> tooSmall(2);
+    BOOST_CHECK_THROW(bufferOnGPU.copyToHostAsync(tooSmall.data(), tooSmall.size()), std::invalid_argument);
+}
+
+#ifndef NDEBUG
+BOOST_AUTO_TEST_CASE(TestCopyFromHostRejectsDevicePointerInDebug)
+{
+    // Exercises detail::gpuMemcpyHostToDevice's debug pointer-kind checks
+    std::vector<double> data {{1, 2, 3, 4}};
+    auto sourceOnGPU = Opm::gpuistl::GpuBuffer<double>(data);
+    auto destinationOnGPU = Opm::gpuistl::GpuBuffer<double>(data.size());
+    BOOST_CHECK_THROW(destinationOnGPU.copyFromHost(sourceOnGPU.data(), data.size()), std::invalid_argument);
+}
+BOOST_AUTO_TEST_CASE(TestCopyToHostRejectsDevicePointerInDebug)
+{
+    std::vector<double> data {{1, 2, 3, 4}};
+    auto sourceOnGPU = Opm::gpuistl::GpuBuffer<double>(data);
+    auto destinationOnGPU = Opm::gpuistl::GpuBuffer<double>(data.size());
+    BOOST_CHECK_THROW(sourceOnGPU.copyToHost(destinationOnGPU.data(), data.size()), std::invalid_argument);
+}
+#endif
