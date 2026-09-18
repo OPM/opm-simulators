ecec608aab32ee59636e37fefff6b1daa7a08847
Kai Bao <kai.bao@sintef.no>
2026-09-18 14:14:42 +0200

    Add the COMPVD equilibration regression test

--- regressionTests.cmake
+++ regressionTests.cmake
@@ -118,6 +118,23 @@ add_test_compareECLFiles(
 
 add_test_compareECLFiles(
   CASENAME
+    equil_1d_compvd_oil
+  FILENAME
+    EQUIL_1D_COMPVD_OIL
+  SIMULATOR
+    flow_comp
+  REFERENCE_SIMULATOR
+    flow_comp
+  ABS_TOL
+    ${abs_tol}
+  REL_TOL
+    ${rel_tol}
+  DIR
+    compositional/equilibration
+)
+
+add_test_compareECLFiles(
+  CASENAME
     spe12
   FILENAME
     SPE1CASE2
