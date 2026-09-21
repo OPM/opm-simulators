90cb74a101263903c09eb280935b916a6882f2ad
Kai Bao <kai.bao@sintef.no>
2026-09-21 21:44:43 +0200

    Share initialization and fix parallel transmissibility output
    Extract the common simulator time, model-property and transmissibility initialization into FlowProblem helpers while preserving the initialization order of each model.
    
    Use global transmissibilities on the I/O rank for parallel compositional TRANX, TRANY, TRANZ and NNC output, as black oil already does. Release the global transmissibilities after extracting the output and initialize local transmissibilities only once.

--- opm/simulators/flow/FlowProblemBlackoil.hpp
+++ opm/simulators/flow/FlowProblemBlackoil.hpp
@@ -313,68 +313,15 @@ public:
      */
     void finishInit()
     {
-        // TODO: there should be room to remove duplication for this
-        // function, but there is relatively complicated logic in the
-        // function calls here.  Some refactoring is needed.
         FlowProblemType::finishInit();
 
         auto& simulator = this->simulator();
-
-        auto finishTransmissibilities = [updated = false, this]() mutable
-        {
-            if (updated) { return; }
-
-            this->transmissibilities_.finishInit([&vg = this->simulator().vanguard()](const unsigned int it) {
-                return vg.gridIdxToEquilGridIdx(it);
-            });
-
-            updated = true;
-        };
-
-        // calculating the TRANX, TRANY, TRANZ and NNC for output purpose
-        // for parallel running, it is based on global trans_
-        // for serial running, it is based on the transmissibilities_
-        // we try to avoid for the parallel running, has both global trans_ and transmissibilities_ allocated at the same time
-        if (enableEclOutput_) {
-            if (simulator.vanguard().grid().comm().size() > 1) {
-                if (simulator.vanguard().grid().comm().rank() == 0)
-                    eclWriter_->setTransmissibilities(&simulator.vanguard().globalTransmissibility());
-            } else {
-                finishTransmissibilities();
-                eclWriter_->setTransmissibilities(&simulator.problem().eclTransmissibilities());
-            }
-
-            std::function<unsigned int(unsigned int)> equilGridToGrid = [&simulator](unsigned int i) {
-                return simulator.vanguard().gridEquilIdxToGridIdx(i);
-            };
-
-            this->eclWriter_->extractOutputTransAndNNC(equilGridToGrid);
-        }
-        simulator.vanguard().releaseGlobalTransmissibilities();
+        const bool transmissibilitiesFinished =
+            this->prepareTransmissibilityOutput_(*eclWriter_, enableEclOutput_);
 
         const auto& eclState = simulator.vanguard().eclState();
         const auto& schedule = simulator.vanguard().schedule();
-
-        // Set the start time of the simulation
-        simulator.setStartTime(schedule.getStartTime());
-        simulator.setEndTime(schedule.simTime(schedule.size() - 1));
-
-        // We want the episode index to be the same as the report step index to make
-        // things simpler, so we have to set the episode index to -1 because it is
-        // incremented by endEpisode(). The size of the initial time step and
-        // length of the initial episode is set to zero for the same reason.
-        simulator.setEpisodeIndex(-1);
-        simulator.setEpisodeLength(0.0);
-
-        this->initGravity_(eclState);
-
-        if (this->enableTuning_) {
-            // if support for the TUNING keyword is enabled, we get the initial time
-            // steping parameters from it instead of from command line parameters
-            const auto& tuning = schedule[0].tuning();
-            this->initialTimeStepSize_ = tuning.TSINIT.has_value() ? tuning.TSINIT.value() : -1.0;
-            this->maxTimeStepAfterWellEvent_ = tuning.TMAXWC;
-        }
+        this->initializeSimulatorTime_();
 
         // conserve inner energy instead of enthalpy if TEMP is used
         // or THERMAL and parameter ConserveInnerEnergyThermal is true (default false)
@@ -383,30 +330,16 @@ public:
         bool conserveInnerEnergy = isTemp || (isThermal && Parameters::Get<Parameters::ConserveInnerEnergyThermal>());
         FluidSystem::setEnergyEqualEnthalpy(conserveInnerEnergy);
 
-        if (FluidSystem::phaseIsActive(FluidSystem::oilPhaseIdx) &&
-            FluidSystem::phaseIsActive(FluidSystem::gasPhaseIdx)) {
-            this->maxOilSaturation_.resize(this->model().numGridDof(), 0.0);
-        }
-
-        this->readRockParameters_(simulator.vanguard().cellCenterDepths(),
-                                  [&simulator](const unsigned idx)
-                                  {
-                                      std::array<int,dim> coords;
-                                      simulator.vanguard().cartesianCoordinate(idx, coords);
-                                      std::ranges::transform(coords, coords.begin(),
-                                                             [](const auto c) { return c + 1; });
-                                      return coords;
-                                  });
-
-        this->readMaterialParameters_();
-        this->readThermalParameters_();
+        this->initializeModelProperties_();
 
         // write the static output files (EGRID, INIT)
         if (enableEclOutput_) {
             this->eclWriter_->writeInit();
         }
 
-        finishTransmissibilities();
+        if (!transmissibilitiesFinished) {
+            this->finishTransmissibilities_();
+        }
 
         const auto& initconfig = eclState.getInitConfig();
         this->tracerModel_.init(initconfig.restartRequested());
