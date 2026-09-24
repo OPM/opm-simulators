b666a0b35e8989ebb2d3ad75f8ac5c07992029d4
Kai Bao <kai.bao@sintef.no>
2026-09-24 13:40:00 +0200

    Fail Jenkins early on invalid opm-common checkout requests

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -11,33 +11,12 @@ declare -A upstreamRev
 upstreamRev[opm-common]=master
 upstreamRev[opm-grid]=master
 
-if grep -q "opm-common=" <<< $ghprbCommentBody
-then
-    if test -n "$absolute_revisions"
-    then
-        upstreamRev[opm-common]=$(echo $ghprbCommentBody | sed -r 's/.*opm-common=([^ ]+).*/\1/g')
-    else
-        upstreamRev[opm-common]=pull/$(echo $ghprbCommentBody | sed -r 's/.*opm-common=([0-9]+).*/\1/g')/merge
-    fi
-fi
-
 # No downstreams currently
 declare -a downstreams
 declare -A downstreamRev
 
-# Clone opm-common
-if ! test -d $WORKSPACE/deps/opm-common
-then
-    mkdir -p $WORKSPACE/deps/opm-common
-    pushd $WORKSPACE/deps/opm-common
-    repo_root=${OPM_REPO_ROOT:-git@github.com:OPM}
-    git init .
-    git remote add origin ${repo_root}/opm-common
-    git fetch --depth 1 origin ${upstreamRev[opm-common]}:branch_to_build
-    test $? -eq 0 || exit 1
-    git checkout branch_to_build
-    popd
-fi
+# Fetch opm-common before loading its shared Jenkins helpers.
+source "$WORKSPACE/jenkins/checkout-opm-common.sh"
 
 source $WORKSPACE/deps/opm-common/jenkins/build-opm-module.sh
 
